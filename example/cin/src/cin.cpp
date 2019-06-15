a0f58d84676761560d26b8c872ab9cfd97c85239
roberthsu2004 <roberthsu2004@gmail.com>
2019-06-15 11:47:08 +0800

    modify

---
+++ example/cin/src/cin.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : cin.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "請輸入姓名:";
+	cin >> name;
+	cout << "Hello! " << name;
+	return 0;
+}

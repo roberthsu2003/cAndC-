3b9815eb8f0f08e6f1b4a495a3c75af64106c9c7
roberthsu2004 <roberthsu2004@gmail.com>
2019-06-15 12:22:50 +0800

    modify

---
+++ example/cin_s/src/cin_s.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : cin_s.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int englishScore = 60;
+	cout << "請輸入您的英文分數:";
+	cin >> englishScore;
+	cout << "Hello ! 您的英文分數是: " << englishScore;
+	return 0;
+}

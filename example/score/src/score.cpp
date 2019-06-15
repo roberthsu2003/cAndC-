acece4168e6ca319b069ab12fba87d68924f8211
roberthsu2004 <roberthsu2004@gmail.com>
2019-06-15 14:08:11 +0800

    modify

---
+++ example/score/src/score.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : score.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "姓名\t座號\t國文\t數學\t英文\n";
+	cout << "張三曹\t01\t89\t78\t\78\n";
+	cout << "張四曹\t02\t80\t68\t\64\n";
+	cout << "張五曹\t03\t92\t83\t\84\n";
+	return 0;
+}

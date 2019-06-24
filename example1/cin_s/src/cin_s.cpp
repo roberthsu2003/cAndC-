4c0506431b7f6c15140f191821f30574aed46e28
robert <roberthsu2003@gmail.com>
2019-06-24 21:25:35 +0800

    Update cin_s.cpp

--- example1/cin_s/src/cin_s.cpp
+++ example1/cin_s/src/cin_s.cpp
@@ -1,16 +1,12 @@
-//============================================================================
-// Name        : cin_s.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+/*
+ 這個專案很好玩
+ 大家一起來寫程式
+*/
 #include <iostream>
 using namespace std;
 
 int main() {
-	int englishScore = 60;
+	int englishScore = 60; //宣告一個變數
 	cout << "原本的分數是:" << englishScore << endl;
 	cout << "請輸入新的分數:";
 	cin >> englishScore;

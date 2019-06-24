4a8b32e4a00b91ee2525cd15fbb089df2db3ca25
robert <roberthsu2003@gmail.com>
2019-06-24 20:06:30 +0800

    modify

---
+++ example1/sample1/src/sample1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : sample1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "½Ð¿é¤J©m¦W:";
+	cin >> name;
+	cout << "Hello! " << name;
+	return 0;
+
+}

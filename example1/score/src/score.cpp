5872a801878020c3f7121349f67743a7036e38f8
robert <roberthsu2003@gmail.com>
2019-06-26 19:30:14 +0800

    modify

--- example1/score/src/score.cpp
+++ example1/score/src/score.cpp
@@ -12,6 +12,13 @@ using namespace std;
 int main() {
 	cout << "姓名\t座號\t電話" << endl;
 	cout << "李振聲\t" << 11 << "\t0229424563\n";
-	cout << "張四聲\t" << 13 << "\t0229457567";
+	cout << "張四聲\t" << 13 << "\t0229457567\n";
+
+	string str1;
+	string str2 = "這是字串";
+	string str3 = "這是\"Ｃ++\",非常簡單";
+	cout << "str1的內容是:" << str1 << endl;
+	cout << "str2的內容是:" << str2 << endl;
+	cout << "str3的內容是:" << str3 << endl;
 	return 0;
 }

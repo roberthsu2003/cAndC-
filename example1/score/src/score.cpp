a1fb3e38255a15adae56ccc719a9510003010f29
robert <roberthsu2003@gmail.com>
2019-06-26 19:12:41 +0800

    modify

---
+++ example1/score/src/score.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : score.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "姓名\t座號\t電話" << endl;
+	cout << "李振聲\t" << 11 << "\t0229424563\n";
+	cout << "張四聲\t" << 13 << "\t0229457567";
+	return 0;
+}

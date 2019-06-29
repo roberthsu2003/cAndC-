ba167e6a0c18aba59738a096e2cd42fb06f7340a
roberthsu2004 <roberthsu2004@gmail.com>
2019-06-29 16:18:34 +0800

    modify

---
+++ example/callByReference/src/callByReference.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : callByReference.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+void turbo(int&);
+
+int main() {
+	int speed;
+	cout << "請輸入初始速度:";
+	cin >> speed;
+	turbo(speed);
+	cout << "加速後的速度:" << speed << endl;
+	return 0;
+}
+void turbo(int& speed){
+	cout << "加速前速度:" << speed << endl;
+	speed += 10;
+}

7fd3698e82507050e9ed25c2f61dc2b9d794e8d5
roberthsu2004 <roberthsu2004@gmail.com>
2019-06-29 09:55:25 +0800

    modify

---
+++ example/array1/src/array1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : array1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
+	cout << scores[0] << "," << scores[1] << "," << scores[2];
+	return 0;
+}

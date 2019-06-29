956e2266d98767546e74dd98fac6581b03286dfd
roberthsu2004 <roberthsu2004@gmail.com>
2019-06-29 14:22:59 +0800

    modify

--- example/function2/src/function2.cpp
+++ example/function2/src/function2.cpp
@@ -1,13 +1,11 @@
 //============================================================================
 // Name        : function2.cpp
 //輸入攝氏溫度，求華氏溫度
-
+//function 的原型宣告
 #include <iostream>
 using namespace std;
 
-float temperature(int value){
-	return 1.8 * value + 32;
-}
+float temperature(int);
 
 int main() {
 	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
@@ -19,4 +17,8 @@ int main() {
 	result = temperature(value);
 	cout << "華氏溫度=" << result << endl;
 	return 0;
+}
+
+float temperature(int value){
+	return 1.8 * value + 32;
 }

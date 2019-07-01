bbc307e91900f524c88a30091d9deaf76a23037e
robert <roberthsu2003@gmail.com>
2019-07-01 21:31:34 +0800

    modify

--- example1/forNest1/src/forNest1.cpp
+++ example1/forNest1/src/forNest1.cpp
@@ -1,10 +1,6 @@
 //============================================================================
 // Name        : forNest1.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+//利用2層迴圈列印「井」字，將其排列成直角三角形
 
 #include <iostream>
 using namespace std;

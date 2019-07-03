5a77eaa574c852e79b58056c4d98fa732ade461c
robert <roberthsu2003@gmail.com>
2019-07-03 19:37:21 +0800

    Update dowhile2.cpp

--- example1/dowhile2/src/dowhile2.cpp
+++ example1/dowhile2/src/dowhile2.cpp
@@ -1,10 +1,6 @@
 //============================================================================
 // Name        : dowhile2.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+//預設的密碼為5678，使用者若輸入的密碼錯誤，將不斷出現輸入密碼訊息，直到輸入的密碼正確才顯示正確訊息
 
 #include <iostream>
 using namespace std;
@@ -14,7 +10,7 @@ int main() {
 	do{
 		cout << "請輸入密碼:";
 		cin >> password;
-	}while(password != "1234");
+	}while(password != "5678");
 	cout << "恭喜!你的密碼正確了!請進";
 	return 0;
 }

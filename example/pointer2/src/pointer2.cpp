b845abdd12b032f301f083827e343fa39a370508
roberthsu2004 <roberthsu2004@gmail.com>
2019-07-06 15:40:39 +0800

    modify

---
+++ example/pointer2/src/pointer2.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : pointer2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n[3] = {1, 2, 3};
+	cout << "陣列n的位址=" << n << endl;
+	cout << "陣列元素n[0]的位址=" << &n[0] << endl;
+	cout << "陣列元素n[1]的位址=" << &n[1] << endl;
+	cout << "陣列元素n[2]的位址=" << &n[2] << endl;
+
+	return 0;
+}

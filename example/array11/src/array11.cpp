58176ee68c6893f6db3c8ebe4fc000320d20f311
roberthsu2004 <roberthsu2004@gmail.com>
2019-07-06 10:50:58 +0800

    modify

---
+++ example/array11/src/array11.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : array11.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void showArray(int tempary[], int count){
+
+	for(int i=0; i<count; i++){
+		cout << "any[" << i << "]=" << tempary[i] << " \t";
+	}
+	cout << "\n==========================================";
+}
+
+int main() {
+	int ary[] = {212, 328, 765, 986};
+	int count = sizeof(ary)/ sizeof(ary[0]);
+	cout << "呼叫showArray";
+	showArray(ary, count);
+	return 0;
+}

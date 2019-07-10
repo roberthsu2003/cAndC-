c1a31e443a42fdc000ed2adb842beac5c1558a26
robert <roberthsu2003@gmail.com>
2019-07-10 19:19:54 +0800

    modify

---
+++ example1/return1/src/return1.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : return1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+void showNum(int n){
+	int i = 1;
+	while(true){
+		if(i > n){
+			return;
+		}
+		cout << i << " ";
+		i++;
+	}
+}
+int main() {
+	int m;
+	cout << "請輸入數字 n:";
+	cin >> m;
+	showNum(m);
+	return 0;
+}

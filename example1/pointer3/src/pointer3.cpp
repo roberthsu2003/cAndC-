6ed94f1520b5f9458c630dbbdf25d9a2f42e15fe
robert <roberthsu2003@gmail.com>
2019-07-12 19:35:14 +0800

    modify

---
+++ example1/pointer3/src/pointer3.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : pointer3.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n=10;
+	int *p;
+	p = &n;
+	cout << "n=" << n << endl;
+	cout << "&n=" << &n << endl;
+	cout << "p=" << p << endl;
+	cout << "*p=" << *p << endl;
+	cout << "*&n=" << *&n << endl;
+	return 0;
+}

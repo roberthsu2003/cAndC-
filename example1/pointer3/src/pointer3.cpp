698525cd97e63e17a5a12905c798b0dd98e02444
robert <roberthsu2003@gmail.com>
2019-07-12 19:51:39 +0800

    modify

--- example1/pointer3/src/pointer3.cpp
+++ example1/pointer3/src/pointer3.cpp
@@ -1,22 +1,18 @@
 //============================================================================
-// Name        : pointer3.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+// Name        : pointer4.cpp
+//宣告n=10,指標p指向n的位址，以指標p在取記憶體的內容
 
 #include <iostream>
 using namespace std;
 
 int main() {
 	int n=10;
-	int *p;
-	p = &n;
-	cout << "n=" << n << endl;
-	cout << "&n=" << &n << endl;
-	cout << "p=" << p << endl;
-	cout << "*p=" << *p << endl;
-	cout << "*&n=" << *&n << endl;
+	int *p = &n;
+	cout << "變數n的值=" << n << endl;
+	cout << "指標p的記憶體內容=" << *p << endl;
+
+	*p=20;
+	cout << "改變後變數n的值=" << n << endl;
+
 	return 0;
 }

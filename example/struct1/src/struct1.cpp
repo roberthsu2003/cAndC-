93cb347e0da9fda62b6beb10a4fd7e8ec0056e51
roberthsu2004 <roberthsu2004@gmail.com>
2019-07-13 15:46:13 +0800

    modify

---
+++ example/struct1/src/struct1.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : struct1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+//宣告(定義)結構
+struct Student{
+	char name[8];
+	int score;
+};
+
+int main() {
+	//建立結構實體
+	struct Student david = {"David", 90};
+	cout << "姓名" << "\t" << "成績" << endl;
+	cout << david.name << "\t" << david.score;
+	return 0;
+}

319d67c4f2a24410b03a36ee94bc6d406668a260
robert <roberthsu2003@gmail.com>
2019-07-17 19:50:50 +0800

    modify

---
+++ example1/FirstClass1/src/FirstClass1.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : FirstClass1.cpp
+// Author      : 
+
+
+#include <iostream>
+using namespace std;
+//定義class
+class Person{
+	public:
+	int tall;
+	string name;
+	void walk(int var){
+		cout << name << "每小時可跑" << var << "公里" << endl;
+	}
+
+};
+
+
+int main() {
+	Person david;
+	david.tall = 180;
+	david.name = "David";
+	cout << david.name << " 身高=" << david.tall << endl ;
+	david.walk(15);
+
+	Person robert;
+	robert.tall = 183;
+	robert.name = "Robert";
+	cout << robert.name << " 身高=" << robert.tall << endl;
+	robert.walk(20);
+	return 0;
+}

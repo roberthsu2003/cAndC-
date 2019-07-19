9ae83646b6d1d848de997b49a15cdb6ecde28a80
robert <roberthsu2003@gmail.com>
2019-07-19 19:43:50 +0800

    modify

---
+++ example1/arduino/sketch_jul19a/sketch_jul19a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Are you hearing me?");
+  delay(1000);
+}

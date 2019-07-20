ef4c13f8608a13503dd020e9307f241ec9bc2ff0
roberthsu2004 <roberthsu2004@gmail.com>
2019-07-20 14:26:52 +0800

    Update sketch_jul20a.ino

--- example/arduino/sketch_jul20a/sketch_jul20a.ino
+++ example/arduino/sketch_jul20a/sketch_jul20a.ino
@@ -1,11 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
   
   Serial.println("can you see me?");
-  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
 }

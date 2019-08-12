ee66319a0fde83e8b2e51da369082f16fb9aab51
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-12 15:15:23 +0800

    Update cincout.cpp

--- 範列/cincout/src/cincout.cpp
+++ 範列/cincout/src/cincout.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
 	string name; //建立變數
-	name = "robert"; //給變數值
+	cin >> name; //輸入name
 	cout << "這是我第一支的c和c++的程式 for " + name << endl; // 使用cout輸出資料
 	return 0;
 }

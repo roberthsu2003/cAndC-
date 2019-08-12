ba41620454662c8a7859490d00b1ab8290af9df1
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-12 15:36:40 +0800

    Update cincout.cpp

--- 範列/cincout/src/cincout.cpp
+++ 範列/cincout/src/cincout.cpp
@@ -7,11 +7,17 @@
 //============================================================================
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
+
 
 int main() {
 	string name; //建立變數
 	cin >> name; //輸入name
 	cout << "這是我第一支的c和c++的程式 for " + name << endl; // 使用cout輸出資料
+	puts("這是使用puts輸出的");
+	printf("Hello!World");
 	return 0;
 }

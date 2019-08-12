42250e4b23a02e6da2132ae7a7776c72acec813c
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-12 14:33:03 +0800

    modify

---
+++ 範列/cincout/src/cincout.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cincout.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World Robert!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}

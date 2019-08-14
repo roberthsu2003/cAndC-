ad705a709e7bcd06c5edb4912a57923394a9068e
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-14 13:48:29 +0800

    modify

---
+++ 範列/question1/src/question1.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : question1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "姓名 \t 座號 \t 電話\n";
+	cout << "李振聲 \t 11 \t 0229435437\n";
+	cout << "張四聲 \t 22 \t 0229765457\n";
+	return 0;
+}

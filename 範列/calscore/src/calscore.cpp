9774f921fdcba28babd1b3aa64de405888dfabe1
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-14 15:57:02 +0800

    modify

---
+++ 範列/calscore/src/calscore.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : calscore.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned short chinese, english, math;
+	cout << "請輸入國文成績(0-100):";
+	cin >> chinese;
+	cout << "請輸入英文成績(0-100):";
+	cin >> english;
+	cout << "請輸入數學成績(0-100):";
+	cin >> math;
+
+	int sum = chinese + english + math;
+	float average = (float)sum / 3;
+
+	cout << "您的總分為" << sum << "分,平均為" << average << "分\n";
+	return 0;
+}

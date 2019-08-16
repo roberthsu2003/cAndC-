d92097aedcb86221c50ec569ed58a1cc13518b55
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-16 14:49:07 +0800

    Update mathop.cpp

--- 範列/mathop/src/mathop.cpp
+++ 範列/mathop/src/mathop.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 int main() {
+	/*
 	int n=0,m=0;
 	cout << "請輸入被除數(整數):";
 	cin >> n;
@@ -17,5 +18,20 @@ int main() {
 	cin >> m;
 
 	cout << "商:" << n/m << ",餘數:" << n%m << endl;
+	*/
+
+	//前置遞增
+		int x = 1;
+		int y = ++x;
+		//x = x + 1;
+		//y = x;
+		cout << "x = " << x << " y = "  << y << endl;
+
+	//後置遞增
+			int m = 1;
+			int n = m++;
+			//n = m;
+			//m = m + 1
+			cout << "m = " << m << " n = "  << n << endl;
 	return 0;
 }

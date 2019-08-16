2c685d30a0b9b2be6369064b5db363c2fd626ec2
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-16 15:48:16 +0800

    modify

--- 範列/complex/src/complex.cpp
+++ 範列/complex/src/complex.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : mathop.cpp
+// Name        : complex.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -10,6 +10,13 @@
 using namespace std;
 
 int main() {
-	cout << (false || true);
+	float num, result;
+	cout << "請輸入任意數:";
+	cin >> num;
+	result = num;
+	result *= num;
+	cout << "此數的平方是:" << result << "\n";
+	result *= num;
+	cout << "此數的立方是:" << result << "\n";
 	return 0;
 }

4475e446854dc49e32b3bfa05c362433d56ac3df
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-21 14:02:09 +0800

    modify

---
+++ 範列/while2/src/while2.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : while2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int deposit=0, num=0, inputNum;
+	while(deposit < 30000){
+		num++;
+		cout << "請輸入第" << num << "個月份的存款:";
+		cin >> inputNum;
+		deposit += inputNum;
+	}
+	cout << "恭喜!已經存夠了, 存了" << num << "個月的總存款為:" << deposit << "元";
+	return 0;
+}

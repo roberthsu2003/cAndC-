944328ad7270d57a25b6e4393b7d2e2263fbe05b
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-26 15:51:43 +0800

    Update function2.cpp

--- 範列/function2/src/function2.cpp
+++ 範列/function2/src/function2.cpp
@@ -9,19 +9,27 @@
 #include <iostream>
 using namespace std;
 
-float temperature(int cValue){
-	return 1.8 * cValue + 32;
-}
+float temperature(int);
+
 
 int main() {
 	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
 	cout << "----------------\n";
 	int value;
 	float result;
-	cout << "請輸入攝氏溫度:";
-	cin >> value;
-	result = temperature(value);
-	cout << "華氏溫度=" << result << endl;
-
+	string runAgain;
+	do{
+		cout << "請輸入攝氏溫度:";
+		cin >> value;
+		result = temperature(value);
+		cout << "華氏溫度=" << result << endl;
+		cout << "程式還要繼續嗎?(輸入N...結束):";
+		cin >> runAgain;
+	}while(runAgain != "N");
+	cout << "程式結束";
 	return 0;
+}
+
+float temperature(int cValue){
+	return 1.8 * cValue + 32;
 }

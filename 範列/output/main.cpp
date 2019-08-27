232f5d9ffcd6fa0c1361c38479aa5006c8fe1cb4
Robert Hsu <roberthsu2003@gmail.com>
2019-08-27 21:34:07 +0800

    modify

--- 範列/output/main.cpp
+++ 範列/output/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-
 using namespace std;
-//-fexec-charset=BIG-5
-int main()
-{
-    string name;
-    cout << "c and c++ programming design." << endl;
-    cout << "I like C++" << endl;
-    cout << "We all like c" << endl;
-    cout << "請輸入中文姓名:";
-    cin >> name;
-    cout << "您的姓名是" << name << endl;
-    return 0;
+
+int main() {
+	unsigned short chinese = 0, english = 0, math = 0;
+	cout << "請輸入國文成績(0~100):";
+	cin >> chinese;
+
+	cout << "請輸入英文成績(0~100):";
+	cin >> english;
+
+	cout << "請輸入數學成績(0~100):";
+	cin >> math;
+
+	int sum = chinese + english + math;
+	float average = (float)sum / 3;
+	cout << "您的總分為:" << sum << "分, 平均為" << average << "分\n";
+
+	return 0;
 }

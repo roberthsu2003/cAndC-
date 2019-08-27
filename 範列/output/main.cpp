6731a5031ad28578b973c176ff9885b4a99ee703
Robert Hsu <roberthsu2003@gmail.com>
2019-08-27 21:11:29 +0800

    modify

--- 範列/output/main.cpp
+++ 範列/output/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
+//-fexec-charset=BIG-5
 int main()
 {
     cout << "c and c++ programming design." << endl;
     cout << "I like C++" << endl;
-    cout << "We all like c";
+    cout << "We all like c" << endl;
+    cout << "請輸入中文姓名:";
     return 0;
 }

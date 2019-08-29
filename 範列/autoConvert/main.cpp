56b418c7c68c5eb683703451809c82b62c9494f9
Robert Hsu <roberthsu2003@gmail.com>
2019-08-29 21:11:58 +0800

    Update main.cpp

--- 範列/autoConvert/main.cpp
+++ 範列/autoConvert/main.cpp
@@ -4,5 +4,8 @@ int main() {
   float PI = 3.14159;
   int radius = 10;
   //自動轉換資料類型
-  cout << "圓面積=" << PI * radius * radius;
-}
+  cout << "圓面積=" << PI * radius * radius << endl;
+   //如果是指定式，編譯器會自動將=右邊的資料型別轉換為左邊的資料型別。
+  int area;
+  area = PI * radius * radius;
+  cout << "area = " << area; 

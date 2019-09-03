20de8a19fd69f472f12e3821f4bde47956b2cdd6
Robert Hsu <roberthsu2003@gmail.com>
2019-09-03 20:50:18 +0800

    Create main.cpp

---
+++ 範列/sizeof/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = sizeof(char);
+  int b = sizeof(double);
+  int c = sizeof(a);
+  int d = sizeof(a+b);
+  cout << "a=" << a << endl;
+  cout << "b=" << b << endl;
+  cout << "c=" << c << endl;
+  cout << "d=" << d << endl;
+  
+  return 0;
+}

e830fd7f3b6e1fd80db61fddc8cf3235d6360f16
Robert Hsu <roberthsu2003@gmail.com>
2019-09-03 21:26:56 +0800

    Create main.cpp

---
+++ 範列/ladders/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float top, bottom, height, area;
+  cout << "請輸入梯形的上底(公分):";
+  cin >> top;
+
+  cout << "請輸入梯形的下底(公分):";
+  cin >> bottom;
+
+   cout << "請輸入梯形的高(公分):";
+   cin >> height;
+
+   area = (top + bottom) * height / 2;
+   cout << "梯形的面積:" << area << "平方公分\n";
+   
+   return 0;
+}

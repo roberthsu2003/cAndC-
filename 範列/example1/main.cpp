3e279401daefb31b92ad220aadc9e095b8d27650
Robert Hsu <roberthsu2003@gmail.com>
2019-09-19 21:12:08 +0800

    Update main.cpp

--- 範列/example1/main.cpp
+++ 範列/example1/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using  namespace std;
+
 int main() {
     int firstHour,firstMin;
     int secondHour,secondMin;
     int totalMins;
+    int timeSegments;
+    int payMoney;
 
     printf("請輸入進場時間:");
     scanf("%d %d",&firstHour, &firstMin);
@@ -11,25 +18,19 @@ int main() {
 
     totalMins = (secondHour-firstHour) * 60 + (secondMin - firstMin);
 
-    cout << "進場時間是" << firstHour << "-" << firstMin << endl;
-    cout << "出場時間是" << secondHour << "-" << secondMin << endl;
+    timeSegments = totalMins /30;
 
-    cout << "停車的總分數" << totalMins << endl;
-}int main() {
-    int firstHour,firstMin;
-    int secondHour,secondMin;
-    int totalMins;
-
-    printf("請輸入進場時間:");
-    scanf("%d %d",&firstHour, &firstMin);
-
-    printf("請輸入出場時間:");
-    scanf("%d %d",&secondHour, &secondMin);
-
-    totalMins = (secondHour-firstHour) * 60 + (secondMin - firstMin);
+    if(totalMins <= 120){
+        payMoney = timeSegments * 30;
+    }else if(totalMins <= 240){
+        payMoney = timeSegments * 40;
+    }else{
+        payMoney = timeSegments * 60;
+    }
 
     cout << "進場時間是" << firstHour << "-" << firstMin << endl;
     cout << "出場時間是" << secondHour << "-" << secondMin << endl;
 
     cout << "停車的總分數" << totalMins << endl;
+    cout << "停車的總費用是" << payMoney << endl;
 }

d05a365cb1485db80c0cbc02dee97bfcef12c698
Robert Hsu <roberthsu2003@gmail.com>
2020-01-14 16:09:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,12 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i = 10;
-	int j = 20;
-	printf("i=%d\n",i);
-	printf("i=%d,j=%d\n",i,j);
+int main() {
+	std::cout << "Hello!World\n";
+	std::cout << "Hello!again!\n";
+	std::cout << "another Hello\n";
 	return 0;
 }

74c093d77f41306369d97b5282e5aeac80de95f1
Robert Hsu <roberthsu2003@gmail.com>
2020-01-14 15:42:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,9 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello,徐國堂");
-	printf("Hi\n");
-	printf("Robert\n")
-	system("pause");
+	int i = 10;
+	int j = 20;
+	printf("i=%d\n",i);
+	printf("i=%d,j=%d\n",i,j);
 	return 0;
 }

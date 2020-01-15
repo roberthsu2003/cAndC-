aa83739956ff0d5b6f46c6ec9b4c498fa7be5a36
Robert Hsu <roberthsu2003@gmail.com>
2020-01-15 14:43:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,8 +6,11 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	cout << "姓名\t\t座號\t\t電話\n";
-	cout << "李振聲\t\t45\t\t09453445\n";
-	cout << "李振聲\t\t23\t\t09453445\n";
+	char characterA;
+	characterA = 'a';
+	cout << characterA << "\n";
+	char characterB;
+	characterB = 97;
+	cout << characterB << "\n";
 	return 0;
 }

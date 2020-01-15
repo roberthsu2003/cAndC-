806d9c15099350d800136f49dc8efb7307ab7fa1
Robert Hsu <roberthsu2003@gmail.com>
2020-01-15 15:44:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,9 +6,18 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	char* name1 = "robert";
-	string name2 ="jenny";
-	cout << "\nname1:" << name1 << "\n";
-	cout << "\nname2:" << name2 << "\n";
+	unsigned short chinese=0, english=0, math=0;
+	cout << "請輸入國文成績(0~100):";
+	cin >> chinese;
+	
+	cout << "請輸入英文成績(0~100):";
+	cin >> english;
+	
+	cout << "請輸入數學成績(0~100):";
+	cin >> math;
+	int sum = chinese + english + math;
+	float average = (float)sum/3;
+	cout << "您的總分為:" << sum << "分, 平均為" << average << "分\n";
+	
 	return 0;
 }

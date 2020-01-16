ad792368af937fa5970c600a429822fa41dddcc2
Robert Hsu <roberthsu2003@gmail.com>
2020-01-16 16:02:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,17 +6,13 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	float top, bottom, height, area;
-	cout << "請輸入梯形的上底(公分):";
-	cin >> top;
+	//char* password;
+	string password;
+	cin >> password;
 	
-	cout << "請輸入梯形的下底(公分):";
-	cin >> bottom;
+	if(password=="1234"){
+		cout << "密碼正確，歡迎光臨";
+	}
 	
-	cout << "請輸入梯形的高(公分):";
-	cin >> height;
-	area = (top + bottom) * height / 2;
-	
-	cout << "梯形的面積:" << area << "平方公分\n";
 	return 0;
 }

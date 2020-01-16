7bd7b7ed07bcc06aabb7d51543c6b13c7e06810b
Robert Hsu <roberthsu2003@gmail.com>
2020-01-16 14:52:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,11 +6,13 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int x = 1;
-	int y = x++;
-	//x = x + 1;
-	//y = x;
-	cout << "x = " << x << " y = "  << y << endl;
-
+	float num,result;
+	cout << "請輸入任意數:";
+	cin >> num;
+	result = num;
+	result *= num;
+	cout << "此數的平方是:" << result << endl;
+	result *= num;
+	cout << "此數的立方是:" << result << endl;
 	return 0;
 }

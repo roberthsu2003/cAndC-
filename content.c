7bbcd14ae9c0ddccc19d36d0c2d1ec4891af291a
Robert Hsu <roberthsu2003@gmail.com>
2020-01-16 14:20:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,12 +6,11 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int n=0, m=0;
-	printf("請輸入被除數(整數):");
-	cin >> n;
-	cout << "請輸入除數(整數,不可以為0):";
-	cin >> m;
-	
-	cout << "商:" << n/m << "餘數:" << n%m << "\n";
+	int x = 1;
+	int y = x++;
+	//x = x + 1;
+	//y = x;
+	cout << "x = " << x << " y = "  << y << endl;
+
 	return 0;
 }

b08abd0f9700347dbf8bfb409f80357a82d12e6d
Robert Hsu <roberthsu2003@gmail.com>
2020-01-16 15:41:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,21 +6,17 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	float in1, in2, in3, max;
-	cout << "請輸入第一個數:";
-	cin >> in1;
+	float top, bottom, height, area;
+	cout << "請輸入梯形的上底(公分):";
+	cin >> top;
 	
-	cout << "請輸入第二個數:";
-	cin >> in2;	
+	cout << "請輸入梯形的下底(公分):";
+	cin >> bottom;
 	
-	cout << "請輸入第三個數:";
-	cin >> in3;
+	cout << "請輸入梯形的高(公分):";
+	cin >> height;
+	area = (top + bottom) * height / 2;
 	
-	max = in1 > in2 ? in1 : in2;
-	max = max > in3 ? max : in3;
-	
-	cout << "請輸入三個數中最大的數為:" << max << "\n";
-	
-	
+	cout << "梯形的面積:" << area << "平方公分\n";
 	return 0;
 }

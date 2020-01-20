f721d6bab0090eac447a305176b12696e696cfa8
Robert Hsu <roberthsu2003@gmail.com>
2020-01-20 15:38:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,11 +6,14 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	for(int i=1; i<=15; i++){
-		for(int j=1; j<=15; j++){
-			cout << i << "*" << j << "=" << i*j << "\t";
-		}
-		cout << "\n";
+	int deposit=0,num=0,inputNum;
+	while(deposit<30000){
+		num++;
+		cout << "請輸入第" << num << "個月份的存款:";
+		cin >> inputNum;
+		deposit += inputNum;
 	}
+	
+	cout << "恭喜!已經存夠了,存了" << num << "個月的總存款為:" << deposit << "元";
 	return 0;
 }

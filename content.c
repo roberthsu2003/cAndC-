de2eef83a87c6392c54570b369ba0fc46b4018ed
Robert Hsu <roberthsu2003@gmail.com>
2020-01-20 14:21:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,31 +6,11 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	char op;
-	int num1 = 20, num2 = 4;
-	cout << "num1=20, num2=4";
-	cout << "請輸入要執行的運算(+-*/):";
-	cin >> op;
-	switch(op){
-		case '+':
-			cout << "num1+num2=" << num1+num2 << "\n";
-			break;
-		
-		case '-':
-			cout << "num1-num2=" << num1-num2 << "\n";
-			break;
-		
-		case '*':
-			cout << "num1*num2=" << num1*num2 << "\n";
-			break;
-			
-		case '/':
-			cout << "num1/num2=" << num1/num2 << "\n";
-			break;
-		
-		default:
-			cout << "無法執行運算!\n";
-		
+	int sum = 0;
+	for(int i=2;i<=10;i+=2){
+		sum += i;
+		cout << "第" << i/2 << " 次迴圈的 i = " << i << "總和為 " << sum << "\n";
 	}
+
 	return 0;
 }

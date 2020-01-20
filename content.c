d49686255cf23d07e4669ef0b4ab52d64be5ccfd
Robert Hsu <roberthsu2003@gmail.com>
2020-01-20 16:14:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,11 +6,19 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	string password;
+	int num = 0;
+	int score=0;
+	int sum=0;
 	do{
-		cout << "請輸入密碼:";
-		cin >> password;
-	}while(password != "5678");
-	cout << "恭喜!你的密碼正確了!請進";
+		cout << "請輸入第" << num+1 << "學生的成績:";
+		cin >> score;
+		if (score < 0){
+			break;
+		}
+		sum += score;
+		num +=1;	
+	}while(true);
+	
+	cout << "全型總成績為:" << sum << "平均分數為" << (float)sum / num;
 	return 0;
 }

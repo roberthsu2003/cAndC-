dc8589c52f10b15268ebf46c808d2b3da44a898d
Robert Hsu <roberthsu2003@gmail.com>
2020-01-20 16:26:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,15 +10,20 @@ int main() {
 	int score=0;
 	int sum=0;
 	do{
-		cout << "請輸入第" << num+1 << "學生的成績:";
+		cout << "請輸入第" << num+1 << "個數值:";
 		cin >> score;
 		if (score < 0){
 			break;
 		}
+		num +=1;
+		
+		if(score % 2 == 1){
+			continue;
+		}
 		sum += score;
-		num +=1;	
+			
 	}while(true);
 	
-	cout << "全型總成績為:" << sum << "平均分數為" << (float)sum / num;
+	cout << "所有輸入的正偶的加總是:" << sum ;
 	return 0;
 }

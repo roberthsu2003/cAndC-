962d35efdb7b210b20b344f39210b280f7719cdc
Robert Hsu <roberthsu2003@gmail.com>
2020-01-20 14:38:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,11 +6,16 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int sum = 0;
-	for(int i=2;i<=10;i+=2){
-		sum += i;
-		cout << "第" << i/2 << " 次迴圈的 i = " << i << "總和為 " << sum << "\n";
+	int n=0,total=0;
+	for(int i=1;i<=7;i++){
+		if(i == 7){
+			cout << "請輸入星期日的支出:" << endl;	
+		}else{
+			cout << "請輸入星期" << i << "的支出:";	
+		}
+		cin >> n;
+		total += n;
 	}
-
+	cout << "本星期的支出為:" << total << "元\n";
 	return 0;
 }

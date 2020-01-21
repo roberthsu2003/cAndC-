ed99e3b108ea1c9ffb986228e24b11682b0d4357
Robert Hsu <roberthsu2003@gmail.com>
2020-01-21 13:51:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,24 +6,11 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int num = 0;
-	int score=0;
-	int sum=0;
-	do{
-		cout << "請輸入第" << num+1 << "個數值:";
-		cin >> score;
-		if (score < 0){
-			break;
-		}
-		num +=1;
-		
-		if(score % 2 == 1){
-			continue;
-		}
-		sum += score;
-			
-	}while(true);
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2]= 90;
 	
-	cout << "所有輸入的正偶的加總是:" << sum ;
+	cout << scores[0] << "," << scores[1] << ", " << scores[2];
 	return 0;
 }

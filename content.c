24bc27100ae2682fc04bfa884b08eabc6b8d8b81
Robert Hsu <roberthsu2003@gmail.com>
2020-01-21 16:09:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,12 +6,35 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int n[] = {1, 3, 5, 7, 9};
-	int totalSize = sizeof(n);
-	int oneSize = sizeof(n[0]);
-	int counts = totalSize / oneSize;
-	cout << "陣列的大小為:"
-	 << totalSize
-	 << ",一個元素的大小是" << oneSize << ",陣列的數量是:" << counts << endl;
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+	float array[nums], temp;
+	
+	for(int i=0; i<nums; i++){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
+	}
+	
+	cout << "排序前:\n";
+	for(int i=0; i<nums; i++){
+		cout << array[i] << " ";
+	}
+	
+	
+	for(int i=0; i<nums-1;i++){
+		for(int j=i+1; j<nums; j++){
+			if (array[i] < array[j]){
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	cout << "\n";
+	cout << "排序後:\n";
+	for (int i=0; i<nums; i++){
+		cout << array[i] << " ";
+	}
 	return 0;
 }

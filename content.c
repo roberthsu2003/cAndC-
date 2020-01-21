adcce2663a23218d249224b75cd98e55aaa9fdd2
Robert Hsu <roberthsu2003@gmail.com>
2020-01-21 14:51:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,14 +6,19 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int a=0, b=0, c=0, d=0;
-	printf("a=");
-	scanf("%d",&a);
-	printf("b=");
-	scanf("%d",&b);
-	printf("a+b=%d\n",a+b);
-	printf("c d=");
-	scanf("%d %d", &c, &d);
-	printf("c*d=%d\n",c*d);
+	char letter;
+	int age;
+	char url[30];
+	float price;
+	printf("請輸入字元:");
+	scanf("%c",&letter);
+	printf("請輸入age:");
+	scanf("%d",&age);
+	printf("請輸入字串:");
+	scanf("%s",url);
+	printf("請輸入價格:");
+	scanf("%f",&price);
+	printf("這是字元:%c\n", letter);
+	printf("這是整數:%d,這是字串:%s,這是浮點數:%f\n",age, url, price);
 	return 0;
 }

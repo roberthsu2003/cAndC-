1410ae005c9a42ec982ffea8d5a637eadf740813
Robert Hsu <roberthsu2003@gmail.com>
2020-01-21 14:35:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,21 +6,14 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	const int nums = 5;
-	int scores[nums];
-	int sum = 0;
-	float ave;
-	for(int i=0;i<nums;i++){
-		cout << "請輸入第" << i+1 << "位學生的成績";
-		cin >> scores[i];
-	}
-	
-	for(int i=0; i<nums; i++){
-		sum += scores[i];
-	}
-	
-	ave = (float)sum / nums;
-	
-	cout << "全班總成績為:" << sum << "分,平均為" << ave << "分\n";	
+	int a=0, b=0, c=0, d=0;
+	printf("a=");
+	scanf("%d",&a);
+	printf("b=");
+	scanf("%d",&b);
+	printf("a+b=%d\n",a+b);
+	printf("c d=");
+	scanf("%d %d", &c, &d);
+	printf("c*d=%d\n",c*d);
 	return 0;
 }

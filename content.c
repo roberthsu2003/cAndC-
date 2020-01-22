1a98059f78ef0787741233972c2e16c2dcc0b8b7
Robert Hsu <roberthsu2003@gmail.com>
2020-01-22 13:59:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,37 +4,11 @@
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void sayHello(){
+	cout << "歡迎光臨";
+}
 
 int main() {
-	int nums;
-	cout << "請輸入要排序的數值個數:";
-	cin >> nums;
-	float array[nums], temp;
-	
-	for(int i=0; i<nums; i++){
-		cout << "請輸入第" << i+1 << "個數值:";
-		cin >> array[i];
-	}
-	
-	cout << "排序前:\n";
-	for(int i=0; i<nums; i++){
-		cout << array[i] << " ";
-	}
-	
-	
-	for(int i=0; i<nums-1;i++){
-		for(int j=i+1; j<nums; j++){
-			if (array[i] < array[j]){
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-	cout << "\n";
-	cout << "排序後:\n";
-	for (int i=0; i<nums; i++){
-		cout << array[i] << " ";
-	}
+	sayHello();
 	return 0;
 }

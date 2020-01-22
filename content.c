2be67f7578aa30137fd59b8bfd0182465d2fa4ae
Robert Hsu <roberthsu2003@gmail.com>
2020-01-22 16:04:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,11 +5,11 @@
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+int dice();
 int main() {
 	unsigned int seed = (unsigned int)time(NULL);
 	srand(seed);
-	int n;
+	
 	while(true){
 		cout << "請按任意鍵擲骰子,使用E結束遊戲";
 		char ch = getchar();
@@ -17,11 +17,19 @@ int main() {
 			cout << "擲骰子遊戲結束!" << endl;
 			break;
 		}else{
-			n = 1 + rand() % (6+1-1);
-			cout << "您所擲的點數為:" << n << "點" << endl;
+			for (int i=1; i<=4; i++){
+				cout << "您第"<< i << "次所擲的點數為:" << dice() << "點" << endl;
+			}
+			
 		}
 		
 		
 	}
 	return 0;
+}
+
+int dice(){
+	int n;
+	n = 1 + rand() % (6+1-1);
+	return n;	
 }

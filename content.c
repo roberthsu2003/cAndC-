3b2f9ef5c9c535e1c6bb42db5f6af1228cf97ee6
Robert Hsu <roberthsu2003@gmail.com>
2020-01-22 14:12:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,11 +4,17 @@
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void sayHello(){
-	cout << "歡迎光臨";
+float temperature(int value){
+	return 1.8 * value + 32;
 }
-
 int main() {
-	sayHello();
+	int value;
+	float result;
+	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
+	cout << "------------------\n";
+	cout << "請輸入攝氏溫度:";
+	cin >> value;
+	result = temperature(value);
+	cout << "華氏溫度=" << result << endl;
 	return 0;
 }

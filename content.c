877f84e9243c1593466051631a3deccbf7a2389d
Robert Hsu <roberthsu2003@gmail.com>
2020-01-22 15:28:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,27 +1,20 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void showNum(int);
 
 int main() {
-	int inputNum;
-	cout << "請輸入數字 n:";
-	cin >> inputNum;
-	showNum(inputNum);
-	cout << endl;
-	return 0;
-}
-
-void showNum(int n){
-	int i = 1;
-	while(true){
-		if (i > n){
-			return;	
-		}
-		cout << i << " ";
-		i++;
+	unsigned int seed = (unsigned int)time(NULL);
+	srand(seed);
+	for(int i=0; i<5; i++){
+			cout << rand() << endl;
 	}
+	
+	int r = 90 + rand() % (100+1-90);
+	cout << r;
+
+	return 0;
 }

e4ba051c8c1af1d7bae35a176aacc1746d28a8f0
Robert Hsu <roberthsu2003@gmail.com>
2020-01-22 14:44:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,27 +4,24 @@
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-float temperature(int);
+void showNum(int);
 
 int main() {
-	int value;
-	float result;
-	char runAgain;
-	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
-	cout << "------------------\n";
-	do{
-		cout << "請輸入攝氏溫度:";
-		cin >> value;
-		result = temperature(value);
-		cout << "華氏溫度=" << result << endl;
-		cout << "程式還要繼續嗎?(輸入N...結束):";
-		cin >> runAgain;
-	}while(runAgain!='N');
-
-	cout << "程式結束";
+	int inputNum;
+	cout << "請輸入數字 n:";
+	cin >> inputNum;
+	showNum(inputNum);
+	cout << endl;
 	return 0;
 }
 
-float temperature(int value){
-	return 1.8 * value + 32;
+void showNum(int n){
+	int i = 1;
+	while(true){
+		if (i > n){
+			return;	
+		}
+		cout << i << " ";
+		i++;
+	}
 }

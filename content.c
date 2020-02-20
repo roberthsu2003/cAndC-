62126fcffe41953afdfe0e2855e6f1f42a28be4c
Robert Hsu <roberthsu2003@gmail.com>
2020-02-20 21:33:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,46 +1,23 @@
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-using namespace std;
-
-int main() {
-  cout << "姓名\t\t座號\t\t電話\n";
-  cout << "李振聲\t\t" << 11 << "\t\t0229435437\n";
-  cout << "張四聲\t\t" << 13 << "\t\t0229439937\n";
-
-  char chrB;
-  chrB = 'A';
-  cout << chrB << "\n";
-
-  char chrC;
-  chrC = 97;
-  cout << chrC << "\n";
-
-  //字串
-  string str1;
-  string str2 = "這是字串";
-  string str3 = "這是\"C++\",非常簡單";
-
-  cout << "\nstr1:" << str1 << "\n";
-  cout << "str2:" << str2 << endl;
-  cout << "str3:" << str3 << "\n";
-
-  //字元陣列(C語言)
-  char *name = "徐國堂";
-  cout << "name=" << name << "\n";
-  printf("name=%s\n",name);
-
-  bool bolA = true;
-  bool bolB = false;
-
-  cout << bolA << endl;
-  cout << bolB << endl;
-
-  float PI = 3.14159;
-  int radius = 10;
-  cout << "圓面積=" << PI * radius * radius << endl;
-
-  int area;
-  area = PI * radius * radius;
-  cout << "area=" << area << endl;
-}
+	using namespace std;
+	
+	int main() {
+		unsigned short chinese = 0, english = 0, math = 0;
+		float average;
+		cout << "請輸入國文成績(0~100):";
+		cin >> chinese;
+	
+		cout << "請輸入英文成績(0~100):";
+		cin >> english;
+	
+		cout << "請輸入數學成績(0~100):";
+		cin >> math;
+	
+		int sum = chinese + english + math;
+		//average = (float)sum / 3;
+		//average = (float)(sum / 3);
+		average = sum / 3;
+		cout << "您的總分為:" << sum << "分, 平均為" << average << "分\n";
+	
+		return 0;
+	}

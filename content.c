aab222ef27efc8755d3de9eb009296315f0f4886
Robert Hsu <roberthsu2003@gmail.com>
2020-02-20 19:45:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,4 +5,12 @@ int main() {
   cout << "姓名\t\t座號\t\t電話\n";
   cout << "李振聲\t\t" << 11 << "\t\t0229435437\n";
   cout << "張四聲\t\t" << 13 << "\t\t0229439937\n";
+
+  char chrB;
+  chrB = 'A';
+  cout << chrB << "\n";
+
+  char chrC;
+  chrC = 97;
+  cout << chrC << "\n";
 }

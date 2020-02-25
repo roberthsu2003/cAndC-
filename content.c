099bf712e183135f29cf83e7e9c2d1470b74e265
Robert Hsu <roberthsu2003@gmail.com>
2020-02-25 19:01:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,20 @@
 #include <iostream>
-	using namespace std;
-	
-	int main() {
-		unsigned short chinese = 0, english = 0, math = 0;
-		float average;
-		cout << "請輸入國文成績(0~100):";
-		cin >> chinese;
-	
-		cout << "請輸入英文成績(0~100):";
-		cin >> english;
-	
-		cout << "請輸入數學成績(0~100):";
-		cin >> math;
-	
-		int sum = chinese + english + math;
-		//average = (float)sum / 3;
-		//average = (float)(sum / 3);
-		average = sum / 3;
-		cout << "您的總分為:" << sum << "分, 平均為" << average << "分\n";
-	
-		return 0;
-	}
+
+using namespace std;
+
+int main() {
+	unsigned int firstDay = 0, secondDay = 0, thirdDay = 0;
+	cout << "請輸入第1天的支出:";
+	cin >> firstDay;
+
+	cout << "請輸入第2天的支出:";
+	cin >> secondDay;
+
+	cout << "請輸入第3天的支出:";
+	cin >> thirdDay;
+
+	int sum = firstDay + secondDay + thirdDay;
+	float average = sum / 3.0;
+
+	cout << "您的總支出為" << sum << "元, 平均每天支出" << average << "元";
+}

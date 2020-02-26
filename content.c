3752e0020031938d3fdfde6f11d8f8e9badf8337
Robert Hsu <roberthsu2003@gmail.com>
2020-02-26 19:24:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,16 +2,6 @@
 using namespace std;
 
 int main() {
-  float in1, in2, in3, max;
-  cout << "請輸入第一個數:";
-  cin >> in1;
-  cout << "請輸入第二個數:";
-  cin >> in2;
-
-  max = in1 > in2 ? in1 : in2;
-  cout << "請輸入第三個數:";
-  cin >> in3;
-  max = in3 > max ? in3 : max;
-
-  cout << "輸入三個數中最大數為:" << max << "\n";
+	double m = 5;
+	int n = 10;
 }

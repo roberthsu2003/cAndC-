8c9ea3b2bcc7c3cad87e98decb60714cbcf45494
Robert Hsu <roberthsu2003@gmail.com>
2020-02-27 21:11:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,28 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int money;
-	int payMoney;
-	cout << "請輸入購買金額:";
-	cin >> money;
-	if (money >= 100000){
-		payMoney = money * 0.8;
-	}else if (money >= 50000){
-		payMoney = money * 0.85;
-	}else if (money >= 30000){
-		payMoney = money * 0.9;
-	}else if (money >= 10000){
-		payMoney = money * 0.95;
-	}else{
-		payMoney = money;
+	char op;
+	int num1 = 20, num2 = 4;
+	cout << "請輸入要執行的運算(+-*/):";
+	cin >> op;
+
+	switch(op){
+		case '+':
+			cout << "num1+num2=" << num1 + num2 << "\n";
+			break;
+		case '-':
+			cout << "num1-num2=" << num1 - num2 << "\n";
+			break;
+		case '*':
+			cout << "num1*num2=" << num1 * num2 << "\n";
+			break;
+		case '/':
+			cout << "num1/num2=" << num1 / num2 << "\n";
+			break;
+		default:
+			cout << "無法執行運算!\n";
 	}
 
-	cout << "實付金額是:" << payMoney << "元\n";
-
+	return 0;
+}

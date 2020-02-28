6f745d1af3e0c78649d49cd625ad08c5be14cede
Robert Hsu <roberthsu2003@gmail.com>
2020-02-28 20:20:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,19 +2,19 @@
 using namespace std;
 
 
-//call by reference 
-void turbo(int &);
+//call by address 
+void turbo(int *);
 
 int main() {
   int speed;
   cout << "請輸入初始速度:";
   cin >> speed;
-  turbo(speed);
+  turbo(&speed);
   cout << "加速後速度:" << speed << endl;
   return 0;
 }
 
 //call by value
-void turbo(int &mySpeed){
-	 mySpeed += 10;
+void turbo(int *mySpeed){
+	 *mySpeed += 10;
 }

624a691766998667684e397c67319dbd646446da
Robert Hsu <roberthsu2003@gmail.com>
2020-02-28 19:19:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() {
-  int n[3] = {1,2,3};
-  //陣列變數
-  cout << "使用陣列變數-取記憶體位址"  << &n[0] << endl;
-  cout << "使用陣列變數-取值:" << n[0] << endl;
-  cout << "使用指標變數-取記憶體位址" << n+0 << endl;
-  cout << "使用指標變數-取值" << *(n+0) << endl;
+	const char* str1 = "one";
+	char str2[] = "Two";
+	cout << "以陣列顯示str1字串" << endl;
+	for(int i=0; i<3; i++){
+		cout << "str1[" << i << "]=" << str1[i] << endl;
+	}
 
+	cout << "以指標變數顯示str2字串" << endl;
+	for(int i=0; i<3; i++){
+		cout << "*(str2 +" << i << ")=" << *(str2+i) << endl;
+	}
 }

0ae9d1d8d252730b9a6f5a0ca0c61f5356134da9
Robert Hsu <roberthsu2003@gmail.com>
2020-02-28 19:44:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	const char* str1 = "one";
-	char str2[] = "Two";
-	cout << "以陣列顯示str1字串" << endl;
-	for(int i=0; i<3; i++){
-		cout << "str1[" << i << "]=" << str1[i] << endl;
-	}
+void sub1(int, int &);
 
-	cout << "以指標變數顯示str2字串" << endl;
-	for(int i=0; i<3; i++){
-		cout << "*(str2 +" << i << ")=" << *(str2+i) << endl;
-	}
+int main() {
+  int ary[] = {1, 2, 3};
+  cout << "ary陣列元素 ary[0]傳值,ary[1]傳搖器後" << endl;
+  sub1(ary[0] ,ary[1]);
+  for(int i=0; i<3; i++){
+	  cout << ary[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
+
+void sub1(int a, int &b){
+	a = 5;
+	b = 5;
 }

9eed91f0601cd3559655554d393e00c2f615dfd4
Robert Hsu <roberthsu2003@gmail.com>
2020-03-02 20:15:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,21 @@
 #include <iostream>
 using namespace std;
-namespace Human{
-	class Person{
+class Person{
 	public:
 		int tall;
 		string name;
-		void walk(int var){
-			cout << name << "每小時可跑" << var << "公里" << endl;
-		}
-	};
+		void walk(int);
+};
+
+void Person::walk(int var){
+	cout << name << "每小時可跑" << var << " 公里" << endl;
 }
-
-using namespace Human;
-
 
 int main() {
 	Person david;
-	david.tall = 180;
+	david.tall = 183;
 	david.name = "David";
 	cout << david.name << " 身高=" << david.tall << endl;
-	david.walk(15);
-
-	Person robert;
-	robert.tall = 183;
-	robert.name = "Robert";
-	cout << robert.name << " 身高=" << robert.tall << endl;
-	robert.walk(20);
-
+	david.walk(20);
 	return 0;
 }

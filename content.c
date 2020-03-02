922b7bb72af6cb08b764baf832ac988914c374e2
Robert Hsu <roberthsu2003@gmail.com>
2020-03-02 19:27:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,13 +3,21 @@ using namespace std;
 
 typedef struct Student{
 	string name;
-	int score;
+	int scores[5];
 } Student;
 
 int main() {
-  Student stus[3] = {{"robert", 94}, {"david", 91}, {"alice", 94}};
-  int stuCount = sizeof(stus) / sizeof(stus[0]);
-  for(int i=0; i<stuCount; i++){
-	  cout << "第" << i+1 << "位學生是" << stus[i].name  << "分數是" << stus[i].score << endl; 
-  }
+	Student stus[3] = {{"robert",{78, 98, 78, 63, 83}},{"david",{78, 85, 67, 73, 73}},{"alice",{68, 98, 74, 63, 82}}};
+
+	int stuCout = sizeof(stus) / sizeof(stus[0]);
+
+	for (int i=0; i<stuCout; i++){
+		int sum = 0;
+		for(int j=0; j<5; j++){
+			sum += stus[i].scores[j];
+		}
+
+		cout << "第" << i+1 << "位學生是" << stus[i].name << "總分數是" << sum << endl;
+	}
+
 }

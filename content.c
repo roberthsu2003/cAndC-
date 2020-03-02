fb64755ef72acf9d585b85efa7f231cb77f388d9
Robert Hsu <roberthsu2003@gmail.com>
2020-03-02 18:55:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,14 @@
 #include <iostream>
 using namespace std;
 
-struct student{
-	int id;
-	const char *name;
-	int chinese, math, english;
+struct Student{
+	const char* name;
+	int score;
 };
 
 int main() {
-	struct student david;
-	david.id = 99001;
-	david.name = "rober";
-	david.chinese = 75;
-	david.math = 86;
-	david.english = 90;
-
-	cout << "id=" << david.id << endl;
-	cout << "name=" << david.name << endl;
-	cout << "chinese=" << david.chinese << endl;
-	cout << "math=" << david.math << endl;
-	cout << "english=" << david.math << endl;
+	struct Student david = {"David", 90};
+	cout << "姓名" << "\t" << "成績" << endl;
+	cout << david.name << "\t" << david.score;
+	return 0;
 }

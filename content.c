819f56b46b2955b838a1a1c0a8830ea004918330
Robert Hsu <roberthsu2003@gmail.com>
2020-03-03 20:49:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,15 @@
 using namespace std;
 
 int main() {
-	for(int i=1; i<=5; i++){
-		for(int j=0; j<i; j++){
-			cout << "#";
-		}
-		cout << endl;
-	}
+  int deposit = 0;
+  int num = 0;
+  int inputNum;
+  while(deposit < 30000){
+	  num++;
+	  cout << "請輸入第" << num << "個月份的存款:";
+	  cin >> inputNum;
+	  deposit += inputNum;
+  }
+
+  cout << "恭喜!您已經存夠了,存了" << num << "個月的總存款為:" << deposit << "元。";
 }

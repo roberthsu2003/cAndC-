aeedb5f758fceced2c750a28ef7f187e1cfb63e2
Robert Hsu <roberthsu2003@gmail.com>
2020-03-03 21:30:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,22 +2,19 @@
 using namespace std;
 
 int main() {
-	string password;
-	/*
-	cout << "請輸入密碼:";
-	cin >> password;
-
-	while(password!="1234"){
-		cout << "請輸入密碼:";
-		cin >> password;
-	}
-	*/
-
+	int sum = 0;
+	int num = 0;
+	int score;
 	do{
-		cout << "請輸入密碼:";
-		cin >> password;
-	}while(password != "1234");
+		num += 1;
+		cout << "請輸入第" << num << "學生的成績:";
+		cin >> score;
+		if(score < 0){
+			break;
+		}
 
-	cout << "恭喜!您的密碼正確了!請進";
+		sum += score;
+	}while(true);
 
+	cout << "全班總成績為:" << sum << "平均分數為" << (float)sum/num;
 }

33b54181e9de61c2272160762d37110ab39422f7
Robert Hsu <roberthsu2003@gmail.com>
2020-03-03 19:17:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,57 +1,11 @@
-//Car.h
-class Car{
-	private:
-		int speed;
-	
-	public:
-		Car();
-		Car(int);
-		void turbo();
-		void setSpeed(int);
-		int getSpeed();
-};
-//end
-
-//Car.cpp
-#include "Car.h"
-
-Car::Car(){
-	speed = 70;
-}
-
-Car::Car(int n){
-	speed = n;
-}
-
-void Car::turbo(){
-	speed += 10;
-}
-
-void Car::setSpeed(int var){
-			if(var < 60){
-				speed = 60;
-			}else if(var > 100){
-				speed = 100;
-			}else{
-				speed = var;
-			}
-		
-}
-
-int Car::getSpeed(){
-	return speed;
-}
-
-//end
-
 #include <iostream>
-#include "Car.h"
 using namespace std;
 
 int main() {
-	Car BMW;	
-	cout << "現在速度=" << BMW.getSpeed() << endl;
-	Car BMW1(95);
-	cout << "BMW1的速度=" << BMW1.getSpeed() << endl;
-	return 0;
+	int sum = 0;
+	for(int i=2; i<=10; i+=2){
+		sum += i;
+		cout << "第" << i/2 << " 次迴圈的 i=" << i << ",總和為" << sum << "\n";
+  }
+  return 0;
 }

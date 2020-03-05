d69b9d18f69b2f6f66182cf6e3077d45ad1e8016
Robert Hsu <roberthsu2003@gmail.com>
2020-03-05 19:15:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
+
+	cout << scores[0] << "," << scores[1] << "," << scores[2];
+	return 0;
+}

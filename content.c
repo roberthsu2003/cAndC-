17fb2ac07bab08dbeb9fc3bba783c3420f55f271
Robert Hsu <roberthsu2003@gmail.com>
2020-03-10 20:48:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,19 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 int main() {
-//c++ string
-string str = "DevC++";
-str = "JavaC++";
-cout << "c++字串:" << str << endl;
-
-//c字元陣列
-char str1[7] = {'D', 'e', 'v', 'C', '+', '+', '\0'};
-cout << str1 << endl;
-
-//字元陣列
-char str2[] = "DevC++";
-cout << str2 << endl;
-str2[0] = 'G';
-cout << str2 << endl;
-
+	int maxIndex = 48;
+	int lot[49];
+	for(int i=0; i<49; i++){
+		lot[i] = i+1;
+	}
+	srand(time(NULL));
+	for(int i=0; i<7; i++){
+		cout<< lot[rand()%(maxIndex+1)] << endl;
+	}
+	
+	
 }

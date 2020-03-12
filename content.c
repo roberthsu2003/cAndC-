b69302d4d7e866db5802f9893c21d4cc4cb0b8c1
Robert Hsu <roberthsu2003@gmail.com>
2020-03-12 19:19:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,13 @@
-https://repl.it/@roberthsu2003/function1
+#include <iostream>
+using namespace std;
+
+//建立(定義)function
+void sayHello(){
+	cout << "歡迎光臨!" << endl;
+}
+
+int main() {
+//呼叫function
+	sayHello();
+	return 0;
+}

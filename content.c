88cea8786e19bdaef119d93b6aec61e8666c347b
Robert Hsu <roberthsu2003@gmail.com>
2020-03-12 20:49:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void showNum(int n){
-	int i = 1;
-	while(true){
-		if(i > n){
-			return;
-		}
-		cout << i << " ";
-		i++;
+void showArray(int tempary[], int count){
+	for(int i=0; i< count; i++){
+		cout << "any[" << i << "]=" << tempary[i] << "\t";
 	}
+	cout << "\n\n";
 }
 
 int main() {
-	int inputNum;
-	cout << "請輸入數字 n:";
-	cin >> inputNum;
-	showNum(inputNum);
-	cout << endl;
+	int ary[] = {212, 328, 765, 986};
+	int count = sizeof(ary) / sizeof(ary[0]);
+	cout << "呼叫showArray\n";
+	showArray(ary, count);
 	return 0;
 }

76c2fa1409a4447241250a2c7f916b99851e08ac
Robert Hsu <roberthsu2003@gmail.com>
2020-03-12 19:46:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
-
-float temperature(int value){
-	return 1.8 * value + 32;
-}
+//函式原型
+float temperature(int);
 
 int main() {
 	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
@@ -15,4 +13,8 @@ int main() {
 	result = temperature(value);
 	cout << "華氏溫度=" << result << endl;
 	return 0;
+}
+
+float temperature(int value){
+	return 1.8 * value + 32;
 }

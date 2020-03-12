86fff1278ef4965cccb54ba95c210d9d7dbc8812
Robert Hsu <roberthsu2003@gmail.com>
2020-03-12 20:03:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
-//函式原型
-float temperature(int);
 
-int main() {
-	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
-	cout << "-----------------------\n";
-	int value;
-	float result;
-	cout << "請輸入攝氏溫度:";
-	cin >> value;
-	result = temperature(value);
-	cout << "華氏溫度=" << result << endl;
-	return 0;
+void showNum(int n){
+	int i = 1;
+	while(true){
+		if(i > n){
+			return;
+		}
+		cout << i << " ";
+		i++;
+	}
 }
 
-float temperature(int value){
-	return 1.8 * value + 32;
+int main() {
+	int inputNum;
+	cout << "請輸入數字 n:";
+	cin >> inputNum;
+	showNum(inputNum);
+	cout << endl;
+	return 0;
 }

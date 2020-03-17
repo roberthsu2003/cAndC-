94139efe00d01b46996f984178b0b6ae85858431
Robert Hsu <roberthsu2003@gmail.com>
2020-03-17 20:17:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
 using namespace std;
 
 int main() {
 	unsigned int seed = time(NULL);
 	srand(seed);
+	
+	while(true){
+		char ch = getchar();
+		if(ch == 'E'){
+			cout << "遊戲結束!" << endl;
+			break;
+		}else{
+			int n = 1 + rand() % (6+1-1);
+			cout << "您所擲的點數為:" << n << "點" << endl; 
+		}
 
-	for(int i=0; i<5; i++){
-		cout << 10 + rand() % (20+1-10) << endl;
 	}
+	
 	return 0;
 }

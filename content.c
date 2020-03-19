33e0625104fdded9ef93801b9a92ca5761dd271b
Robert Hsu <roberthsu2003@gmail.com>
2020-03-19 19:34:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-	int n[3] = {1,2,3};
+	char n[] = {'r','o','b', 'e', 'r', 't'};
 	cout << "陣列變數語法" << n[0] << endl;
 
 	cout << "指標變數的語法" << *(n+0) << endl;
 
-	int *p = n;
+	char *p = "jenny";
 
 	cout << "陣列變數語法" << p[0] << endl;
 
 	cout << "指標變數的語法" << *(p+0) << endl;
 
+	string s = "Alice";
 }

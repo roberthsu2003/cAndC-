ffea7bd9a449dabf4aecea959234c1aa6f3eaf54
Robert Hsu <roberthsu2003@gmail.com>
2020-03-19 19:23:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,20 +2,15 @@
 using namespace std;
 
 int main() {
-	int n = 10;
-	int m = 20;
-	int *p;
-	p = &n;
+	int n[3] = {1,2,3};
+	cout << "陣列變數語法" << n[0] << endl;
 
-	cout << "變數n的值=" << n << endl;
-	cout << "指標p指向的記憶體位址=" << p << endl;
-	cout << "*p的值=" << *p << endl;
-	cout << "----------------------------" << endl;
+	cout << "指標變數的語法" << *(n+0) << endl;
 
-	p = &m;
-	cout << "變數m的值=" << m << endl;
-	cout << "指標p指向的記憶體位址=" << p << endl;
-	cout << "*p的值=" << *p << endl;
-	cout << "----------------------------" << endl;
-	return 0;
+	int *p = n;
+
+	cout << "陣列變數語法" << p[0] << endl;
+
+	cout << "指標變數的語法" << *(p+0) << endl;
+
 }

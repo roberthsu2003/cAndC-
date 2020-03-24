f738127d7c539bba1646693f384463da16d37fe6
Robert Hsu <roberthsu2003@gmail.com>
2020-03-24 19:51:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-//定義結構
-struct Student{
-	const char* name;
+
+typedef struct Student{
+	string name;
 	int score;
-};
+}Student;
 
 int main() {
-	struct Student david;
-	david.name = "David";
-	david.score = 283;
-	cout << "姓名" << david.name << endl;
-	cout << "分數" << david.score << endl;
+	Student stus[3] = {{"robert", 94},{"david", 91},{"alice",94}}; 
+	for(int i=0; i<3; i++){
+		Student student = stus[i];
+		cout << "第" << i+1 << "位學生是" << student.name << "分數是" << student.score << endl;
+	}
 }

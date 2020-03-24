e1b9dc8f197f18d10911777f70323c9655aac6fa
Robert Hsu <roberthsu2003@gmail.com>
2020-03-24 21:13:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,6 +5,12 @@ class Person{
 	public:
 	int tall;
 	string name;
+
+	//實體的方法
+	void walk(int var){
+		cout << name << "每小時可以跑多少" << var << "公里" << endl;
+	}
+
 };
 
 
@@ -12,11 +18,13 @@ int main() {
 	Person david;
 	david.name = "David";
 	david.tall = 170;
+	david.walk(20);
 
 	Person robert;
 	robert.name = "Robert";
 	robert.tall = 183;
-
+	robert.walk(25);
+	
 	cout << david.name << "身高=" << david.tall << endl;
 	cout << robert.name << "身高=" << robert.tall << endl;
 }

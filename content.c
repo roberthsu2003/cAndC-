04e8798c115b60760be535eb6498ea5b2764a544
Robert Hsu <roberthsu2003@gmail.com>
2020-03-26 21:01:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,44 +1,11 @@
-#include <iostream>
-using namespace std;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+}
 
-class Car{
-	private:
-		int speed;
-	public:
-		//speed(getter, setter)
-		void setSpeed(int sp){
-			if(sp < 60){
-				speed = 60;
-			}else if(sp > 100){
-				speed = 100;
-			}else{
-				speed = sp;
-			}
-		}
-
-		int getSpeed(){
-			return  speed;
-		}
-
-		void turbo(){
-			setSpeed(speed + 10);
-		}
-
-		//自訂建構式
-		Car(){
-			speed = 70;
-		}
-
-		Car(int n){
-			setSpeed(n);
-		}
-
-};
-
-int main() {
-	Car bmw;
-	cout << "執行Car bmw建立物件後,速度=" << bmw.getSpeed() << endl;
-
-	Car bmw1(45);
-	cout << "執行Car bmw1建立物件後,速度=" << bmw1.getSpeed() << endl;
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
 }

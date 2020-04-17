baa2ee8fa22c851523b9aa1d6c297490bd51c21a
Robert Hsu <roberthsu2003@gmail.com>
2020-04-17 11:14:21 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,6 +1,5 @@
-
 @interface Car : NSObject{
-    int num1, num2;
+    int num1, num2;
 }
 
 -(void) setData;
@@ -15,23 +14,32 @@
 
 @implementation Car
 -(void) setData{
-    
+    num1 = 100;
+    num2 = 200;
 }
 -(int) sum{
-    
+    return num1 + num2;
 }
 -(void) output{
-    
+    NSLog(@"num1=%d, num2=%d",num1, num2)
 }
+
 @end
+
 =========================
 
 #import <Foundation/Foundation.h>
+#import "Car.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-       Car *car = [[Car alloc] init];
+        Car *car = [[Car alloc] init];
         NSLog(@"Car is %@",car);
+        [car setData];
+        NSLog(@"sum=%i",[car sum]);
+        
+        [car output];
+        
     }
     return 0;
 }

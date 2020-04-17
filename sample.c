43c5b7a178369ca3caff8db1c81964ad0df72499
Robert Hsu <roberthsu2003@gmail.com>
2020-04-17 10:36:00 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -30,11 +30,8 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"data type bytes");
-        NSLog(@"int:%lu",sizeof(int));
-        NSLog(@"double:%lu",sizeof(double));
-        NSLog(@"char:%i",sizeof(char));
+       Car *car = [[Car alloc] init];
+        NSLog(@"Car is %@",car);
     }
     return 0;
 }

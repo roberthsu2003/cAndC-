a772aba12d8b869e0c29292bda272e8091cc2f9b
Robert Hsu <roberthsu2003@gmail.com>
2020-04-17 15:07:27 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,24 +1,60 @@
-//
-//  ViewController.m
-//  sample4
-//
-//  Created by apple on 2020/4/17.
-//  Copyright © 2020 pu. All rights reserved.
-//
 
-#import "ViewController.h"
+#import <Foundation/Foundation.h>
 
-@interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *message;
+NS_ASSUME_NONNULL_BEGIN
 
+@interface Student : NSObject
+-(void) setChinese:(double) a setEnglish:(double) b setMath:(double) c;
+-(void) average;
 @end
 
-@implementation ViewController
+NS_ASSUME_NONNULL_END
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.message.text = @"Hello! PU";
+
+======================================
+
+
+#import "Student.h"
+@interface Student(){
+        int chinese;
+        int english;
+        int math;
 }
 
-
 @end
+
+
+@implementation Student
+-(void) setChinese:(double) a setEnglish:(double) b setMath:(double) c{
+    chinese = a;
+    english = b;
+    math = c;
+    NSLog(@"Chinese=%.2d, English=%.2d, mathecatic=%.2d", chinese, english, math);
+}
+
+-(void) average{
+    double aver;
+    aver = (chinese + english + math) / 3.0;
+    NSLog(@"average = %.2f", aver);
+    
+}
+@end
+
+
+=======================
+
+#import <Foundation/Foundation.h>
+#import "Student.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Student *stu1 = [[Student alloc] init];
+        Student *stu2 = [[Student alloc] init];
+        NSLog(@"Student John:");
+        [stu1 setChinese:78 setEnglish:89 setMath:92];
+        [stu2 setChinese:95 setEnglish:85 setMath:90];
+        [stu1 average];
+        [stu2 average];
+    }
+    return 0;
+}

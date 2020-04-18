b0f1f347d9a38f13f49c346ec95ebfd9e533b5d7
Robert Hsu <roberthsu2003@gmail.com>
2020-04-18 11:46:16 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,57 +1,70 @@
 //
 //  main.m
-//  sample8
+//  sample9
 //
 //  Created by apple on 2020/4/18.
 //  Copyright © 2020 pu. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-@interface Parent:NSObject
+@interface Coordinate:NSObject
 {
-    int num1;
+    int x;
+    int y;
 }
-@property int num1;
--(void) setData;
+@property int x, y;
 @end
 
-@implementation Parent
-@synthesize  num1;
--(void) setData{
-    num1 = 666;
-}
+@implementation Coordinate
+@synthesize x, y;
+
 @end
 
-@interface Child:Parent{
-    int num2;
+@interface Circle: Coordinate
+{
+    int radius;
 }
-@property int num2;
--(void)setData:(int) n withNum2:(int) m;
--(void)print;
+@property int radius;
+-(void) setX:(int) a andY:(int) b;
+-(void) setRadius:(int) r;
+-(double) area;
 @end
 
-@implementation Child
-@synthesize num2;
+@implementation Circle
+@synthesize radius;
 
--(void)print{
-    NSLog(@"num1=%i, num2=%i", num1, num2);
+-(void) setX:(int) a andY:(int) b{
+    x = a;
+    y = b;
 }
 
--(void)setData:(int) n withNum2:(int) m{
-    num1 = n;
-    num2 = m;
+/*
+-(void) setRadius:(int) r{
+    NSLog(@"自訂的setRadius");
+    radius = r;
 }
+*/
+
+-(double) area{
+    return 3.1415962 * radius * radius;
+}
+
 
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Child *child = [[Child alloc] init];
-        [child setData:100 withNum2:200];
-        NSLog(@"num1=%i", child.num1);
-        NSLog(@"num2=%i", child.num2);
-        [child print];
-       
+        Coordinate *obj = [[Coordinate alloc] init];
+        obj.x = 10;
+        obj.y = 20;
+        
+        Circle *circle = [[Circle alloc] init];
+        [circle setX:66 andY:88];
+        circle.radius = 10;
+        
+        NSLog(@"Coordinate is (%i, %i)", circle.x, circle.y);
+        NSLog(@"radius=%i", circle.radius);
+        NSLog(@"area=%.2f", [circle area]);
         
     }
     return 0;

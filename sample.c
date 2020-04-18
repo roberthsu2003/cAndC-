c954e91361f9a206b40d42d2d7c1ad7de96e90fc
Robert Hsu <roberthsu2003@gmail.com>
2020-04-18 09:53:38 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,49 +1,41 @@
 //
-//  MyClass.h
-//  sample5
+//  main.m
+//  sample6
 //
-//  Created by apple on 2020/4/17.
+//  Created by apple on 2020/4/18.
 //  Copyright © 2020 pu. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface MyClass : NSObject
+@interface MyClass:NSObject
 {
     int num1, num2;
     int total;
 }
 @property int num1, num2;
+
+-(int)total;
 @end
 
-NS_ASSUME_NONNULL_END
 
-
-======================================
-
-#import "MyClass.h"
 
 @implementation MyClass
 @synthesize num1, num2;
 
+-(int)total{
+    return num1 + num2;
+}
 @end
-
-
-=======================
-
-#import <Foundation/Foundation.h>
-#import "MyClass.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         MyClass *obj = [[MyClass alloc] init];
-        int total;
         [obj setNum1:100];
         [obj setNum2:200];
         
-        NSLog(@"num1=%d, num2=%d", [obj num1], [obj num2])
+        int total = [obj total];
+        NSLog(@"num1=%d, num2=%d", obj.num1, obj.num2);
+        NSLog(@"total =%d", total);
     }
     return 0;
 }

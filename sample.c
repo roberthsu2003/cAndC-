1affefca116dfa3fc0c919ad84a7806673af3a99
Robert Hsu <roberthsu2003@gmail.com>
2020-04-18 10:29:58 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,41 +1,50 @@
 //
 //  main.m
-//  sample6
+//  sample7
 //
 //  Created by apple on 2020/4/18.
 //  Copyright © 2020 pu. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-@interface MyClass:NSObject
-{
+@interface MyClass:NSObject{
     int num1, num2;
     int total;
 }
 @property int num1, num2;
-
--(int)total;
+-(MyClass *) initWith:(int) n andM:(int) m;
+-(void) setNum1:(int) n andNum2:(int)m;
+-(int) total;
 @end
-
 
 
 @implementation MyClass
 @synthesize num1, num2;
 
--(int)total{
-    return num1 + num2;
+
+-(MyClass *) initWith:(int) n andM:(int) m{
+    self = [super init];
+    if(self){
+        [self setNum1:n andNum2:m];
+    }
+    return self;
+}
+
+-(void) setNum1:(int) n andNum2:(int)m{
+    self.num1 = n;
+    self.num2 = m;
+}
+
+-(int) total{
+    return self.num1 + self.num2;
 }
 @end
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        MyClass *obj = [[MyClass alloc] init];
-        [obj setNum1:100];
-        [obj setNum2:200];
-        
+        MyClass *obj = [[MyClass alloc] initWith:100 andM:200];
         int total = [obj total];
-        NSLog(@"num1=%d, num2=%d", obj.num1, obj.num2);
-        NSLog(@"total =%d", total);
     }
     return 0;
 }

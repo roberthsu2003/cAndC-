aafefcb0ba2d8313196658a2eedd879de82dbdeb
Robert Hsu <roberthsu2003@gmail.com>
2020-05-10 14:59:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,7 @@
 using namespace std;
 
 int main() {
-string str1;
-string str2 = "這是字串";
-string str3 = "這是\"C++\",非常簡單";
-cout << "\nstr1:" << str1 << "\n";
-cout << "str2:" << str2 << endl;
-cout << "str3:" << str3 << "\n";
+float PI = 3.14159;
+int radius = 10;
+cout << "圓面積=" << PI * radius * radius;
 }

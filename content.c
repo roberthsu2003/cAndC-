cbb0e64f5dce8f19044a8a19005983c4b11659dd
Robert Hsu <roberthsu2003@gmail.com>
2020-05-10 12:17:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-
+	
 int main() {
-	int score;
-	cout << "請輸入您的英文分數:";
-	cin >> score;
-	cout << "您好\n";
-	cout << "徐國堂";
-	cout << "您的分數是" << score;
+	string name;
+	cout << "請輸入中文姓名:";
+	cin >> name;
+	cout << "Hello! " << name;
+	return 0;
+}	
 

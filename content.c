687e6685348e97913ca20c57afc272e76c15be57
Robert Hsu <roberthsu2003@gmail.com>
2020-05-10 11:45:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,11 @@
-#define LED 3
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED,OUTPUT);
-  Serial.begin(115200);
-}
 
-void loop() {
-  digitalWrite(LED,HIGH);
-  Serial.println("open");
-  delay(1000);
-  digitalWrite(LED,LOW);
-  Serial.println("close");
-  delay(1000);
-  
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score;
+	cout << "請輸入您的英文分數:";
+	cin >> score;
+	cout << "您好\n";
+	cout << "徐國堂";
 }

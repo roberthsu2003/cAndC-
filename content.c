4505d5cdd4048fc13a708046210575c723048e57
Robert Hsu <roberthsu2003@gmail.com>
2020-05-10 16:10:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,30 @@
+/*
+outlay_s.cpp
+小張要了解三天出遊花費的總支出和平均，設計程式讓小張輸入三天的支出後，自動計算其支出總額及平均
+
+------
+請輸入第一天的支出: 321
+請輸入第二天的支出: 123
+請輸入第三天的支出: 567
+您的總支出為 xxxx 元，平均每天支出 xxx.xxx 元
+*/
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
-	unsigned short chinese=0, english=0, math=0;
-	float average;
-	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;
+  unsigned int firstDay = 0, secondDay = 0, thirdDay = 0;
+  cout << "請輸入第1天的支出:";
+  cin >> firstDay;
 
-	cout << "請輸入英文成績(0~100):";
-	cin >> english;
+  cout << "請輸入第2天的支出:";
+  cin >> secondDay;
 
-	cout << "請輸入數學成績(0~100):";
-	cin >> math;
+  cout << "請輸入第3天的支出:";
+  cin >> thirdDay;
 
-	int sum = chinese + english + math;
-	average = sum / 3;
-
-	cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
+  int sum = firstDay + secondDay + thirdDay;
+  float average = sum / 3.0;
+  cout << "您的總支出為" << sum << "元，平均每天支出" << average << "元";
 }

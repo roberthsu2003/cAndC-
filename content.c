96c6f16abc3f50150498dbdeca73d1fe2c53c6c7
Robert Hsu <roberthsu2003@gmail.com>
2020-05-17 14:17:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,16 @@
-////讓使用者輸入三個任意數，程式會顯示三數中的最大數。
+//讓使用者輸入密碼,如果輸入的密碼正確(1234), 要顯示「密碼正確!歡迎光臨!」。如果不正確就不做任何事
 #include <iostream>
 using namespace std;
 
 int main() {
-	float in1, in2, in3, max;
-	cout << "請輸入第一個數:";
-	cin >> in1;
-
-	cout << "請輸入第二個數:";
-	cin >> in2;
-
-	max = in1 > in2 ? in1 : in2;
-
-	cout << "請輸入第三個數:";
-	cin >> in3;
-
-	max = max > in3 ? max : in3;
-	cout << "輸入三個數中最大的數為:" << max << "\n";
+	string password;
+	cout << "請輸入密碼:";
+	cin >> password;
+	if(password == "1234"){
+		//true程式區塊
+		cout << "密碼正確!歡迎光臨!\n";
+	}else{
+		//false程式區塊
+		cout << "密碼錯誤!\n";
+	}
 }

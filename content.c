4c163a95234c5c1ce66d029211816dccf586cc27
Robert Hsu <roberthsu2003@gmail.com>
2020-05-17 13:47:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,20 @@
+////讓使用者輸入三個任意數，程式會顯示三數中的最大數。
 #include <iostream>
 using namespace std;
 
 int main() {
-	int a = sizeof(char);
-	int b = sizeof(double);
-	int c = sizeof(a);
-	int d = sizeof(a+b);
+	float in1, in2, in3, max;
+	cout << "請輸入第一個數:";
+	cin >> in1;
 
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
-	cout << "c=" << c << endl;
-	cout << "d=" << d << endl;
+	cout << "請輸入第二個數:";
+	cin >> in2;
+
+	max = in1 > in2 ? in1 : in2;
+
+	cout << "請輸入第三個數:";
+	cin >> in3;
+
+	max = max > in3 ? max : in3;
+	cout << "輸入三個數中最大的數為:" << max << "\n";
 }

36170bda0945d036cdb163e63be473b08c233ef4
Robert Hsu <roberthsu2003@gmail.com>
2020-05-24 10:12:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,25 +1,27 @@
-//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
-
 #include <iostream>
 using namespace std;
 
 int main() {
-	int money;
-	int payMoney;
-	cout << "請輸入購買的金額:";
-	cin >> money;
+ char op;
+ int num1=20, num2 = 4;
+ cout << "num1=20,num2=4\n";
+ cout << "請輸入要執行的運算(+ - * /):";
+ cin >> op;
+ switch(op){
+	 case '+':
+		cout << "num1+num2=" << num1+num2 << "\n";
+		break;
+	 case '-':
+		cout << "num1-num2=" << num1-num2 << "\n";
+		break;
+	 case '*':
+		cout << "num1*num2=" << num1*num2 << "\n";
+		break;
+	 case '/':
+		cout << "num1/num2=" << num1/num2 << "\n";
+		break;
+	 default:
+	 	cout << "無法執行運算!\n";
+ }
 
-	if(money >= 100000){
-		payMoney = money * 0.8;
-	}else if(money >= 50000){
-		payMoney = money * 0.85;
-	}else if(money >= 30000){
-		payMoney = money * 0.9;
-	}else if(money >= 10000){
-		payMoney = money * 0.95;
-	}else{
-		payMoney = money;
-	}
-
-	cout << "實付金額是:" << payMoney << "元\n";
 }

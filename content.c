7f7df0752bf0f58f1591e31cb79cab72015131bd
Robert Hsu <roberthsu2003@gmail.com>
2020-05-31 10:28:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,9 +3,6 @@ using namespace std;
 
 //建立陣列,利用索引編號存值和取出值
 int main() {
-	int scores[3];
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
+	int scores[] = {90, 80, 70};
 	cout << scores[0] << "," << scores[1] << "," << scores[2];
 }

aa1365edb03ac8cddfaa2141b1ed39f212655685
Robert Hsu <roberthsu2003@gmail.com>
2020-05-31 15:30:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,20 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
 using namespace std;
 
 int main() {
-	int nums[] = {2, 5, 3, 4};
-	int numsCount = sizeof(nums)/sizeof(nums[0]);
-	for(int i=0;i<numsCount-1;i++){
-		printf("i=%d\n",i);
-		for(int j=i+1; j<numsCount;j++){
-			if(nums[i] > nums[j]){
-				int temp = nums[i];
-				nums[i] = nums[j];
-				nums[j] = temp;
-			}
-		}
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+
+	float array[nums];
+
+	for (int i=0; i<nums; i++){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
 	}
 
-	for(int i=0; i<numsCount; i++){
-		printf("%d ",nums[i]);
+	cout << "排序前:\n";
+	for(int i=0; i<nums; i++){
+		cout << array[i] << " ";
 	}
 }

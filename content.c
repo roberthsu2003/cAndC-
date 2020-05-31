dc413d6134d042299891d75db528b0a93c7955c3
Robert Hsu <roberthsu2003@gmail.com>
2020-05-31 10:14:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,11 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
 using namespace std;
-int main() {
-	int min = 1;
-	int max = 99;
-	int keyin;
-	int guess;
-	int count = 0;
-	srand((unsigned long)time(NULL));
-	guess = rand() % max + min;
-	cout << "==============猜數字遊戲==========\n\n";
-	do{	
-		
-		cout << "猜數字範圍" << min << "~" << max << ":";
-		cin >> keyin;
-		count ++;
-		if(keyin >=min && keyin <=max){
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-				cout << "您悄了" << count << "次\n\n";
-				break;
-			}else if(keyin > guess){
-				max = keyin;
-				cout << "再小一點\n";
-			}else if(keyin < guess){
-				min = keyin;
-				cout << "再大一點!\n";
-			}
-		}else{
-			cout << "請輸入提示範內的數字!";
-		}
-	}while(true);
 
+//建立陣列,利用索引編號存值和取出值
+int main() {
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
+	cout << scores[0] << "," << scores[1] << "," << scores[2];
 }

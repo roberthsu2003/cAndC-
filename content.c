dbf34fc1f2443f352686350d78c0ff202ac2f4c4
Robert Hsu <roberthsu2003@gmail.com>
2020-05-31 13:58:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
 using namespace std;
-int main() {
-	char letter;
-	int age;
-	char url[30];
-	float price;
 
-	printf("請輸入字元:");
-	scanf("%c", &letter);
-	printf("請輸入age:");
-	scanf("%d",&age);
-	printf("請輸入字串:");
-	scanf("%s", url);
-	printf("請輸入價格:");
-	scanf("%f", &price);
-	printf("這是字元:%c\n",letter);
-	printf("這是整數:%d,這是字串:%s,這是浮點數:%f\n",age, url, price);
+int main() {
+	int i = 10;
+	printf("i的記憶體大小%dbyte\n",(int)sizeof(i));
+
+	float f = 10.12;
+	printf("f的記憶體大小%dbyte\n",(int)sizeof(f));
+
+	double d=10.12;
+	printf("f的記憶體大小%dbyte\n",(int)sizeof(d));
+
+	long l=10;
+	printf("l的記憶體大小%dbyte\n",(int)sizeof(l));
+
+	long long int lli=10;
+	printf("lli的記憶體大小%dbyte\n",(int)sizeof(lli));
+
+	int n[]={1, 3, 5, 7, 9, 11, 13, 15, 17,19};
+	printf("n的記憶體大小%dbyte\n",(int)sizeof(n));
+	printf("n[0]的記憶體大小%dbyte\n",(int)sizeof(n[0]));
+	printf("n的數量%d個\n",(int)(sizeof(n)/sizeof(n[0])));
 }

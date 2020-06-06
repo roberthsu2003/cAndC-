7912fef45159e3ee6cf5b4da630ae8366ff6b99e
Robert Hsu <roberthsu2003@gmail.com>
2020-06-06 17:01:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,20 +2,19 @@
 using namespace std;
 
 int main() {
-	unsigned char chinese, english, math;
+	unsigned short chinese=0, english=0, math=0;
 	
 	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;
-
+	cin >> chinese;	
 
 	cout << "請輸入英文成績(0~100):";
-	cin >> english;
+	cin >> english;	
 
 	cout << "請輸入數學成績(0~100):";
 	cin >> math;
 
 	int sum = chinese + english + math;
-	float average = sum / 3;
+	float average = sum / 3.0;
 	cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
 	
 

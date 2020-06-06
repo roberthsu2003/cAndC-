b019c871bcea1b1b2fa71c3ec524af8f3d46008f
Robert Hsu <roberthsu2003@gmail.com>
2020-06-06 12:19:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
 	//宣告變數
 	int englishScore;
-	englishScore = 60;
-	cout << "我的英文分數是:" << englishScore;
+	englishScore = 0; //指定值給變數
+	cout << "請輸入您的英文分數:"; //輸出控制台
+	cin >> englishScore;
+	cout << "您的英文分數是:" << englishScore;
 }

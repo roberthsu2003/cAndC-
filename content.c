a4e8384ada23f4230034c39b99bd3785e151e28c
Robert Hsu <roberthsu2003@gmail.com>
2020-06-06 16:31:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,16 +2,13 @@
 using namespace std;
 
 int main() {
-	bool bolA = true;
-	bool bolB = false;
+	float PI = 3.14159;
+	int radius = 10;
+	cout << "圓面積=" << radius * radius * PI << endl;
 
-	cout << bolA << "\n";
-	cout << bolB << endl;
+	int area = radius * radius * PI;
+	cout << "圓面積=" << area << endl;
 
-	bool bolC = -156;
-	bool bolD = 0;
-
-	cout << bolC << "\n";
-	cout << bolD << endl;
-
+	float area1 = radius * radius * PI;
+	cout << "圓面積=" << area1;
 }

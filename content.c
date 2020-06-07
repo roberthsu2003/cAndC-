dd5f2e025837a633b92dc28c16a9f5c2c9c1a627
Robert Hsu <roberthsu2003@gmail.com>
2020-06-07 15:09:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-int factorial(int n){
-	if(n==0){
-		return 1;
-	}else{
-		return n * factorial(n-1);
-	}
+int add(int a, int b){
+	return a + b;
+}
+
+string add(string a, string b){
+	return a + b;
 }
 
 int main() {
-	int n;
-	unsigned long long int total;
-	printf("請輸入數字n:");
-	scanf("%d", &n);
-	total = factorial(n);
-	printf("%d! = %lld",n,total);
+	printf("add(2,3)=%d\n",add(2,3));
+	printf("===============================\n");
+	cout << "add(\"Nice\", \"Play\") = " << add("Nice", "Play") << endl;
 	return 0;
+;
 }

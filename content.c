3eb7c43e22fc47d5a7b716dfc3854a5334699b61
Robert Hsu <roberthsu2003@gmail.com>
2020-06-07 12:16:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 using namespace std;
 
-float temperature(int);
-
-int main() {
-	int value;
-	char runAgain;
-	float result;
-	do{
-		cout << "請輸入攝氏溫度:";
-		cin >> value;
-		result = temperature(value);
-		printf("華氏溫度=%.2f\n",result);
-		printf("程式還要繼續嗎?(輸入N....結束))\n");	
-		scanf("%s",&runAgain);
-	}while(runAgain != 'N');
-	printf("程式結束");
+void showNum(int n){
+	int i = 1;
+	while(true){
+		if(i > n){
+			return;
+		}
+		cout << i << " ";
+		i++;
+	}
 }
 
-float temperature(int value){
-	return 1.8 * value + 32;
+int main() {
+	int inputNum;
+	printf("請輸入數字n:");
+	scanf("%d", &inputNum);
+	showNum(inputNum);
+	printf("\n");
+	return 0;
 }

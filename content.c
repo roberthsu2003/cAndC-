82288236aff9c3d8c942e8366b3b41e604c8a12b
Robert Hsu <roberthsu2003@gmail.com>
2020-06-07 09:54:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,17 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
+//sayHello function 
+void sayHello(){
+	printf("歡迎光臨");
+	
+}
+
+//main function 
 int main() {
-	unsigned short chinese=0, english=0, math=0;
-	
-	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;	
-
-	cout << "請輸入英文成績(0~100):";
-	cin >> english;	
-
-	cout << "請輸入數學成績(0~100):";
-	cin >> math;
-
-	int sum = chinese + english + math;
-	float average = sum / 3.0;
-	cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
-	
-
+	sayHello();
+	return 0;
 }

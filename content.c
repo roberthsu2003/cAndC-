8aa92a2f36d77e5f69a40fd5c527f7cba0cb9d75
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 15:56:08 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,24 @@
+//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
 using namespace std;
 
 int main() {
-string password;
-//cout << "請輸入密碼:";
-printf("請輸入密碼:");
-cin >> password;
-//雙向選擇
-if(password == "1234"){
-	cout << "密碼正確!歡迎光臨";
-}else{
-	cout << "密碼錯誤!\n請重新輸入\n";
-}
+	int money;
+	int payMoney;
+	cout << "請輸入購買金額:";
+	cin >> money;
+	if(money >=100000){
+		payMoney = money * 0.8;
+	}else if(money >= 80000){
+		payMoney = money * 0.85;
+	}else if(money >= 50000){
+		payMoney = money * 0.9;
+	}else if(money >= 30000){
+		payMoney = money * 0.95;
+	}else{
+		payMoney = money;
+	}
+
+	cout << "實付金額是:" << payMoney << "元\n";
 
 }

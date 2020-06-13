2fb4e96dffed98e9d279ccfe692a35ea698d0598
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 15:23:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+
 using namespace std;
 
 int main() {
-int a=0, b=0, c=0, d=0;
-cout << "請輸入a:";
-scanf("%d", &a);
-printf("您輸入的a是%d\n", a);
+string password;
+//cout << "請輸入密碼:";
+printf("請輸入密碼:");
+cin >> password;
+//單向選擇
+if(password == "1234"){
+	cout << "密碼正確!歡迎光臨";
+}
 
-printf("請輸入b:");
-scanf("%d",&b);
-printf("您輸入的b是%d\n", b);
-
-printf("請輸入c和d的值(格式:c-d):");
-scanf("%d-%d",&c,&d);
-printf("您輸入的c是%d\n", c);
-printf("您輸入的d是%d\n", d);
 }

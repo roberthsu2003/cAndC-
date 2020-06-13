d046ff5dfaf9f82b8f862ef03ab8c6e29c09f2a3
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 11:49:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,7 @@
 #include <iostream>
-using  namespace std;
+using namespace std;
 
 int main() {
-int x = 1;
-int y = ++x;
-cout << "x=" << x << "\ny=" << y << endl;
-
-cout << "後置" << endl;
-int m =1;
-int n = m++;
-cout << "m=" << m << "\nn=" << n << endl;
-
-cout << "左邊沒有等於" << endl;
-++x;
-cout << "x=" << x << endl;
+	int a = 1, b = 1;
+	cout << "a==b:" << (a == b);
 }

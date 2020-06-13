913ac6cb7eeb668e42ffaa757ea9da2a21e62d75
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 10:36:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,12 @@
-#include <iostream>
-#include <stdio.h>
-#define PI 3.14159
+//輸入半徑,計算出圓面積
 
+#include <iostream>
+#define PI 3.14159
 using namespace std;
 
 int main() {
-	//const double PI = 3.14159;
-	cout << "我的圓周率" << PI << endl;
-	printf("我的圓周率為%f",PI);
+	double radius;
+	cout << "請輸入圓半徑:";
+	cin >> radius;
+	cout << "半徑" << radius << "的圓面積是" << radius * radius * PI;
 }

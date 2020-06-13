de385ec5744bbcba3875a3bac39d28622bb07408
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 10:14:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,9 @@
 #include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
-using namespace std;
-static int total = 0;
 
-void getData(string name){
-	static int n = 0;
-	n++;
-	cout << "第" << n << "位候選人是:" << name << endl;
-	total ++;
-}
 
 int main() {
-	getData("David");
-	getData("Amy");
-	getData("Tony");
-	cout << "共有" << total << "位登記" << endl;
+	const float PI = 3.14159;
+	std::cout << "我的圓周率" << PI;
+	//printf("我的圓周率:%f",PI);
 }

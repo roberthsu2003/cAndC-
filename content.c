af02ae464b6d1af4720f2b886dc319596f60925a
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 11:37:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,17 @@
-
-//讓使用者輸入被除數(整數)及除數(整數，不可以是零)，程式會顯示兩數相除的商及餘數。
-
 #include <iostream>
-using namespace std;
+using  namespace std;
 
 int main() {
-	int n=0, m=0;
+int x = 1;
+int y = ++x;
+cout << "x=" << x << "\ny=" << y << endl;
 
-	cout << "請輸入被除數(整數):";
-	cin >> n;
-	cout << "請輸入除數(整數,不可以為0):";
-	cin >> m;
-	cout << "商:" << n/m << "餘數:" << n%m << "\n";
- }
+cout << "後置" << endl;
+int m =1;
+int n = m++;
+cout << "m=" << m << "\nn=" << n << endl;
+
+cout << "左邊沒有等於" << endl;
+++x;
+cout << "x=" << x << endl;
+}

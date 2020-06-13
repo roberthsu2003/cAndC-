facb890b0c085fde766a5ceb7325feeea68619a2
Robert Hsu <roberthsu2003@gmail.com>
2020-06-13 12:11:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,14 @@
+//請使用者輸入一個任意數，程式會顯示此數的平方值及立方值
 #include <iostream>
 using namespace std;
 
 int main() {
-	int a = 1, b = 1;
-	cout << "a==b:" << (a == b);
+	float num,result;
+	cout << "請輸入任意數:";
+	cin >> num;
+	result = num;
+	result *= num;
+	cout << "此數的平方是:" << result << "\n";
+	result *= num;
+	cout << "此數的立方是:" << result << "\n";
 }

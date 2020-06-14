480c754ef1ea592b5edc6a503981484355aa13ec
Robert Hsu <roberthsu2003@gmail.com>
2020-06-14 11:53:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main() {
-	int n=10;
-	int *p;
+	int n = 10;
+	int m = 20;
+	int *p; //建立指標變數
 	p = &n;
-	cout << "n=" << n << endl;
-	cout << "&n=" << &n << endl;
-	cout << "p=" << p << endl;
-	cout << "*p=" << *p << endl;
-	cout << "*&n=" << *&n << endl;
+	cout << "變數n的值=" << n << endl;
+	cout << "指標p指向的記憶體位址=" << p << endl;
+	cout << "*p的值=" << *p << endl;
+	cout << "-------------------------" << endl;
+
+	p = &m;
+	cout << "變數m的值=" << m << endl;
+	cout << "指標p指向的記憶體位址=" << p << endl;
+	cout << "*p的值=" << *p << endl;
+	cout << "-------------------------" << endl;
 }

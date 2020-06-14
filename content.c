c754266c577070e1534de320a2f7eafa92c530b2
Robert Hsu <roberthsu2003@gmail.com>
2020-06-14 14:26:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
 int main() {
-	int array[] = {1, 2, 3, 4, 5};
-	cout << "使用陣列變數取出值" << array[1] << endl;
-	cout << "使用陣列變數取出位址" << &array[1] << endl;
-	cout << "使用指標變數取出位址" << (array+1) << endl;
-	cout << "使用指標變數取出值" << *(array+1) << endl;
+//指標變數
+	const char* str1 = "one";
+	char str2[] = "two";
+	cout << "以陣列顯示str1字串" << endl;
+	for(int i=0; i<3; i++){
+		printf("str1[%d]=%c\n",i,str1[i]);	
+	}
+
+	cout << "以指標變數顯示str2字串" << endl;
+	for(int i=0; i<3; i++){
+		printf("*(str2 + %d)=%c\n",i,*(str2+i));
+	}
+
+	str1 = str2;
+
+	cout << "以陣列顯示str2字串" << endl;
+	for(int i=0; i<3; i++){
+		printf("str1[%d]=%c\n",i,str1[i]);	
+	}
+
+	
 }

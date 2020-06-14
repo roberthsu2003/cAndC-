c04d2e4474d46442a38699a5ae8d4d3fbdc31712
Robert Hsu <roberthsu2003@gmail.com>
2020-06-14 10:02:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,15 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
-
 int main() {
-	int n, sum=0;
-	
-	for(int i=1; i<=7; i++){
-		if(i == 7){
-			cout << "請輸入星期日的支出:";
-		}else{
-			cout << "請輸入星期" << i << "的支出:";
-		}
-		
-		cin >> n;
-		sum += n;
-	}
-
-	cout << "本星期的支出為:" << sum <<"元\n";
+	double m = 5;
+	int n = 10;
+	cout << "變數m的值=" << m << endl;
+	cout << "變數n的值=" << n << endl;
+	cout << "變數&m的位址=" << &m << endl;
+	cout << "變數&n的位址=" << &n << endl;
+	cout << "變數m的記憶體=" << sizeof(m) << "Bytes" << endl;
+	cout << "變數n的記憶體=" << sizeof(n) << "Bytes" << endl;
 }

3c6fad776630fc604f325203f197fd3fb6145345
Robert Hsu <roberthsu2003@gmail.com>
2020-06-14 11:27:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,10 +3,11 @@ using namespace std;
 
 int main() {
 	int n=10;
-	int *p = &n;
-	cout << "變數n的值=" << n << endl;
-	cout << "變數n的位址=" << &n << endl;
-	cout << "指標變數p的值=" << p << endl;
-	cout << "指標變數p的記憶體位址是=" << &p << endl;
-	cout << "指標變數指向的值=" << *p << endl;
+	int *p;
+	p = &n;
+	cout << "n=" << n << endl;
+	cout << "&n=" << &n << endl;
+	cout << "p=" << p << endl;
+	cout << "*p=" << *p << endl;
+	cout << "*&n=" << *&n << endl;
 }

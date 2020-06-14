641a61ad86b3c7d47e1d08c9b4127b9c8616a9e5
Robert Hsu <roberthsu2003@gmail.com>
2020-06-14 14:44:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,19 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
 using namespace std;
+void sub1(int, int &);
 
 int main() {
-//指標變數
-	const char* str1 = "one";
-	char str2[] = "two";
-	cout << "以陣列顯示str1字串" << endl;
-	for(int i=0; i<3; i++){
-		printf("str1[%d]=%c\n",i,str1[i]);	
-	}
+  int a = 10;
+  int b = 20;
+  cout << "a=" << a << endl;
+  cout << "b=" << b << endl;
+  sub1(a,b);
+  cout << "a=" << a << endl;
+  cout << "b=" << b << endl;
 
-	cout << "以指標變數顯示str2字串" << endl;
-	for(int i=0; i<3; i++){
-		printf("*(str2 + %d)=%c\n",i,*(str2+i));
-	}
+}
 
-	str1 = str2;
-
-	cout << "以陣列顯示str2字串" << endl;
-	for(int i=0; i<3; i++){
-		printf("str1[%d]=%c\n",i,str1[i]);	
-	}
-
-	
+void sub1(int x, int &y){
+	x = 100;
+	y = 200;
 }

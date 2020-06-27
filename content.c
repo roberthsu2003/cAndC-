6b67a8669b9594424aae6ea92a5047e43043d912
Robert Hsu <roberthsu2003@gmail.com>
2020-06-27 16:13:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,20 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 using namespace std;
 
 int main() {
- int n[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
- int totalSize = sizeof(n);
- int oneSize = sizeof(n[0]);
- int counts = totalSize / oneSize;
- //cout << "陣列的大小為:" << totalSize << ""
- printf("陣列的大小為:%d,一個元素的大小是%d,陣列的數量是%d",totalSize, oneSize, counts);
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+	float array[nums];
+
+	for(int i=0; i<nums; i++){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
+	}
+
+	cout << "排序前:\n";
+	for(int i=0; i<nums; i++){
+		
+		cout << array[i] << " ";
+	}
 }

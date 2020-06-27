0fb22b58932525e7a5ad4e5aad7b9dd72c64cad9
Robert Hsu <roberthsu2003@gmail.com>
2020-06-27 15:55:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
 using namespace std;
 
 int main() {
-	const int nums = 5;
-	int scores[nums];
-	int sum=0;
-	float ave=0;
-	for(int i=0; i<nums; i++){
-		//cout << "請輸入第" << i+1 << "位學生的成績";
-		//cin >> scores[i];
-		printf("請輸入第%d位學生的成績",i+1);
-		scanf("%d",&scores[i]);
-	}
-
-	for(int i=0; i<nums; i++){
-		sum += scores[i];
-	}
-
-	ave = (float)sum/nums;
-	//cout << "全班總成績為:" << sum << "分,平均為" << ave << "分\n";
-	printf("全班總成績為:%d分,平均為%.2f分\n",sum, ave);
+ int n[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
+ int totalSize = sizeof(n);
+ int oneSize = sizeof(n[0]);
+ int counts = totalSize / oneSize;
+ //cout << "陣列的大小為:" << totalSize << ""
+ printf("陣列的大小為:%d,一個元素的大小是%d,陣列的數量是%d",totalSize, oneSize, counts);
 }

3032d5fe65b223a0946dc84ac328e8daaac78cc6
Robert Hsu <roberthsu2003@gmail.com>
2020-06-27 10:46:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	for(int i=1; i<=9;i++){		
-		for(int j=1; j<=9; j++){
-			cout << i << "*" << j << "=" << i*j << "\t";
+	for(int i=1; i<=5; i++){
+		for(int j=1;j<=i;j++){
+			cout << "#";
 		}
 		cout << "\n";
 	}

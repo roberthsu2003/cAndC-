817c96dff48e6bcbfb4d175a682135062de1e2db
Robert Hsu <roberthsu2003@gmail.com>
2020-07-11 16:35:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -18,3 +18,23 @@ int main() {
 	total = factorial(n);
 	printf("%.0f! = %.0f\n",n,total);
 }
+
+
+
+#include <iostream>
+using namespace std;
+
+double factorial(int n){
+	int t = 1;
+	for(int i=1; i<=n; i++){
+		t = t * i;
+	}
+	return t;
+}
+int main() {
+	int n,total;
+	cin >> n;
+	total = factorial(n);
+	cout << n << "! = " << factorial(n) << endl;
+
+}

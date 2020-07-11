da843a1763379777cf57c04c69922330ed065226
Robert Hsu <roberthsu2003@gmail.com>
2020-07-11 15:46:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,25 +1,17 @@
 #include <iostream>
 using namespace std;
-
-void showArray(int tempary[], int count){
-	for(int i=0; i<count; i++){
-		cout << tempary[i] << " ";
-	}
-	cout << endl;
-}
-
-void sub2(int tempary[], int count){
-	for(int i=0; i<count; i++){
-		tempary[i] *= 2;
-	}
-}
+void turbo(int&);
 
 int main() {
-	int ary[] = {212, 328, 765, 986};
-	showArray(ary, 4);
-
-	sub2(ary, 4);
-
-	showArray(ary, 4);
+	int speed;
+	cout << "請輸入初始速度:";
+	cin >> speed;
+	turbo(speed);
+	cout << "加速後的速度:" << speed << endl;
 	return 0;
+}
+
+void turbo(int& speed){
+	cout << "加速前的速度:" << speed << endl;
+	speed += 10;
 }

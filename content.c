af5b61baa61682d97951a808b7633c1deae7ae7f
Robert Hsu <roberthsu2003@gmail.com>
2020-07-11 11:04:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using namespace std;
+
 int main() {
 	//c語言的字元陣列
 	char name[] = "robert";
 	char age[] = "18";
 	//c++字串
-	std::string myName = "Jenny";
-	std::string myAge = "20";
+	string myName = "Jenny";
+	string myAge = "20";
 	//c語言的輸出
 	printf("Hello C++ %s age=%s\n",name,age);
 
 	//c++語言的輸出
-	std::cout << "Hello C++ " << myName << " age=" << myAge << "\n";
+	cout << "Hello C++ " << myName << " age=" << myAge << "\n";
 
-	std::cout << "Hello C++ " << name << " age=" << age << "\n";
+	cout << "Hello C++ " << name << " age=" << age << "\n";
 
 }

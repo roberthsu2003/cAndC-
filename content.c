5ce9dd53b5154d8db5bca907cf03061ba54d266a
Robert Hsu <roberthsu2003@gmail.com>
2020-07-11 16:49:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,40 +1,18 @@
-//遞迴(Recursive)
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-using namespace std;
-
-double factorial(double n){
-	if(n==0){
-		return 1;
-	}else{
-		return n * factorial(n-1);
-	}
-}
-
-int main() {
-	double n, total;
-	cin >> n;
-	total = factorial(n);
-	printf("%.0f! = %.0f\n",n,total);
-}
-
-
-
 #include <iostream>
 using namespace std;
-
-double factorial(int n){
-	int t = 1;
-	for(int i=1; i<=n; i++){
-		t = t * i;
-	}
-	return t;
+//多載(overloading)
+int add(int a, int b){
+	return a + b;
 }
-int main() {
-	int n,total;
-	cin >> n;
-	total = factorial(n);
-	cout << n << "! = " << factorial(n) << endl;
 
+string add(string a, string b){
+	return a + b;
+}
+
+
+int main() {
+	cout << "add(2, 3)=" << add(2, 3) << endl;
+	cout << "=================\n";
+	cout << "add()=" << add("Nice", "play") << endl;
+	return 0;
 }

bbb0c2fc6960873bef930ab910b96cbde4655a34
Robert Hsu <roberthsu2003@gmail.com>
2020-07-11 10:26:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,5 @@
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-
-using namespace std;
 
 int main() {
-	//c++ 字串
-	string str = "DevC++";
-	//c語言的字元陣列
-	char str1[] = {'D', 'e', 'v', 'C', '+', '+', '\0'};
-	char str2[] = "DevC++";
-	printf("%s\n",str1);
-	cout << str << endl;
-	cout << str1 << endl;
-	printf("%s\n",str2);
+	printf("Hello C++");
 }

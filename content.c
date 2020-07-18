73d9dc3f14b3744762611356146cc1073c3d3ed5
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 12:07:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int turbo(int);
+void turbo(int*);
 
 int main() {
   int speed;
   cout << "請輸入初始速度:";
   cin >> speed;
-  speed = turbo(speed);
+  turbo(&speed);
   cout << "加速後速度:" << speed << endl;
 }
 
-//call by value
-int turbo(int mySpeed){
-	cout << "加速前速度:" << mySpeed << endl;
-	mySpeed += 10;
-	return mySpeed;
+//call by address
+void turbo(int *mySpeed){
+	cout << "加速前速度:" << *mySpeed << endl;
+	*mySpeed += 10;	
 }

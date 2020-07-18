00a1da6094d3ff562b92822cf17c3fe8b22cffce
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 16:07:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,9 +7,21 @@ typedef struct student{
 }student;
 
 int main() {
-	student stus[3] = {{"robert", 93},{"david", 91},{"alice", 94}};
-	int stuCount = sizeof(stus) / sizeof(stus[0]);
+	int stuCount = 3;
+	student stus[stuCount];
 
+	for(int i=0; i<stuCount; i++){
+		string myName;
+		int myScore;
+		cout << "第" << i+1 << "位學生的姓名:";
+		cin >> myName;
+		cout << "第" << i+1 << "位學生的分數:";
+		cin >> myScore;
+		stus[i] = {myName, myScore};
+	}
+
+	cout << "=============================\n\n";
+	
 	for(int i=0; i<stuCount; i++){
 		cout << "第" << i+1 << "位學生是" << stus[i].name << ",分數是" << stus[i].score << endl;
 	}

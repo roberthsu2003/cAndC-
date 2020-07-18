a516af2b62fbbe7a262f9f96d43c1d23b7728d81
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 10:01:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,11 @@
-//宣告變數 double m=5, int n = 10, 顯示變數m,n的值，位址和記憶體大小。
 #include <iostream>
 using namespace std;
 
 int main() {
-	double m = 5;
-	int n = 10;
-	cout << "變數m的值=" << m << endl;
-	cout << "變數n的值=" << n << endl;
-	cout << "變數&m的位址=" << &m << endl;
-	cout << "變數&n的位址=" << &n << endl;
-	cout << "變數m的記憶體=" << sizeof(m) << "Bytes" << endl;
-	cout << "變數n的記憶體=" << sizeof(n) << "Bytes" << endl;
+	int n[3] = {1, 2, 3};
+	cout << "陣列n的位址=" << n << endl;
+	cout << "陣列元素n[0]的位址=" << &n[0] << endl;
+	cout << "陣列元素n[1]的位址=" << &n[1] << endl;
+	cout << "陣列元素n[2]的位址=" << &n[2] << endl;
+	return 0;
 }

344e8d24c5e434270dacf85c44c761af1aa6c9c1
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 14:28:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,29 @@
 using namespace std;
 
 int main() {
-	int array[] = {1, 2, 3, 4, 5};
-	cout << "使用陣列變數取出值" << array[1] << endl;
-	cout << "使用陣列變數取出位址" << &array[1] << endl;
-	cout << "使用指標變數取出位址" << (array + 1) << endl;
-	cout << "使用指標變數取出值" << *(array+1) << endl;
+	char str1[] = "one";
+	const char *str2 = "two";
+	cout << "以陣列變數顯示str1字串" << endl;
+	for(int i=0; i<3; i++){
+		cout << "str1[" << i << "]" << str1[i] << endl;
+	}
+
+	
+	cout << "以指標變數顯示str2字串" << endl;
+	for(int i=0; i<3; i++){
+		cout << "*(str2+" << i << ")" << *(str2 + i) << endl;
+	}
+
+	cout << "以指標變數顯示str1字串" << endl;
+	for(int i=0; i<3; i++){
+		cout << "*(str1+" << i << ")" << *(str1 + i) << endl;
+	}
+
+	cout << "以陣列變數顯示str2字串" << endl;
+	for(int i=0; i<3; i++){
+		cout << "str2[" << i << "]" << str2[i] << endl;
+	}
+
+
+	
 }

2f61c906223a35f7214e768e5262c6ae06ae95c7
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 11:51:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void swap(int*, int*);
+void swap(int&, int&);
 
 int main() {
 	int a = 666, b = 777;
 	cout << "a=" << a << ",b=" << b << endl;
-	swap(&a, &b);
+	swap(a, b);
 	cout << "a=" << a << ",b=" << b << endl;
 
 }
 
-void swap(int *n, int *m){
-	int temp = *n;
-	*n = *m;
-	*m = temp;
+void swap(int &n, int &m){
+	int temp = n;
+	n = m;
+	m = temp;
 }

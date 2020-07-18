42ab189d87afae8956c355cfdaab73a912330f20
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 16:41:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -13,7 +13,20 @@ typedef struct singer{
 int main() {
 	singer allSinger[] = {{"周華見",713,600,310},{"劉得華",999, 512,215},{"張學有",543, 689, 287},{"梁朝為",1125, 387, 769}};
 
+	int total=0;
 	for(int i=0; i<4; i++){
-		cout << allSinger[i].name << endl;
+		singer user = allSinger[i];
+		total += user.area1;
+		total += user.area2;
+		total += user.area3;
+	}
+	
+	for(int i=0; i<4; i++){
+		singer user = allSinger[i];
+		int userScore = user.area1 + user.area2 + user.area3;
+		cout << user.name << endl;
+		cout << "總得票分數為:" << userScore << endl;
+		cout << "總得票率為:" << (float)userScore / total * 100 << "%" << endl;
+		cout << "==============\n";
 	}
 }

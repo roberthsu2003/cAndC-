c71550d5b54a026d771b0758a6377cd352470a1f
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 14:08:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
-	int array[] = {1, 2, 3};
-	cout << "array[2]=" << array[2] << endl;
-	int *pary = array;
-	cout << "*(pary+2)=" << *(pary+2) << endl;
+	int array[] = {1, 2, 3, 4, 5};
+	cout << "使用陣列變數取出值" << array[1] << endl;
+	cout << "使用陣列變數取出位址" << &array[1] << endl;
+	cout << "使用指標變數取出位址" << (array + 1) << endl;
+	cout << "使用指標變數取出值" << *(array+1) << endl;
 }

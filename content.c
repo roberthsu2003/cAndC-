844e22b1f8fc34b8dcd7d59b465a5faedbdfb93e
Robert Hsu <roberthsu2003@gmail.com>
2020-07-18 14:47:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,29 +2,10 @@
 using namespace std;
 
 int main() {
-	char str1[] = "one";
-	const char *str2 = "two";
-	cout << "以陣列變數顯示str1字串" << endl;
-	for(int i=0; i<3; i++){
-		cout << "str1[" << i << "]" << str1[i] << endl;
-	}
-
-	
-	cout << "以指標變數顯示str2字串" << endl;
-	for(int i=0; i<3; i++){
-		cout << "*(str2+" << i << ")" << *(str2 + i) << endl;
-	}
-
-	cout << "以指標變數顯示str1字串" << endl;
-	for(int i=0; i<3; i++){
-		cout << "*(str1+" << i << ")" << *(str1 + i) << endl;
-	}
-
-	cout << "以陣列變數顯示str2字串" << endl;
-	for(int i=0; i<3; i++){
-		cout << "str2[" << i << "]" << str2[i] << endl;
-	}
-
-
-	
+	int *p = new int(1);
+	cout << "指標p的位址=" << p << endl; 
+	cout << "*p=" << *p << endl;
+	*p = 2;
+	cout << "*p=" << *p << endl;
+	delete p;
 }

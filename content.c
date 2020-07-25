29863589d3181427fee09f5a1cb058db7cc44ae3
Robert Hsu <roberthsu2003@gmail.com>
2020-07-25 10:22:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,17 @@
 #include <iostream>
+using namespace std;
 
 class Person{
-
+	public:
+		string name;
 };
 
 int main() {
 	Person david;
+	david.name = "David";
+	cout << "david's name" << david.name << endl;
+	
 	Person robert;
+	robert.name = "Robert";
+	cout << "robert's name" << robert.name << endl;
 }

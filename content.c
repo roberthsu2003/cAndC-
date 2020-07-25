2393c397554cbf924fcbe1f133ae0a74721de0df
Robert Hsu <roberthsu2003@gmail.com>
2020-07-25 11:52:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,38 @@
 #include <iostream>
 using namespace std;
-namespace Ford{
+
 	class Car{
 		public:
-			static int count;
+			//類別的欄位(靜態成員變數)
+			static int turboCount;
 			int speed;
 			void turbo(int);
 	};
 
+	int Car::turboCount = 0;
+
 	void Car::turbo(int s){
 		speed += s;
+		turboCount += 1;
 		//使用this的寫法
 		//this -> speed += s;
 	}
+	
 
-}
 
-using namespace Ford;
+
 
 int main() {
 	Car car1;
 	car1.speed = 70;
 	cout << "現在的速度=" << car1.speed << endl;
-	car1.turbo(10);
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
 	cout << "加速後的速度=" << car1.speed << endl;
-	Car::count = 5;
-
-	//Ford::Car car2;
-	//car2.count = 10;
-
-	//cout << "count=" << car1.count << endl;
+	cout << "已經加速多少次:" << Car::turboCount << endl;
+	
 }

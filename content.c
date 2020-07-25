6d87b20ff2820eebfcf4b39914c26f5dca40147f
Robert Hsu <roberthsu2003@gmail.com>
2020-07-25 14:43:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,15 +8,26 @@ namespace Ford{
 			//預設的建構
 			//建構式不要宣告傳出值
 			Car();
+
+			//建構式的多載
+			Car(int);
 	};
 
 	Car::Car(){
-		speed = 70;
+		this -> speed = 70;
 	}
+
+	Car::Car(int s){
+		this -> speed = s;
+	}
+
 }
 
 
 int main() {
 	Ford::Car car1;
 	cout << "使用Car()建構式建立後,速度是:" << car1.speed << endl;
+
+	Ford::Car car2(100);
+	cout << "使用Car(int s)建構式建立後,速度是:" << car2.speed << endl;
 }

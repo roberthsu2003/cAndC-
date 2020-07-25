533edf1b2bdd364f62124a7ded85e192b1803daa
Robert Hsu <roberthsu2003@gmail.com>
2020-07-25 14:49:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,12 @@
 #include <iostream>
 using namespace std;
 
-namespace Ford{
-	class Car{
-		public:
-			int speed;
-			//預設的建構
-			//建構式不要宣告傳出值
-			Car();
-
-			//建構式的多載
-			Car(int);
-	};
-
-	Car::Car(){
-		this -> speed = 70;
-	}
-
-	Car::Car(int s){
-		this -> speed = s;
-	}
-
-}
-
+#define RATE 32.78
 
 int main() {
-	Ford::Car car1;
-	cout << "使用Car()建構式建立後,速度是:" << car1.speed << endl;
-
-	Ford::Car car2(100);
-	cout << "使用Car(int s)建構式建立後,速度是:" << car2.speed << endl;
+	double us, nt;
+	cout << "請輸入您有多少美金:";
+	cin >> us;
+	nt = us * RATE;
+	cout << "您有" << nt << "台幣" << endl;
 }

4d42779d88b15f29207860b6c8b4e022990d2b59
Robert Hsu <roberthsu2003@gmail.com>
2020-07-25 16:35:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -13,25 +13,36 @@ int main() {
 	person1.walk(50);
 	cout << add(3,5) << endl;
 	cout << "class method:" << Person::sub(10, 3) << endl;
+	Person::count = 20;
+	cout << "class static field=" << Person::count;
 }
-
 
 
 
 
 Person.h
 
+
+#define PI 3.14159
+#define SQUARE(x) (x) * (x)
+
+//預防被重覆的#include
+#ifndef PERSON_H_
+#define PERSON_H_
+
 #include <iostream>
 using namespace std;
 
 class Person{
 	public:
+	static int count;	
 	int tall;
 	string name;
 	void walk(int); //輸出走路的速度
 	int static sub(int a, int b){
 		return a - b;
 	}
+	
 };
 
 int add(int, int);
@@ -42,9 +53,11 @@ int add(int, int);
 
 
 
+
 Person.cpp
 #include "Person.h"
 
+int Person::count = 0;
 void Person::walk(int w){
 	cout << name << "每小時可跑" << w << "公里" << endl;
 }

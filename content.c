404515294973b821dd5c8fd3f1bf3233dcaa99fb
Robert Hsu <roberthsu2003@gmail.com>
2020-07-25 11:38:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,20 +3,31 @@ using namespace std;
 namespace Ford{
 	class Car{
 		public:
+			static int count;
 			int speed;
 			void turbo(int);
 	};
 
 	void Car::turbo(int s){
 		speed += s;
+		//使用this的寫法
+		//this -> speed += s;
 	}
 
 }
 
+using namespace Ford;
+
 int main() {
-	Ford::Car car1;
+	Car car1;
 	car1.speed = 70;
 	cout << "現在的速度=" << car1.speed << endl;
 	car1.turbo(10);
 	cout << "加速後的速度=" << car1.speed << endl;
+	Car::count = 5;
+
+	//Ford::Car car2;
+	//car2.count = 10;
+
+	//cout << "count=" << car1.count << endl;
 }

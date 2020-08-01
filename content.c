075cac8aaaf8e7ec41fd1045d5fea94e498ceb99
Robert Hsu <roberthsu2003@gmail.com>
2020-08-01 10:22:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,6 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	std::cout << "Hello! C";
+	std::cout << "Hello! C" << std::endl;
+	system("pause");
 	return 0;
 }

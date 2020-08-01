b74767bab061261336b9dd52e365717a61fff43c
Robert Hsu <roberthsu2003@gmail.com>
2020-08-01 14:20:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,24 +2,11 @@
 using namespace std;
 
 int main() {
-	string str1;
-	string str2 = "這是字串";
-	string str3 = "這是\"c++\",非常簡單";
-	cout << "\nstr1:" << str1 << "\n";
-	cout << "str2:" << str2 << endl;
-	cout << "str3:" << str3 << "\n";
-
-	bool bolA = true;
-	bool bolB = false;
-
-	cout << bolA << endl;
-	cout << bolB << endl;
-
-	bool bolC = 5;
-	bool bolD = -1;
-	bool bolE = 0;
-
-	cout << "bolC:" << bolC << endl;
-	cout << "bolD:" << bolD << endl;
-	cout << "bolE:" << bolE << endl; 
+	float PI = 3.14159;
+	int radius = 10;
+	cout << "圓面積=" << PI * radius * radius << endl; //自動轉換
+	cout << "圓面積=" << (int)(PI * radius * radius) << endl;//手動轉換
+	cout << "圓面積=" << (int)PI * radius * radius << endl;//手動轉換
+	cout << PI << endl;
+	cout << radius << endl;
 }

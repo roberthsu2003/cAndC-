c8ffe5a4993d2bf5dc7f2700c46e540005b79e9c
Robert Hsu <roberthsu2003@gmail.com>
2020-08-01 12:28:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,7 @@
 using namespace std;
 
 int main() {
-	string name;
-	cout << "請輸入中文姓名:";
-	cin >> name;
-	cout << "Hello! " << name << endl;
+	cout << "姓名\t\t座號\t\t電話\n";
+	cout << "李振聲\t\t11\t\t0926565656\n";
+	cout << "張四聲\t\t12\t\t0938383838\n";
 }

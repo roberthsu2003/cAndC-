52362cc9dbdc979049ad995081698880ab89ee73
Robert Hsu <roberthsu2003@gmail.com>
2020-08-08 16:09:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
-	for(int i=1; i<=5; i++){
-		for(int j=1;j<=i;j++){
-			printf("#");
-		}
-		printf("\n");
+	int deposit = 0, inputNum,num=0;
+	while(deposit < 30000){
+		++num;
+		printf("請輸入第%d個月的存款:", num);
+		cin >> inputNum;
+		deposit += inputNum;
 	}
+	
+	printf("恭喜!總共存了%d個月，已經存夠了，總存款為:%d元", num, deposit);
 }

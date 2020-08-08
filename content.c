546401878ac1451dcb9cebd35baa6a99ce4d80e9
Robert Hsu <roberthsu2003@gmail.com>
2020-08-08 09:50:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,11 +2,6 @@
 using namespace std;
 
 int main() {
-	int x = 1;
-	int y = ++x;
-	cout << "x=" << x << " y= " << y << endl;
-
-	int m = 1;
-	int n = m++;
-	cout << "m=" << m << " n= " << n << endl;
+	int i =1, b =2;
+	cout <<  (i != b) << endl;
 }

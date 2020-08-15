f407c540a14fb96274933b61f1f29d31ffc1184d
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 12:26:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,17 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
-	int nums[] = {256, 731, 943, 389, 142, 645, 829, 945, 371, 418};
-	string names[] = {"stu1","stu2","stu3","stu4","stu5","stu6","stu7","stu8","stu9","stu10"};
-	int inputNum;
-	int i;
-	int n = sizeof(nums) / sizeof(nums[0]);
-	bool isFound = false;
-	cout << "請輸入中獎者的編號";
-	cin >> inputNum;
-	for(i=0; i<n; i++){
-		if(nums[i] == inputNum){
-			isFound = true;
-			break;
-		}
-	}
+	string str1="DevC++";
+	cout << "str1:" << str1 << endl;
 
-	if(isFound == true){
-		cout << "中獎者的姓名為:" << names[i];
-	}else{
-		cout << "無此中獎號碼!";
-	}
+	char str2[] = {'D', 'e', 'v', 'C', '+', '+','\0'};
+	cout << "str2:" << str2 << endl;
 
-	cout << "(共比對" << i << "次)\n";
+	char str3[] = "DevC++";
+	cout << "str3:" << str3 << endl;
+	str1 = "VirtureC++";
+	cout << "str1:" << str1 << endl;
 
+	//str2 = "VirtureC++"
 }

5b18e239f16a5418ec9dde4b47dc2312e9a828f4
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 15:58:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,24 @@
 #include <iostream>
+#include "myFunction.h"
 using namespace std;
 
-//function的原型
-float temperature(int);
+
 
 int main() {
 	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
 	cout << "----------------------------\n";
-
-	int value;
-	float result;
-	cout << "請輸入攝氏溫度:";
-	cin >> value;
-	result = temperature(value);
-	cout << "華氏溫度=" << result << endl;
+	char runAgain='N';
+	do{
+		int value;
+		float result;
+		cout << "請輸入攝氏溫度:";
+		cin >> value;
+		result = temperature(value);
+		cout << "華氏溫度=" << result << endl;
+		cout << "程式還要繼續嗎?(輸入N...結束):";
+		cin >> runAgain;
+	}while(runAgain != 'N');
+	
 	return 0;
 }
 
-float temperature(int value){
-	return value * 1.8 + 32;
-}

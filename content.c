37341f467657480ad49e149d914fb1f24ada6051
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 11:27:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,19 @@
-//建立一個包含五個元素的整數陣列，讓使用者輸入五位學生的成績，然後計算班級總成績及平均成績
 #include <iostream>
 using namespace std;
 
 int main() {
-	const int nums = 5;
-	unsigned short scores[nums];
-	int sum=0;
-	float ave;
-	for(int i=0; i<nums; i++){
-		printf("請輸入第%d位學生的成績:",i+1);
-		scanf("%hd",&scores[i]);
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+	float array[nums];
+
+	for(int i=0;i<nums;i++){
+		printf("請輸入第%d個數值:", i+1);
+		cin >> array[i];
 	}
 
-	for(int i=0; i<nums; i++){
-		sum += scores[i];
+	cout << "排序前\n";
+	for(int i=0;i<nums;i++){
+		cout << array[i] << " ";
 	}
-	ave = (float)sum / nums;
-	printf("全班成績為:%d分,平均為%.2f分\n",sum,ave);
-
 }

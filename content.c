d4e4f2aa178588f6460a6286a42ea8f5375a10c7
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 10:14:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,12 +2,6 @@
 using namespace std;
 
 int main() {
-	int scores[3];
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
-
-	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
-	printf("%d,%d,%d\n",scores[0], scores[1], scores[2]);
-	return 0;
+	int scores[3] = {70, 80, 90};
+	printf("%d,%d,%d\n", scores[0], scores[1], scores[2]);
 }

76fbfb4d9fe8012fdfc6b0b1ea6834496e33229b
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 16:12:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,18 @@
 #include <iostream>
-#include "myFunction.h"
+int turbo(int);
+
 using namespace std;
 
-
-
 int main() {
-	cout << "攝氏10度轉華氏溫度=" << temperature(10) << endl;
-	cout << "----------------------------\n";
-	char runAgain='N';
-	do{
-		int value;
-		float result;
-		cout << "請輸入攝氏溫度:";
-		cin >> value;
-		result = temperature(value);
-		cout << "華氏溫度=" << result << endl;
-		cout << "程式還要繼續嗎?(輸入N...結束):";
-		cin >> runAgain;
-	}while(runAgain != 'N');
-	
+	int speed;
+	cout << "請輸入初始速度:";
+	cin >> speed;
+	speed = turbo(speed);
+	cout << "加速後的速度:" << speed << endl;
 	return 0;
 }
 
+int turbo(int speed){
+	cout << "加速前的速度:" << speed << endl;
+	return speed += 10;
+}

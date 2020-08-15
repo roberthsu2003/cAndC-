5171ea3aa257e0ce495c6244c7140a7d4e9bba5b
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 11:44:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,7 +5,7 @@ int main() {
 	int nums;
 	cout << "請輸入要排序的數值個數:";
 	cin >> nums;
-	float array[nums];
+	float array[nums],temp;
 
 	for(int i=0;i<nums;i++){
 		printf("請輸入第%d個數值:", i+1);
@@ -16,4 +16,22 @@ int main() {
 	for(int i=0;i<nums;i++){
 		cout << array[i] << " ";
 	}
+
+	for(int i=0; i<nums-1;i++){
+		for(int j=i+1; j<nums; j++){
+			if(array[i] > array[j]){
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+			}
+		}
+	}
+	cout << "\n";
+	cout << "由小到大排序\n";
+	for(int i=0;i<nums;i++){
+		cout << array[i] << " ";
+	}
+
+	cout << "\n";
+	cout << "由大到小排序\n";
 }

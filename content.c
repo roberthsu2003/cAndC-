847986e0e9b17b4f2ad9bdf2603f249fc8baf689
Robert Hsu <roberthsu2003@gmail.com>
2020-08-15 14:11:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,14 @@
 #include <iostream>
-using namespace std;
 
-	int main() {
-	int scores[2][5] = {{85, 82, 90, 74, 69},{94, 67, 25, 67, 89}};
-	for(int i=0; i<2; i++){
-		for(int j=0; j<5; j++){
-			printf("第%d位學生第%d科成績:%d\n",i+1, j+1, scores[i][j]);
-			cout << "==============================================\n";
-		}
+int main() {
+	int lot[49];
+	int num = 7;
+	int choose[7];
+	
+	for(int i=0; i<49; i++){
+		lot[i] = i + 1;
+		printf("%d,", lot[i]);
 	}
+
+	
 }

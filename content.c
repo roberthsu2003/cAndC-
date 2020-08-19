8f881bb282b2f2b99866fe67ce08a67800e277fc
Robert Hsu <roberthsu2003@gmail.com>
2020-08-19 20:54:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,5 @@
 #include <iostream>
-void turbo(int&);
-
-using namespace std;
 
 int main() {
-	int speed;
-	cout << "請輸入初始速度:";
-	cin >> speed;
-	turbo(speed);
-	cout << "加速後的速度:" << speed << endl;
-	return 0;
-}
-
-void turbo(int &speed){
-	cout << "加速前的速度:" << speed << endl;
-	speed += 10;
+  printf("Hello!World!");
 }

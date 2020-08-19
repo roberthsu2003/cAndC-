f7f764f8caaec8847be6b18ad3415ccee0e8040d
Robert Hsu <roberthsu2003@gmail.com>
2020-08-19 21:56:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	std::cout << "Hello! 徐國堂!" << std::endl;
-	std::cout << "這是我的第一堂C語言課程" << std::endl;
-	std::cout << "這也是我第一次的線上課程" << std::endl;
+	cout << "Hello! 徐國堂!" << endl;
+	cout << "這是我的第一堂C語言課程" << endl;
+	cout << "這也是我第一次的線上課程" << endl;
 }

1a2456e5a7caac0daf7744fcb96f8bc431fabbdd
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 21:55:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,24 @@
+//範例:請讓使用者輸入三科成績後，自動計算其總分及平均
+
+	
 #include <iostream>
+using namespace std;
 
 int main() {
-	short a = 123;
-	int b;
-	b = (int)a;
+	unsigned short chinese =0, english = 0, math =0;
+	int sum=0;
+	float average=0;
+
+	cout << "請輸入國文成績(0~100):";
+	cin >> chinese;
+
+	cout << "請輸入英文成績(0~100):";
+	cin >> english;
+
+	cout << "請輸入數學成績(0~100):";
+	cin >> math;
+
+	sum = chinese + english + math;
+	average = (float)sum / 3;
+	printf("您的總分為:%d分,平均為%.2f分\n", sum,average);
 }

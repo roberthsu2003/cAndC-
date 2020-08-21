db3e9b6ea474a58b0994cb3582f107250b327928
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 21:43:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,7 @@
 #include <iostream>
 
 int main() {
-	unsigned short a = 3000;
+	short a = 123;
 	int b;
-	b = a;
-	printf("b=%d\n", b);
-
-	char c;
-	c = a;//由大轉小，資料會移失
-	printf("c=%d\n", c);
+	b = (int)a;
 }

e59428071c011266f8b34e864a910bae0ece3159
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 19:49:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,6 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-	int englishScore = 60;
-	cout << "請輸入您的英文分數:";
-	cin >> englishScore;
-	cout << "Hello! 您的英文分數是:" << englishScore << endl;
-
+	//c語言的輸出
+	printf("Hello! Robert\n");
 }

9e24756d9b66109ab9d857148c024b3e8355cf73
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 21:10:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,8 @@
 #include <iostream>
-#include <stdio.h>
-
 using namespace std;
 int main() {
-	char myChar = 100;
-	printf("myChar:%d\n", myChar); 
-	unsigned short myShort = 60000;
-	printf("myShort:%d\n", myShort);
-	int myInt = 210000000;
-	printf("myInt:%d\n", myInt);
-	long long myLong = 2100000000000000000;
-	printf("myLong:%lld\n", myLong);
-	float myFloat = 3.145;
-	printf("myFloat:%.2f\n", myFloat);
-	double myDouble = 35689.34567;
-	printf("myDouble:%.4f\n", myDouble);
+	float PI = 3.14159;
+	int radius = 10;
+	//PI * radius * radius 自動轉換為float
+	cout << "圓面積=" << PI * radius * radius << endl;
 }

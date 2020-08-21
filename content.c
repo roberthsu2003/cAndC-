e6bdcc4f8801e56891d8259903a5cc8c202dba96
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 20:40:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,14 +5,14 @@ using namespace std;
 int main() {
 	char myChar = 100;
 	printf("myChar:%d\n", myChar); 
-	short myShort = 20000;
+	unsigned short myShort = 60000;
 	printf("myShort:%d\n", myShort);
 	int myInt = 210000000;
 	printf("myInt:%d\n", myInt);
-	long long myLong = 21000000000;
+	long long myLong = 2100000000000000000;
 	printf("myLong:%lld\n", myLong);
 	float myFloat = 3.145;
-	printf("myFloat:%f\n", myFloat);
+	printf("myFloat:%.2f\n", myFloat);
 	double myDouble = 35689.34567;
-	printf("myFloat:%f\n", myDouble);
+	printf("myDouble:%.4f\n", myDouble);
 }

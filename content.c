217b2f688e77b164e1801f745db88ab2f5ee1a3e
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 21:31:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,12 @@
 #include <iostream>
-using namespace std;
+
 int main() {
-	float PI = 3.14159;
-	int radius = 10;
-	//PI * radius * radius 自動轉換為float
-	float area = PI * radius * radius;
-	cout << "圓面積=" << area << endl;
+	unsigned short a = 3000;
+	int b;
+	b = a;
+	printf("b=%d\n", b);
+
+	char c;
+	c = a;//由大轉小，資料會移失
+	printf("c=%d\n", c);
 }

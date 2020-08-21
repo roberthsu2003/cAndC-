bb89de9902dfd5bd4456344f92886023d4ab7857
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 20:13:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,7 +6,9 @@ using namespace std;
 int main() {
 	//使用c的輸出
 	int englishScore;
+	int chinese = 75;
+	int math = 85;
 	printf("請輸入您的英文分數:");
 	cin >> englishScore;
-	printf("Hello ! 您的英文分數是: %d\n", englishScore);
+	printf("Hello ! 您的英文分數是: %d,國文分數是:%d,數學分數是:%d\n", englishScore,chinese,math);
 }

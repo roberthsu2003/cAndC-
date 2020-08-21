edd8a6b4c41169abfa3d1e600bfe5d0d9088b427
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 20:33:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 
 using namespace std;
-
 int main() {
-	//使用c的輸出
-	int englishScore;
-	int chinese = 75;
-	int math = 85;
-	printf("請輸入您的英文分數:");
-	cin >> englishScore;
-	printf("Hello ! 您的英文分數是: %d,國文分數是:%d,數學分數是:%d\n", englishScore,chinese,math);
+	char myChar = 100;
+	printf("myChar:%d\n", myChar); 
+	short myShort = 20000;
+	printf("myShort:%d\n", myShort);
+	int myInt = 210000000;
+	printf("myInt:%d\n", myInt);
+	long long myLong = 21000000000;
+	printf("myLong:%lld\n", myLong);
+	float myFloat = 3.145;
+	printf("myFloat:%f\n", myFloat);
+	double myDouble = 35689.34567;
+	printf("myFloat:%f\n", myDouble);
 }

f158a69c3389be3b004a3b2f469a1318f3662720
Robert Hsu <roberthsu2003@gmail.com>
2020-08-21 21:20:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,5 +4,6 @@ int main() {
 	float PI = 3.14159;
 	int radius = 10;
 	//PI * radius * radius 自動轉換為float
-	cout << "圓面積=" << PI * radius * radius << endl;
+	float area = PI * radius * radius;
+	cout << "圓面積=" << area << endl;
 }

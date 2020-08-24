31642f6638a0a6a6d5b78fede76e9ab6176c11c4
Robert Hsu <roberthsu2003@gmail.com>
2020-08-24 20:16:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,25 @@
+//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
+
 #include <iostream>
 using namespace std;
+
 int main() {
-	int inputNum;
-	string message;
-	cout << "請輸入數字:";
-	cin >> inputNum;
-	if(inputNum % 2 == 0){
-		message = "偶數";
+	int money;
+	int payMoney;
+	cout << "請輸入購買金額:";
+	cin >> money;
+
+	if(money >= 100000){
+		payMoney = money * 0.8;
+	}else if(money >= 50000){
+		payMoney = money * 0.85;
+	}else if(money >= 30000){
+		payMoney = money * 0.9;
+	}else if(money >= 10000){
+		payMoney = money * 0.95;
 	}else{
-		message = "奇數";
+		payMoney = money;
 	}
-	cout << "您輸入的" << inputNum << "是" << message << endl;
+
+	cout << "實付金額是:" << payMoney << "元\n";
 }

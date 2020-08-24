11eac578915b5f5738c5f99d57f8d80f8e78e951
Robert Hsu <roberthsu2003@gmail.com>
2020-08-24 21:26:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,10 @@
-//撰寫一個可以判斷輸入數字為正負數以及奇偶數的的程式
 #include <iostream>
 using namespace std;
 
 int main() {
-	int inputNumber;
-	cout << "請輸入數字:";
-	string message;
-	cin >> inputNumber;
-	
-	if(inputNumber < 0){
-		if(inputNumber % 2 == 0){
-			message = "負偶數";
-		}else{
-			message = "負奇數";
-		}
-	}else{
-		if(inputNumber % 2 == 0){
-			message = "正偶數";
-		}else{
-			message = "正奇數";
-		}
+	int sum = 0;
+	for(int i=2; i<=10; i+=2){
+		sum += i;
+		cout << "第" << i/2 << "次迴圈," << "總和為" << sum << endl;
 	}
-
-	cout << "您輸入的" << inputNumber << "是" << message << endl;
 }

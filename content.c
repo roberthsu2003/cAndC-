cdd2b4946adbe154e3912b2d14da1c35d5279c62
Robert Hsu <roberthsu2003@gmail.com>
2020-08-24 19:22:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,14 @@
-//範例:請讓使用者輸入三科成績後，自動計算其總分及平均
+//讓使用者輸入密碼,如果輸入的密碼正確(1234), 要顯示「密碼正確!歡迎光臨!」。如果不正確就不做任何事
 
-	
 #include <iostream>
 using namespace std;
 
 int main() {
-	unsigned short chinese =0, english = 0, math =0;
-	int sum=0;
-	float average=0;
+	string password;
+	cout << "請輸入密碼:";
+	cin >> password;
 
-	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;
-
-	cout << "請輸入英文成績(0~100):";
-	cin >> english;
-
-	cout << "請輸入數學成績(0~100):";
-	cin >> math;
-
-	sum = chinese + english + math;
-	average = (float)sum / 3;
-	printf("您的總分為:%d分,平均為%.2f分\n", sum,average);
-}
+	if(password == "1234"){
+		cout << "密碼正確!歡迎光臨!" << endl;
+	}
+	cout << "程式結束\n";

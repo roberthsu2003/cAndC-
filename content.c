81055dd3a4512f65af1bd1837620f2fdab27960f
Robert Hsu <roberthsu2003@gmail.com>
2020-08-24 21:02:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,25 +1,26 @@
-//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
-
+//撰寫一個可以判斷輸入數字為正負數以及奇偶數的的程式
 #include <iostream>
 using namespace std;
 
 int main() {
-	int money;
-	int payMoney;
-	cout << "請輸入購買金額:";
-	cin >> money;
-
-	if(money >= 100000){
-		payMoney = money * 0.8;
-	}else if(money >= 50000){
-		payMoney = money * 0.85;
-	}else if(money >= 30000){
-		payMoney = money * 0.9;
-	}else if(money >= 10000){
-		payMoney = money * 0.95;
+	int inputNumber;
+	cout << "請輸入數字:";
+	string message;
+	cin >> inputNumber;
+	
+	if(inputNumber < 0){
+		if(inputNumber % 2 == 0){
+			message = "負偶數";
+		}else{
+			message = "負奇數";
+		}
 	}else{
-		payMoney = money;
+		if(inputNumber % 2 == 0){
+			message = "正偶數";
+		}else{
+			message = "正奇數";
+		}
 	}
 
-	cout << "實付金額是:" << payMoney << "元\n";
+	cout << "您輸入的" << inputNumber << "是" << message << endl;
 }

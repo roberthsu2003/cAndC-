a94fa8668a277953577e9a55d0b3f9289983a506
Robert Hsu <roberthsu2003@gmail.com>
2020-08-28 21:22:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	int nums;
+	float temp;
 	cout << "請輸入要排序的數值個數:";
 	cin >> nums;
 	float array[nums];
@@ -13,6 +14,22 @@ int main() {
 	}
 
 	cout << "排序前:\n";
+	for(int i=0; i<nums;i++){
+		cout << array[i] << " ";
+	}
+
+	//陣列排序
+	for(int i=0; i < nums-1; i++){
+		for(int j=i+1;j<nums;j++){
+			if(array[i] > array[j]){
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+
+	cout << "\n排序後由小到大:\n";
 	for(int i=0; i<nums;i++){
 		cout << array[i] << " ";
 	}

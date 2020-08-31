2e534964cd78b250ddd0631907f9ee27aca0c2f4
Robert Hsu <roberthsu2003@gmail.com>
2020-08-31 21:24:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
-//自訂的function
-void sayHello(){
-	cout << "歡迎光臨" << endl;
+
+float temperature(int celsius){
+	return 1.8 * celsius + 32;
 }
 
 int main() {
-  sayHello();
-  cout << "程式結束" << endl;
-  return 0;
+	cout << "攝氏10度轉華氏溫度" << temperature(10) << endl;
+	cout << "==================\n";
+	int v1;
+	float result;
+	cout << "請輸入攝氏溫度:";
+	cin >> v1;
+	result = temperature(v1);
+	cout << "華氏溫度=" << result << endl;
+	return 0;
 }

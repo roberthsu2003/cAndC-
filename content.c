4c49b5cdfe17f76522b623bb7fcd4d793498e062
Robert Hsu <roberthsu2003@gmail.com>
2020-10-06 20:13:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,13 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
-	//c++
-	cout << "姓名\t座號\t電話\n";
-	cout << "李振聲\t11\t\t0229435437\n";
-	cout << "張四聲\t21\t\t0729435437\n";
-	//c
-	printf("=========================\n");
-	printf("姓名\t座號\t電話\n");
-	printf("李振聲\t11\t\t0229435437\n");
-	printf("張四聲\t21\t\t0729435437\n");
+	char characterA;
+	characterA = 'A';
+	cout << characterA << "\n";
+
+
+	char characterB;
+	characterB = 67;
+	cout << characterB << "\n";
 }

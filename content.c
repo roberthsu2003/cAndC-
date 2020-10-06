50cc2a55dae94c3da6f8a1019f6ea54e56fe7b55
Robert Hsu <roberthsu2003@gmail.com>
2020-10-06 19:16:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,6 @@
 #include <iostream>
 
 int main() {
-	int englishScore = 80;
-	std::cout << "英文分數:" << englishScore << "\n";
-	std::cin >> englishScore;
-	std::cout << "你的英文分數:" << englishScore << "\n";
+	int englishScore = 60;
+  	std::cout << "您的英文分數是:" << englishScore;
 }

08bd6c4e138560ac9457fce3817f13f00670c764
Robert Hsu <roberthsu2003@gmail.com>
2020-10-06 19:59:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,14 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
-	int englishScore = 60;
-	cout << "請輸入您的英文分數:";
-	cin >> englishScore;
-  	cout << "您的英文分數是:" << englishScore << "\n";
-	return 0;
+	//c++
+	cout << "姓名\t座號\t電話\n";
+	cout << "李振聲\t11\t\t0229435437\n";
+	cout << "張四聲\t21\t\t0729435437\n";
+	//c
+	printf("=========================\n");
+	printf("姓名\t座號\t電話\n");
+	printf("李振聲\t11\t\t0229435437\n");
+	printf("張四聲\t21\t\t0729435437\n");
 }

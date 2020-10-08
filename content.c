cc304547da99c0b95ede48ac4b22c40c557384b4
Robert Hsu <roberthsu2003@gmail.com>
2020-10-08 20:33:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,19 @@
-//請使用者輸入一個任意數，程式會顯示此數的平方值及立方值
 #include <iostream>
 
 int main() {
-	float num,result;
-	printf("請輸入任意數");
+	float result = 0;
+	float num;
+	printf("請輸入第一個數:");
 	scanf("%f",&num);
-	result = num;
-	result *= num;
-	printf("此數的平方是:%.2f\n", result);
-	result *= num;
-	printf("此數的立方是:%.2f\n", result);
-	return 0;
+	result += num;
+
+	printf("請輸入第二個數:");
+	scanf("%f",&num);
+	result += num;
+
+	printf("請輸入第三個數:");
+	scanf("%f",&num);
+	result += num;
+
+	printf("三個數的總和為:%.2f\n", result);
 }

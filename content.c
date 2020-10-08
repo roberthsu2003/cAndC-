7bf96f6e34bc1ce6268aa6d1dad8db907e7e7c78
Robert Hsu <roberthsu2003@gmail.com>
2020-10-08 19:49:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,6 @@
-//讓使用者輸入被除數(整數)及除數(整數，不可以是零)，程式會顯示兩數相除的商及餘數。
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
-	int n=0,m=0;
-	printf("請輸入被除數(整數):");
-	scanf("%i",&n);
-	printf("請輸入除數(整數,不可以為0):");
-	scanf("%i", &m);
-	printf("商:%i,餘數:%i\n",n/m,n%m);
-  return 0;
+int main() {
+	int a = 2, b = 2;
+	printf("a==b:%d\n",a==b);
 }

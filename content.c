655087d3d7e8484c49405fe70a76ec09fc1b14f9
Robert Hsu <roberthsu2003@gmail.com>
2020-10-08 21:15:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,10 @@
-//讓使用者輸入三個任意數，程式會顯示三數中的最大數。
 #include <iostream>
+using namespace std;
 
 int main() {
 	float in1, in2, in3, max;
-	printf("請輸入第一個數:");
+	string q1 = "請輸入第一個數:";
+	cout << q1;
 	scanf("%f", &in1);
 
 	printf("請輸入第二個數:");

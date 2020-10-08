cc680e7c4532c940a07ceb6071ff44a88ed9c35d
Robert Hsu <roberthsu2003@gmail.com>
2020-10-08 21:54:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,15 @@
 //讓使用者輸入密碼,如果輸入的密碼正確(1234), 要顯示「密碼正確!歡迎光臨!」。
 //如果不正確就顯示密碼錯誤訊息
 #include <iostream>
+#include <string.h>
 //using namespace std;
 
 int main() {
-	std::string password;
+	char password[20];
 	printf("請輸入密碼:");
-	std::cin >> password;
+	scanf("%s", password);
 
-	if(password == "1234"){
+	if(strcmp(password, "1234")){
 		//true程式區塊
 		printf("歡迎光臨!\n");
 	}else{

960eb9c5e313ea6ad1ed2f24b713d1644f1fc63d
Robert Hsu <roberthsu2003@gmail.com>
2020-10-13 21:58:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-	for(int i=1; i<=9; i++){
-		for(int j=1; j<=9; j++){
-			printf("%i*%i=%i\t",i,j,i*j);
+	for(int i=1; i<=20; i++){
+		for(int j=1; j<=i; j++){
+			printf("#");
 		}
 		printf("\n");
 	}

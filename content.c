0506fe1742d9be9e1b364d873d0245e2a3a9044d
Robert Hsu <roberthsu2003@gmail.com>
2020-10-13 20:59:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,28 +1,26 @@
-///國文100同時數學100,獎金1000
-//國文100或者數學100,獎金500
-//巢狀判斷
-
+//讓使用者輸入加、減、乘、除運算子, 就會顯示運算結果
 #include <iostream>
 
 int main() {
-	int chinese;
-	int math;
-	int bonus = 0;
-	printf("請輸入國文成績和數學成績(國文,英文):");
-	scanf("%i,%i",&chinese,&math);
-	if(chinese == 100){
-		if(math == 100){
-			bonus = 1000;
-		}else{
-			bonus = 500;
-		}
-	}else{
-		if(math == 100){
-			bonus = 500;
-		}else{
-			bonus = 0;
-		}
+	char op;
+	int num1 = 20, num2 = 4;
+	printf("num1=20,num2=4\n");
+	printf("請輸入要執行的運算(+,-,*,/):");
+	scanf("%c", &op);
+	switch(op){
+		case '+':
+			printf("num1+num2=%i", num1 + num2);
+			break;
+		case '-':
+			printf("num1-num2=%i", num1 - num2);
+			break;
+		case '*':
+			printf("num1*num2=%i", num1 * num2);
+			break;
+		case '/':
+			printf("num1/num2=%i", num1 / num2);
+			break;
+		default:
+			printf("無法執行運算!\n");
 	}
-
-	printf("獎金是:%i\n", bonus);
 }

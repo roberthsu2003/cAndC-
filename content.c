206636e77322d17c192f2a280efacb80b0588754
Robert Hsu <roberthsu2003@gmail.com>
2020-10-13 20:17:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,12 @@
-/*
-讓使用者輸入成績, 若成績在90分以上就顯示「優等」, 80-89分顯示「甲等」，70-79分顯示「乙等」，60-69分顯示「丙等」，60分以下顯示「丁等」。
+//國文100同時數學100,獎金1000
+//國文100或者數學100,獎金500
 
-顯示:
-請輸入成績(0-100):89
-甲等
-
-*/
 #include <iostream>
-using namespace std;
 
 int main() {
-	int score;
-	printf("請輸入成績(0-100):");
-	scanf("%i",&score);
-	string message;
-	if(score >= 90){
-		message = "優等";
-	}else if (score >=80){
-      message = "甲等";
-    }else if (score >= 70){
-      message = "乙等";
-    }else if (score >= 60){
-      message = "丙等";
-    }else{
-      message = "丁等";
-    }
-	cout << "成績是" << message;
+	int chinese;
+	int math;
+	printf("請輸入國文成績和數學成績(國文,英文):");
+	scanf("%i,%i",&chinese,&math);
+	printf("%i,%i",chinese,math);
 }

fd0c8f2e2fad4c362ef8aff22ed1285499665634
Robert Hsu <roberthsu2003@gmail.com>
2020-10-13 21:16:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,7 @@
-//讓使用者輸入加、減、乘、除運算子, 就會顯示運算結果
 #include <iostream>
 
 int main() {
-	char op;
-	int num1 = 20, num2 = 4;
-	printf("num1=20,num2=4\n");
-	printf("請輸入要執行的運算(+,-,*,/):");
-	scanf("%c", &op);
-	switch(op){
-		case '+':
-			printf("num1+num2=%i", num1 + num2);
-			break;
-		case '-':
-			printf("num1-num2=%i", num1 - num2);
-			break;
-		case '*':
-			printf("num1*num2=%i", num1 * num2);
-			break;
-		case '/':
-			printf("num1/num2=%i", num1 / num2);
-			break;
-		default:
-			printf("無法執行運算!\n");
+	for(int i=1; i<=10; i++){
+		printf("%i\n",i);
 	}
 }

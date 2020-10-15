4eb63747d333fcee7401669bc40dad2938144034
Robert Hsu <roberthsu2003@gmail.com>
2020-10-15 21:58:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <string.h>
+
 int main() {
-	char name1[] = "robert";
-	char name2[] = "robert";
-	if(strcmp(name1, name2) == 0){
-		printf("文字相同");
+	const char *name1;
+	name1 =  "robert";
+	const char *name2;
+	name2 = "alice";
+
+	name1 = "Dear robert";
+	name2 = "Dear robert";
+
+	printf("%s,%s\n", name1, name2);
+
+	if(name1 == name2){
+		printf("相同\n");
 	}else{
-		printf("文字不相同");
+		printf("不相同\n");
 	}
 }

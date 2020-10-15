90bf158a93bb21cd9d3f647c342e593a0a2d4cd8
Robert Hsu <roberthsu2003@gmail.com>
2020-10-15 20:15:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,18 @@
-//預設的密碼為5678，使用者若輸入的密碼錯誤，將不斷出現輸入密碼訊息，直到輸入的密碼正確才顯示正確訊息
-
+//小美是一位教師，請你以while迴圈方式為小美設計一個輸入成績的程式，如果輸入負數表示成績輸入結束，在輸入成績結束後顯示班上總成績及平均成績。請使用break
 #include <iostream>
-using namespace std;
 
 int main() {
-	string defaultPassword = "5678";
-	string password;
-	
-	do{
-		printf("請輸入密碼:");
-		cin >> password;
-	}while(password != defaultPassword);
+	int sum = 0;
+	int num = 0;
+	int score=0;
+	do{		
+		printf("請輸入第%i位學生的成績:",num+1);
+		scanf("%i", &score);
+		if(score<0)	break;
+		sum += score;
+		num += 1;		
+	}while(true);
 
-	printf("恭喜!你的密碼正確了!請進");
+	printf("全班總成績為:%i ,平均分數為:%.2f", sum, (float)sum/num);
 
 }

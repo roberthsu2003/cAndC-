c714da4c129ba267ed0dac2bc8c0cef8e5cc6ace
Robert Hsu <roberthsu2003@gmail.com>
2020-10-15 21:03:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,9 @@
-//請設計一個程式，讓使用者輸入數值，只有加總正偶數值，不加總正奇數值，如果輸入負數，結束程式。
 #include <iostream>
 
 int main() {
-	int sum = 0;
-	int num=1;
-	int inputNum=0;
-	do{
-		printf("請輸入第%i個數值", num);
-		scanf("%i", &inputNum);
-		if(inputNum < 0) break;
-		num++;
-		if(inputNum % 2 == 1) continue;
-		sum += inputNum;
-
-	}while(true);
-	printf("所有輸入的正偶數的加總是:%i", sum);
-
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
+	printf("%i,%i,%i\n", scores[0], scores[1], scores[2]);
 }

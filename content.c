c7e8c4da90691765c0cfc125bdcd582a659a89d9
Robert Hsu <roberthsu2003@gmail.com>
2020-10-15 21:33:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string.h>
 
+using namespace std;
 int main() {
 	//char name[10] = {'r', 'o', 'b', 'e', 'r', 't','\0','z','z','z'};
 	char name[20] = "robert";
+	strcpy(name,"alice");
+	
 	printf("%s\n", name);
+	
 }

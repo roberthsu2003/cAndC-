99b719ae21a5ea24c4ea0eb620d961d89ee762e4
Robert Hsu <roberthsu2003@gmail.com>
2020-10-20 21:46:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,31 @@
-//陣列的應用:泡沫排序
-
+//循序搜尋
 #include <iostream>
 
 int main() {
- int arrays[] = {20, 50, 12, 35};
- printf("排序前:\n");
+	int nums[] = {256, 731, 943, 389, 142, 645, 829, 945, 371, 418};
+	const char *names[] = {"stu1","stu2","stu3","stu4","stu5","stu6","stu7","stu8","stu9","stu10"};
 
- int nums = sizeof(arrays) / sizeof(arrays[0]);
- for (int i=0; i< nums; i++){
-	 printf("%i ", arrays[i]);
- }
+	int inputNum;
+	int n = sizeof(nums) /sizeof(nums[0]);
+	int foundNum;
+	bool isFound = false;
+	printf("請輸入中獎者的編號");
 
- //陣列排序
- for(int start=0; start < nums-1; start++){
-	 for(int end=start+1; end < nums; end++){
-		 if(arrays[start] > arrays[end]){
-			 int temp = arrays[start];
-			 arrays[start] = arrays[end];
-			 arrays[end] = temp;
-		 }
-	 }
- }
- 
- printf("\n排序後:\n");
- for (int i=0; i< nums; i++){
-	 printf("%i ", arrays[i]);
- }
+	scanf("%i", &inputNum);
+
+	for(int i=0; i<n; i++){
+		if(nums[i] == inputNum){
+			foundNum = i;
+			isFound = true;
+			break;
+		}
+	}
+
+
+	if(isFound == true){
+		printf("中獎者的姓名為:%s\n", names[foundNum]);
+	}else{
+		printf("沒有中獎\n");
+	}
 
 }

59103969250258efbbef729434fb0e8f1487af8a
Robert Hsu <roberthsu2003@gmail.com>
2020-10-20 19:39:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,17 @@
 #include <iostream>
 
-int main() {	
+int main() {
+	int min = 1;
+	int max = 100;
+	int keyin;	
 	srand(time(NULL));
-	int randNum = rand();
-	printf("%i\n", randNum % 100 + 1);
+	int guess = rand() % max + min;
+
+	printf("===============猜數字遊戲=================:\n\n");
+	do{
+		printf("猜數字範圍%i~%i:",min, max);
+		scanf("%i",&keyin);
+		break;
+	}while(true);
+	printf("遊戲結束\n");
 }

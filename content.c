eb02a36e4d2de28781d5439fbd1ba42d925f8f5e
Robert Hsu <roberthsu2003@gmail.com>
2020-10-20 20:55:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,19 @@
 #include <iostream>
 
 int main() {
-	int size = sizeof(int);
-	printf("%i\n",size);
-	printf("%i\n", (int)sizeof(int));
-	printf("%u\n",(unsigned int)time(NULL));
+	short myShort;
+	int myInt;
+	float myFloat;
+	double myDouble;
+	int myArray[5];
+	
+	printf("myShort:%u\n", (unsigned int)sizeof(myShort));
+	printf("myInt:%u\n", (unsigned int)sizeof(myInt));
+	printf("myFloat:%u\n", (unsigned int)sizeof(myFloat));
+	printf("myDouble:%u\n", (unsigned int)sizeof(myDouble));
+	printf("myArray:%u\n", (unsigned int)sizeof(myArray));
+	printf("myArray[0]:%u\n", (unsigned int)sizeof(myArray[0]));
+	int arraySize = sizeof(myArray) / sizeof(myArray[0]);
+	
+	printf("陣列的size是%i", arraySize);
 }

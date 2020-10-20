b0591dc8e9acd59186bea43f41871f156f2b1164
Robert Hsu <roberthsu2003@gmail.com>
2020-10-20 21:12:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,13 @@
+//陣列的應用:泡沫排序
+
 #include <iostream>
 
 int main() {
-	short myShort;
-	int myInt;
-	float myFloat;
-	double myDouble;
-	int myArray[5];
-	
-	printf("myShort:%u\n", (unsigned int)sizeof(myShort));
-	printf("myInt:%u\n", (unsigned int)sizeof(myInt));
-	printf("myFloat:%u\n", (unsigned int)sizeof(myFloat));
-	printf("myDouble:%u\n", (unsigned int)sizeof(myDouble));
-	printf("myArray:%u\n", (unsigned int)sizeof(myArray));
-	printf("myArray[0]:%u\n", (unsigned int)sizeof(myArray[0]));
-	int arraySize = sizeof(myArray) / sizeof(myArray[0]);
-	
-	printf("陣列的size是%i", arraySize);
+ int arrays[] = {20, 50, 12, 35};
+ printf("排序前:\n");
+
+ int nums = sizeof(arrays) / sizeof(arrays[0]);
+ for (int i=0; i< nums; i++){
+	 printf("%i ", arrays[i]);
+ }
 }

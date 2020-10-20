a26d9f65908e5fbdabea0b8ecde6fdb38ad473ce
Robert Hsu <roberthsu2003@gmail.com>
2020-10-20 19:46:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,7 +3,8 @@
 int main() {
 	int min = 1;
 	int max = 100;
-	int keyin;	
+	int keyin;
+	int count=0;	
 	srand(time(NULL));
 	int guess = rand() % max + min;
 
@@ -11,6 +12,12 @@ int main() {
 	do{
 		printf("猜數字範圍%i~%i:",min, max);
 		scanf("%i",&keyin);
+		count += 1;
+		if(keyin >= min && keyin <= max){
+			printf("正確的範圍\n");
+		}else{
+			printf("請輸入提示範圍內的數字! \n");
+		}
 		break;
 	}while(true);
 	printf("遊戲結束\n");

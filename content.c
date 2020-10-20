0af48ca9855db5981f131b75b485c9464c6003c4
Robert Hsu <roberthsu2003@gmail.com>
2020-10-20 19:27:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,7 @@
 #include <iostream>
 
-int main() {
-	const char *name1;
-	name1 =  "robert";
-	const char *name2;
-	name2 = "alice";
-
-	name1 = "Dear robert";
-	name2 = "Dear robert";
-
-	printf("%s,%s\n", name1, name2);
-
-	if(name1 == name2){
-		printf("相同\n");
-	}else{
-		printf("不相同\n");
-	}
+int main() {	
+	srand(time(NULL));
+	int randNum = rand();
+	printf("%i\n", randNum % 100 + 1);
 }

c24892a9b6fcf8243ea166839d1ba8e7c239a0e1
Robert Hsu <roberthsu2003@gmail.com>
2020-10-27 21:47:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,10 +6,20 @@ float temperature(int);
 int main() {
 	int value;
 	float result;
-	std::cout << "請輸入攝氏溫度:";
-	std::cin >> value;
-	result = temperature(value);
-	std::cout << "華氏溫度=" << result << std::endl;
+	std::string runAgain;
+	do{
+		std::cout << "請輸入攝氏溫度:";
+		std::cin >> value;
+		result = temperature(value);
+		std::cout << "華氏溫度=" << result << std::endl;
+		std::cout << "程式還要繼續嗎?(輸入N...結束):";
+		std::cin >> runAgain;
+		if(runAgain == "N"){
+			break;
+		}
+
+	}while(true);
+	
 	return 0;
 }
 

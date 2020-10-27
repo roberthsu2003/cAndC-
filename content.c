04754712340478d25750a07e95d8159d40c2daa4
Robert Hsu <roberthsu2003@gmail.com>
2020-10-27 19:45:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,11 @@
- #include <iostream>
+#include <iostream>
 
 int main() {
-	printf("C的輸出\n");
-	std::cout << "C++的輸出\n";
+	//c的輸出
+	int score;
+	scanf("%i",&score);
+	printf("C的輸出=%i\n",score);
+	//c++輸出
+	std::cin >> score;
+	std::cout << "C++的輸出=" << score << std::endl;
 }

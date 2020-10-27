2798304f5c87a8b2b09ebdf9421b56428dfec9d4
Robert Hsu <roberthsu2003@gmail.com>
2020-10-27 19:22:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,6 @@
-#include <iostream>
+ #include <iostream>
 
 int main() {
-	int score[2][3] = {{85, 82, 90},{76, 95, 89}};
-	for(int i=0; i<2; i++){
-		for(int j=0; j<3; j++){
-			printf("第%i位學生第%i科成績:%i\n", i+1, j+1, score[i][j]);
-		}
-	}
+	printf("C的輸出\n");
+	std::cout << "C++的輸出\n";
 }

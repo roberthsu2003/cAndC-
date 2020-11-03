1f1f2936a68a93242c6f1821059d62565812f1f2
Robert Hsu <roberthsu2003@gmail.com>
2020-11-03 21:35:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void swap(int&, int*);
+
 int main() {
-	int array[] = {10, 20, 30, 40, 50};
-	//array = {50, 40, 30, 20 ,10}
+	int a=666;
+	int b=888;
+	swap(a, &b);
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+}
 
-	int *point = array;
-	int array1[] = {50, 40, 30, 20, 10};
-	point = array1;
-	cout << "*(point+0)=" << *(point+0) << endl;
-
-	char name[] = "Hello! World";
-	//name = "robert";
-	
-	const char *name123 = "Hello! World";
-	name123 = "robert";
-	name123 = "jenny";
-	cout << "name123=" << name123 << endl;
-	cout << "*(name123+1)=" << *(name123+1) << endl;
-
-	cout << "name123[1]=" << name123[1] << endl;
-
-
-	
+void swap(int& m, int* n){
+	int temp = m;
+	m = *n;
+	*n = temp;
 }

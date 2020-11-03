008bb3bac131516d9e7241facce86bce3a5903d2
Robert Hsu <roberthsu2003@gmail.com>
2020-11-03 20:42:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void swap(int*, int*);
 int main() {
-	int a = 666;
-	int b = 888;
-	swap(&a, &b);
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
-}
-
-void swap(int *m, int *n){
-	int temp = *m;
-	*m = *n;
-	*n = temp;
+	int array[] = {10, 20, 30, 40, 50};
+	cout << "array[1]的值" << array[1] << endl;
+	cout << "&array[1]的記憶位址" << &array[1] << endl;
+	cout << "array+1的記憶位址" << array + 1 << endl;
+	cout << "*(&array[1])的值" << *(&array[1]) << endl;
+	cout << "*(array+1)的的值" << *(array + 1) << endl;
 }

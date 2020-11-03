2f1eb88d9c7b6259f868ffe3094f408e5982ac1a
Robert Hsu <roberthsu2003@gmail.com>
2020-11-03 19:51:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,13 +2,12 @@
 using namespace std;
 
 int main() {
-	int n=10;
-	int* p = &n;
-	cout << "p的儲存的值是" << p << endl;
-	cout << "p的記憶體位址" << &p << endl;
-	cout << "由n取出值" << n << endl;
-	*p = 20;
-	cout << "由n取出值" << n << endl;
-	n = 10;
-	cout << "由p取出值" << *p << endl;
+	int n = 10;
+	int m = 20;
+	int *p;
+	p = &n;
+	cout << "*p的值" << *p << endl;
+	p = &m;
+	cout << "*p的值" << *p << endl;
+
 }

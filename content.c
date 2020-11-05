a254f62ef0793f219e5bf9c92236fc11b6a72771
Robert Hsu <roberthsu2003@gmail.com>
2020-11-05 19:43:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
-//自訂一個結構
-struct student{
+//自訂一個結構,並且建立一個全新的結構型別Student
+typedef struct student{
 	int id;
 	const char *name;
 	int chinese,math,english;
-};
+}Student;
+
+typedef int myInt;
 
 int main() {
 	//建立結構變數,同時設定成員值
-	struct student david = {99001, "robert", 75, 86, 90};	
+	Student david = {99001, "robert", 75, 86, 90};	
 
 	//取出值
 	cout << david.id << endl;
 	cout << david.name << endl;
 	cout << david.chinese << endl;
-	cout << david.english << endl;
 	cout << david.math << endl;
+	cout << david.english << endl;
+
+	myInt i = 10;
+	int j = i;
 }

dfebf72774eb922bfd0f103b78c2cc4cdb8ee4c6
Robert Hsu <roberthsu2003@gmail.com>
2020-11-05 20:59:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,12 @@
 #include <iostream>
+#define RATE 32.78
+
 using namespace std;
 
-typedef struct rectangle{
-	string name;
-	int width;
-	int height;
-} Rectangle;
-
 int main() {
-	Rectangle recs[] = {{"rec1", 56, 35}, {"rec2", 74, 26}, {"rec3", 93, 14}};
-
-	int count = sizeof(recs) / sizeof(recs[0]);
-
-	for(int i=0; i<count; i++){
-		Rectangle rec = recs[i];
-		cout << "矩型名稱:" << rec.name << endl;
-		cout << "寬度:" << rec.width << endl;
-		cout << "長度:" << rec.height << endl;
-		cout << "面積:" << rec.width * rec.height << endl;
-		cout << "================================\n";		
- 	}
-
+	double us, nt;
+	cout << "請輸入您有多少美金:";
+	cin >> us;
+	nt = us * RATE;
+	cout << "您有" << nt << "台幣" << endl;
 }

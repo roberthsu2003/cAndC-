090f85fdd3003a2aa3345f2ff802a4e63ba3627a
Robert Hsu <roberthsu2003@gmail.com>
2020-11-05 20:11:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,5 +8,13 @@ typedef struct student{
 
 int main() {
 	//建立3位學生,放在陣列內
-	Student stus[3] = {{"robert", 94},{"david", 91},{"alice", 94}};
+	Student stus[] = {{"robert", 94},{"david", 91},{"alice", 94},{"jenny", 68}};
+	
+	int stuCount = sizeof(stus) / sizeof(stus[0]);
+
+	for(int i=0; i<stuCount; i++){
+		Student stu=stus[i];
+		cout << "第" << i+1 << "位學生是" << stu.name << "分數是" << stu.score << endl;
+	}
+
 }

138688331d4f480866ff147a5f4e6e096db1a3f7
Robert Hsu <roberthsu2003@gmail.com>
2020-11-10 20:37:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,4 +1,25 @@
 #include <iostream>
+#include "Human.h"
+
+using namespace std;
+using namespace Human;
+
+int main() {
+	Person david;
+	david.name = "David";
+	david.height = 180;
+	david.weight = 60;
+	cout << "david's name:" << david.name << endl;
+	cout << "david's height:" << david.height << endl;
+	david.walk(40);
+	cout << "david'd bmi:" << david.bmi() << endl;
+	printf("david'd bmi:%.2f\n", david.bmi());
+
+}
+
+
+Human.h
+#include <iostream>
 using namespace std;
 
 namespace Human{
@@ -17,25 +38,11 @@ namespace Human{
 
 using namespace Human;
 
+
 void Person::walk(int var){
 	cout << name << "每小時可跑" << var << "公里" << endl;
 }
 
 float Person::bmi(void){
 	return weight / ((height/100.0) * (height/100.0));
-}
-
-
-
-int main() {
-	Person david;
-	david.name = "David";
-	david.height = 180;
-	david.weight = 60;
-	cout << "david's name:" << david.name << endl;
-	cout << "david's height:" << david.height << endl;
-	david.walk(40);
-	cout << "david'd bmi:" << david.bmi() << endl;
-	printf("david'd bmi:%.2f\n", david.bmi());
-
 }

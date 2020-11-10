f8541c79a35de981bb866950b0bcfcb29604546c
Robert Hsu <roberthsu2003@gmail.com>
2020-11-10 21:44:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,6 +5,7 @@ using namespace std;
 
 int main() {
 	Car car1;
+	cout << "car1現在的速度=" << car1.speed << endl;
 	car1.speed = 70;
 	cout << "car1現在的速度=" << car1.speed << endl;
 	car1.turbo(1);
@@ -16,6 +17,7 @@ int main() {
 	cout << "car1加速後的速度=" << car1.speed << endl;
 }
 
+
 Car.h
 #include <iostream>
 
@@ -24,11 +26,16 @@ class Car{
 	public:
 		int speed;
 		void turbo(int);
+		Car();
 };
 
 
 Car.cpp
 #include "Car.h"
+
+Car::Car(){
+	speed = 20;
+}
 
 void Car::turbo(int s){
 	speed += s;

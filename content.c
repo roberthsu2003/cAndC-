02af7e86b2ae27892b5871ac2381a3dae03237dd
Robert Hsu <roberthsu2003@gmail.com>
2020-11-12 19:23:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,24 @@
 #include <iostream>
-#include "Car.h"
-
-using namespace std;
 
 int main() {
-	Car car0;
-	cout << "car0現在的速度=" << car0.speed << endl;
 	
-
-	Car car1(70);
-	cout << "car1現在的速度=" << car1.speed << endl;
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-	cout << "car1加速後的速度=" << car1.speed << endl;
 }
 
 
 Car.h
 #include <iostream>
+using namespace std;
 
+#idndef __CAR__
+#define __CAR__
 
 class Car{
 	public:
 		int speed;
-		void turbo(int);
-		//建構式overload
 		Car();
-		Car(int);
 };
+
+#endif
 
 
 Car.cpp
@@ -39,12 +26,4 @@ Car.cpp
 
 Car::Car(){
 	speed = 20;
-}
-
-Car::Car(int s){
-	speed = s;
-}
-
-void Car::turbo(int s){
-	speed += s;
 }

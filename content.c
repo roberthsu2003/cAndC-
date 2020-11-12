54dee1f101d1cde3053cf9f9892f13b571452098
Robert Hsu <roberthsu2003@gmail.com>
2020-11-12 19:39:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "Car.h"
 
 int main() {
-	
+	Car car1;
+	car1.speed = 70;
+
+	Car* car2 = new Car();
+	car2->speed = 70;
+
+	Car* car3 = new Car(70);
+	car3->speed = 90;
 }
 
 
@@ -16,6 +24,7 @@ class Car{
 	public:
 		int speed;
 		Car();
+		Car(int speed);
 };
 
 #endif
@@ -26,4 +35,8 @@ Car.cpp
 
 Car::Car(){
 	speed = 20;
+}
+
+Car::Car(int speed){
+	this->speed = speed;
 }

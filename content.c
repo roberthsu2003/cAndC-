3e88f8f06c01c0d6630a96513d62ec6467b7041b
Robert Hsu <roberthsu2003@gmail.com>
2020-11-30 21:52:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
   int englishScore = 60;
-  std::cout << "請輸入您的英文分數:";
-  std::cin >> englishScore;
-  std::cout << "Hello ! 您的英文分數是:" << englishScore;
+  cout << "請輸入您的英文分數:";
+  cin >> englishScore;
+  cout << "Hello ! 您的英文分數是:" << englishScore;
   return 0;
 }

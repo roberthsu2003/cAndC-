212cbf59405e88ad68a54086f32dc0f3f887b1bb
Robert Hsu <roberthsu2003@gmail.com>
2020-12-02 21:40:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,12 @@
 #include <iostream>
+#define PI1 3.14159
+
 using namespace std;
 
+
 int main() {
-	unsigned short chinese, english, math;
-	int sum;
-	float average;
+	const double PI = 3.14159;
+	cout << "PI=" << PI << endl;
 
-	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;
-
-	cout << "請輸入英文成績(0~100):";
-	cin >> english;
-
-	cout << "請輸入數學成績(0~100):";
-	cin >> math;
-
-	sum = chinese + english + math;
-	//average = (float)sum / 3;
-	//average = (float)(sum/3);
-	average = sum / 3.0;
-
-	printf("您的總分為:%d分,平均為%.2f分",sum, average);
+	cout << "PI1=" << PI1 << endl;
 }

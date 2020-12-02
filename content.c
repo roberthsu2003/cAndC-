36d0213642b7094cf386a13c8832eebc6ec893a2
Robert Hsu <roberthsu2003@gmail.com>
2020-12-02 19:44:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
 int main() {
 	char characterA;
 	characterA = 'A';
-	cout << characterA << "\n";
+	//cout << characterA << "\n"; //c++ 的輸出
+	printf("%c\n",'B'); //c語言的輸出
 
 	char characterB;
 	characterB = 65;
-	cout << characterB << "\n";
+	//cout << characterB << "\n";
+	printf("%c\n",'C');
 }

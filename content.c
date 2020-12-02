9d7190e65c8da80d9c9b7f273d3574be840971ec
Robert Hsu <roberthsu2003@gmail.com>
2020-12-02 21:59:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,7 @@
 #include <iostream>
-#define PI1 3.14159
-
 using namespace std;
 
-
 int main() {
-	const double PI = 3.14159;
-	cout << "PI=" << PI << endl;
-
-	cout << "PI1=" << PI1 << endl;
+	cout << (3 == 3) << endl;
+	cout << (5 == 3) << endl;
 }

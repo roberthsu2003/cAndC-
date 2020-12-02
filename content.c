e9e9266d5bebaabaab087cb1b66186f805059485
Robert Hsu <roberthsu2003@gmail.com>
2020-12-02 19:55:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <stdlib.h>
-
 using namespace std;
 
-int main() {
-	char characterA;
-	characterA = 'A';
-	//cout << characterA << "\n"; //c++ 的輸出
-	printf("%c\n",'B'); //c語言的輸出
 
-	char characterB;
-	characterB = 65;
-	//cout << characterB << "\n";
-	printf("%c\n",'C');
+int main() {
+	cout << "姓名\t座號\t電話\n";
+	cout << "李振聲\t11\t\t0229435437\n";
+	cout << "張四聲\t22\t\t0226435437\n";
+
+	cout << "===========================\n";
+
+	printf("姓名\t座號\t電話\n");
+	printf("李振聲\t11\t\t0229435437\n");
+	printf("張四聲\t22\t\t0226435437\n");
 }

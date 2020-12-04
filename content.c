a2d94ad1ef6a2a1bf32feeaf9b507c7976c78f6a
Robert Hsu <roberthsu2003@gmail.com>
2020-12-04 19:56:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-
 using namespace std;
 
 int main() {
-	int n,m;
-	printf("請輸入被除數(整數):");
-	cin >> n;
+	int x = 1;
+	int y = ++x;
+	printf("x=%d, y=%d\n",x,y);
 
-	printf("請輸入除數(整數,不可以為0):");
-	cin >> m;
-
-	printf("商:%d,餘數:%d\n",n/m,n%m);
+	x = 1;
+	y = x++;
+	printf("x=%d, y=%d\n",x,y);
 }

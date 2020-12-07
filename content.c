9ae5ffdd017a8123b0ba11152648882f63f837b0
Robert Hsu <roberthsu2003@gmail.com>
2020-12-07 19:59:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,18 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
-	int money,payMoney;
-	printf("請輸入購買金額:");
-	cin >> money;
-	if (money < 10000){
-		payMoney = money;
-	}else if(money < 30000){
-		payMoney = money * 0.95;
-	}else if(money < 50000){
-		payMoney = money * 0.9;
-	}else if(money < 100000){
-		payMoney = money * 0.85;
-	}else{
-		payMoney = money * 0.8;
-	}
+	int a=0, b=0, c=0, d=0;
+	printf("請輸入a的內容:");
+	scanf("%d",&a);
+	printf("a=%d\n", a);
 
-	printf("實體金額是:%d\n", payMoney);
-	return 0;
+	printf("請輸入b的內容:");
+	scanf("%d",&b);
+	printf("b=%d\n", b);
+
+	printf("請輸入c和d的內容(c,d)");
+	scanf("%d,%d", &c, &d);
+
+	printf("c=%d\n", c);
+	printf("d=%d\n", d);
 }

81381083b8e203e20605568dfbb4a302dff4f1e1
Robert Hsu <roberthsu2003@gmail.com>
2020-12-07 20:12:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,19 @@
+#include <iostream>
 #include <stdio.h>
 
 int main() {
-	int a=0, b=0, c=0, d=0;
-	printf("請輸入a的內容:");
-	scanf("%d",&a);
-	printf("a=%d\n", a);
+	int chinese;
+	int math;
+	int bonus = 0;
+	printf("請輸入國文成績和數學成績(國文,英文):");
+	scanf("%d,%d", &chinese, & math);
+	if(math==100 && chinese==100){
+		bonus = 1000;
+	}else if(math==100 || chinese==100){
+		bonus = 500;
+	}else{
+		bonus = 0;
+	}
 
-	printf("請輸入b的內容:");
-	scanf("%d",&b);
-	printf("b=%d\n", b);
-
-	printf("請輸入c和d的內容(c,d)");
-	scanf("%d,%d", &c, &d);
-
-	printf("c=%d\n", c);
-	printf("d=%d\n", d);
+	printf("獎金是:%i\n", bonus);
 }

9e851728cdd2dffea1642983c84c4b716938671f
Robert Hsu <roberthsu2003@gmail.com>
2020-12-07 19:37:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,16 +5,16 @@ int main() {
 	int money,payMoney;
 	printf("請輸入購買金額:");
 	cin >> money;
-	if (money >= 100000){
-		payMoney = money * 0.8;
-	}else if(money >= 50000){
-		payMoney = money * 0.85;
-	}else if(money >= 30000){
-		payMoney = money * 9;
-	}else if(money >=10000){
-		payMoney = money * 95;
-	}else{
+	if (money < 10000){
 		payMoney = money;
+	}else if(money < 30000){
+		payMoney = money * 0.95;
+	}else if(money < 50000){
+		payMoney = money * 0.9;
+	}else if(money < 100000){
+		payMoney = money * 0.85;
+	}else{
+		payMoney = money * 0.8;
 	}
 
 	printf("實體金額是:%d\n", payMoney);

dce5c08a5bf83cfa59a6e939a5d4a983839f3528
Robert Hsu <roberthsu2003@gmail.com>
2020-12-07 21:58:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,13 @@
 #include <iostream>
 
 int main() {
-	for (int i=1; i<=9; i++){
-		
-		for(int j=1; j<=9; j++){
-			printf("%d*%d=%d\t",i,j, i*j);
-		}
-		printf("\n");
+	int deposit = 0, num=0, inputNum;
+	while(deposit < 30000){
+		num++;
+		printf("請輸入第%d個月份的存款:",num);
+		scanf("%d", &inputNum);
+		deposit += inputNum;
 	}
+
+	printf("恭喜!已經存夠了，存了%d個月的總存款為:%d元",num,deposit);
 }

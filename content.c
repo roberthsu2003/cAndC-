181bf77d7e1503179c4853a3220a4435628478b5
Robert Hsu <roberthsu2003@gmail.com>
2020-12-09 20:10:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,9 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	int num=0;
-	int inputNum;
-	int sum = 0;
-	do{
-		num += 1;		
-		printf("請輸入第%d個數值:", num);
-		scanf("%d", &inputNum);
-		if(inputNum < 0){
-			break;
-		}else if(inputNum % 2 == 1){
-			continue;
-		}
-
-		sum += inputNum;
-
-	}while(true);
-
-	printf("所有輸入的正偶數的加總是:%d\n", sum);
+	srand(time(NULL));
+	printf("亂數值是%d\n", rand() % 100 + 1);
 }

20945e013e179fcfa7804b8b23da3f05c67ad01f
Robert Hsu <roberthsu2003@gmail.com>
2020-12-09 19:10:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	int deposit = 0, num=0, inputNum;
-	while(deposit < 30000){
-		num++;
-		printf("請輸入第%d個月份的存款:",num);
-		scanf("%d", &inputNum);
-		deposit += inputNum;
+	int i=0;
+	while(i<=5){
+		cout << i << endl;
+		i++;
 	}
-
-	printf("恭喜!已經存夠了，存了%d個月的總存款為:%d元",num,deposit);
 }

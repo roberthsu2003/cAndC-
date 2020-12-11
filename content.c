bbe51f88fdfe1c8fc391dc977f1ae3dfce9809cc
Robert Hsu <roberthsu2003@gmail.com>
2020-12-11 20:23:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,32 +1,10 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-	//c++ 字串
-	string str1 = "DevC++";
-	cout << "str1:" << str1 << endl;
-	char str2[] = {'D', 'e', 'v', 'C', '+', '+', '\0'};
-	cout << "str2:" << str2 << endl;
-	char str3[] = "DevC++";
-	printf("str3:%s\n",str3);
-	str3[0] = 'R';
-	printf("str3:%s\n",str3);
-	printf("陣列變數裏面儲存的是%x\n",str3);
-	printf("第一個元素的記憶體位址%x\n", &str3[0]);
-	char *str4 = "DevC++";
-	printf("str4:%s\n",str4);
-	str4 = "Hello!World!";
-	printf("str4:%s\n",str4);
-	str4 = "Hello!C++!";
-	printf("str4:%s\n",str4);
-
-	//真正設定c語言字串
-	const char* name = "徐國堂";
-	const char* sex = "男";
-	printf("name=%s\n", name);
-	printf("sex=%s\n", sex);
-	name = "robert Hsu";
-	sex = "man";
-	printf("name=%s\n", name);
-	printf("sex=%s\n", sex);
+	int scores[2][3] = {{85, 82, 90},{76, 95, 89}};
+	for(int i=0; i<2; i++){
+		for(int j=0; j<3; j++){
+			printf("第位%d學生第%d科成績:%d\n", i+1, j+1,scores[i][j]);
+		}
+	}
 }

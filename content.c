1a7232cf001cdfa9e93c47d23eddcdb75e90920e
Robert Hsu <roberthsu2003@gmail.com>
2020-12-11 19:51:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,4 +9,24 @@ int main() {
 	cout << "str2:" << str2 << endl;
 	char str3[] = "DevC++";
 	printf("str3:%s\n",str3);
+	str3[0] = 'R';
+	printf("str3:%s\n",str3);
+	printf("陣列變數裏面儲存的是%x\n",str3);
+	printf("第一個元素的記憶體位址%x\n", &str3[0]);
+	char *str4 = "DevC++";
+	printf("str4:%s\n",str4);
+	str4 = "Hello!World!";
+	printf("str4:%s\n",str4);
+	str4 = "Hello!C++!";
+	printf("str4:%s\n",str4);
+
+	//真正設定c語言字串
+	const char* name = "徐國堂";
+	const char* sex = "男";
+	printf("name=%s\n", name);
+	printf("sex=%s\n", sex);
+	name = "robert Hsu";
+	sex = "man";
+	printf("name=%s\n", name);
+	printf("sex=%s\n", sex);
 }

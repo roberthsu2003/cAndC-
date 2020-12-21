a5432fed2e48cd8bcbe55f43568a6274ee46cb26
Robert Hsu <roberthsu2003@gmail.com>
2020-12-21 21:53:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,20 +4,14 @@ main.cpp
 #include <stdio.h>
 #include "tool.h"
 
-
-
-
 int main() {
-	int inputValue;
-	float result;
-	std::cout << "請輸入攝氏溫度:";
-	std::cin >> inputValue;
-	result = temperature(inputValue);
-	//c++的輸出
-	std::cout << "華氏溫度=" << result << std::endl;
-	//c語言輸出
-	printf("華氏溫度=%.2f\n",result);
-	std::cout << "PI=" << PI << std::endl;
+	
+
+	int inputNum;
+	std::cout << "請輸入數字 n:";
+	std::cin >> inputNum;
+	showNum(inputNum);
+	std::cout << std::endl;
 	return 0;
 }
 
@@ -30,11 +24,24 @@ tool.h
 
 //宣告function的原型
 float temperature(int);
+void showNum(int);
 
 
 tool.cpp
 #include "tool.h"
+#include <iostream>
 
 float temperature(int value){
 	return 1.8 * value + 32;
+}
+
+void showNum(int n){
+	int i = 1;
+	while(true){
+		if(i > n){
+			return;
+		}
+		std::cout << i << " ";
+		i++;
+	}
 }

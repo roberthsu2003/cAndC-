6feae8469b959cf4eaf1643c8ea241485729720d
Robert Hsu <roberthsu2003@gmail.com>
2020-12-21 21:11:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 
-float temperature(int value){
-	return 1.8 * value + 32;
-}
+//宣告function的原型
+float temperature(int);
 
 int main() {
 	int inputValue;
@@ -13,4 +12,9 @@ int main() {
 	result = temperature(inputValue);
 	std::cout << "華氏溫度=" << result << std::endl;
 	printf("華氏溫度=%.2f\n",result);
+	return 0;
+}
+
+float temperature(int value){
+	return 1.8 * value + 32;
 }

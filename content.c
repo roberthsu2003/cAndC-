a63c3e4d429d34eb0e6174999b053b041f6daaec
Robert Hsu <roberthsu2003@gmail.com>
2020-12-21 21:23:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,10 @@
+main.cpp
+
 #include <iostream>
 #include <stdio.h>
+#include "tool.h"
 
-//宣告function的原型
-float temperature(int);
+
 
 int main() {
 	int inputValue;
@@ -10,10 +12,18 @@ int main() {
 	std::cout << "請輸入攝氏溫度:";
 	std::cin >> inputValue;
 	result = temperature(inputValue);
+	//c++的輸出
 	std::cout << "華氏溫度=" << result << std::endl;
+	//c語言輸出
 	printf("華氏溫度=%.2f\n",result);
 	return 0;
 }
+
+
+tool.h
+
+//宣告function的原型
+float temperature(int);
 
 float temperature(int value){
 	return 1.8 * value + 32;

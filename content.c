8407b50e63ce959b43d690c178f529097eebbe2e
Robert Hsu <roberthsu2003@gmail.com>
2020-12-21 20:49:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,14 @@
 #include <iostream>
 
-int main() {
-	int englishScore = 60;
-	//輸出
-	std::cout << "請輸入您的英文分數:";
-	//輸入
-	std::cin >> englishScore;
+float temperature(int value){
+	return 1.8 * value + 32;
+}
 
-	std::cout << "Hello! 您的英文分數是:" << englishScore << "\n";
+int main() {
+	int inputValue;
+	float result;
+	std::cout << "請輸入攝氏溫度:";
+	std::cin >> inputValue;
+	result = temperature(inputValue);
+	std::cout << "華氏溫度=" << result << std::endl;
 }

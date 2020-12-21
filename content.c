8c3d75f896748148e86e9fd41e292e89f9160ddb
Robert Hsu <roberthsu2003@gmail.com>
2020-12-21 19:47:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,5 @@
-
-tool.h
-
-//宣告原型
-int add(int, int);
-int mul(int, int);
-
-//實作原型
-int add(int a, int b){
-	return a + b;
-}
-
-int mul(int a, int b){
-	return a * b;
-}
-
-
-main.cpp
 #include <iostream>
-#include "tool.h"
-
-using namespace std;
-
-
 
 int main() {
-	cout << "add(2, 3)=" << add(2, 3) << endl;
-	cout << "mul(2, 3)=" << mul(2, 3) << endl;
+  std::cout << "Hello World!\n";
 }
-

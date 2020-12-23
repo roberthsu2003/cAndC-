206e2a715f04749f2eeae48daa50ab692f8359ec
Robert Hsu <roberthsu2003@gmail.com>
2020-12-23 20:52:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,17 +2,12 @@
 using namespace std;
 
 int main() {
-	int n = 10;
-	int m = 20;
-	int *p;
-	p = &n;
-	cout << "變數n的值=" << n << endl;
-	cout << "指標p指向的記憶體位址=" << p << endl;
-	cout << "*p的值=" << *p << endl;
-	cout << "-----------------------" << endl;
-	p = &m;
-	cout << "變數m的值=" << m << endl;
-	cout << "指標p指向的記憶體位址=" << p << endl;
-	cout << "*p的值=" << *p << endl;
-	return 0;
+	int a = 666, b= 888;
+	cout << "a=" << a << ",b=" << b << endl;
+	//2數交換
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout << "a=" << a << ",b=" << b << endl;
 }

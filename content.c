eb236a94fc4e74ec20b8ffce2d37729f22dd7010
Robert Hsu <roberthsu2003@gmail.com>
2020-12-23 21:57:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,21 @@
 #include <iostream>
 using namespace std;
 
+//由於使用c++語言,參數使用參考，所以可以使用call by reference
+void swap(int &n, int &m){
+	//2數交換
+	int temp;
+	temp = n;
+	n = m;
+	m = temp;	
+}
+
 int main() {
-	int array[] = {1, 2, 3};
-	cout << array[2] << endl;
-	cout << *(array+2) << endl;
+	int a = 666, b= 888;
+	cout << "交換前" << endl;
+	cout << "a=" << a << ",b=" << b << endl;
 
-	//字元陣列和陣列變數
-	char name[] = "robert";
-	cout << name[2] << endl;
-	cout << *(name+2) << endl;
-
-	//字元陣列和指標變數
-	const char *sex = "man";
-	cout << sex[2] << endl;
-	cout << *(sex+2) << endl;
-
-	//以後要建立c語言的字串(字元陣列)
-	//const char *變數名稱
-	const char *teacherName = "myTeacher";
-	cout << teacherName << endl;
-
-	//使用c++語言的字串
-	string bossName = "myBoss";
-	cout << bossName << endl;
-	
-
+	cout << "交換後" << endl;
+	swap(a,b);
+	cout << "a=" << a << ",b=" << b << endl;
 }

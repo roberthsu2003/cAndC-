7251e4e09d20414b58d0aa4111472511fce22b92
Robert Hsu <roberthsu2003@gmail.com>
2020-12-23 19:20:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,47 +1,6 @@
-main.cpp
-
 #include <iostream>
-#include <stdio.h>
-#include "tool.h"
+using namespace std;
 
 int main() {
-	
-
-	int inputNum;
-	std::cout << "請輸入數字 n:";
-	std::cin >> inputNum;
-	showNum(inputNum);
-	std::cout << std::endl;
-	return 0;
-}
-
-
-
-
-tool.h
-
-#define PI 3.14159
-
-//宣告function的原型
-float temperature(int);
-void showNum(int);
-
-
-tool.cpp
-#include "tool.h"
-#include <iostream>
-
-float temperature(int value){
-	return 1.8 * value + 32;
-}
-
-void showNum(int n){
-	int i = 1;
-	while(true){
-		if(i > n){
-			return;
-		}
-		std::cout << i << " ";
-		i++;
-	}
+	cout << "Hello World!\n";
 }

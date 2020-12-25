a99352b39ba6a78cf5052c077e065fe9143d575f
Robert Hsu <roberthsu2003@gmail.com>
2020-12-25 20:06:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,10 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main() {
-	const char *p = "robert Hsu";
-	cout << "字串=" << p << endl;
-	cout << "p的大小" << strlen(p) << endl;
-	int count = strlen(p) - 1;
-	for(int i=count; i >= 0; i--){
-		cout << *(p+i);
-	}
-	cout << endl;
-	return 0;
+	//c++ 的字串輸入
+	string name;
+	cout << "請輸入姓名:";
+	cin >> name;
+	cout << name << endl;
 }

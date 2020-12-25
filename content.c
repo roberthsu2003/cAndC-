49580756da93d67764fade54014b5e14863c80b6
Robert Hsu <roberthsu2003@gmail.com>
2020-12-25 19:26:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,17 @@
 #include <iostream>
 using namespace std;
 
-//由於使用c++語言,參數使用參考，所以可以使用call by reference
-void swap(int &n, int &m){
-	//2數交換
-	int temp;
-	temp = n;
-	n = m;
-	m = temp;	
-}
+void showAry(int*);
 
 int main() {
-	int a = 666, b= 888;
-	cout << "交換前" << endl;
-	cout << "a=" << a << ",b=" << b << endl;
+	int ary[] = {1, 2, 3};
+	cout << "1指標變數當作參數:" << endl;
+	showAry(ary);
+}
 
-	cout << "交換後" << endl;
-	swap(a,b);
-	cout << "a=" << a << ",b=" << b << endl;
+void showAry(int *tempAry){
+	for(int i=0; i<=2; i++){
+		cout << "ary[" << i << "]=" << tempAry[i] << "\t";
+	}
+	cout << "\n----------------------------" << endl;
 }

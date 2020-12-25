7389a927fa9f2d1d1a795bc94a70f4559b10ce03
Robert Hsu <roberthsu2003@gmail.com>
2020-12-25 19:59:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-	const char *p = "Hello";
+	const char *p = "robert Hsu";
 	cout << "字串=" << p << endl;
 	cout << "p的大小" << strlen(p) << endl;
+	int count = strlen(p) - 1;
+	for(int i=count; i >= 0; i--){
+		cout << *(p+i);
+	}
+	cout << endl;
+	return 0;
 }

eb459d0efe4ab8f5ff8077ce6edbfa3c9461b14c
Robert Hsu <roberthsu2003@gmail.com>
2020-12-25 20:16:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
 int main() {
 	//c++ 的字串輸入
+	/*
 	string name;
 	cout << "請輸入姓名:";
 	cin >> name;
 	cout << name << endl;
+	*/
+
+	//c語言的字串輸入
+	char p1[50];
+	cout << "請輸入字串:";
+	fgets(p1, sizeof(p1), stdin);
+	cout << p1 << endl;
+	return 0;
 }

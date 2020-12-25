379a4d556b77f40992d223d3dbe530bed2acf519
Robert Hsu <roberthsu2003@gmail.com>
2020-12-25 21:51:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
+struct student{
+	string id;
+	string name;
+	int chinese, math, english;
+};
+
 int main() {
 	/*
-	int n;	
-	int *p = &n;
-	*p = 10;	
-	cout << *p;
+	struct student david;
+	david.id = "A10001";
+	david.name = "david";
+	david.chinese = 78;
+	david.math = 98;
+	david.english = 65;
 	*/
 
-	int *p = new int;
-	*p = 10;
-	cout << *p << endl;
-	
+	struct student david = {"A10001", "david", 78, 98, 65};
 
-	int *a = new int[50];
-	a[0] = 1;
-	a[2] = 2;
-	cout << *(a+0) << endl;
-	cout << *(a+1) << endl;
+	cout << "學號:" << david.id << endl;
+	cout << "姓名:" << david.name << endl;
+	cout << "國文:" << david.chinese << endl;
+	cout << "數學:" << david.math << endl;
+	cout << "英文:" << david.english << endl;
 }

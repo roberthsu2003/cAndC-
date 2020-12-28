d93e36d55463e9b280e6e459082ec24a7fd591b0
Robert Hsu <roberthsu2003@gmail.com>
2020-12-28 21:05:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main() {
-	double us, nt;
-	cout << "請輸入您有多少美金:";
-	cin >> us;
-	nt = us * RATE;
-	cout << "您有" << nt << "台幣" << endl;
+	int s, result;
+	cout << "請輸入數字:";
+	cin >> s;
+	result = SQUARE(s+1);
+	cout << s << "的平方為:" << result << endl;
 }
+
 tool.h
+
 #define RATE 32.78
+//marco 巨集
+#define SQUARE(x) (x) * (x)
 //定義一個結構
 typedef struct student{
 	const char *name;

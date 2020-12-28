fab533380812d996176260e3f825bf27c5c5fb47
Robert Hsu <roberthsu2003@gmail.com>
2020-12-28 20:45:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,16 @@
-main.cpp
 #include <iostream>
 #include "tool.h"
-
 using namespace std;
 
-
-
 int main() {
-	
-
-	student stus[] = {
-		{"david",80,68,98},
-		{"robert",80,79,98},
-		{"john",80,64,98}
-	};
-
-	int num = sizeof(stus) / sizeof(stus[0]);
-	
-	for(int i=0; i<num; i++){
-		int sum = stus[i].chinese + stus[i].english + stus[i].math;
-		cout << "name:" << stus[i].name << endl;
-		cout << "chinese:" << stus[i].chinese << endl;
-		cout << "english:" << stus[i].english << endl;
-		cout << "math:" << stus[i].math << endl;
-		cout << "總分:" << sum << endl;
-		cout << "平均:" <<  (float)sum / num << endl;
-		cout << "===============================" << endl;
-	}
-
+	double us, nt;
+	cout << "請輸入您有多少美金:";
+	cin >> us;
+	nt = us * RATE;
+	cout << "您有" << nt << "台幣" << endl;
 }
-
 tool.h
+#define RATE 32.78
 //定義一個結構
 typedef struct student{
 	const char *name;

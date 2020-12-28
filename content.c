87f5acba5d16becb2722d0bafb875a5bb5edd9f5
Robert Hsu <roberthsu2003@gmail.com>
2020-12-28 20:08:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -11,13 +11,22 @@ int main() {
 
 	student stus[] = {
 		{"david",80,68,98},
-		{"robert",80,68,98},
+		{"robert",80,79,98},
 		{"john",80,64,98}
 	};
 
-	cout << stus[0].name << endl;
-	cout << stus[1].name << endl;
-	cout << stus[2].name << endl;
+	int num = sizeof(stus) / sizeof(stus[0]);
+	
+	for(int i=0; i<num; i++){
+		int sum = stus[i].chinese + stus[i].english + stus[i].math;
+		cout << "name:" << stus[i].name << endl;
+		cout << "chinese:" << stus[i].chinese << endl;
+		cout << "english:" << stus[i].english << endl;
+		cout << "math:" << stus[i].math << endl;
+		cout << "總分:" << sum << endl;
+		cout << "平均:" <<  (float)sum / num << endl;
+		cout << "===============================" << endl;
+	}
 
 }
 

206c4d322cb6277fc8d9a4b36ca989df8d32c944
Robert Hsu <roberthsu2003@gmail.com>
2020-12-30 21:19:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -13,6 +13,8 @@ namespace Human{
 			int chinese;
 			int english;
 			int math;
+			Student(void);
+			Student(string, string, int, int, int);
 			int sum(void);
 			float average(void);
 	};	
@@ -20,10 +22,26 @@ namespace Human{
 
 #endif
 
-
 Student.cpp
+#include <iostream>
 #include "Student.h"
 using namespace Human;
+using namespace std;
+
+Student::Student(void){
+	name = "name";
+	id = "A10001";
+	chinese = 0;
+	english = 0;
+	math = 0;
+}
+Student::Student(string name, string id, int chinese, int english, int math){
+	this->name = name;
+	this->id = id;
+	this->chinese = chinese;
+	this->english = english;
+	this->math = math;
+}
 
 int Student::sum(){
 		return chinese + english + math;
@@ -45,12 +63,7 @@ using namespace std;
 using namespace Human;
 
 int main() {
-	Student stu1;
-	stu1.name = "stu1";
-	stu1.id = "A10001";
-	stu1.chinese = 87;
-	stu1.english = 96;
-	stu1.math = 76;
+	Student stu1("stu1", "A10001", 87, 96, 76);	
 
 	cout << "學生姓名:" << stu1.name << endl;
 	cout << "學號:" << stu1.id << endl;
@@ -59,5 +72,8 @@ int main() {
 	cout << "數學:" << stu1.math << endl;
 	cout << "總分:" << stu1.sum() << endl;
 	printf("平均:%.2f\n",stu1.average());
+
+
+	Student stu2;
 
 }

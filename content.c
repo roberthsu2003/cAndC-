039dbe6fc85f0287b21ed25952cb687d5a89769a
Robert Hsu <roberthsu2003@gmail.com>
2020-12-30 19:58:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,39 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
-class Person{
+class Student{
 	public:
-		int tall;
 		string name;
-		void walk(int var){
-			cout << name << "每小時可跑" << var << "公里" << endl;
+		string id;
+		int chinese;
+		int english;
+		int math;
+		int sum(){
+			return chinese + english + math;
+		}
+
+		float average(){
+			return sum() / 3.0;
 		}
 };
 
 int main() {
-	Person david;
-	david.tall = 180;
-	david.name = "David";
-	cout << david.name << " 身高=" << david.tall << endl;
-	david.walk(15);
+	Student stu1;
+	stu1.name = "stu1";
+	stu1.id = "A10001";
+	stu1.chinese = 87;
+	stu1.english = 96;
+	stu1.math = 76;
 
-	Person robert;
-	robert.tall = 183;
-	robert.name = "Robert";
-	cout << robert.name << " 身高=" << robert.tall << endl;
-	robert.walk(15);
+	cout << "學生姓名:" << stu1.name << endl;
+	cout << "學號:" << stu1.id << endl;
+	cout << "國文:" << stu1.chinese << endl;
+	cout << "英文:" << stu1.english << endl;
+	cout << "數學:" << stu1.math << endl;
+	cout << "總分:" << stu1.sum() << endl;
+	printf("平均:%.2f\n",stu1.average());
 
-	Person *john = new Person();
-	john->tall = 170;
-	john->name = "John";
-	cout << john->name << " 身高=" << john->tall << endl;
-	john->walk(15);
 }

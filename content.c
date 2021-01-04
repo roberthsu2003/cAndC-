3683b658a6d9af915c8c60448883af59644ec319
Robert Hsu <roberthsu2003@gmail.com>
2021-01-04 21:45:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,42 +1,16 @@
-//Car.h
+#include <stdio.h>
+#define PI 3.14159
 
-#ifndef __CAR__
-#define __CAR__
-
-namespace Ford{
-	class Car{
-		private:
-			int speed=0;
-			
-		public:
-		void turbo(int);
-		int getSpeed();
-	};
+float area(int r){
+	return PI * r * r;
 }
 
-#endif
-
-//Car.cpp
-
-#include "Car.h"
-using namespace Ford;
-
-void Car::turbo(int s){
-	speed += s;
-}
-int Car::getSpeed(){
-	return speed;
-}
-
-
-//main.cpp
-#include <iostream>
-#include "Car.h"
-using namespace Ford;
-using namespace std;
-
-int main() {
-	Car car1;
-	car1.turbo(50);
-	cout << car1.getSpeed() << endl;
+int main(void) {
+	int radius;
+	float are;
+	printf("請輸入半徑");
+	scanf("%i", &radius);
+	are = area(radius);
+	printf("面積是:%.2f\n", are);
+  return 0;
 }

f3e3a39719887aba8b136e6becff4e7344e6c285
Robert Hsu <roberthsu2003@gmail.com>
2021-01-04 21:23:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,55 +3,33 @@
 #ifndef __CAR__
 #define __CAR__
 
-class Car{
-	public:
-	//類別的欄位(靜態成員變數)
-	static int turboCount;
-	int speed;
-	void turbo(int);
-	//類別的方法(靜態方法)
-	//必需實作在class內
-	int static getCount(){
-		return turboCount;
-	}
-};
+namespace Ford{
+	class Car{
+		private:
+			int speed=0;
+			
+		public:
+		void turbo(int);
+		int getSpeed();
+	};
+}
 
 #endif
 
 //Car.cpp
-#include "Car.h"
 
-//靜態成員變數一定要在外部給預設值
-int Car::turboCount = 0;
+#include "Car.h"
+using namespace Ford;
+
 void Car::turbo(int s){
 	speed += s;
-	turboCount += 1;
 }
+int Car::getSpeed(){
+	return speed;
+}
+
 
 //main.cpp
 
-#include <iostream>
-#include "Car.h"
-using namespace std;
 
-int main() {
-	Car car1;
-	car1.speed = 70;
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-	car1.turbo(1);
-
-	Car car2;
-	car2.speed = 40;
-	car2.turbo(2);
-	car2.turbo(2);
-	car2.turbo(2);
-	car2.turbo(2);
-	car2.turbo(2);
-
-	cout << "加速了多少次(靜態成員變數):" << Car::turboCount << endl;
-
-	cout << "加速了多少次:(靜態方法)" << Car::getCount() << endl;
 }

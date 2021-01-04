18356a36c3b552d0eb2e5b16401afc36fb0d524d
Robert Hsu <roberthsu2003@gmail.com>
2021-01-04 20:16:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -14,15 +14,44 @@ class Car{
 	int static getCount(){
 		return turboCount;
 	}
-}
+};
 
 #endif
 
 //Car.cpp
 #include "Car.h"
 
+//靜態成員變數一定要在外部給預設值
 int Car::turboCount = 0;
 void Car::turbo(int s){
 	speed += s;
 	turboCount += 1;
+}
+
+//main.cpp
+
+#include <iostream>
+#include "Car.h"
+using namespace std;
+
+int main() {
+	Car car1;
+	car1.speed = 70;
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
+	car1.turbo(1);
+
+	Car car2;
+	car2.speed = 40;
+	car2.turbo(2);
+	car2.turbo(2);
+	car2.turbo(2);
+	car2.turbo(2);
+	car2.turbo(2);
+
+	cout << "加速了多少次(靜態成員變數):" << Car::turboCount << endl;
+
+	cout << "加速了多少次:(靜態方法)" << Car::getCount() << endl;
 }

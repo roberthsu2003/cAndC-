4f5017cca9346edf8e92858cf27b461fa6947534
Robert Hsu <roberthsu2003@gmail.com>
2021-01-04 22:00:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,38 @@
-#include <stdio.h>
+// Circle.h
+#ifndef __CIRCLE__
+#define __CIRCLE__
 #define PI 3.14159
+class Circle{
+	public:
+	int radius;
+	Circle(int);
+	float area(void);
+};
+#endif
 
-float area(int r){
-	return PI * r * r;
+
+//Circle.cpp
+#include "Circle.h"
+
+Circle::Circle(int r){
+	radius = r;
 }
 
-int main(void) {
+float Circle::area(void){
+	return PI * radius * radius;
+}
+
+
+//main.cpp
+#include <iostream>
+#include "Circle.h"
+using namespace std;
+
+int main() {
 	int radius;
-	float are;
-	printf("請輸入半徑");
-	scanf("%i", &radius);
-	are = area(radius);
-	printf("面積是:%.2f\n", are);
-  return 0;
+	cout << "請輸入半徑:";
+	cin >> radius;
+	Circle c1(radius);
+	cout << "您的面積是:" << c1.area();
+
 }

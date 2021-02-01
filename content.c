1081f1bcd64223eade81f7cde1914d3234f7cad0
Robert Hsu <roberthsu2003@gmail.com>
2021-02-01 16:54:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,12 +3,10 @@ using namespace std;
 
 int main() {
 	int scores[3] = {70, 80, 90};
-	/*
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
-	*/
-	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
+	for(int i=0; i < 3; i++){
+		cout << scores[i] << ",";
+	}
+	cout << endl;
 
 	return 0;
 }

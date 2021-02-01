6585d96cfae7ec2d58aa2a45e5b3e7d114b41553
Robert Hsu <roberthsu2003@gmail.com>
2021-02-01 15:59:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
-	string name;
-	cout << "請輸入您的中文姓名:";
-	cin >> name;
-	cout << "Hello!" << name << endl;
+	int englishScore;
+	cout << "請輸入您的英文分數:";
+	cin >> englishScore;
+	cout << "Hello! 您的英文分數是:" << englishScore << endl;
+	return 0;
 }

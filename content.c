0ed781508a22333c2970f426dc6c2473498cb898
Robert Hsu <roberthsu2003@gmail.com>
2021-02-02 14:38:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,8 +4,15 @@ using namespace std;
 int main() {
 	const int nums = 5;
 	int scores[nums];
+	int sum = 0;
 	for(int i=0; i<nums; i++){
 		cout << "請輸入第" << i+1 << "位學生的成績:";
 		cin >> scores[i];
 	}
+
+	for(int i=0; i<nums; i++){
+		sum += scores[i];
+	}
+
+	cout << "全班總成績為:" << sum << "分" << endl;
 }

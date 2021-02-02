59502cf6839e638d6266bb024c0a8287337a20ff
Robert Hsu <roberthsu2003@gmail.com>
2021-02-02 15:34:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	unsigned int nums;
+	int min;
 	cout << "請輸入數值數量:";
 	cin >> nums;
 
@@ -12,5 +13,17 @@ int main() {
 		cout << "請輸入第" << i+1 << "數值:";
 		cin >> values[i];
 	}
-	cout << "程式結束";
+	cout << "陣列內的值有";
+	for(int i=0; i< nums; i++){
+		cout << values[i] << " - ";
+	}
+	min = values[0];
+
+	for(int i=1; i<nums; i++){
+		min = min <= values[i] ? min: values[i];
+	}
+
+	cout << "最小值是:" << min << endl;
+
+
 }

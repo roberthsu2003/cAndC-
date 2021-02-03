51b0c0de750c6e27ef2b1ab38db6fe013fa7d638
Robert Hsu <roberthsu2003@gmail.com>
2021-02-03 15:50:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,18 @@
 
 using namespace std;
 
-int main() {	
+int main() {
+	int num = 7;
+	int lot[49];
+	int choose[8];
+
+	for(int i=0; i<49; i++){
+		lot[i] = i+1;
+	}
+
 	srand(time(NULL));
-	cout << (rand() % 50) + 1 << endl;
+	for(int i=0; i < num; i++){
+		cout << (rand() % 49) + 1 << endl;
+	}
+	
 }

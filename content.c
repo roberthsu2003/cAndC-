92f117cb789815d12b350e91ca7c0cb1c3d5b2a9
Robert Hsu <roberthsu2003@gmail.com>
2021-02-03 14:29:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,13 +5,28 @@ int main() {
 	//c 語言的字元陣列
 	char str[] = {'D','e','v', 'C', '+', '+', '\0'};
 	cout << "str字元陣列:" << str << endl;
-	char str1[] = "DevC++";
+	char str1[] = "DevC++";	
 	cout << "str1字元陣列:" << str1 << endl;
+
 	const char *str2 = "DevC++";
 	cout << "str2字元陣列:" << str2 << endl;
 
 	//c++ 語言string
 	string str3 = "DevC++";
+	cout << "str3字串:" << str3 << endl;
+
+
+	//c 語言的字元陣列可以修改內容
+	str1[3] = 'G';
+	cout << "str1字元陣列:" << str1 << endl;
+
+	//c 語言的字元陣列無法更換整串內容
+	//str1 = "VirtualC++";
+
+	str2 = "VirtualC++";
+	cout << "str2字元陣列:" << str2 << endl;
+
+	str3 = "VirtualC++";
 	cout << "str3字串:" << str3 << endl;
 
 }

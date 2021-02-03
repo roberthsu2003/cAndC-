cffd9cf13d6d3a65672b472e844ee8aa43900d8b
Robert Hsu <roberthsu2003@gmail.com>
2021-02-03 15:02:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,7 @@
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	int scores[2][3] = {{85, 82, 90},{76, 95, 89}};
-	for(int i=0; i<2; i++){
-		for(int j=0; j<3; j++){
-			cout << "第" << i+1 << "位學生第" << j+1 << "科成績:" << scores[i][j] << endl;
-		}
-	}
+  std::cout << "Hello World!\n";
 }

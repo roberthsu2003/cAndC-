a233f29733fea4447ca44e0759817af6268d67bb
Robert Hsu <roberthsu2003@gmail.com>
2021-02-04 15:23:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,24 @@
 #include <iostream>
 #include "tools.h"
+
 using namespace std;
 
-
-
 int main() {
-	int inputValue;
-	float result;
-	cout << "請輸入攝氏溫度:";
-	cin >> inputValue;
-	result = temperature(inputValue);
-	cout << "華氏溫度=" << result << endl;
-	sayHello();
-	return 0;
+	int speed;
+	cout << "請輸入初始速度:";
+	cin >> speed;
+	speed = turbo(speed);
+	cout << "加速後的速度:" << speed << endl;
+	return 0;	
 }
 
 
 tools.h
 #include <iostream>
 using namespace std;
-//宣告函式原型
-float temperature(int);
-void sayHello();
-//實作原型
-float temperature(int value){
-	return 1.8 * value + 32;
-}
-void sayHello(){
-	cout << "Hello!World";
+
+int turbo(int);
+int turbo(int speed){
+	speed += 10;
+	return speed;
 }

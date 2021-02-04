ce3276ab2cf77bd29d96f6449be95d1afa6f5d61
Robert Hsu <roberthsu2003@gmail.com>
2021-02-04 14:38:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
-void sayHello(){
-	cout << "歡迎光臨!";
+float temperature(int value){
+	return 1.8 * value + 32;
 }
 
 int main() {
-  sayHello();
-  return 0;
+	int inputValue;
+	float result;
+	cout << "請輸入攝氏溫度:";
+	cin >> inputValue;
+	result = temperature(inputValue);
+	cout << "華氏溫度=" << result;
+	return 0;
 }

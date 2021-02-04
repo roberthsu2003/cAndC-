14043fcfa78c569710667a30feca2ea879b4c202
Robert Hsu <roberthsu2003@gmail.com>
2021-02-04 14:23:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,44 +1,12 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
 
 using namespace std;
 
-void ltoGenerator(){
-	int num = 7;
-	int lot[49];
-	int choose[num];
-	int maxIndex = 48;
-
-	for(int i=0; i<49; i++){
-		lot[i] = i+1;
-	}
-	
-	for(int i=0; i<num; i++){
-		int randIndex = rand() % (maxIndex+1);
-		choose[i] = lot[randIndex];
-		lot[randIndex] = lot[maxIndex];
-		maxIndex--;
-	}
-	
-	
-	
-	for(int i=0; i<num-1; i++){
-		cout << choose[i] << " ";
-	}
-
-	cout << "\n特別號:" << choose[num-1] << "\n\n";
+void sayHello(){
+	cout << "歡迎光臨!";
 }
 
-
 int main() {
-	srand(time(NULL));
-	int set;
-	cout << "請輸入需要幾組大樂透號碼:";
-	cin >> set;
-	cout << "本期大樂透電腦選號號碼如下:\n\n";	
-	for(int i=0; i<set; i++){
-		ltoGenerator();
-	}
-	return 0;	
+  sayHello();
+  return 0;
 }

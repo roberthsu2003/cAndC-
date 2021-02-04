846d38de2158dd7b0ef6bc21eb41f209fa58a95a
Robert Hsu <roberthsu2003@gmail.com>
2021-02-04 16:05:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,12 +3,6 @@ using namespace std;
 
 int main() {
 	int n = 10;
-	double m = 5;
-	cout << "變數n的值:" << n << endl;
-	cout << "變數m的值:" << m << endl;
-	cout << "變數n的記憶體位址:" << &n << endl;
-	cout << "變數m的記憶體位址:" << &m << endl;
-	cout << "變數n的記憶體=" << sizeof(n) << "Bytes" << endl;
-	cout << "變數m的記憶體=" << sizeof(m) << "Bytes" << endl;
-	
+	int *p = &n;
+	cout << "指標p的儲存內容" << p << endl;
 }

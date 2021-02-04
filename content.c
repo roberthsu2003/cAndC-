0c4430ff4e641fc75024a5a690b83af98b4492c0
Robert Hsu <roberthsu2003@gmail.com>
2021-02-04 16:30:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "tools.h"
+
 using namespace std;
 
 int main() {
-	int n = 10;
-	int *p = &n;
-	cout << "指標p的儲存內容:" << p << endl;
-	cout << "指標p指向的內容:" << *p << endl;
+	int speed;
+	cout << "請輸入初始速度:";
+	cin >> speed;
+	turbo(&speed);
+	cout << "加速後的速度:" << speed << endl;
+}
+
+
+
+tools.h
+#include <iostream>
+using namespace std;
+//原型宣告
+void turbo(int*);
+
+//宣作
+void turbo(int* mySpeed){
+	*mySpeed += 10;
 }

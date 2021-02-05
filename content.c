1399cd5e4e27342d1385594ff4c206f047a70401
Robert Hsu <roberthsu2003@gmail.com>
2021-02-05 15:22:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+struct student{
+	string name;
+	float chinese;
+	float english;
+	float math;
+};
+
 int main() {
-	int *p = new int(10);
-	cout << "值是" << *p << endl;
-	delete p;
-	return 0;
+	struct student john;
+	john.name = "張三";
+	john.chinese = 78.0;
+	john.english = 95;
+	john.math = 84;
+
+	struct student alice;
+	alice.name = "李四";
+	alice.chinese = 81;
+	alice.english = 83;
+	alice.math = 85;
+
+	cout << john.name << "總分是:" << john.chinese + john.english + john.math <<",平均是" << (john.chinese + john.english + john.math) / 3 << endl;
+
+	cout << alice.name << "總分是:" << alice.chinese + alice.english + alice.math <<",平均是" << (alice.chinese + alice.english + alice.math) / 3 << endl;
 }

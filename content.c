c25b72c6281051e43c5b9d109fdd692f175a999b
Robert Hsu <roberthsu2003@gmail.com>
2021-02-05 15:56:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,8 +4,8 @@ using namespace std;
 
 
 int main() {
-	struct student john = {"張三", 78.0, 95, 84};	
-	struct student alice = {"李四", 81, 83, 85};
+	student john = {"張三", 78.0, 95, 84};	
+	student alice = {"李四", 81, 83, 85};
 	
 
 	cout << john.name << "總分是:" << sum(john) <<",平均是" << average(john) << endl;
@@ -19,18 +19,18 @@ tools
 #include <iostream>
 using namespace std;
 
-struct student{
+typedef struct student{
 	string name;
 	float chinese;
 	float english;
 	float math;
-};
+} student;
 
-float sum(struct student s){
+float sum(student s){
 	return s.chinese + s.english + s.math;
 }
 
-float average(struct student s){
+float average(student s){
 	float mySum = sum(s);
 	return mySum / 3.0;
 }

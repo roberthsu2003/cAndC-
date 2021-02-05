9d749ad11301be6cf31a0a1c5981e3be37fc14b5
Robert Hsu <roberthsu2003@gmail.com>
2021-02-05 14:18:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int array[] = {1,2,3};
-	cout << array[1] << endl;
-	cout << array+1 << endl;
-	cout << *(array+1) << endl;
+	int ary[] = {1, 2, 3};
+	const char *name = "robert";
+
+	for(int i=0; i<3; i++){
+		cout << ary[i] << endl;
+	}
+
+	for(int i=0; i<6; i++){
+		cout << *(name+i) << endl;
+	}
 }

8be84a2f6e391c8086ccd79a8002c0c6d023bdb7
Robert Hsu <roberthsu2003@gmail.com>
2021-02-05 14:59:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void showAry(int*);
-void sub2(int[]);
-
 int main() {
-	int ary[] = {1, 2, 3};
-	cout << "以指標變數當參數:"  << endl;
-	showAry(ary);
-	cout << "以陣列變數當作參數:" << endl;
-	sub2(ary);
-	showAry(ary);
+	int *p = new int(10);
+	cout << "值是" << *p << endl;
+	delete p;
 	return 0;
-}
-
-void showAry(int *tempAry){
-	for(int i=0; i<3; i++){
-		cout << "ary[" << i << "]=" << tempAry[i] << "\t";
-	}
-
-	cout << "\n---------------------------" << endl;
-}
-
-void sub2(int tempAry[]){
-	for(int i=0; i<3; i++){
-		*(tempAry+i) *= 2;
-	}
 }

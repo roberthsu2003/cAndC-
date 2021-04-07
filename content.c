7179cdff9188c3da6f84cfccf41d0be8f7d8c9fe
Robert Hsu <roberthsu2003@gmail.com>
2021-04-07 21:33:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,11 @@
-#include <iostream>
+#include <stdio.h> //使用c語言的輸入輸出工具
+#include <iostream> //使用c++輸入輸出
+
 using namespace std;
 
 int main() {
-	string name;
-	cout << "請輸入中文姓名:";
-	cin >> name;
-	cout << "Hello! " << name;
-	return 0;
+  printf("Hello! Robert\n"); // c的輸出
+
+  cout << "Hello! Susan\n";  // c++
+
 }

caefbd90db8f9534afe7c9d524f4176073662fe5
Robert Hsu <roberthsu2003@gmail.com>
2021-04-09 21:11:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,20 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
 int main() {
-	double PI = 3.1415987;
-	int radius = 9;
-	
-	cout << "圓面積=" << PI * radius * radius << endl;
-	printf("圓面積=%.2f\n半徑是=%d\n", PI * radius * radius, radius);
+	unsigned short chinese = 0, english = 0, math = 0;
+	cout << "請輸入國文成績(0~100):" ;
+	cin >> chinese;
 
+	cout << "請輸入英文成績(0~100):" ;
+	cin >> english;
+
+	cout << "請輸入數學成績(0~100):" ;
+	cin >> math;
+
+	int sum = chinese + english + math;
+	//float average = (float)sum / 3;
+	float average = sum / 3.0;
+	cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
+	printf("您的總分為%d分,平均為:%.2f分\n",sum,average) ;
 }

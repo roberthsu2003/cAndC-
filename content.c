49a1f61c7b71f79490f79371aab33f6a4c44ff44
Robert Hsu <roberthsu2003@gmail.com>
2021-04-09 21:58:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,14 @@
+////讓使用者輸入被除數(整數)及除數(整數，不可以是零)，程式會顯示兩數相除的商及餘數。
 #include <iostream>
-#define PI 3.14159
-
 using namespace std;
-const double MYPI = 3.14159;
-int main() {
 
-  	cout << "PI=" << PI << endl;
-	cout << "MYPI=" << MYPI << endl;
+int main() {
+	int n,m;
+	cout << "請輸入被除數(整數):";
+	cin >> n;
+
+	cout << "請輸入除數(整數,不可以為0):";
+	cin >> m;
+
+	cout << "商:" << n/m << "餘數:" << n%m << "\n";
 }

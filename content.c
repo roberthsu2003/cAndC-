1268010c9a55e1b95d88850381d268740c3f2214
Robert Hsu <roberthsu2003@gmail.com>
2021-04-09 19:47:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main() {
-	cout << "姓名\t座號\t電話\n";
-	cout << "李振聲\t11\t\t0926345876\n";
-	cout << "張四聲\t21\t\t0935765234\n";
+	char characterA;
+	characterA = 'A';
+	cout << characterA << "\n";
+
+	char characterB;
+	characterB = 65;
+	cout << characterB << "\n";
 }

6814c54d170fcb23458b6e3b4af757bce4249bea
Robert Hsu <roberthsu2003@gmail.com>
2021-04-12 19:26:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main() {
 	int x = 1;
 	int y = ++x;
 	cout << "x=" << x << ",y=" << y << endl;
+	printf("x=%d,y=%d\n",x,y);
 
 	int m = 1;
 	int n = m++;
 	cout << "m=" << m << ",n=" << n << endl;
-
+	printf("m=%d,n=%d\n",m,n);
 }

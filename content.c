faee2da880ddcad7b04270d9e0a215990802dec7
Robert Hsu <roberthsu2003@gmail.com>
2021-04-12 20:16:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <stdio.h>
+
 using namespace std;
-//請使用者輸入一個任意數，程式會顯示此數的平方值及立方值(使用複合指定運算子)
+
 int main() {
-	float num,result;
-	cout << "請輸入任意數:";
-	//cin >> num;
-	scanf("%f",&num);
-	result = num;
-	result *= num;
-	cout << "此數的平方是:" << result << "\n";
-	result *= num;
-	cout << "此數的立方是:" << result << "\n";
+	float num1, num2 , num3;
+	float result=0.0;
+	cout << "請輸入3個數值,格式[值1,值2,值3]:";
+	scanf("%f,%f,%f",&num1, &num2, &num3);
+	result += num1;
+	result += num2;
+	result += num3;
+	cout << "3數總和為:" << result << endl;
+
 }

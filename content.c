159b113e4c1a841a0f86a785e9ba0e9a4349c033
Robert Hsu <roberthsu2003@gmail.com>
2021-04-12 21:34:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-//讓使用者輸入密碼,如果輸入的密碼正確(1234), 要顯示「密碼正確!歡迎光臨!」。如果不正確就顯示密碼錯誤訊息
 
 int main() {
-	string pass;
-	cout << "請輸入密碼:";
-	cin >> pass;
-	if(pass == "1234"){
-		cout << "歡迎光臨!\n";
+	int inputNumber;
+	string message;
+	cout << "請輸入數字:";
+	cin >> inputNumber;
+	
+	if(inputNumber % 2 == 0){
+		message = "偶數";
 	}else{
-		cout << "密碼錯誤!\n";
-		cout << "請重新輸入!\n";
+		message = "奇數";
 	}
 
-	cout << "程式結束\n";
-
+	cout << "您輸入的" << inputNumber << "是" << message << endl;
 }

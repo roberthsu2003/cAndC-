44d4046d659629667ceb5f9cb85d5344122fc4b9
Robert Hsu <roberthsu2003@gmail.com>
2021-04-12 20:37:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,21 @@
 #include <iostream>
-#include <stdio.h>
-
+//讓使用者輸入三個任意數，程式會顯示三數中的最大數。
 using namespace std;
-
 int main() {
-	float num1, num2 , num3;
-	float result=0.0;
-	cout << "請輸入3個數值,格式[值1,值2,值3]:";
-	scanf("%f,%f,%f",&num1, &num2, &num3);
-	result += num1;
-	result += num2;
-	result += num3;
-	cout << "3數總和為:" << result << endl;
+	float in1, in2, in3, max;
+	cout << "請輸入第一個數:";
+	cin >> in1;
 
+	cout << "請輸入第二個數:";
+	cin >> in2;
+
+	max = in1 > in2 ? in1 : in2 ;
+
+	cout << "請輸入第三個數:";
+	cin >> in3;
+
+	max = max > in3 ? max : in3;
+
+	cout << "輸入三個數中最大的數為:" << max << "\n";
+	 
 }

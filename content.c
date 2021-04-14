d784858826f771c804527baa9a22d04b67b50213
Robert Hsu <roberthsu2003@gmail.com>
2021-04-14 21:30:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,14 @@
-//小明想要存錢買一輛機車,機車每輛30000元，他將每月存的錢輸入，當存款足夠買機車時，就顯示提示訊息告知。
 #include <iostream>
 using namespace std;
 
 int main() {
-	int deposit=0,num=0,inputNum;
-	while(deposit < 30000){
-		num += 1;
-		cout << "請輸入第" << num << "個月份的存款:";
-		cin >> inputNum;
-		deposit += inputNum;
+	for(int i=0; i<5; i++){
+		cout << i << endl;
 	}
 
-	cout << "恭喜!已經存夠了，存了" << num << "個月的總存款為:" << deposit << "元。";
-
-	return 0;
-
-
+	int i=0;
+	while(i<5){
+		cout << i << endl;
+		i++;
+	}
 }

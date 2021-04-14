f5ef56d51d4e47c7c6ea310b88b5c6b2823c0a18
Robert Hsu <roberthsu2003@gmail.com>
2021-04-14 20:21:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,11 @@
+//計算2 - 10所有偶數的總和
 #include <iostream>
 using namespace std;
 
 int main() {
-	for(int i=1; i<=5; i++){
-		printf("i=%d\n",i);
+	int sum = 0;
+	for(int i=2; i<=1000; i+=2){
+		sum += i;
+		printf("第%d次迴圈的i=%d,總和為:%d\n",i/2,i,sum);
 	}
-
 }

3df6f4a929d57e3990276281702b7651f8b323fb
Robert Hsu <roberthsu2003@gmail.com>
2021-04-16 21:55:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,6 +2,18 @@
 #include "tools.h"
 
 int main() {
-	guessOfguess();
+	//guessOfguess();
+	printf("%.2f\n",PI);
+	printf("%d\n",BIG);
+	printf("%d\n",MEDIUM);
+	printf("%d\n",SMALL);
 	return 0;
 }
+
+
+tools
+#include <iostream>
+#define PI 3.14159
+#define BIG 10
+#define MEDIUM 5
+#define SMALL 1

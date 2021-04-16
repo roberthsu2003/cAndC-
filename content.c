0325eea928160433fc5f49ca1c8efec911912b06
Robert Hsu <roberthsu2003@gmail.com>
2021-04-16 21:13:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,41 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void sayHello(){
+	cout << "歡迎光臨!" << endl;
+}
+
+float temperature(int value){
+	return 1.8 * value + 32;
+}
+
 int main() {
-	int max = 100;
-	int min = 1;
-	int guess;
-	int keyin;
-	int count = 0;
-	srand(time(NULL));
-	guess = rand() % max + min;
-
-	cout <<  "===============猜數字遊戲=================:\n\n";
-	
-	do{
-		cout << "猜數字範圍" << min << "~" << max << ":";
-		cin >> keyin;
-		count += 1;
-
-		if(keyin >= min && keyin <= max){
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-			cout << "您猜了" << count << "次\n\n";
-				break;
-			}else if( keyin > guess){
-				max = keyin - 1;
-				cout << "再小一點\n";
-			}else if(keyin < guess){
-				min = keyin + 1;
-				cout << "再大一點\n";
-			}
-
-			cout << "您猜了" << count << "次\n\n";
-
-		}else{
-			cout << "請輸入提示範圍內的數字! \n";
-		}
-
-
-	}while(true);
+  sayHello();
+  int inputT;
+  int result;
+  cout << "請輸入攝氏溫度:";
+  cin >> inputT;
+  result = temperature(inputT);
+  cout << "華氏溫度=" << result << endl;
+  return 0;
 }

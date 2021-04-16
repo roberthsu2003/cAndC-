d9d1bf682dd8a75e6d1279f9d3da72d73270c272
Robert Hsu <roberthsu2003@gmail.com>
2021-04-16 21:43:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,7 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include "tools.h"
-using namespace std;
-
-
 
 int main() {
-	sayHello();
-	int inputT;
-	int result;
-	cout << "請輸入攝氏溫度:";
-	cin >> inputT;
-	result = temperature(inputT);
-	cout << "華氏溫度=" << result << endl;
+	guessOfguess();
 	return 0;
-}
-
-
-
-
-tools.h
-#include <iostream>
-using namespace std;
-
-void sayHello(){
-	cout << "歡迎光臨!" << endl;
-}
-
-float temperature(int value){
-	return 1.8 * value + 32;
 }

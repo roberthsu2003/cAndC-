dd658176692e8ff2d5d8dd15bc8e0f1c2e07b1dc
Robert Hsu <roberthsu2003@gmail.com>
2021-04-16 21:25:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,30 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "tools.h"
 using namespace std;
 
+
+
+int main() {
+	sayHello();
+	int inputT;
+	int result;
+	cout << "請輸入攝氏溫度:";
+	cin >> inputT;
+	result = temperature(inputT);
+	cout << "華氏溫度=" << result << endl;
+	return 0;
+}
+
+
+
+
+tools.h
 void sayHello(){
 	cout << "歡迎光臨!" << endl;
 }
 
 float temperature(int value){
 	return 1.8 * value + 32;
-}
-
-int main() {
-  sayHello();
-  int inputT;
-  int result;
-  cout << "請輸入攝氏溫度:";
-  cin >> inputT;
-  result = temperature(inputT);
-  cout << "華氏溫度=" << result << endl;
-  return 0;
 }

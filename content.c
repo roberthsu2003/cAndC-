ae05f402c87104cd86c0f498b98f5f44dc3671c1
Robert Hsu <roberthsu2003@gmail.com>
2021-04-19 19:36:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,20 @@
 #include <iostream>
+#define nums 5
 using namespace std;
-
 int main() {
-	int scores[3];
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
-	cout << scores[0] << "," << scores[1] << "," << scores[2];
-	return 0;
+	int scores[nums];
+	int sum = 0;
+	double ave;
+	for(int i=0; i<nums; i++){
+		cout << "請輸入第" << i+1 << "位學生的成績";
+		cin >> scores[i];
+	}
+
+	for(int i=0; i<nums; i++){
+		sum += scores[i];
+	}
+
+	ave = (float)sum / nums;
+
+	printf("全班總成績為:%d分,平均為%.2f分",sum, ave);
 }

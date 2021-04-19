45cbd1206fb0abec54c318d4c46bf8486920be94
Robert Hsu <roberthsu2003@gmail.com>
2021-04-19 19:55:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,19 @@
 #include <iostream>
-#define nums 5
 using namespace std;
+
 int main() {
-	int scores[nums];
-	int sum = 0;
-	double ave;
-	for(int i=0; i<nums; i++){
-		cout << "請輸入第" << i+1 << "位學生的成績";
-		cin >> scores[i];
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+
+	float array[nums];
+	for(int i=0; i < nums; i++){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
 	}
 
-	for(int i=0; i<nums; i++){
-		sum += scores[i];
+	cout << "排序前:\n";
+	for(int i=0; i < nums; i++){
+		cout << array[i] << " ";
 	}
-
-	ave = (float)sum / nums;
-
-	printf("全班總成績為:%d分,平均為%.2f分",sum, ave);
 }

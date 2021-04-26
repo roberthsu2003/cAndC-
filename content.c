8c75448dcfee77a3bc98d715e9793f1448a6f32e
Robert Hsu <roberthsu2003@gmail.com>
2021-04-26 20:09:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
 	//c++的字串型別
-	std::string str1 = "DevC++";
-	std::cout << "str1=" << str1 << std::endl;
+	string str1 = "DevC++";
+	cout << "str1=" << str1 << endl;
 	str1 = "Java";
-	std::cout << "str1=" << str1 << std::endl;
+	cout << "str1=" << str1 << endl;
+
+	//c語言的字元陣列
+	//char str2[] = {'D','e','v','C','+','+','\0'};
+	char str2[] = "DevC++";
+	//使用2種方法都可以輸出
+	cout << "str2:" << str2 << endl;
+	printf("str2:%s\n", str2);
+	//字元陣列無法改值
+	//str2 = "java";
+
+	//字義c語言的字串，請使用const char* 變數名稱
+	const char* str3 = "DevC++";
+	cout << "str3:" << str3 << endl;
+	printf("str3:%s\n", str3);
+	str3 = "Java";
+	cout << "str3:" << str3 << endl;
+	printf("str3:%s\n", str3);
+
 }

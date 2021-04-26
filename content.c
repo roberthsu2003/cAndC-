c87f949d29153b2531b6768245c82b44260215a8
Robert Hsu <roberthsu2003@gmail.com>
2021-04-26 21:42:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,11 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-int randomScore(){
-	return rand() % 41 + 60;
-} 
+int add(int a, int b){
+	return a + b;
+}
 
 int main() {
-	srand(time(NULL));
-	int students[50][5];
-	for(int i=0; i<50; i++){
-		for(int j=0; j<5; j++){
-			students[i][j] = randomScore();
-			cout << students[i][j] << " ";
-		}
-		cout << endl;
-	}
+  int total = add(37, 63);
+  cout << "total=" << total << endl;
 }

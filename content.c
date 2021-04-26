7eee8916db4d5dd8032764d47e0552cd92cf30be
Robert Hsu <roberthsu2003@gmail.com>
2021-04-26 21:49:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int add(int a, int b){
-	return a + b;
-}
+//原型宣告
+//告訴compiler,我有這個function,等一下我後面會建立
+//原型宣告，參數只可用資料類型
+
+int add(int,int);
 
 int main() {
   int total = add(37, 63);
   cout << "total=" << total << endl;
+  return 0;
+}
+
+int add(int a, int b){
+	return a + b;
 }

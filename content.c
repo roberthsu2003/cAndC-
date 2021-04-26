606026f1ff51348d529ee7372a499c2e60f57ab1
Robert Hsu <roberthsu2003@gmail.com>
2021-04-26 21:56:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,24 @@
-#include <iostream>
-using namespace std;
+tool.h
 
+#include <iostream>
 //原型宣告
 //告訴compiler,我有這個function,等一下我後面會建立
 //原型宣告，參數只可用資料類型
 
 int add(int,int);
+
+int add(int a, int b){
+	return a + b;
+}
+
+
+main.cpp
+#include <iostream>
+#include "tool.h"
+
+using namespace std;
+
+
 
 int main() {
   int total = add(37, 63);
@@ -13,6 +26,5 @@ int main() {
   return 0;
 }
 
-int add(int a, int b){
-	return a + b;
-}
+
+

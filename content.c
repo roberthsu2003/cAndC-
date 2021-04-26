0ae5af6bc8dd86e1d065169d817f99aebce2ceae
Robert Hsu <roberthsu2003@gmail.com>
2021-04-26 20:35:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,16 @@
 #include <iostream>
-
+#include <time.h>
 using namespace std;
 
+int randomScore(){
+	return rand();
+}
+
 int main() {
-	//c++的字串型別
-	string str1 = "DevC++";
-	cout << "str1=" << str1 << endl;
-	str1 = "Java";
-	cout << "str1=" << str1 << endl;
-
-	//c語言的字元陣列
-	//char str2[] = {'D','e','v','C','+','+','\0'};
-	char str2[] = "DevC++";
-	//使用2種方法都可以輸出
-	cout << "str2:" << str2 << endl;
-	printf("str2:%s\n", str2);
-	//字元陣列無法改值
-	//str2 = "java";
-
-	//字義c語言的字串，請使用const char* 變數名稱
-	const char* str3 = "DevC++";
-	cout << "str3:" << str3 << endl;
-	printf("str3:%s\n", str3);
-	str3 = "Java";
-	cout << "str3:" << str3 << endl;
-	printf("str3:%s\n", str3);
-
+	srand(time(NULL));
+	int scores[] = {randomScore(),randomScore(),randomScore(),randomScore(),randomScore()};
+	for(int i=0;i<5;i++){
+		cout << scores[i] << " ";
+	}
+	cout << endl;
 }

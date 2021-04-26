068d5e503418d02ea1a302bb38fb18e6a2939c5c
Robert Hsu <roberthsu2003@gmail.com>
2021-04-26 19:31:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,6 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-using namespace std;
 
 int main() {
-	int num = 7;
-	int lot[49];
-	int choose[num];
-	int maxIndex = 48;
-
-	for(int i=0; i<=maxIndex; i++){
-		lot[i] = i+1;
-	}
-
-	srand(time(NULL));
-
-	for(int i=0; i<num; i++){
-		int randIndex = rand() % (maxIndex+1);
-		choose[i] = lot[randIndex];
-		lot[randIndex] = lot[maxIndex];
-		maxIndex--;
-	}
-
-	cout << "本期大樂透電腦選號號碼如下:\n\n";
-
-	for(int i=0; i<num-1; i++){
-		cout << choose[i] << " ";
-	}
-	cout << "\n\n特別號:" << choose[num-1] << "\n\n";
+	int scores = 78;
+	printf("學生的分數是%d\n",scores);
 }

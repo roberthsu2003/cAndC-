fc67aa607b3a65f1e18208b53e3c6716b057b23c
Robert Hsu <roberthsu2003@gmail.com>
2021-04-28 21:01:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
-//多載(overloading)
-int area(int a){
-	return a * a;
-}
+//n是全域變數
+int n;
 
-int area(int a, int b){
-	return a * b;
+void showNum(){
+	int value = n;
+	cout << "========================" << endl;
+	for(int n=value; n>=1; n--){
+		//n是區域變數
+		cout << "區域變數 n=" << n << endl;
+	}
+	cout << "========================" << endl;
+	cout << "結速for 迴圈後全域變數n=" << n << endl;
+
 }
 
 int main() {
-	int side1;
-	int side2;
-	int side3;
-	cout << "請輸入正方形的邊:";
-	cin >> side1;
-	cout << "正方形的面積是:" << area(side1) << endl;
-	cout << "請輸入長方形的2邊(x,y):";
-	scanf("%d,%d",&side2,&side3);
-	cout << "矩形面積是=" << area(side2, side3) << endl; 
+	cout << "請輸入全域變數n=";
+	cin >> n;
+	showNum();
 	return 0;
 }

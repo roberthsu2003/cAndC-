b846da70bac9df4ce95cf1141571cec780c4ce1b
Robert Hsu <roberthsu2003@gmail.com>
2021-04-28 19:18:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,7 @@
-tool.h
-
 #include <iostream>
-//原型宣告
-//告訴compiler,我有這個function,等一下我後面會建立
-//原型宣告，參數只可用資料類型
-
-int add(int,int);
-
-int add(int a, int b){
-	return a + b;
-}
-
-
-main.cpp
-#include <iostream>
-#include "tool.h"
-
-using namespace std;
-
-
+#include "randomNum.h"
 
 int main() {
-  int total = add(37, 63);
-  cout << "total=" << total << endl;
+  game();
   return 0;
 }
-
-
-

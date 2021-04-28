92682b3da79b960c0dce9f79877e6dff6ab61d4d
Robert Hsu <roberthsu2003@gmail.com>
2021-04-28 19:54:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,21 @@
 #include <iostream>
-#include "randomNum.h"
+using namespace std;
+
+int factorial(int n){
+	if(n==1){
+		return 1;
+	}else{
+		return n * factorial(n-1);
+	}
+	
+}
 
 int main() {
-  game();
+  int n;
+  long long int total;
+  cout << "請輸入數字n:";
+  cin >> n;
+  total = factorial(n);
+  printf("%d! = %lld\n",n,total);
   return 0;
 }

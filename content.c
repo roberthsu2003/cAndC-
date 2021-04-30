75cb76c20121e863d64bc30f4201556d1c302670
Robert Hsu <roberthsu2003@gmail.com>
2021-04-30 20:23:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void turbo(int *mySpeed){
-	cout << "加速前的速度:" << *mySpeed << endl;
-	*mySpeed += 10;
+void swap(int *n, int *m){
+	int temp = *n;
+	*n = *m;
+	*m = temp;
 }
+
 int main() {
-	int speed;
-	cout << "請輸入初始速度:";
-	cin >> speed;
-	turbo(&speed);
-	cout << "加速後的速度:" << speed << endl;
+	int a=666, b=777;
+	cout << "a=" << a << ",b=" << b << endl;
+	swap(&a, &b);
+	cout << "a=" << a << ",b=" << b << endl;
 	return 0;
 }

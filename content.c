79f93c364d6bbf16ac1346d7a2a5295f67d01e61
Robert Hsu <roberthsu2003@gmail.com>
2021-04-30 21:11:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int array[] = {1,2,3,4,5};
-	cout << "取出array第一個值:" << array[0] << endl;
-	cout << "取出array第一個值的記憶體位址" << array + 0 << endl;
-	cout << "做用指標變數操作法取出第一個的值" << *(array+0) << endl;
+	char str1[] = "two";
+	cout << str1 << endl;
+	//陣列變數無法換新的內容
+	//str1 = "three";
+
+	const char *str2 = "two";
+	cout << str2 << endl;
+	str2 = "three";
+	cout << str2 << endl;
+	//指標變數可以更換的內容
+	//str1 = "three";
+
+	string str3 = "two";
+	str3 = "thress";
+	cout << str3 << endl;
 }

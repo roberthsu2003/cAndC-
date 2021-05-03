8ca913febebdfb358e945dc716b81dccbe023e6f
Robert Hsu <roberthsu2003@gmail.com>
2021-05-03 19:45:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,27 +1,17 @@
 #include <iostream>
 using namespace std;
 //定義結構
-struct student{
+typedef struct student{
 	int id;
 	const char *name;
 	int chinese,math,english;
-};
+} Student;
 
 int main() {
 	//建立結構變數
-	struct student david = {990011, "robert", 75, 86, 90};
-
-	//結構變數給值
-	/*
-	david.id = 990011;
-	david.name = "robert";
-	david.chinese = 75;
-	david.math = 86;
-	david.english = 90;
-	*/
+	Student david = {990011, "robert", 75, 86, 90};
 
 	
-
 	//取出結構變數的值
 	cout << "id=" << david.id << endl;
 	cout << "name=" << david.name << endl;

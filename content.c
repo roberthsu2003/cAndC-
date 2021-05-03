12312cde20991af73639bd8744e4b18a7508313f
Robert Hsu <roberthsu2003@gmail.com>
2021-05-03 19:36:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,14 +9,18 @@ struct student{
 
 int main() {
 	//建立結構變數
-	struct student david;
+	struct student david = {990011, "robert", 75, 86, 90};
 
 	//結構變數給值
+	/*
 	david.id = 990011;
 	david.name = "robert";
 	david.chinese = 75;
 	david.math = 86;
 	david.english = 90;
+	*/
+
+	
 
 	//取出結構變數的值
 	cout << "id=" << david.id << endl;

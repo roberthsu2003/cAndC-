7ef8eefd330cb3b272a872376d38aa35327dc1a1
Robert Hsu <roberthsu2003@gmail.com>
2021-05-05 20:05:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,20 @@
 #include <iostream>
+#include "tools.h"
+#include <time.h>
+
 using namespace std;
 
-//#define SQUARE(x) x * x
-#define SQUARE(x) (x) * (x)
-
 int main() {
-	int s, result;
-	cout << "請輸入數字:";
-	cin >> s;
-	result = SQUARE(s+1);
-	cout << s << "的平方為:" << result << endl;
-	
+	srandom(time(NULL));
+	Student students[50];
+	for(int i=0; i<50; i++){
+		students[i] = createStudent(i+1);
+	}
+
+	cout << "姓名\t國文\t英文\t數學\t總分\t平均" << endl;
+	for (int i=0; i<50; i++){
+		Student s = students[i];
+		cout << s.name << "\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << "\t\t" << sum(s) << "\t\t" << average(s) << endl;
+	}
 	return 0;
 }

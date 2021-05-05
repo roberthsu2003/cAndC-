12b726c9b7fecd960e278fc6b65d96dee0a8db18
Robert Hsu <roberthsu2003@gmail.com>
2021-05-05 19:21:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,8 +8,22 @@ typedef enum direction{
 	West
 } direction;
 
+typedef enum Day{
+	saturday = 6,
+	sunday = 0,
+	monday,
+	tuesday,
+	wednesday,
+	thursday,
+	friday
+}Day;
+
 int main() {
 	direction dest = East;
 	cout << dest << endl;
+
+
+	Day today = wednesday;
+	cout << today << endl;
 	return 0;
 }

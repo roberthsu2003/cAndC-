d94e5b3e49c60a8c29ec2764c5596518dbeeec86
Robert Hsu <roberthsu2003@gmail.com>
2021-05-05 21:37:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,5 +10,21 @@ class Student{
 };
 
 int main() {
-  std::cout << "Hello World!\n";
+	Student stu1;
+	stu1.name = "徐國堂";
+	stu1.chinese = 87;
+	stu1.english = 76;
+	stu1.math = 68;
+
+	cout << "stu1的name是:" << stu1.name << endl;
+
+
+	Student stu2;
+	stu2.name = "張一樂";
+	stu2.chinese = 89;
+	stu2.english = 65;
+	stu2.math = 93;
+
+	cout << "stu2的name是:" << stu2.name << endl;
+	
 }

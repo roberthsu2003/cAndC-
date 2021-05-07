89aa313977d41a442bfa66dc0986d705522fb094
Robert Hsu <roberthsu2003@gmail.com>
2021-05-07 19:27:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,42 +1,33 @@
 #include <iostream>
 using namespace std;
 
-class Student{
+class Person{
+	private:
 	public:
-	string name;
-	int chinese;
-	int english;
-	int math;
-	//method
-	int sum(){
-		return chinese + english + math;
-	}
+		//field欄位
+		string name;
+		int tall;
+		//method方法
+		void walk(int var){
+			cout << name << "每小時可以跑多少" << var << "公里" << endl;
+		}
 
-	float average(){
-		return sum() / 3.0;
-	}
 };
 
 int main() {
-	Student stu1;
-	stu1.name = "徐國堂";
-	stu1.chinese = 87;
-	stu1.english = 76;
-	stu1.math = 68;
+	Person david;
+	david.name = "David Hsu";
+	cout << "david的姓名:" << david.name << endl;
+	david.tall = 180;
+	cout << "david的高度是:" << david.tall << "公分" << endl;
+	david.walk(8);
 
-	cout << "stu1的name是:" << stu1.name << endl;
-	cout << "stu1的總分是:" << stu1.sum() << endl;
-	printf("stu1的平均是:%.2f\n", stu1.average());
+	cout << "=======================\n";
 
-
-	Student stu2;
-	stu2.name = "張一樂";
-	stu2.chinese = 89;
-	stu2.english = 65;
-	stu2.math = 93;
-
-	cout << "stu2的name是:" << stu2.name << endl;
-	cout << "stu\2的總分是:" << stu2.sum() << endl;
-	printf("stu2的平均是:%.2f\n", stu2.average());
-	
+	Person robert;
+	robert.name = "Robert Hsu";
+	cout << "robert的姓名:" << robert.name << endl;
+	robert.tall = 183;
+	cout << "robert的高度是:" << robert.tall << "公分" << endl;
+	robert.walk(8);
 }

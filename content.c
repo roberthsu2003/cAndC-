0f54fb2aa7c501840cd4133ff2f9c9b017d0b858
Robert Hsu <roberthsu2003@gmail.com>
2021-05-07 20:15:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,6 @@
 #include <iostream>
+#include "source.h"
 using namespace std;
-namespace Human{
-	class Person{
-		private:
-		public:
-			//field欄位
-			string name;
-			int tall;
-			int weight;
-			//method方法
-			//定義方法原型,告知有這個method,但定義在類別外面
-			void walk(int);
-			float bmi(void);
-	};
-	
-	//這是Person類別內的方法
-	void Person::walk(int var){
-				cout << name << "每小時可以跑多少" << var << "公里" << endl;
-			}
-
-	//這是Person類別內的方法
-	float Person::bmi(void){
-		return weight / ((tall/100.0) * (tall/100.0));
-	}
-}
-
 using namespace Human;
 
 int main() {
@@ -48,3 +24,37 @@ int main() {
 	robert.weight = 80;
 	printf("robert的bmi是%.2f\n",robert.bmi());
 }
+
+
+//sourc.h
+#include <iostream>
+using namespace std;
+namespace Human{
+	class Person{
+		private:
+		public:
+			//field欄位
+			string name;
+			int tall;
+			int weight;
+			//method方法
+			//定義方法原型,告知有這個method,但定義在類別外面
+			void walk(int);
+			float bmi(void);
+	};
+}
+	
+//sourc.cpp
+#include "source.h"
+using namespace Human;
+
+//這是Person類別內的方法
+	void Person::walk(int var){
+				cout << name << "每小時可以跑多少" << var << "公里" << endl;
+			}
+
+	//這是Person類別內的方法
+	float Person::bmi(void){
+		return weight / ((tall/100.0) * (tall/100.0));
+	}
+

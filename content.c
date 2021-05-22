6d5e9468cef6092fa8269faf3d937f6794c69fcf
Robert Hsu <roberthsu2003@gmail.com>
2021-05-22 15:17:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	string name;
-	cout << "請輸入中文姓名:";
-	cin >> name;
-	cout << "Hello! " << name << endl;
-	return 0;
+	int score1 = 100;
+	unsigned int score2 = 100;
+	short score3 = 100;
+	unsigned short score4 = 100;
+	unsigned long long score5 = 100;
+	float pi1 = 3.14159;
+	double pi2 = 34567.12345;
+
+	cout << "score1=" << score1 << endl;
+	cout << "score2=" << score2 << endl;
+	cout << "score3=" << score3 << endl;
+	cout << "score4=" << score4 << endl;
+	cout << "score5=" << score5 << endl;
+	cout << "pi1=" << pi1 << endl;
+	cout << "pi2=" << pi2 << endl;
+	cout << "=====================" << endl;
 }

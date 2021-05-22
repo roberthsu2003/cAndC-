0df4ded7313956b76426cff627a9f4a853d403fc
Robert Hsu <roberthsu2003@gmail.com>
2021-05-22 12:10:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("請輸入您姓名:\n");
+  printf("請輸出各科的成績:\n");
+  int math;
+  math = 90;
+  printf("math=%d\n",math);
+
+  int chinese = 80;
+  printf("chinese=%d\n", chinese);
+
+  int english = 70;
+  printf("english=%d\n", english);
+
+  printf("數學=%d,國文=%d,英文=%d\n",math,chinese,english);
+
   return 0;
 }

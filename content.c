be369a098e119c588ace041d54d2920ced7ba9d3
Robert Hsu <roberthsu2003@gmail.com>
2021-05-29 16:39:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int sum = 0;
-	int start = 2;
-	int end = 1000;
-	for(int i=start; i<=end; i+=2){
-		sum += i;
-	}
 
-	printf("%d至%d的偶數總合是%d\n",start,end,sum);
+int main() {
+	for(int i=1; i<=9; i++){
+		for(int j=1; j<=9; j++){
+			printf("%d*%d=%d\t", i, j, i*j);
+		}
+		cout << "\n";
+	}
 }

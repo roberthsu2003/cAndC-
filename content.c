c701cfde79d37131a9c3684429f997c09f96a635
Robert Hsu <roberthsu2003@gmail.com>
2021-06-05 16:12:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,20 @@
-#include <iostream>
-using namespace std;
-int main() {
-	int scores[] = {70, 80, 90};	
-	cout << scores[0] << "," << scores[1] << "," << scores[2];
+#include <stdio.h>
+
+int main(void) {
+  const int NUMS = 5;
+  int scores[NUMS];
+  int sum = 0;
+  double ave;
+  for(int i=0; i< NUMS; i++){
+	  printf("請輸入第%d位學生的成績",i+1);
+	  scanf("%d",&scores[i]);
+  }
+
+  for(int i=0;i < NUMS; i++){
+	  sum += scores[i];
+  }
+
+  ave = (double)sum / NUMS;
+  printf("全班總成績為:%d分,平均為%.2f分\n",sum,ave);
+  return 0;
 }

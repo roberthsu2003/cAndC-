af9e21d0e86f015e39e7bc7d5bb06e8da5e35501
Robert Hsu <roberthsu2003@gmail.com>
2021-06-05 11:14:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,12 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 int main() {
-	int inputNum;
-	int num=0;
-	int sum=0;
-	do{
-		cout << "請輸入第" << num + 1 << "個數值:";
-		cin >> inputNum;
-		if(inputNum<0) break;
-		num++;
-		if(inputNum % 2 == 1) continue;
-		sum += inputNum;
-
-	}while(true);
-	cout << "所有輸入的正偶數的加總是:" << sum;
+	int guess;
+	srand(time(NULL));
+	guess = rand();
+	cout << "亂數值是" << (guess % 10)+1  << endl;
 }

727a62223f588efb3b0348210a9731a299063e49
Robert Hsu <roberthsu2003@gmail.com>
2021-06-05 10:46:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int score=0;
-	int num = 0; //學生數
-	int sum = 0; //總分
-	do {
-		
-		cout << "請輸入第" << num <<"學生的成績:";
-		cin >> score;
-		if(score>0){
-			sum += score;
-			num += 1;
-		}	
-		
-	}while(score > 0);
-	//cout << "全班總成績為:" << sum << "平均分數為" << (float)sum / num;
+	int inputNum;
+	int num=0;
+	int sum=0;
+	do{
+		cout << "請輸入第" << num + 1 << "個數值:";
+		cin >> inputNum;
+		if(inputNum<0) break;
+		num++;
+		if(inputNum % 2 == 1) continue;
+		sum += inputNum;
 
-	printf("全班總成績為:%d,平均分數為%.2f",sum,(float)sum / num);
-	return 0;
-
+	}while(true);
+	cout << "所有輸入的正偶數的加總是:" << sum;
 }

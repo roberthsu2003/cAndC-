afde7f7b4801aa0ddb3b4d16283087fc11a622b0
Robert Hsu <roberthsu2003@gmail.com>
2021-06-05 10:09:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,12 +2,19 @@
 using namespace std;
 
 int main() {
-	int deposit = 0,num=0, inputNum;
-	while(deposit < 30000){
-		num++;
-		cout << "請輸入第" << num << "個月份的存款:";
-		cin >> inputNum;
-		deposit += inputNum;
+	int score=0;
+	int num = 0; //學生數
+	int sum = 0; //總分
+	while(true){
+		cout << "請輸入第" << num+1 <<"學生的成績:";
+		cin >> score;
+		if(score<0) break;
+		sum += score;
+		num += 1;
 	}
-	cout << "恭喜!已經存夠了，存了" << num << "個月的總存款為:" << deposit << "元。"; 
+	//cout << "全班總成績為:" << sum << "平均分數為" << (float)sum / num;
+
+	printf("全班總成績為:%d,平均分數為%.2f",sum,(float)sum / num);
+	return 0;
+
 }

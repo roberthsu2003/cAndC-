100f12515efa84aaeb11ade826588343f7d7efdd
Robert Hsu <roberthsu2003@gmail.com>
2021-06-05 10:30:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,13 +5,16 @@ int main() {
 	int score=0;
 	int num = 0; //學生數
 	int sum = 0; //總分
-	while(true){
-		cout << "請輸入第" << num+1 <<"學生的成績:";
+	do {
+		
+		cout << "請輸入第" << num <<"學生的成績:";
 		cin >> score;
-		if(score<0) break;
-		sum += score;
-		num += 1;
-	}
+		if(score>0){
+			sum += score;
+			num += 1;
+		}	
+		
+	}while(score > 0);
 	//cout << "全班總成績為:" << sum << "平均分數為" << (float)sum / num;
 
 	printf("全班總成績為:%d,平均分數為%.2f",sum,(float)sum / num);

0233eaa2027465eae16d7a235c92ffe2135d35b2
Robert Hsu <roberthsu2003@gmail.com>
2021-06-08 20:04:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,6 @@
-#include <iostream>
-#include <time.h>
-using namespace std;
+#include <stdio.h>
 
-int randomScore(){
-	return rand() % 41 + 60;
-} 
-
-int main() {
-	srand(time(NULL));
-	int students[50][5];
-	for(int i=0; i<50; i++){
-		for(int j=0; j<5; j++){
-			students[i][j] = randomScore();
-			cout << students[i][j] << " ";
-		}
-		cout << endl;
-	}
+int main(void) {
+  printf("Hello C語言\n");
+  return 0;
 }

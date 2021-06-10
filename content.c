278bd33212a07b327d0c407af653a7249809d9e7
Robert Hsu <roberthsu2003@gmail.com>
2021-06-10 20:02:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,7 @@
-#include <iostream>
+#include <stdio.h>
 
-int main() {
-  printf("姓名\t\t座號\t電話\n");
-  printf("李振聲\t\t11\t\t0229435437\n");
-  printf("張四聲\t\t21\t\t0729449437\n");
-  std::cout << "姓名\t\t座號\t電話\n";
-  std::cout << "李振聲\t\t11\t\t0229435437\n";
-  std::cout << "張四聲\t\t21\t\t0729449437\n";
+int main(void) {
+  const char* name = "徐國堂";
+  printf("我的名字是%s\n",name);
+  return 0;
 }

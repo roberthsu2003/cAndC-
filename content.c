145926c1fbaae274c53f2a99f06f0b42006d2237
Robert Hsu <roberthsu2003@gmail.com>
2021-06-10 19:43:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
+int main() {
   printf("姓名\t\t座號\t電話\n");
   printf("李振聲\t\t11\t\t0229435437\n");
   printf("張四聲\t\t21\t\t0729449437\n");
-  return 0;
+  std::cout << "姓名\t\t座號\t電話\n";
+  std::cout << "李振聲\t\t11\t\t0229435437\n";
+  std::cout << "張四聲\t\t21\t\t0729449437\n";
 }

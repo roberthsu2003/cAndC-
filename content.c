1df92133b593eb0868edfbee1721c39fac379e60
Robert Hsu <roberthsu2003@gmail.com>
2021-06-10 20:11:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
-  const char* name = "徐國堂";
-  printf("我的名字是%s\n",name);
-  return 0;
+int main() {
+  const char* cname = "徐國堂";
+  printf("我的名字是%s\n",cname);
+
+  //c++
+  std::string anotherName = "robert";
+  std::cout << anotherName << "\n";
+  std::cout << cname << "\n";
 }

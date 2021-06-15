ea09dfb01961e9d0c4f091810451d1978a686adf
Robert Hsu <roberthsu2003@gmail.com>
2021-06-15 20:25:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
-
 int main() {
- int x = 1;
- int y = ++x;
- cout << "x = " << x << " y= " << y << endl;
-
- int m = 1;
- int n = m++;
- cout << "m = " << m << " n= " << n << endl;
+	int  a = 2, b = 2;
+	cout << "a==b:" << (a==b) << endl;;
 }

8cb34adf0e69db111fb28741e767b2952d7006b3
Robert Hsu <roberthsu2003@gmail.com>
2021-06-15 21:35:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,15 +2,12 @@
 using namespace std;
 
 int main() {
-	string pass;
-	cout << "請輸入密碼:";
-	cin >> pass;
-	if(pass == "1234"){
-		cout << "歡迎光臨\n";
+	int inputNumber;
+	cout << "請輸入數字:";
+	cin >> inputNumber;
+	if(inputNumber % 2 == 0){
+		cout << "偶數" << endl;
 	}else{
-		cout << "密碼錯誤!\n";
-		cout << "請重新輸入!\n";
+		cout << "奇數" << endl;
 	}
-
-	return 0;
 }

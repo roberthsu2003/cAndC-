857ee93b08a1810745335b0a3c28517765fe0b34
Robert Hsu <roberthsu2003@gmail.com>
2021-06-15 20:00:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,13 +2,10 @@
 using namespace std;
 
 int main() {
-	int n,m;	
-	cout << "請輸入被除數(整數):";
-	cin >> n;
-	cout << "請輸入除數(整數,不可以為0):";
-	cin >> m;
-
-	cout << "商:" << n/m << "餘數:" << n%m << "\n";
-
-	return 0;
+	int x = 1;
+	//x = x + 1;
+	//++x;
+	x++;
+	++x;
+	cout << "x:" << x << endl;
 }

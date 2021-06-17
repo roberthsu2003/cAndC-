dc1b32904c2c55351b5a45ec0c22a1faa84a737e
Robert Hsu <roberthsu2003@gmail.com>
2021-06-17 19:20:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,17 +2,23 @@
 using namespace std;
 
 int main() {
- int scores;
- cout << "請輸入學生分數:";
- cin >> scores;
+	int money;
+	int payMoney;
+	cout << "請輸入購買金額:";
+	cin >> money;
 
- if(scores>=80){
-	 cout << "特優" << endl;
-  }else if(scores >= 60){
-	 cout << "優" << endl;
-  }else{
-	 cout << "有待改進" << endl;
-  }
+	//多向選擇
+	if(money >= 100000){
+		payMoney = money * 0.8;
+	}else if(money >= 50000){
+		payMoney = money * 0.85;
+	}else if(money >= 30000){
+		payMoney = money * 0.9;
+	}else if (money >= 10000){
+		payMoney = money * 0.95;
+	}else{
+		payMoney = money;
+	}
 
-  cout << "程式結束" << endl;
+	cout << "實付金額是:" << payMoney << "元\n";
 }

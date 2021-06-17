65979cf522a345ed1420c36c92922ca844775b86
Robert Hsu <roberthsu2003@gmail.com>
2021-06-17 20:03:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,17 +7,12 @@ int main() {
 	int bonus=0;
 	printf("請同時輸入國文成績和數學成績(國文,數學):");
 	scanf("%d,%d", &chinese, &math);
-	//巢狀判斷
-	if(chinese == 100){
-		if(math==100){
-			bonus = 1000;
-		}else{
-			bonus = 500;
-		}
-	}else{
-		if(math==100){
-			bonus = 500;
-		}
+	//邏輯運算子
+	if(math==100 && chinese==100){
+		bonus = 1000;
+	}else if (math == 100 || chinese == 100){
+		bonus = 500;
 	}
+	
 	printf("獎金是:%d\n", bonus);
 }

46776b0ff38da1c06e335a99f345a7ea6fa8bf97
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 16:24:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,5 +7,18 @@ struct student{
 };
 
 int main() {
-  std::cout << "Hello World!\n";
+	/*
+	struct student david;
+	david.name = "Davie";
+	david.id = "A1001";
+	david.chinese = 78;
+	david.english = 98;
+	david.math = 87;
+	*/
+	struct student david = {"David","A1001",78,98,87};
+	cout << "學生姓名:" << david.name << endl;
+	cout << "學生學號:" << david.id << endl;
+	cout << "國文:" << david.chinese << endl;
+	cout << "英文:" << david.english << endl;
+	cout << "數學:" << david.math << endl;
 }

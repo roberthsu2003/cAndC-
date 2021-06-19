e23bfb148727bed53de321725c0cd48feebb999d
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 10:33:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+	int apples = 3;
+	int oranges = 5;
+	//c語言的輸出
+	printf("apples有%d個,oranges有%d個\n",apples,oranges);
+
+	//c++的輸出
+	std::cout << "apples有" << apples << "個,oranges有" << oranges << "個\n";
 }

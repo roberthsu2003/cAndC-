a7b497ac727bdcbeb2d3ce73257a55f302a68d50
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 15:22:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int nums = 4;
-	float array[nums];
-	float temp;
-	for(int i=0; i < 4; i++){
+	int nums;
+	cout << "請輸入排序元素的數量:";
+	cin >> nums;
+	int array[nums];
+	int temp;
+	for(int i=0; i < nums; i++){
 		cout << "請輸入第" << i+1 << "個數值:";
 		cin >> array[i];
 	}
 
 	cout <<  "排序前:\n";
-	for(int i=0; i < 4; i++){
+	for(int i=0; i < nums; i++){
 		cout << array[i] << " ";
 	}
 
@@ -22,7 +25,7 @@ int main() {
 	//前面的
 	for(int i=0; i<nums-1; i++){
 		for(int j=i+1;j<nums;j++){
-			if (array[i] > array[j]){
+			if (array[i] < array[j]){
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
@@ -30,7 +33,7 @@ int main() {
 		}
 	}
 
-	for(int i=0; i < 4; i++){
+	for(int i=0; i < nums; i++){
 		cout << array[i] << " ";
 	}
 

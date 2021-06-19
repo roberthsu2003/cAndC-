3abbd94e724f80c43d9dacad0ddcb80b1b3fea74
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 16:11:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,48 +1,11 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 using namespace std;
+//建立student結構
+struct student{
+	string name,id;
+	int chinese,english,math;	
+};
 
 int main() {
-	int nums;
-	cout << "請輸入排序元素的數量:";
-	cin >> nums;
-	int array[nums];
-	int temp;
-	srandom(time(NULL));
-	for(int i=0; i < nums; i++){
-		//cout << "請輸入第" << i+1 << "個數值:";
-		array[i] = random() % 100;
-	}
-
-	cout <<  "排序前:\n";
-	for(int i=0; i < nums; i++){
-		cout << array[i] << " ";
-	}
-
-	cout << "\n\n";
-
-	cout << "排序後:\n";
-
-	//泡沫排序法
-	//前面的
-	for(int i=0; i<nums-1; i++){
-		for(int j=i+1;j<nums;j++){
-			if (array[i] < array[j]){
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-
-	for(int i=0; i < nums; i++){
-		cout << array[i] << " ";
-	}
-
-	cout << "\n\n";
-	
-
+  std::cout << "Hello World!\n";
 }

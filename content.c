e6990b6d2e35b0fa3976df6c076052442d30705b
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 11:49:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,7 +3,12 @@ using namespace std;
 
 int main() {
 	//建立陣列，同時給值
-	int scores[3] = {78, 90, 92};
+	int scores[3];
+	//利用迴圈讀取陣列所有元素的值
+	for(int i=0; i<3;i++){
+		cout << "請輸入分數:";
+		cin >> scores[i];
+	}
 	cout << "國文分數:" << scores[0] << endl;
 	cout << "英文分數:" << scores[1] << endl;
 	cout << "數文分數:" << scores[2] << endl;

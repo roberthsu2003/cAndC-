185e122519725abe83f3ba041027af023e7c34cd
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 16:36:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,28 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 //建立student結構
-struct student{
+typedef struct student{
 	string name,id;
 	int chinese,english,math;	
-};
+} student;
 
 int main() {
-	/*
+	srandom(time(NULL));
+	
 	struct student david;
 	david.name = "Davie";
 	david.id = "A1001";
-	david.chinese = 78;
-	david.english = 98;
-	david.math = 87;
+	david.chinese = (random() % 51) + 50;
+	david.english = (random() % 51) + 50;
+	david.math = (random() % 51) + 50;
+	
+	/*
+	student david = {"David","A1001",78,98,87};
 	*/
-	struct student david = {"David","A1001",78,98,87};
 	cout << "學生姓名:" << david.name << endl;
 	cout << "學生學號:" << david.id << endl;
 	cout << "國文:" << david.chinese << endl;

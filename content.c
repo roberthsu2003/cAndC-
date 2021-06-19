62ebabf76d291ffd4aa145f9bc69d2f1ded40717
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 10:19:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,5 @@
 #include <iostream>
-using namespace std;
+
 int main() {
-	string password;
-	do{
-		cout << "請輸入密碼:";
-		cin >> password;
-	}while(password != "5678");	
-	cout << "恭喜!你的密碼正確了!請進";
-	return 0;
+  std::cout << "Hello World!\n";
 }

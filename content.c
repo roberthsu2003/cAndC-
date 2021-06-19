ca7175de657b6abd62cfb7420a87dd83d2055f8e
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 11:22:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 	//c++的輸入和輸出
 	int englishScore;
-	std::cout << "請輸入您的英文分數:";
-	std::cin >> englishScore;
-	std::cout << "Hello! 您的英文分數是:" << englishScore << std::endl;
+	cout << "請輸入您的英文分數:";
+	cin >> englishScore;
+	cout << "Hello! 您的英文分數是:" << englishScore << endl;
 }

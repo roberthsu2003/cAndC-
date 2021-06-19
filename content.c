c570027b85e2ca8172667301bd7aca17447ab559
Robert Hsu <roberthsu2003@gmail.com>
2021-06-19 12:10:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
 int main() {
+
 	//建立陣列，同時給值
 	int scores[3];
+	int sum=0;
 	//利用迴圈讀取陣列所有元素的值
 	for(int i=0; i<3;i++){
 		cout << "請輸入分數:";
 		cin >> scores[i];
+		sum += scores[i];
 	}
 	cout << "國文分數:" << scores[0] << endl;
 	cout << "英文分數:" << scores[1] << endl;
 	cout << "數文分數:" << scores[2] << endl;
+	cout << "總分為:" << sum << endl;
+	//cout << "平均:" << sum/3.0 << endl; 
+	printf("平均:%.2f\n", sum/3.0);
 }

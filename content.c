76fa915ba4b0cec359b208073d8edb54414277a3
Robert Hsu <roberthsu2003@gmail.com>
2021-06-22 21:51:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,18 +7,24 @@ using namespace std;
 int main() {
 	//int student[3] = {70, 80, 90};
 	srand(time(NULL));
-	int student[50][3];
+	int scores = 10;
+	int student[50][scores];
 	for(int i=0; i<50; i++){
-		for(int j=0; j<3;j++){
+		for(int j=0; j<scores;j++){
 			student[i][j] = rand() % 51 + 50;
 		}
 	}
 
 	for(int i=0; i<50; i++){
 		cout << "第" << i+1 << "位學生" << endl;
-		for(int j=0; j<3;j++){			
+		int sum=0;
+		for(int j=0; j<scores;j++){			
 			cout << student[i][j] << ", ";
+			sum += student[i][j];
 		}
+		cout << "\n";
+		cout << "總分:" << sum << endl;
+		cout << "平均:" << (float)sum/scores << endl;
 		cout << "\n======================\n";
 	}
 	

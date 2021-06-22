09321e7f2af3bb5681025f37ad264d49db1a2b00
Robert Hsu <roberthsu2003@gmail.com>
2021-06-22 21:09:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 int main() {
+	//int student[3] = {70, 80, 90};
+	srand(time(NULL));
 	int student[3];
-	student[0] = 70;
-	student[1] = 80;
-	student[2] = 90;
+	student[0] = rand() % 51 + 50;
+	student[1] = rand() % 51 + 50;
+	student[2] = rand() % 51 + 50;
+	
 	cout << student[0] << "," << student[1] << "," << student[2] << endl;
 }

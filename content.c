fbc74671182a65500b2c261b4cbdf11cd8e3d7ea
Robert Hsu <roberthsu2003@gmail.com>
2021-06-22 20:03:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,10 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 int main() {
-	int inputNum;
-	int num=0;
-	int sum = 0;
-	do{		
-		cout << "請輸入第" << ++num << "數值:";
-		cin >> inputNum;
-		if(inputNum < 0){
-			break;
-		}
-		if(inputNum % 2 == 1){
-			continue;
-		}
-		sum += inputNum;
-
-	}while(true);
-
-	cout << "所有輸入的正偶數的加總是:" << sum << endl;
+	srand(time(NULL));
+	cout << rand() % 10 + 1 << endl;
 }

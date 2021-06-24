63471fb45ebb30815e2a8eae1a06f5fd03165d06
Robert Hsu <roberthsu2003@gmail.com>
2021-06-24 19:49:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,26 @@
+main.cpp
+
+#include <iostream>
+#include <stdio.h>
+#include "tool.h"
+using namespace std;
+
+
+int main() {
+  int c;
+  float result;
+  cout << "請輸入攝氏溫度:";
+  cin >> c;
+  result = temperature(c); //呼叫function
+  printf("華氏溫度=%.2f\n",result);
+  return 0;
+}
+
+tool.h
+//函式原型
+float temperature(int);
+//定義function
+float temperature(int value){
+	return value * 1.8 + 32;
+}
 

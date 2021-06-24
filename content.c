4898ddf69abf58d0a5b14249956938cebd24249b
Robert Hsu <roberthsu2003@gmail.com>
2021-06-24 19:48:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
-using namespace std;
-//函式原型
-float temperature(int);
 
-int main() {
-  int c;
-  float result;
-  cout << "請輸入攝氏溫度:";
-  cin >> c;
-  result = temperature(c); //呼叫function
-  printf("華氏溫度=%.2f\n",result);
-  return 0;
-}
-
-//定義function
-float temperature(int value){
-	return value * 1.8 + 32;
-}

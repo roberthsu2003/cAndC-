150a12b98ce532846f591696874f78aecb4fac8f
Robert Hsu <roberthsu2003@gmail.com>
2021-06-26 10:41:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,11 @@
 using namespace std;
 
 int main() {
-	int x = 10;
-	int *p;
-	p = &x;
-	cout << "x=" << x << endl;
-	cout << "*p=" << *p << endl;
+	int a = 666, b = 777;
+	cout << "a=" << a << ",b=" << b << endl;
+	int temp = a;
+	a = b;
+	b = temp;
+	cout << "a=" << a << ",b=" << b << endl;
+	return 0;
 }

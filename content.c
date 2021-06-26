5f454b5170e488902be7b982d7f8d52c93dab409
Robert Hsu <roberthsu2003@gmail.com>
2021-06-26 11:29:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,24 @@
+main.cpp
 
+#include <iostream>
+#include "tools.h"
+
+using namespace std;
+int main() {
+	int a = 666, b = 777;
+	cout << "a=" << a << ",b=" << b << endl;
+	//呼叫swap的function
+	swap(&a,&b);
+	cout << "a=" << a << ",b=" << b << endl;
+	return 0;
+}
+
+
+tools.h
+
+//定義function
+void swap(int *n, int *m){
+	int temp = *n;
+	*n = *m;
+	*m = temp;
+}

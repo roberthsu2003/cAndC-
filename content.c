35401a83a6428399ecf686aba437e37390c68f07
Robert Hsu <roberthsu2003@gmail.com>
2021-06-26 10:31:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,12 +2,9 @@
 using namespace std;
 
 int main() {
-	int n[3] = {10, 20 ,30};
-	cout << "陣列n的位址=" << n << endl;
-	cout << "陣列元素n[0]的位址=" << &n[0] << endl;
-	cout << "陣列元素n[1]的位址=" << &n[1] << endl;
-	cout << "陣列元素n[2]的位址=" << &n[2] << endl;
-	cout << "陣列元素n+0的位址=" << n+0 << endl;
-	cout << "陣列元素n+1的位址=" << n+1 << endl;
-	cout << "陣列元素n+2的位址=" << n+2 << endl;
+	int x = 10;
+	int *p;
+	p = &x;
+	cout << "x=" << x << endl;
+	cout << "*p=" << *p << endl;
 }

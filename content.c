b455110b172dfa9dc7ad51b6dc905de35bdd779c
Robert Hsu <roberthsu2003@gmail.com>
2021-06-26 15:36:08 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,13 @@ main.cpp
 
 #include <iostream>
 #include "data.h"
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 int main() {
-	Student std = {"robert", 79, 80, 70};
+	srand(time(NULL));
+	Student std = createStudent(1);
 	cout << "姓名:" << std.name << endl;
 	cout << "國文:" << std.chinese << endl;
 	cout << "數學:" << std.math << endl;
@@ -13,8 +16,12 @@ int main() {
 }
 
 
+
 data.h
+
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
 typedef struct student{
@@ -24,6 +31,14 @@ typedef struct student{
 	int math;
 }Student;
 
+Student createStudent(int num){
+	Student s;
+	s.name = "學生" + to_string(num);
+	s.chinese = 50 + (rand() % 51);
+	s.english = 50 + (rand() % 51);
+	s.math = 50 + (rand() % 51);
+	return s;
+}
 
 
 

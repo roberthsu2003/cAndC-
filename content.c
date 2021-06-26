ca8056c0584398dfe9d1127450b1b15063b62259
Robert Hsu <roberthsu2003@gmail.com>
2021-06-26 15:18:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,28 +1,17 @@
 main.cpp
 
 #include <iostream>
-#include "rectangle.h"
+#include "data.h"
 
 using namespace std;
-
-
-
 int main() {
-	Rectangle rec;
-	cout << "請輸入width:";
-	cin >> rec.width;
-	cout << "請輸入height:";
-	cin >> rec.height;
-	cout << "矩形的面績是:" << rec.width * rec.height << endl;
+	Student std = {"robert", 79, 80, 70};
+	cout << "姓名:" << std.name << endl;
+	cout << "國文:" << std.chinese << endl;
+	cout << "數學:" << std.math << endl;
+	cout << "英文:" << std.english << endl;
 }
 
-
-rectangle.h
-
-typedef struct Rectangle{
-	int width;
-	int height;
-}Rectangle;
 
 
 

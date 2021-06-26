5c99b5b7e0914438bcc16ffae189c7a20d050e33
Robert Hsu <roberthsu2003@gmail.com>
2021-06-26 15:02:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,20 @@
+main.cpp
+
+#include <iostream>
+#include "rectangle.h"
+
+using namespace std;
+
+
+
+int main() {
+	Rectangle rec;
+	cout << "請輸入width:";
+	cin >> rec.width;
+	cout << "請輸入height:";
+	cin >> rec.height;
+	cout << "矩形的面績是:" << rec.width * rec.height << endl;
+}
+
+
 

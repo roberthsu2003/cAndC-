ac1400f381bae488942e128ce49dc06cbfa81305
Robert Hsu <roberthsu2003@gmail.com>
2021-07-01 19:55:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <stdio.h>
 
 int main() {
   std::cout << "Hello C++!\n";
   std::cout << "這是C++的第一個輸出程式" << std::endl;
+  printf("Hello! c語言\n");
+  printf("這是c語言的輸出程式\n");
 }

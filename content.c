0f0e558b49011236d2ac80667113dcd50b79d37f
Robert Hsu <roberthsu2003@gmail.com>
2021-07-01 20:33:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <stdio.h>
-
 using namespace std;
 
 int main() {
-  int chinese;
-  int english;
-  cout << "Hello C++!\n";
-  cout << "這是C++的第一個輸出程式" << endl;
-  printf("Hello! c語言\n");
-  printf("這是c語言的輸出程式\n");
-  cout << "請輸入國文分數:";
-  cin >> chinese;
-  cout << "你的國文分數是" << chinese << endl;
-
-  printf("請輸入英文分數:");
-  scanf("%d",&english);
-  printf("您的英文分數是:%d\n",english);
-  return 0;
+	int scores[3] = {70, 80, 90};
+	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
+	return 0;
 }

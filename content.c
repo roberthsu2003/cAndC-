273b63c2d2635bddaa97e8084b745c775d5e6140
Robert Hsu <roberthsu2003@gmail.com>
2021-07-01 21:48:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,14 +6,32 @@ using namespace std;
 
 int main() {	
 	srand(time(NULL));
-	int length = 4;
+	int length = 10;
 	int scores[length];
+	int temp;
 
 	for(int i=0; i<length; i++){		
 		scores[i] = (rand() % 100) + 1;
 	}
+	cout << "初始值:\n";
+	for(int i=0; i<length; i++){		
+		cout << scores[i] << " ";
+	}
+	cout << endl;
 
-	for(int i=0; i<length; i++){
+	//排序
+	for(int i=0; i<length-1; i++){
+		for(int j=i+1; j<length; j++){
+			if (scores[i] > scores[j]){
+				temp = scores[i];
+				scores[i] = scores[j];
+				scores[j] = temp;
+			}
+		}
+	}
+
+	cout << "排序後:\n";
+	for(int i=0; i<length; i++){		
 		cout << scores[i] << " ";
 	}
 

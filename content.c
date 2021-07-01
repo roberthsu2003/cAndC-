46be3872d52f4dfcee5d2d9f553c3aebd5fb8431
Robert Hsu <roberthsu2003@gmail.com>
2021-07-01 20:11:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,6 +3,7 @@
 
 int main() {
   int chinese;
+  int english;
   std::cout << "Hello C++!\n";
   std::cout << "這是C++的第一個輸出程式" << std::endl;
   printf("Hello! c語言\n");
@@ -10,4 +11,9 @@ int main() {
   std::cout << "請輸入國文分數:";
   std::cin >> chinese;
   std::cout << "你的國文分數是" << chinese << std::endl;
+
+  printf("請輸入英文分數:");
+  scanf("%d",&english);
+  printf("您的英文分數是:%d\n",english);
+  return 0;
 }

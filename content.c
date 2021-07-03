20237a756f60421fe4c7677b8cdd8e362e75b646
Robert Hsu <roberthsu2003@gmail.com>
2021-07-03 14:24:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,4 +8,30 @@ int main() {
 }
 
 
+People.h
+#ifndef __PEOPLE__
+#define __PEOPLE__
+
+#include <iostream>
+using namespace std;
+
+class People{
+	public:
+	string name;
+	bool man;
+	int height;
+	int weight;
+	//建構式
+	People(){		
+	}
+
+	People(string n, bool s, int h, int w){
+		name = n;
+		man = s;
+		height = h;
+		weight = w;
+	}
+};
+
+#endif
 

3e66d6388cbb3bddd532d4df7365a929566111f2
Robert Hsu <roberthsu2003@gmail.com>
2021-07-03 16:57:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -12,8 +12,13 @@ int main() {
 	for(int i=0; i<num; i++){
 		string name = "stu" + to_string(i+1);
 		cout << name << endl;
-		string id = "A00000" + to_string(i+1);
+		string id = "A0" + to_string(i+1);
 		students[i] = Student(name, id);
+	}
+	cout << "學生姓名\t學號\t國文\t英文\t數學\t總分\t平均" << endl;
+	for(int i=0; i<num; i++){
+		Student s = students[i];
+		cout << s.name << "\t\t" << s.id << "\t\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << "\t\t" << s.sum() << "\t\t" << s.average() << endl;
 	}
 
 }
@@ -39,7 +44,7 @@ class Student{
 
 	//建構式
 	Student(){
-		
+
 	}
 
 	//建構式
@@ -50,5 +55,15 @@ class Student{
 		english = 50 + rand() % 51;
 		math = 50 + rand() % 51;
 	}
+
+	//method
+	int sum(){
+		return chinese + english + math;
+	}
+
+	float average(){
+		return sum() / 3.0;
+	}
 };
 #endif
+

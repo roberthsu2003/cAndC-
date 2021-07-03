5a520299d15ede34136a58cf830eff13d25fad5f
Robert Hsu <roberthsu2003@gmail.com>
2021-07-03 12:17:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "rectangle.h"
+#include <time.h>
 
 using namespace std;
 
 int main() {
-	Rectangle rec1(10,20);
-	
-
+	srand(time(NULL));
+	Rectangle rec1;
 	cout << " rec1的寬是" << rec1.width << endl;
 	cout << " rec1的高是" << rec1.height << endl;
 	cout << " rec1的面積是" << rec1.area() << endl;
@@ -19,16 +19,24 @@ rectangle.h
 #ifndef __RECTANGLE__H
 #define __RECTANGLE__H
 
+#include <stdlib.h>
+
+
 class Rectangle{
 	public:
 	//field欄位
 	int width;
 	int height;
 
-	//建構式
+	//建構式(int,int)
 	Rectangle(int w, int h){
 		width = w;
 		height = h;
+	}
+	//建構式()
+	Rectangle(){
+		width = rand() % 61 + 40;
+		height = rand() % 61 + 40;
 	}
 
 	//method方法

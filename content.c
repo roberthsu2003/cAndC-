b9fa1d3d63c5a90a900977a4a72882bbad318f0a
Robert Hsu <roberthsu2003@gmail.com>
2021-07-03 14:12:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,48 +1,29 @@
 #include <iostream>
-#include "rectangle.h"
-#include <time.h>
+#include "people.h"
 
 using namespace std;
-
 int main() {
-	srand(time(NULL));
-	Rectangle rec1;
-	cout << " rec1的寬是" << rec1.width << endl;
-	cout << " rec1的高是" << rec1.height << endl;
-	cout << " rec1的面積是" << rec1.area() << endl;
-
+	People p1;
+	p1.name="robert";
+	p1.man = true;
+	p1.height = 172;
+	p1.weight = 70;
+	cout << p1.name << endl;
 }
 
 
-rectangle.h
+people.h
+#ifndef __PEOPLE__
+#define __PEOPLE__
 
-#ifndef __RECTANGLE__H
-#define __RECTANGLE__H
+#include <iostream>
 
-#include <stdlib.h>
-
-
-class Rectangle{
+class People{
 	public:
-	//field欄位
-	int width;
+	std::string name;
+	bool man;
 	int height;
-
-	//建構式(int,int)
-	Rectangle(int w, int h){
-		width = w;
-		height = h;
-	}
-	//建構式()
-	Rectangle(){
-		width = rand() % 61 + 40;
-		height = rand() % 61 + 40;
-	}
-
-	//method方法
-	int area(){
-		return width * height;
-	}
+	int weight;
 };
 
 #endif

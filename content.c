33498a98a926db385c65bf1fa6a56d1fb9f5b114
Robert Hsu <roberthsu2003@gmail.com>
2021-07-03 14:23:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,27 +3,9 @@
 
 using namespace std;
 int main() {
-	People p1;
-	p1.name="robert";
-	p1.man = true;
-	p1.height = 172;
-	p1.weight = 70;
+	People p1("Robert", true, 172, 72);	
 	cout << p1.name << endl;
 }
 
 
-people.h
-#ifndef __PEOPLE__
-#define __PEOPLE__
 
-#include <iostream>
-
-class People{
-	public:
-	std::string name;
-	bool man;
-	int height;
-	int weight;
-};
-
-#endif

d979703f90c712d88cda352e5fa4d75c54b0b769
Robert Hsu <roberthsu2003@gmail.com>
2021-07-03 10:45:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,25 @@
 #include <iostream>
-#define RATE 32.75
-
-using namespace std;
+#include "data.h"
+#include "data.h"
 
 int main() {
-	double us;
-	cout << "請輸入您有多少美金:";
-	cin >> us;
-	double nt = us * RATE;
-	cout << "您有" << nt << "台幣" << endl;
+  std::cout << "Hello World!\n";
 }
+
+
+
+
+
+data.h
+#ifndef __DATA_H__
+#define __DATA_H__
+
+#include <iostream>
+using namespace std;
+
+#define RATE 32.75
+typedef struct student{
+	string name;
+}Student;
+
+#endif

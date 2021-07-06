3652b025e0f8d36682d885d2d7b0660d169a6d6c
Robert Hsu <roberthsu2003@gmail.com>
2021-07-06 21:02:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,45 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
-void swap(int *n, int *m){
-	int temp = *n;
-	*n = *m;
-	*m = temp;
+void sorted(int *a,int length){
+	int temp;
+	for(int i=0; i<length-1; i++){
+		for(int j=i+1; j<length; j++){
+			if (a[i] < a[j]){
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+
 }
 
-int main() {
-	int a = 666;
-	int b = 777;
-	cout << "a=" << a << ",b=" << b << endl;
-	//2數交換,呼叫function swap
-	swap(&a,&b);
+int main() {	
+	srand(time(NULL));
+	int length = 10;
+	int scores[length];	
+
+	for(int i=0; i<length; i++){		
+		scores[i] = (rand() % 100) + 1;
+	}
+	cout << "初始值:\n";
+	for(int i=0; i<length; i++){		
+		cout << scores[i] << " ";
+	}
+	cout << endl;
+
+	//排序
+	sorted(scores,length);
 	
-	cout << "a=" << a << ",b=" << b << endl;
+	cout << "排序後:\n";
+	for(int i=0; i<length; i++){		
+		cout << scores[i] << " ";
+	}
+
+	cout << endl;
+	return 0;
 }

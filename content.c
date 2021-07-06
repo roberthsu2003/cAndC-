18fb4ba186ad8a705dda21c571d9306a2848941a
Robert Hsu <roberthsu2003@gmail.com>
2021-07-06 21:32:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,55 +1,19 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include "tools.h"
-
 using namespace std;
 
+struct Student{
+	string name;
+	string id;
+	int chinese;
+	int english;
+	int math;
+};
 
-
-int main() {	
-	srand(time(NULL));
-	int length = 10;
-	int scores[length];	
-
-	for(int i=0; i<length; i++){		
-		scores[i] = (rand() % 100) + 1;
-	}
-	cout << "初始值:\n";
-	for(int i=0; i<length; i++){		
-		cout << scores[i] << " ";
-	}
-	cout << endl;
-
-	//排序
-	sorted(scores,length);
-	
-	cout << "排序後:\n";
-	for(int i=0; i<length; i++){		
-		cout << scores[i] << " ";
-	}
-
-	cout << endl;
-	return 0;
-}
-
-
-tools.h
-void sorted(int *a,int length);
-
-
-
-tools.cpp
-void sorted(int *a,int length){
-	int temp;
-	for(int i=0; i<length-1; i++){
-		for(int j=i+1; j<length; j++){
-			if (a[i] < a[j]){
-				temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
-
+int main() {
+	struct Student stu1;
+	stu1.name = "Robert";
+	stu1.id = "A78654";
+	stu1.chinese = 78;
+	stu1.english = 97;
+	stu1.math = 95;
 }

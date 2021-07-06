a1281ffd0f06e2060e2e13be96531c6dea0976c9
Robert Hsu <roberthsu2003@gmail.com>
2021-07-06 21:42:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,33 @@
 #include <iostream>
+#include "data.h"
+
 using namespace std;
 
-struct Student{
-	string name;
-	string id;
-	int chinese;
-	int english;
-	int math;
-};
+
 
 int main() {
-	struct Student stu1;
+	Stu stu1;
 	stu1.name = "Robert";
 	stu1.id = "A78654";
 	stu1.chinese = 78;
 	stu1.english = 97;
 	stu1.math = 95;
+	cout << "name=" << stu1.name << endl;
+	cout << "id=" << stu1.id << endl;
+	cout << "chinese=" << stu1.chinese << endl;
+	cout << "english=" << stu1.english << endl;
+	cout << "math=" << stu1.math << endl;
 }
+
+
+data.h
+#include <iostream>
+using namespace std;
+
+typedef struct Student{
+	string name;
+	string id;
+	int chinese;
+	int english;
+	int math;
+}Stu;

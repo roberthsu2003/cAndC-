2e09d95eb093a98220c94f4c7df043192d80dce7
Robert Hsu <roberthsu2003@gmail.com>
2021-07-06 19:54:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	int n[3] = {10, 20, 30};
-	cout << "陣列n的位址=" << n << endl;
-	cout << "陣列n[0]的位址=" << &n[0] << endl;
-	cout << "陣列n[1]的位址=" << &n[1] << endl;
-	cout << "陣列n[2]的位址=" << &n[2] << endl;
-
+	int n=10;
+	int *p = &n;
+	cout << "n的值是" << n << endl;
+	cout << "p指向的值" << *p << endl;
+	*p = 20;
+	cout << "改變後n的值是" << n << endl;
 }

2eab89cf2af1fdd403e4f0e003ddc12e4bb61884
Robert Hsu <roberthsu2003@gmail.com>
2021-07-08 21:46:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,15 +6,6 @@
 
 using namespace std;
 
-Student createStudent(int num){
-	Student s;
-	s.name = "stu" + to_string(num);
-	s.id = "A0001" + to_string(num);
-	s.chinese = rand() % 41 + 60;
-	s.english = rand() % 41 + 60;;
-	s.math = rand() % 41 + 60;;
-	return s;
-};
 
 int main() {
 	Student students[50];
@@ -24,11 +15,12 @@ int main() {
 		students[i] = createStudent(i+1);
 	}
 
-	cout << "姓名\t學號\t國文\t英文\t數學" << endl;
+	cout << "姓名\t學號\t國文\t英文\t數學\t總分\t平均" << endl;
 
 	for(int i=0; i<50; i++){
 		Student s = students[i];
-		cout << s.name << "\t" << s.id << "\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << endl;
+		cout << s.name << "\t" << s.id << "\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << "\t\t" << sum(s);
+		printf("\t\t%.2f\n",average(s));
 	}
 
 	
@@ -49,4 +41,22 @@ typedef struct student{
 	int english;
 	int math;
 }Student;
+
+Student createStudent(int num){
+	Student s;
+	s.name = "stu" + to_string(num);
+	s.id = "A0001" + to_string(num);
+	s.chinese = rand() % 41 + 60;
+	s.english = rand() % 41 + 60;;
+	s.math = rand() % 41 + 60;;
+	return s;
+};
+
+int sum(Student s){
+	return s.chinese + s.english + s.math;
+}
+
+float average(Student s){
+	return sum(s) / 3.0;
+}
 

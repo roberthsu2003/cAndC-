3217f3edd43105cb2eed869fd23adfcd7fb88ac6
Robert Hsu <roberthsu2003@gmail.com>
2021-07-08 20:12:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,18 @@
 #include <iostream>
+#include "student.h"
+#include "stdio.h"
+
 using namespace std;
-
 int main() {
-	char str[] = {'D', 'e', 'v', 'C', '+', '+','\0'};
-	cout << str << endl;
-	const char *name = str;
-	cout << name << endl;
-	name = "robert";
-	cout << name << endl;
-
-	//c語言的字串
-	const char* myName = "Hello! C++";
-	cout << myName << endl;
-	myName = "Hello! C";
-	cout << myName << endl;
+	Student stu1 = {"robert","A10003",78, 90,85};
+	cout << "name=" << stu1.name << endl;
+	cout << "id=" << stu1.id << endl;
+	cout << "國文=" << stu1.chinese << endl;
+	cout << "英文=" << stu1.english << endl;
+	cout << "數學=" << stu1.math << endl;
+	cout << "總分=" << stu1.chinese + stu1.english + stu1.math << endl;
+	printf("平均=%.2f\n", (stu1.chinese + stu1.english + stu1.math)/3.0);
 	
-	//c++語言的字串
-	string yourName = "Learning C++";
-	cout << yourName << endl;
 }
+
+

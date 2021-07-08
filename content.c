63bc31030a345de4c963cbfd1694681b1b9742f0
Robert Hsu <roberthsu2003@gmail.com>
2021-07-08 19:29:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
+
+void listArray(int *a){
+	//陣列變數的操作法
+	cout << a[0] << "," << a[1] << "," << a[2] << endl;
+
+	//指標變數的操作
+	cout << *(a+0) << "," << *(a+1) << "," << *(a+2) << endl;
+}
+
 int main() {
 	int arr[] = {10, 20 , 30};
-	cout << arr[0] << "," << arr[1] << "," << arr[2] << endl;
-
-	int *ptr = arr;
-	cout << "ptr+0=" << ptr+0 << endl;
-	cout << "ptr+1=" << ptr+1 << endl;
-	cout << "ptr+2=" << ptr+2 << endl;
-	cout << *(ptr+0) << "," << *(ptr+1) << "," << *(ptr+2) << endl;
+	listArray(arr);
 }

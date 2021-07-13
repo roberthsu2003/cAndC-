3dc3663e138752d78c8a88dc531ce5b5afcd15ac
Robert Hsu <roberthsu2003@gmail.com>
2021-07-13 21:09:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -12,4 +12,12 @@ int main() {
 	cout << "您有" << nt << "台幣" << endl;
 }
 
+tools.h
 
+#define RATE 32.78
+#define saturday 0
+#define sunday 1
+#define monday 2
+#define tuesday 3
+#define pin1 0x1245
+#define pin2 0x1254

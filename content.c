d246e813b5a23ca0e0afdfc42ca1216bed13bfc0
Robert Hsu <roberthsu2003@gmail.com>
2021-07-13 21:26:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,20 @@
 #include <iostream>
 #include "tools.h"
 
-
 using namespace std;
 
 int main() {
-	double us, nt;
-	cout << "請輸入您有多少美金:";
-	cin >> us;
-	nt = us * RATE;
-	cout << "您有" << nt << "台幣" << endl;
+	int s,result;
+	cout << "請輸入數字:";
+	cin >> s;
+	result = SQUARE(s);
+	cout << s << "的平方為:" << result << endl;
+
+	result = SQUARE(s+1);
+	cout << s+1 << "的平方為:" << result << endl;
 }
+
 
 tools.h
 
-#define RATE 32.78
-#define saturday 0
-#define sunday 1
-#define monday 2
-#define tuesday 3
-#define pin1 0x1245
-#define pin2 0x1254
+#define SQUARE(x) (x) * (x)

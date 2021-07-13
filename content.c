1ccd93d0a9e5ab633433dc02056b906d5f2df26f
Robert Hsu <roberthsu2003@gmail.com>
2021-07-13 20:29:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 //定義一個列舉型別
 typedef enum Direction{
 	North,
@@ -8,6 +8,35 @@ typedef enum Direction{
 	West
 }Direction;
 
+typedef enum Day{
+	saturday,      
+    sunday,   
+    monday,    
+    tuesday,
+    wednesday,   
+    thursday,
+    friday
+}Day;
+
 int main() {
-	Direction dest = North;
+	Direction dest = East;
+
+	switch(dest){
+		case North:
+			cout << "北" << endl;;
+			break;
+		case South:
+			cout << "南" << endl;
+			break;
+		case East:
+			cout << "東" << endl;
+			break;
+		case West:
+			cout << "西" << endl;
+			break;		
+
+	}
+
+	Day day = monday;
+	cout << day << endl;
 }

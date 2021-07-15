408d70791c0dba1c68a356ec5f7f851b08f2d1d9
Robert Hsu <roberthsu2003@gmail.com>
2021-07-15 20:16:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
-namespace Human{
-	class Person{
+class Circle{
 	public:
-		string name;
-		int tall;
-		void walk(int speed){
-			cout << name << "每小時可走" << speed << "公里" << endl;
+		int radius;
+		float areas(){
+			return 3.14159 * radius * radius;
 		}
-	};
-}
-
-using namespace Human;
+	
+		float circumference(){
+			return radius * 2 * 3.14159;
+		}
+};
 
 int main() {
-	Person david;
-	david.name = "David";
-	david.tall = 180;
-	cout << david.name << " 身高=" << david.tall << endl;
-	david.walk(13);
-
-	Person robert;
-	robert.name = "Robert";
-	robert.tall = 170;
-	cout << robert.name << " 身高=" << robert.tall << endl;
-	robert.walk(15);
-
-	Person *john= new Person();
-	john -> name = "John";
-	john -> tall = 178;
-	cout << john -> name << " 身高=" << john -> tall << endl;
-	john -> walk(15);
+	Circle c1;
+	c1.radius = 10;
+	cout << "圓半徑是" << c1.radius << endl;
+	cout << "圓周是" << c1.circumference() << endl;
+	cout << "圓面積" << c1.areas() << endl;
 }

c53ccc0cf432b83746c6230d6b61cb9269feb70d
Robert Hsu <roberthsu2003@gmail.com>
2021-07-15 21:42:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -12,6 +13,10 @@ class Person{
 		this -> height = height;
 		this -> weight = weight;
 	}
+
+	float getBmi(){
+		return weight / pow((height/100.0),2);
+	}
 };
 
 int main() {
@@ -19,9 +24,11 @@ int main() {
 	cout << "姓名:" << p1.name << endl;
 	cout << "身高:" << p1.height << endl;
 	cout << "體重:" << p1.weight << endl;
+	cout << "BMI:" << p1.getBmi() << endl;
 
 	Person p2("robert",170, 72);
 	cout << "姓名:" << p2.name << endl;
 	cout << "身高:" << p2.height << endl;
 	cout << "體重:" << p2.weight << endl;
+	cout << "BMI:" << p2.getBmi() << endl;
 }

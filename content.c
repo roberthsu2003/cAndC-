4da1b8747d3f9f61fa77dd273ee5f9ffc679c4c0
Robert Hsu <roberthsu2003@gmail.com>
2021-07-15 19:29:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,20 @@
 #include <iostream>
 
+using namespace std;
 class Person{
-
+	public:
+		string name;
+		int tall;
 };
 
 int main() {
 	Person david;
+	david.name = "David";
+	david.tall = 180;
+	cout << david.name << " 身高=" << david.tall << endl;
+
+	Person robert;
+	robert.name = "Robert";
+	robert.tall = 170;
+	cout << robert.name << " 身高=" << robert.tall << endl;
 }

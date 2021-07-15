e7ac64448f7dcfcb0b2b2640e75d07a134ed5234
Robert Hsu <roberthsu2003@gmail.com>
2021-07-15 19:18:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,39 +1,9 @@
 #include <iostream>
-#include "tools.h"
-#include "tools.h"
-#include "tools.h"
-#include "tools.h"
 
-using namespace std;
+class Person{
+
+};
 
 int main() {
-	int s,result;
-	cout << "請輸入數字:";
-	cin >> s;
-	result = SQUARE(s);
-	cout << s << "的平方為:" << result << endl;
-
-	result = SQUARE(s+1);
-	cout << s+1 << "的平方為:" << result << endl;
+	Person david;
 }
-
-
-tools.h
-
-#ifndef __TOOLS_H__
-#define __TOOLS_H__
-
-#include <iostream>
-#define SQUARE(x) (x) * (x)
-
-using namespace std;
-
-typedef struct Student{
-	string name;
-	string id;
-	int chinese;
-	int math;
-	int english;
-}Student;
-
-#endif

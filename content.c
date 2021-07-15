6a06ab9caba7b8783ceb5a473beb318a152a392d
Robert Hsu <roberthsu2003@gmail.com>
2021-07-15 19:42:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,6 +5,9 @@ class Person{
 	public:
 		string name;
 		int tall;
+		void walk(int speed){
+			cout << name << "每小時可走" << speed << "公里" << endl;
+		}
 };
 
 int main() {
@@ -12,9 +15,11 @@ int main() {
 	david.name = "David";
 	david.tall = 180;
 	cout << david.name << " 身高=" << david.tall << endl;
+	david.walk(13);
 
 	Person robert;
 	robert.name = "Robert";
 	robert.tall = 170;
 	cout << robert.name << " 身高=" << robert.tall << endl;
+	robert.walk(15);
 }

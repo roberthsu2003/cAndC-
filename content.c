c8c1db524f56d85bbe7c4c2b0457435f624c5516
Robert Hsu <roberthsu2003@gmail.com>
2021-07-20 21:55:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -17,7 +17,7 @@ int main() {
 		students[i] = Student(name,id);
 	}
 
-	sorted(students,num);
+	Student::sorted(students,num);
 
 	cout << "學生姓名\t學號\t國文\t英文\t數學\t總分\t平均\t名次" << endl;
 	
@@ -29,49 +29,4 @@ int main() {
 }
 
 
-student.h
 
-#include <iostream>
-using namespace std;
-
-class Student{
-	public:
-	string name;
-	string id;
-	int chinese;
-	int english;
-	int math;
-
-	Student(){
-
-	}
-
-	Student(string n, string i){
-		name = n;
-		id = i;
-		chinese = rand() % 41 + 60;
-		english = rand() % 41 + 60;
-		math = rand() % 41 + 60;	
-	}
-
-	int sum(){
-		return chinese + english + math;
-	}
-
-	float average(){
-		return sum() / 3.0;
-	}
-};
-
-void sorted(Student s[], int nums){
-	Student temp;
-	for(int i=0; i < nums-1; i++){
-		for(int j=i+1; j < nums; j++){
-			if (s[i].sum() < s[j].sum()){
-				temp = s[i];
-				s[i] = s[j];
-				s[j] = temp;
-			}
-		}
-	}
-}

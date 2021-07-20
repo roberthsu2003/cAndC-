3d11025dc023fbfc01ba19533325ef4e6c32b608
Robert Hsu <roberthsu2003@gmail.com>
2021-07-20 21:45:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -29,3 +29,49 @@ int main() {
 }
 
 
+student.h
+
+#include <iostream>
+using namespace std;
+
+class Student{
+	public:
+	string name;
+	string id;
+	int chinese;
+	int english;
+	int math;
+
+	Student(){
+
+	}
+
+	Student(string n, string i){
+		name = n;
+		id = i;
+		chinese = rand() % 41 + 60;
+		english = rand() % 41 + 60;
+		math = rand() % 41 + 60;	
+	}
+
+	int sum(){
+		return chinese + english + math;
+	}
+
+	float average(){
+		return sum() / 3.0;
+	}
+};
+
+void sorted(Student s[], int nums){
+	Student temp;
+	for(int i=0; i < nums-1; i++){
+		for(int j=i+1; j < nums; j++){
+			if (s[i].sum() < s[j].sum()){
+				temp = s[i];
+				s[i] = s[j];
+				s[j] = temp;
+			}
+		}
+	}
+}

b39ad496729c6464640457f0651e788737ae735e
Robert Hsu <roberthsu2003@gmail.com>
2021-07-20 21:31:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <time.h>
+#include "student.h"
 
+using namespace std;
+
+
+
+int main() {
+	srand(time(NULL));
+	int num = 50;
+	Student students[num];
+
+	for(int i=0; i<num; i++){
+		string name = "stu" + to_string(i+1);
+		string id = "A0" + to_string(i+1);
+		students[i] = Student(name,id);
+	}
+
+	cout << "學生姓名\t學號\t國文\t英文\t數學\t總分\t平均" << endl;
+	
+	for(int i=0; i<num; i++){
+		Student s = students[i];
+		cout << s.name << "\t\t" << s.id << "\t\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << "\t\t"<< s.sum() << "\t\t" << s.average() <<endl;
+	}
+
+}
+
+
+student.h
+
+#include <iostream>
 using namespace std;
 
 class Student{
@@ -14,7 +43,7 @@ class Student{
 	Student(){
 
 	}
-	
+
 	Student(string n, string i){
 		name = n;
 		id = i;
@@ -22,24 +51,12 @@ class Student{
 		english = rand() % 41 + 60;
 		math = rand() % 41 + 60;	
 	}
+
+	int sum(){
+		return chinese + english + math;
+	}
+
+	float average(){
+		return sum() / 3.0;
+	}
 };
-
-int main() {
-	srand(time(NULL));
-	int num = 50;
-	Student students[num];
-
-	for(int i=0; i<num; i++){
-		string name = "stu" + to_string(i+1);
-		string id = "A0" + to_string(i+1);
-		students[i] = Student(name,id);
-	}
-
-	cout << "學生姓名\t學號\t國文\t英文\t數學" << endl;
-	
-	for(int i=0; i<num; i++){
-		Student s = students[i];
-		cout << s.name << "\t\t" << s.id << "\t\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << endl;
-	}
-
-}

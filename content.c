37ace828c244fc07f6ddccf0b379062cf58ca343
Robert Hsu <roberthsu2003@gmail.com>
2021-07-20 21:44:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -17,46 +17,15 @@ int main() {
 		students[i] = Student(name,id);
 	}
 
-	cout << "學生姓名\t學號\t國文\t英文\t數學\t總分\t平均" << endl;
+	sorted(students,num);
+
+	cout << "學生姓名\t學號\t國文\t英文\t數學\t總分\t平均\t名次" << endl;
 	
 	for(int i=0; i<num; i++){
 		Student s = students[i];
-		cout << s.name << "\t\t" << s.id << "\t\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << "\t\t"<< s.sum() << "\t\t" << s.average() <<endl;
+		cout << s.name << "\t\t" << s.id << "\t\t" << s.chinese << "\t\t" << s.english << "\t\t" << s.math << "\t\t"<< s.sum() << "\t\t" << s.average() << "\t\t" << i+1 <<endl;
 	}
 
 }
 
 
-student.h
-
-#include <iostream>
-using namespace std;
-
-class Student{
-	public:
-	string name;
-	string id;
-	int chinese;
-	int english;
-	int math;
-
-	Student(){
-
-	}
-
-	Student(string n, string i){
-		name = n;
-		id = i;
-		chinese = rand() % 41 + 60;
-		english = rand() % 41 + 60;
-		math = rand() % 41 + 60;	
-	}
-
-	int sum(){
-		return chinese + english + math;
-	}
-
-	float average(){
-		return sum() / 3.0;
-	}
-};

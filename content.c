2c1b8d7689e73b838021a81a5cd10b44495d6d27
Robert Hsu <roberthsu2003@gmail.com>
2021-07-20 20:20:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,12 +4,24 @@ using namespace std;
 
 int main() {
 	Person p1;
+	bool m;
+	int height;
+	int weight;
 	cout << "請輸入姓名:";
 	cin >> p1.name;
-	cout << "請輸入身高(cm):";
-	cin >> p1.height;
-	cout << "請輸入體重(kg):";
-	cin >> p1.weight;
+	
+	do{
+		cout << "請輸入身高(cm):";
+		cin >> height;
+		m = p1.setHeight(height);
+	}while(m == false);
+	
+	do{
+		cout << "請輸入體重(kg):";
+		cin >> weight;
+		m = p1.setWeight(weight);
+	}while(m == false);
+	
 
 	cout << p1.name << "您好:" << endl;
 	printf("您的bmi是%.2f\n",p1.getBmi());
@@ -19,60 +31,4 @@ int main() {
 }
 
 
-person.h
 
-#include <iostream>
-using namespace std;
-
-class Person{
-	public:
-	string name;
-	int height;
-	int weight;
-
-	Person(string name,int height, int weight);
-	Person();
-
-	float getBmi();
-	void suggestion();
-};
-
-
-
-person.cpp
-
-#include <math.h>
-#include "person.h"
-
-Person::Person(string name,int height, int weight){
-		this -> name = name;
-		this -> height = height;
-		this -> weight = weight;
-}
-
-Person::Person(){
-	
-}
-
-float Person::getBmi(){
-		return weight / pow((height/100.0),2);
-}
-
-void Person::suggestion(){
-		float bmi = getBmi();
-		string message;
-		if(bmi<18.5){
-			message = "體重過輕";
-		}else if(bmi < 24){
-			message = "正常範圍";
-		}else if(bmi < 27){
-			message = "過重";
-		}else if(bmi < 30){
-			message = "輕度肥胖";
-		}else if(bmi < 35){
-			message = "中度肥胖";
-		}else{
-			message = "重度肥胖";
-		}
-		cout << "提醒:" << message << endl;
-}

56aca34d3f1991213bcacb46de26826959d91832
Robert Hsu <roberthsu2003@gmail.com>
2021-08-03 21:07:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello 徐國堂\n");
-  printf("Hello 我現在正在寫第一個C語言的程式\n");
+	//宣告變數
+	int chinese;
+	int english;
+	//變數給值
+	chinese = 85;
+	english = 90;
+
+	//宣告變數
+	int total;
+	total = chinese + english;
+
+	printf("國文分數是\n");
+	printf("英文分數是\n");
+	printf("總分是\n");
   return 0;
 }

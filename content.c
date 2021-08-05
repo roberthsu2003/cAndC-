8cabfacd3fd4c62894188dab7ea8315a049e187c
Robert Hsu <roberthsu2003@gmail.com>
2021-08-05 21:24:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
 	double PI = 3.1415915987;
 	int radius = 9;
-	//自動轉換
-	cout << "圓面積=" << PI * radius * radius << endl;
-	printf("圓面積=%.2f\n",PI * radius * radius);
+	int area;
+	//數學運算時，會自動轉換型別
+	//如果使用=,會自動轉換型別
+	area = PI * radius * radius;
+	cout << "圓面積=" << area << endl;
+	printf("圓面積=%d\n",area);
 	return 0;
 }

912c17b81352797ce2e28c5f4183d5e60df77e23
Robert Hsu <roberthsu2003@gmail.com>
2021-08-05 19:16:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,27 +1,9 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-	//宣告變數
-	int chinese;
-	int english;
-	//變數給值
-	chinese = 85;
-	english = 90;
-
-	//宣告變數
-	int total;
-	total = chinese + english;
-
-	/*
-	printf("國文分數是%d\n",chinese);
-	printf("英文分數是%d\n",english);
-	printf("總分是%d\n",total);
-	*/
-
-	cout << "國文分數是" << chinese << "\n";
-	cout << "英文分數是" << english << "\n";
-	cout << "總分是" << total << endl;
-  	return 0;
+	int englishScore = 0;
+	std::cout << "請輸入您的英文分數:";
+	std::cin >> englishScore;
+	std::cout << "Hello!您的英文分數是:" << englishScore << std::endl;
+	return 0;
 }

69022ef941cb802156f79f669d8a97bed4dbe9ca
Robert Hsu <roberthsu2003@gmail.com>
2021-08-05 21:12:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,10 @@
 using namespace std;
 
 int main() {
-	//c++ string
-	string str1 = "這是c++建的字串";
-
-	//c const char*
-	const char* str2 = "這是c建立的字串";
-
-	cout << str1 << endl;
-	cout << str2 << endl;
-
+	double PI = 3.1415915987;
+	int radius = 9;
+	//自動轉換
+	cout << "圓面積=" << PI * radius * radius << endl;
+	printf("圓面積=%.2f\n",PI * radius * radius);
 	return 0;
 }

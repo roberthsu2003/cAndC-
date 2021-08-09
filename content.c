18609ea05b8263deaf2f8bb3e7bf4fd3b9587b4e
Robert Hsu <roberthsu2003@gmail.com>
2021-08-09 16:23:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,6 +4,6 @@ int main(void) {
   int chinese = 96;
   int english = 75;
   int sum = chinese + english;
-  printf("您的總分是:\n");
+  printf("您的國文:%d,英文是:%d,總分是:%d\n",chinese,english,sum);
   return 0;
 }

46afeb2340e3c06cc1248a23612cca24f33291da
Robert Hsu <roberthsu2003@gmail.com>
2021-08-09 17:00:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,14 @@
 
 int main() {
 	int chinese = 96;
-	int english = 75;   
+	int english = 75; 
+	/*  
 	printf("您的國文:%d\n",chinese);
 	printf("你的英文是:%d\n",english);
 	printf("總分是%d\n",chinese+english);
+	*/
+	std::cout << "您的國文:" << chinese << "\n";
+	std::cout << "您的英文是:" << english << "\n";
+	std::cout << "總分是" << chinese+english << "\n";
 	return 0;
 }

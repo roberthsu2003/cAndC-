f74c29fef9b721c77892c3b565f69903f8b2a7e7
Robert Hsu <roberthsu2003@gmail.com>
2021-08-09 16:41:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,6 +9,9 @@ int main(void) {
   ============================
   */
   //printf("您的國文:%d,英文是:%d,總分是:%d\n",chinese,english,chinese+english);
-  printf("您的國文:%d\n英文是:%d\n總分是:%d\n",chinese,english,chinese+english);
+  //printf("您的國文:%d\n英文是:%d\n總分是:%d\n",chinese,english,chinese+english);
+  printf("您的國文:%d\n",chinese);
+  printf("你的英文是:%d\n",english);
+  printf("總分是%d\n",chinese+english);
   return 0;
 }

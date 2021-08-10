33b09f2637fe24c43b57a197454cc301240be2f7
Robert Hsu <roberthsu2003@gmail.com>
2021-08-10 16:29:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
 	double PI = 3.1415915987;	
 	int radius;
+	double area;
+
 	cout << "請輸入半徑:";
 	cin >> radius;
-	cout << "圓面積:" << radius * radius * PI << endl;
+	area = radius * radius * PI;
+	cout << "圓面積:" << area << endl;
 	return 0;
 }

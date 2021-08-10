d413602dd48cfce5bf15e0fac71d9893298645dd
Robert Hsu <roberthsu2003@gmail.com>
2021-08-10 15:41:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-	char characterA = 'A';
-	cout << characterA << endl;
+	string name;//c++的型別
+	const char* sex; //c的字串型別
+	cout << "請輸入您的中文姓名:";
+	cin >> name;
+	sex = "女";
 
-	char characterB = 97;
-	cout << characterB << endl;
+	cout << "歡迎光臨:" << name << endl;
+	cout << "您的性別是" << sex << endl; 
+
 	return 0;
+
+
 }

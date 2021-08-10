710a4e2fa683b8a1a2b081f75cb4e4a2dff2ec61
Robert Hsu <roberthsu2003@gmail.com>
2021-08-10 19:17:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,19 +2,12 @@
 using namespace std;
 
 int main() {
-	int chinese, english, math;
-	int sum;
-	float average;
+	int n,m;
+	cout << "請輸入被除數(整數):";
+	cin >> n;
+	cout << "請輸入除數(整數,不可以為0):";
+	cin >> m;
 
-	cout << "請輸入中文分數:";
-	cin >> chinese;
-	cout << "請輸入英文分數:";
-	cin >> english;
-	cout << "請輸入數學分數:";
-	cin >> math;
-
-	sum = chinese + english + math;
-	average = (float)sum / 3;
-	cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
+	cout << "商:" << n / m << " 餘數:" << n % m << endl;
 	return 0;
 }

99bcb8ffe8fb5686fabb8816fab7e5fdedebe7f6
Robert Hsu <roberthsu2003@gmail.com>
2021-08-10 14:15:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 int main() {
 	int chinese = 96;
 	int english = 75; 
@@ -8,8 +8,8 @@ int main() {
 	printf("你的英文是:%d\n",english);
 	printf("總分是%d\n",chinese+english);
 	*/
-	std::cout << "您的國文:" << chinese << "\n";
-	std::cout << "您的英文是:" << english << "\n";
-	std::cout << "總分是" << chinese+english << "\n";
+	cout << "您的國文:" << chinese << endl;
+	cout << "您的英文是:" << english << endl;
+	cout << "總分是" << chinese+english << endl;
 	return 0;
 }

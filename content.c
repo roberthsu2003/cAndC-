a92e0e03166cab672dfb5f194e403b4ca3832868
Robert Hsu <roberthsu2003@gmail.com>
2021-08-10 21:28:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	string password;
-	cout << "請輸入密碼:";
-	cin >> password;
-	//雙向選擇
-	if(password == "1234"){
-		cout << "密碼正確!歡迎光臨!\n";
-	}else{
-		cout << "密碼錯誤,請重新輸入\n";
-	}
-	cout << "程式結束\n";
-	return 0;
+	int num;
+	cout << "請輸入數字:";
+	cin >> num;
 
+	if(num%2 == 0){
+		cout << "輸入的是偶數";
+	}else{
+		cout << "輸入的是奇數";
+	}
+	
+	cout << "程式結束";
+	return 0;
 }

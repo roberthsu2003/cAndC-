07970ede1875cafdd0bf50ad39147c71d291699c
Robert Hsu <roberthsu2003@gmail.com>
2021-08-11 14:42:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int n,m;
-	cout << "請輸入被除數(整數):";
-	cin >> n;
-	cout << "請輸入除數(整數,不可以為0):";
-	cin >> m;
-	cout << "商:" << n/m << "餘數:"<< n % m<< endl;
+	int x = 1;
+	int y = ++x;
+	cout << "x=" << x <<",y="<< y << endl;
+
+	int n = 1;
+	int m = n++;
+
+	cout << "n=" << n <<",m="<< m << endl;
 	return 0;
 }

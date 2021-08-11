627a2f93c0dfa02deaaa08247e4c9f35655089e1
Robert Hsu <roberthsu2003@gmail.com>
2021-08-11 16:55:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int score;
-	cout << "請輸入英文分數:";
-	cin >> score;
-	if(score >= 60){
-		cout << "及格!" << endl;
+	int money;
+	int payMoney;
+	cout << "請輸入購買金額:";
+	cin >> money;
+	
+	if(money >= 100000){
+		payMoney = money * 0.8;
+	}else if(money >= 50000){
+		payMoney = money * 0.85;
+	}else if(money >= 30000){
+		payMoney = money * 0.9;
+	}else if (money >= 10000){
+		payMoney = money * 0.95;
 	}else{
-		cout << "不及格!" << endl;
+		payMoney = money;
 	}
-	cout << "程式結束";
+
+	cout << "實付金額是:" << payMoney << "元\n";
 	return 0;
+
 }

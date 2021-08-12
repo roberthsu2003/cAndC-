3f3851d028ecd885675b56b6bcfc712545d19353
Robert Hsu <roberthsu2003@gmail.com>
2021-08-12 20:13:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,32 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	int chinese;
-	int math;
-	int bonus;
-	printf("請輸入國文成績和數學成績(國文,數學):");
-	scanf("%d,%d",&chinese,&math);
+	char op;
+	int num1 = 20, num2 = 4;
+	cout << "num1=20, num2=4\n";
+	cout << "請輸入要執行的運算(+-*/):";
+	cin >> op;
+	switch(op){
+		case '+':
+		cout << "num1+num2=" << num1 + num2 << endl;
+		break;
 
-	if(chinese == 100 && math == 100){
-		bonus = 1000;
-	}else if (chinese == 100 || math == 100){
-		bonus = 500;
-	}else{
-		bonus = 0;
+		case '-':
+		cout << "num1-num2=" << num1 - num2 << endl;
+		break;
+
+		case '*':
+		cout << "num1*num2=" << num1 * num2 << endl;
+		break;
+
+		case '/':
+		cout << "num1/num2=" << num1 / num2 << endl;
+		break;
+
+		default:
+		cout << "無法運算\n";
 	}
 
-	printf("獎金是:%d\n",bonus);
 	return 0;
 }

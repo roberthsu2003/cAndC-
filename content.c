5a5e57a5ee48a9f0080dc16c53faca1eae25b702
Robert Hsu <roberthsu2003@gmail.com>
2021-08-12 16:36:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int n;
-	int sum=0;
-	for(int i=1; i<=7; i+=1){
-		if(i == 7){
-			cout << "請輸入星期日的支出:";
-		}else{
-			cout << "請輸入星期" << i << " 的支出:";
-		}
-		
-		cin >> n;
-		sum += n;
-	}
 
-	cout << "本星期的支出為:" << sum << "元\n";
+int main() {
+	for(int i=1; i<=9; i++){
+		for(int j=1; j<=9; j++){
+			cout << i << "*" << j << "=" << i*j << "\t";
+		}
+		cout << endl;
+	}
 	return 0;
 }

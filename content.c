1242dd832b7062971d7cab67b71fdadf67349c41
Robert Hsu <roberthsu2003@gmail.com>
2021-08-12 15:08:08 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,21 @@
 #include <iostream>
 
 int main() {
-	int chinese;
+  	int chinese;
 	int math;
 	int bonus;
 
 	printf("請輸入國文成績和數學成績(國文,數學):");
 	scanf("%d,%d",&chinese,&math);
-	if(chinese == 100){
-		//國文100
-		if(math == 100){
-			//國文100
-			//數學100
-			bonus = 1000;
-		}else{
-			//國文100
-			//數學不是100
-			bonus = 500;
-		}
+
+	if(chinese == 100 && math == 100){
+		bonus = 1000;
+	}else if(chinese == 100 || math == 100){
+		bonus = 500;
 	}else{
-		//國文不是100
-		if(math == 100){
-			//國文不是100
-			//數學100
-			bonus = 500;
-		}else{
-			bonus = 0;
-		}
+		bonus = 0;
 	}
 
-	printf("獎金是:%d\n",bonus);
+	printf("獎金是:%i\n",bonus);
+	return 0;
 }

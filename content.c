24672d4fb53b800e8df0ca00030dd478a9495564
Robert Hsu <roberthsu2003@gmail.com>
2021-08-12 21:21:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int sum=0;
-	for(int i=2; i<=10; i+=2){
-		sum += i;
-		cout << "第" << i/2 << "次迴圈的i=" << i << ",總和為" << sum << endl;
+	for(int i=1; i<=9; i++){
+		for(int j=1; j<=9; j++){
+			cout << i << "*" << j << "="  << i * j << "\t";			
+		}
+		cout << "\n";
 	}
 	return 0;
 }

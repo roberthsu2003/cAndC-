f95bb4b97126e066e1d2b789c7323207d0173e15
Robert Hsu <roberthsu2003@gmail.com>
2021-08-12 16:15:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,34 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	char op;
-	int num1 = 20, num2 = 4;
-	cout << "num1=20, num2=4" << endl;
-	cout << "請輸入要執行的運算(+-*/):";
-	cin >> op;
-	switch(op){
-		case '+':
-		cout << "num1+num2=" << num1+num2 << endl;
-		break;
-
-		case '-':
-		cout << "num1-num2=" << num1-num2 << endl;
-		break;
-
-		case '*':
-		cout << "num1*num2=" << num1*num2 << endl;
-		break;
-
-		case '/':
-		cout << "num1/num2=" << num1/num2 << endl;
-		break;
-
-		default:
-		cout << "無法執行運算" << endl;
-
-
+	int n;
+	int sum=0;
+	for(int i=1; i<=7; i+=1){
+		if(i == 7){
+			cout << "請輸入星期日的支出:";
+		}else{
+			cout << "請輸入星期" << i << " 的支出:";
+		}
+		
+		cin >> n;
+		sum += n;
 	}
 
-	cout << "程式結束";
+	cout << "本星期的支出為:" << sum << "元\n";
+	return 0;
 }

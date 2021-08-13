94d7528427add488b3df563dd9721a9e0cb57e7a
Robert Hsu <roberthsu2003@gmail.com>
2021-08-13 16:05:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,20 +4,32 @@ using namespace std;
 int main() {
 	srand(time(NULL));
 	int keyin;
+	int min = 1;
+	int max = 100;
 	int count = 0;
-	int guess = rand() % 10 + 1;
+	int guess = rand() % max + min;
 	cout << "============猜數字遊戲============:\n\n";
 	while(true){
-		cout << "猜數字:";
+		cout << "猜數字範圍" << min << "~" << max << ":"; 
 		cin >> keyin;
 		count += 1;
-		if(keyin == guess){
-			cout << "猜中了" << endl;
-			cout << "您猜了" << count << "次" << endl;
-			break;
+		if(keyin >= min && keyin <= max){
+			if(keyin == guess){
+				cout << "賓果!猜中了答案是" << guess <<endl;
+				cout << "您猜了" << count << "次" << endl;
+				break;
+			}else if(keyin > guess){
+				cout << "再小一點\n";
+				max = keyin;
+			}else if(keyin < guess){
+				cout << "再大一點\n";
+				min = keyin;
+			}
+			cout << "你已經猜了" << count << "次\n\n";
 		}else{
-			cout << "沒有猜中" << endl;
+			cout << "請輸入提示範圍內的數字!\n";
 		}
+		
 	}
 	cout << "遊戲結束" << endl;
 }

ca91999a24319aadb43cf1185f3d1838af50e242
Robert Hsu <roberthsu2003@gmail.com>
2021-08-13 16:45:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int scores[3];
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
+	int scores[] = {70, 80, 90};	
 	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
 	return 0;
 }

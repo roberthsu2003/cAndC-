d21a1bc6a92193a5bc1d7a25b2b31d6499e7183e
Robert Hsu <roberthsu2003@gmail.com>
2021-08-13 15:36:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
+#include <time.h>
 int main() {
-	int inputNum;
-	int sum=0;
+	srand(time(NULL));
+	int keyin;
+	int count = 0;
+	int guess = rand() % 10 + 1;
+	cout << "============猜數字遊戲============:\n\n";
 	while(true){
-		cout << "請輸入數值:";
-		cin >> inputNum;
-		if(inputNum < 0){
+		cout << "猜數字:";
+		cin >> keyin;
+		count += 1;
+		if(keyin == guess){
+			cout << "猜中了" << endl;
+			cout << "您猜了" << count << "次" << endl;
 			break;
+		}else{
+			cout << "沒有猜中" << endl;
 		}
-		//執行底下的一定是大於0的
-		if (inputNum % 2 == 1){
-			continue;
-		}
-		sum += inputNum;
 	}
-	cout << "所有輸入的正偶數的加總是:" << sum << endl;
-	return 0;
+	cout << "遊戲結束" << endl;
 }

4d99c45d1fddecc842b4448595b536d008efcafb
Robert Hsu <roberthsu2003@gmail.com>
2021-08-13 16:56:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int scores[] = {70, 80, 90};	
-	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
+	int scores[5];
+	int sum = 0;
+	float ave;
+
+	for(int i=0; i<5; i+=1){
+		cout << "請輸入第" << i+1 << "位學生的成績";
+		cin >> scores[i];
+	}
+
+	for(int i=0; i<5; i+=1){
+		sum += scores[i];
+	}
+
+	ave = sum / 5.0;
+
+	cout << "全班總成績為:" << sum << "分,平均為" << ave << "分\n";
 	return 0;
+
 }

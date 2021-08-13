a3f6e6029f784b3979517e351269942ae2e9a754
Robert Hsu <roberthsu2003@gmail.com>
2021-08-13 14:12:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,22 +2,13 @@
 using namespace std;
 
 int main() {
-	string password;
-	/*
-	cout << "請輸入密碼:";
-	cin >> password;
-
-	while(password != "1234"){
-		cout << "請輸入密碼:";
-		cin >> password;
+	int deposit=0, inputNum, num=0;
+	while(deposit < 30000){
+		num += 1;
+		cout << "請輸入第" << num << "個月份的存款:";
+		cin >> inputNum;
+		deposit += inputNum;
 	}
-	*/
-
-	do{
-		cout << "請輸入密碼:";
-		cin >> password;	
-	}while(password != "1234");
-
-	cout << "恭喜!你的密碼正確了!請進";
+	cout << "恭喜!已經存夠了，存了"<< num << "個月,共存了"<<deposit << endl;
 	return 0;
 }

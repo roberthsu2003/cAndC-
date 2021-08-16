5787699da3bb5ecf78cdfc9e73d83b54b1d17c67
Robert Hsu <roberthsu2003@gmail.com>
2021-08-16 15:59:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,5 @@
+tool.h
+
 void sorted(int array[],int nums){
 	int temp;
 	for(int i=0;i<nums-1;i+=1){
@@ -9,4 +11,40 @@ void sorted(int array[],int nums){
 			}
 		}
 	}
+}
+
+
+main.cpp
+
+#include <iostream>
+#include "tools.h"
+
+using namespace std;
+
+int main() {
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+	int array[nums];
+	int temp;
+
+	for(int i=0; i<nums; i+=1){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
+	}
+
+	cout << "排序前:\n";
+	for(int i=0; i<nums; i+=1){
+		cout << array[i] << " ";
+	}
+
+	//陣列排序
+	sorted(array,nums);
+	
+	cout << "\n";
+	cout << "排序後:\n";
+	for(int i=0; i<nums; i+=1){
+		cout << array[i] << " ";
+	}
+	return 0;
 }

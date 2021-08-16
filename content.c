7ce9853650b613b540ab08f369dcdd8efa6de3e8
Robert Hsu <roberthsu2003@gmail.com>
2021-08-16 15:01:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,14 @@
 #include <iostream>
 using namespace std;
 
+//沒有傳出值
+//沒有參數
+
+void sayHello(){
+	cout << "歡迎光臨!" << endl;
+}
+
 int main() {
-	int nums;
-	cout << "請輸入要排序的數值個數:";
-	cin >> nums;
-	int array[nums];
-	int temp;
-
-	for(int i=0; i<nums; i+=1){
-		cout << "請輸入第" << i+1 << "個數值:";
-		cin >> array[i];
-	}
-
-	cout << "排序前:\n";
-	for(int i=0; i<nums; i+=1){
-		cout << array[i] << " ";
-	}
-
-	//陣列排序
-	for(int i=0;i<nums-1;i+=1){
-		for(int j=i+1;j<nums;j+=1){
-			if(array[i] < array[j]){
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-	cout << "\n";
-	cout << "排序後:\n";
-	for(int i=0; i<nums; i+=1){
-		cout << array[i] << " ";
-	}
-	return 0;
+  sayHello();
+  return 0;
 }

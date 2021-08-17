8e912241d58158703f6b30e896f4fee2891a8150
Robert Hsu <roberthsu2003@gmail.com>
2021-08-17 21:22:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int scores[3];
+	//一次給值，只可以在建立陣列的時候
+	int scores[3] = {70,80,90};
+	/*
 	scores[0] = 70;
 	scores[1] = 80;
 	scores[2] = 90;
+	*/
 
 	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
 	return 0;

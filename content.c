bbe550b4e3368e83546ba0208375ac41ff66dea8
Robert Hsu <roberthsu2003@gmail.com>
2021-08-17 21:35:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,21 @@
 #include <iostream>
 using namespace std;
-int main() {
-	//一次給值，只可以在建立陣列的時候
-	int scores[3] = {70,80,90};
-	/*
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
-	*/
 
-	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
+int main() {
+	int nums = 5;
+	int scores[nums];
+	int sum=0;
+	float ave;
+	for(int i=0; i<nums; i+=1){
+		cout << "請輸入第" << i+1 << "位學生的成績";
+		cin >> scores[i];
+	}
+
+	for(int i=0; i<nums; i+=1){
+		sum += scores[i];
+	}
+
+	ave = (float)sum / nums;
+	cout << "全班總成績為:" << sum << "分,平均為" << ave << "分\n";
 	return 0;
 }

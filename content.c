aea63f63419c210b2e820c806ba1c29241865497
Robert Hsu <roberthsu2003@gmail.com>
2021-08-18 16:47:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -15,11 +15,15 @@ int main() {
 			students[i][j] = rand() % (max-min+1) + min;
 		}
 	}
-	cout << "國文\t英文\t數學\t自然\t地理" << endl;
+	cout << "國文\t英文\t數學\t自然\t地理\t總分\t平均" << endl;
 	for (int i=0; i<50; i+=1){
+		int sum=0;
 		for(int j=0; j<5; j+=1){
+			sum += students[i][j];
 			cout << students[i][j] << "\t\t";
 		}
+		cout << sum << "\t\t";
+		printf("%.2f",sum/5.0);
 		cout << endl;
 	}
 	return 0;

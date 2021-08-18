d8dd5ac6411d19d259edc25e492223030bca0862
Robert Hsu <roberthsu2003@gmail.com>
2021-08-18 16:12:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-	short s1;
-	int i1;
-	long long int lli;
-	int score[] = {78, 65, 92, 76, 99};
-	//string c++ 才有的型別
-	string name = "robert hsu";
-	cout << "short=" << sizeof(s1) << endl;
-	cout << "int=" << sizeof(i1) << endl;
-	cout << "long long int=" << sizeof(lli) << endl;
-	cout << "string=" << sizeof(name) << endl;
-	cout << "score陣列變數,佔的記憶體大小是" << sizeof(score)  << endl;
-	cout << "score陣列變數內,一個元素的大小是" << sizeof(score[0]) << endl;
-	cout << "score陣列變數的元素數量:" << sizeof(score) / sizeof(score[0]) << endl;
+	int students[50][5];
+	cout << "students佔的記憶體大小是:" << sizeof(students) << endl;
+
+	for (int i=0; i<50; i+=1){
+		for(int j=0; j<5; j+=1){
+			cout << "學生分數" << students[i][j] << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }

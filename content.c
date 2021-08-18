fbb74265bc91016b5acc5be3bfb3712c44687d97
Robert Hsu <roberthsu2003@gmail.com>
2021-08-18 16:29:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int main() {
+	srand(time(NULL));
+	int max = 100;
+	int min = 60;
 	int students[50][5];
 	cout << "students佔的記憶體大小是:" << sizeof(students) << endl;
 
+	//給學生亂數分數
 	for (int i=0; i<50; i+=1){
 		for(int j=0; j<5; j+=1){
-			cout << "學生分數" << students[i][j] << " ";
+			students[i][j] = rand() % (max-min+1) + min;
+		}
+	}
+
+	for (int i=0; i<50; i+=1){
+		for(int j=0; j<5; j+=1){
+			cout << students[i][j] << " ";
 		}
 		cout << endl;
 	}

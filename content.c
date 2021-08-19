a8556b296f24fd3ea655a6d0557013ac607f2a1e
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 21:37:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,29 @@
 #include <iostream>
 #include "tools.h"
+#include <time.h>
+
+using namespace std;
+
+int studentScore(){
+	//450~600
+	return rand() % (600 - 450 + 1) + 450;
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+	srand(time(NULL));
+	int nums;
+	cout << "請輸入班級學生的人數:";
+	cin >> nums;
+	int students[nums];
+	for(int i=0; i<nums; i++){
+		students[i] = studentScore();
+	}
+
+	sorted(students, nums, true);
+	displayArray(students, nums);
+
+	return 0;
+
+
+
 }

76ab7a65a5df3303fec1f3963c99f74321eb64dc
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 15:42:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n = 10;
-	int *p = &n;
-	cout << "n的值是" << n << endl;
+	//int n = 10;
+	int *p = new int(10);
+	//cout << "n的值是" << n << endl;
 	cout << "p的值是" << p << endl;
-	cout << "透過指標變數p,取得n內的值" << *p <<  endl;
+	cout << "透過指標變數p,取得指向址址內的值" << *p <<  endl;
 	return 0;
 }

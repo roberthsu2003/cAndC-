da2648c43c420f24045acc61f8b49311191240b3
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 15:16:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-	int n = 10;
-	double m = 5;
-	cout << "變數n的值=" << n << endl;
-	cout << "變數n的記憶體位址=" << &n << endl;
-	cout << "變數m的值=" << m << endl;
-	cout << "變數m的記憶體位址=" << &m << endl;
+	int n[] = {1, 2, 3, 4};
+	cout << "陣列n內放的記憶體位址是" << n << endl;
+	cout << "陣列元素n[0]的位址=" << &n[0] << endl;
+	cout << "陣列元素n[1]的位址=" << &n[1] << endl; 
+	cout << "陣列元素n[2]的位址=" << &n[2] << endl;
+	cout << "陣列元素n[3]的位址=" << &n[3] << endl;
 	return 0;
 }

1e8b796d2c35399f9ba261865d6ecc47be6682f8
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 16:51:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *n, int *m){
-	int temp = *n;
-	*n = *m;
-	*m = temp;
+void changeValue(int a[]){
+	for(int i=0; i<5; i++){
+		a[i] *= 5;
+	}
 }
 
+void changeValue1(int *a){
+	for(int i=0; i<5; i++){
+		a[i] *= 5;
+	}
+}
+
+
 int main() {
-	int a = 666, b = 777;
-	cout << "交換前 a=" << a << ",b=" << b << endl; 
-	swap(&a,&b);
-	cout << "交換後 a=" << a << ",b=" << b << endl; 
+	int array[] = {1, 2, 3, 4, 5};	
+	changeValue1(array);
+
+	for(int i=0; i<5; i++){
+		cout << array[i] << ",";
+	}
+	cout << endl;
 	return 0;
 }

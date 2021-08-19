0697b0f7908e45f80cfe607c5e6482156b647840
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 16:57:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,15 +8,23 @@ void changeValue(int a[]){
 }
 
 void changeValue1(int *a){
+	//使用陣列變數的操作法
 	for(int i=0; i<5; i++){
 		a[i] *= 5;
+	}
+}
+
+void changeValue2(int *a){
+	//使用指標變數的操作法
+	for(int i=0; i<5; i++){
+		*(a + i) *= 5;
 	}
 }
 
 
 int main() {
 	int array[] = {1, 2, 3, 4, 5};	
-	changeValue1(array);
+	changeValue2(array);
 
 	for(int i=0; i<5; i++){
 		cout << array[i] << ",";

8a4ce7d185626e55889c0498278d2309a954bd71
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 16:19:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
+
+void sub(int* n){
+	*n = 100;
+}
+
 int main() {
-	//int n = 10;
-	int *p = new int(10);
-	//cout << "n的值是" << n << endl;
-	cout << "p的值是" << p << endl;
-	cout << "透過指標變數p,取得指向址址內的值" << *p <<  endl;
-	return 0;
+  int a = 10;
+  sub(&a);
+  cout << "a變數的值是" << a << endl;
+  return 0;
 }

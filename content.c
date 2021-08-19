df3651a66ce52b6e74894d9114226ebb30830ded
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 20:00:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,21 @@
 #include <iostream>
 using namespace std;
+int add(int a, int b){
+	return a + b;
+}
+
+int multiply(int a, int b){
+	return a * b;
+}
 
 int main() {
-	int nums;
-	cout << "請輸入要排序的數值個數:";
-	cin >> nums;
-
-	int array[nums];
-	for(int i=0; i<nums; i+=1){
-		cout << "請輸入第" << i+1 << "個數值:";
-		cin >> array[i];
-	}
-
-	cout << "排序前:\n";
-	for(int i=0; i<nums; i+=1){
-		cout << array[i] << " ";
-	}
-
-	//泡沫排序
-	for(int i=0; i<nums-1; i+=1){
-		for(int j=i+1;j<nums; j+=1){
-			if(array[i] > array[j]){
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-	cout << endl;
-	cout << "排序後:\n";
-	for(int i=0; i<nums; i+=1){
-		cout << array[i] << " ";
-	}
-	return 0;
+	int first;
+	int second;
+	cout << "請輸入第一個數值:";
+	cin >> first;
+	cout << "請輸入第二個數值:";
+	cin >> second;
+	cout << first << "+" << second << "=" << add(first,second) << endl;
+	cout << first << "*" << second << "=" << multiply(first,second) << endl;
+  return 0;
 }

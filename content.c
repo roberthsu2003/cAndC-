46ca37741de3cb2e3d13321da204820fbe6b294d
Robert Hsu <roberthsu2003@gmail.com>
2021-08-19 15:33:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int n[] = {1, 2, 3, 4};
-	cout << "陣列n內放的記憶體位址是" << n << endl;
-	cout << "陣列元素n[0]的位址=" << &n[0] << endl;
-	cout << "陣列元素n[1]的位址=" << &n[1] << endl; 
-	cout << "陣列元素n[2]的位址=" << &n[2] << endl;
-	cout << "陣列元素n[3]的位址=" << &n[3] << endl;
+	int n = 10;
+	int *p = &n;
+	cout << "n的值是" << n << endl;
+	cout << "p的值是" << p << endl;
+	cout << "透過指標變數p,取得n內的值" << *p <<  endl;
 	return 0;
 }

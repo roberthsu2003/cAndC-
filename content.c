eb33a6f3f2f7531f0f0589fd56d64d0f67ddffe7
Robert Hsu <roberthsu2003@gmail.com>
2021-08-20 15:31:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,35 +1,18 @@
 #include <iostream>
-#include "data.h"
-
 using namespace std;
 
-int main() {
-	//建立3個學生
-	student students[3];
-	students[0].name = "David";
-	students[0].chinese = 87;
-	students[0].english = 97;
-	students[0].math = 78;
+typedef struct student{
+	string name;
+	int chinese;
+	int english;
+	int math;
+}student;
 
-	students[1].name = "Alice";
-	students[1].chinese = 76;
-	students[1].english = 97;
-	students[1].math = 73;
-
-	students[2].name = "Jenny";
-	students[2].chinese = 91;
-	students[2].english = 92;
-	students[2].math = 65;
-
-	for(int i=0;i<3;i+=1){
-		cout << "姓名:" << students[i].name << endl;
-		cout << "國文:" << students[i].chinese << endl;
-		cout << "英文:" << students[i].english << endl;
-		cout << "數學:" << students[i].math << endl;
-		int sum = students[i].chinese + students[i].english + students[i].math;
-		cout << "總分:" << sum << endl;
-		printf("%.2f\n\n\n",sum/3.0);
+void studentSorted(student s[],int nums){
+	//泡沫排序法
+	for(int i=0; i < nums-1; i++){
+		for(int j=i+1; j < nums; j++){
+			
+		}
 	}
-	return 0;
-
 }

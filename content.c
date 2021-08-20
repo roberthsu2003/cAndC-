6b2131ffae7808f3da329633e3cc8f9a8e8bc229
Robert Hsu <roberthsu2003@gmail.com>
2021-08-20 14:45:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,10 @@
-main.cpp
-
 #include <iostream>
 #include "data.h"
 
 using namespace std;
 
 int main() {
-	student david;
-	david.name = "David";
-	david.chinese = 78;
-	david.english = 65;
-	david.math = 98;
+	student david = {"David",75, 86, 97};	
 
 	cout << "學生姓名是:" << david.name << endl;
 	cout << "國文分數是:" << david.chinese << endl;
@@ -18,16 +12,3 @@ int main() {
 	cout << "數學分數是是:" << david.math << endl;
 	return 0;
 }
-
-
-data.h
-
-#include <iostream>
-using namespace std;
-
-typedef struct student{
-	string name;
-	int chinese;
-	int english;
-	int math;
-}student;

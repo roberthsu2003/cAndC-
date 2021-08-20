3ef14d0641be3dc78dcf1299cde70984cd16bb7f
Robert Hsu <roberthsu2003@gmail.com>
2021-08-20 16:02:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -23,19 +23,26 @@ int main() {
 	students[2].english = 92;
 	students[2].math = 65;
 
+	//計算總分和平均
+	for(int i=0;i<3;i+=1){
+		students[i].sum = students[i].chinese + students[i].english + students[i].math;
+		students[i].average = students[i].sum / 3.0;
+	}
+	//排名次
+	
+	//輸出
 	for(int i=0;i<3;i+=1){
 		cout << "姓名:" << students[i].name << endl;
 		cout << "國文:" << students[i].chinese << endl;
 		cout << "英文:" << students[i].english << endl;
-		cout << "數學:" << students[i].math << endl;
-		students[i].sum = students[i].chinese + students[i].english + students[i].math;
-		cout << "總分:" << students[i].sum << endl;
-		students[i].average = students[i].sum / 3.0;
+		cout << "數學:" << students[i].math << endl;	
+		cout << "總分:" << students[i].sum << endl;		
 		printf("平均:%.2f\n\n\n",students[i].average);
 	}
 	return 0;
 
 }
+
 
 data.h
 

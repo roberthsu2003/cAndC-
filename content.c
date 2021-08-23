ba0fa25c813749a622e986d37f5a306e0c29b325
Robert Hsu <roberthsu2003@gmail.com>
2021-08-23 16:39:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,7 +6,7 @@ main.cpp
 using namespace std;
 
 int main() {
-	cout << "100+200=" << add(100,200) << endl;
+	guessNumber();
 	return 0;
 }
 
@@ -15,9 +15,56 @@ tools.h
 #define _TOOLS_H_
 //定義function的原型
 int add(int,int);
+
+//猜數字遊戲
+void guessNumber();
 #endif
 
 tools.cpp
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
 int add(int a,int b){
 	return a+b;
+}
+
+void guessNumber(){
+	int guess;
+	int min = 1;
+	int max = 100;
+	int keyin;
+	int count = 0;
+	srand(time(NULL));
+	guess = rand() % max + min;
+	cout << "===============猜數字遊戲=================:\n\n";
+	do{
+		cout << "猜數字範圍" << min << "~" << max << ":";
+		cin >> keyin;
+		count++;
+
+		if(keyin >=min && keyin <=max){
+			if(keyin == guess){
+				cout << "賓果!猜對了,答案是" << guess << endl;
+				cout << "您猜了" << count << "次\n\n";
+				break;
+			}else if (keyin > guess){
+				max = keyin - 1 ;
+				cout << "再小一點!";
+			}else if (keyin < guess){
+				min = keyin + 1;
+				cout << "再大一點!";
+			}
+			cout << "您猜了" << count << "次\n\n";
+		}else{
+			cout << "請輸入提示範圍內的數字! \n";
+		}
+
+
+	}while(true);
+	
+	cout << "遊戲結束\n";
 }

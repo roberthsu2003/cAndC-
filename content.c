e7310ae247feb4e7d21c8eb79b279cfdc8597935
Robert Hsu <roberthsu2003@gmail.com>
2021-08-23 15:35:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,13 @@
 #include <iostream>
-#define SQUARE(x) (x) * (x)
 
-using namespace std;
+#define MAC
+
+#ifdef MAC
+#define OS "MAC_OS_VERSION_10_0"
+#else 
+#define OS "Window10"
+#endif
+
 int main() {
-	int s, result;
-	cout << "請輸入數字:";
-	cin >> s;
-	result = SQUARE(s);
-	cout << s << "的平方值為:" << result << endl;
-	result = SQUARE(s+1);
-	cout << s+1 << "的平方值為:" << result << endl;
-	return 0;
+  std::cout << OS << std::endl;
 }

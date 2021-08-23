69660986f7fa4f1b130e6e8d28d7fb4c32d38c95
Robert Hsu <roberthsu2003@gmail.com>
2021-08-23 14:40:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,54 @@
 #include <iostream>
 using namespace std;
-typedef enum direction{
-	North=10,
-	South,
-	East,
-	West
-}direction;
+
+typedef enum day{
+	monday=1,
+	tuesday,
+	wednesday,
+	thursday,
+	friday,
+	saturday,
+	sunday
+}day;
 
 int main() {
-	direction desc;
-	desc = East;
-	//檢查列舉型別內儲存的值
-	switch (desc){
-		case North:
-		cout << "指向北方" << endl;
+	day today;
+	cout << "請輸入今天是(星期1~日)(請輸入1~7)";	
+	scanf("%d", &today);
+	
+	switch(today){
+		case monday:
+		cout << "今天星期一" << endl;
 		break;
 
-		case South:
-		cout << "指向南方" << endl;
+		case tuesday:
+		cout << "今天星期二" << endl;
 		break;
 
-		case West:
-		cout << "指向西方" << endl;
+		case wednesday:
+		cout << "今天星期三" << endl;
 		break;
 
-		case East:
-		cout << "指向東方" << endl;
+		case thursday:
+		cout << "今天星期四" << endl;
+		break;
+
+		case friday:
+		cout << "今天星期五" << endl;
+		break;
+
+		case saturday:
+		cout << "今天星期六" << endl;
+		break;
+
+		case sunday:
+		cout << "今天星期日" << endl;
 		break;
 
 		default:
-		cout << "不是正確的值";
+		cout << "輸入錯誤" << endl;
+		break;
+
 	}
 
-	return 0;
 }

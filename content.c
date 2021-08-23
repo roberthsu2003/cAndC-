801dd803feb60584a6b9011bc5048ace71231814
Robert Hsu <roberthsu2003@gmail.com>
2021-08-23 14:24:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 typedef enum direction{
-	North,
+	North=10,
 	South,
 	East,
 	West
@@ -9,7 +9,28 @@ typedef enum direction{
 
 int main() {
 	direction desc;
-	desc = North;
-	cout << "desc內的值是:" << desc << endl;
+	desc = East;
+	//檢查列舉型別內儲存的值
+	switch (desc){
+		case North:
+		cout << "指向北方" << endl;
+		break;
+
+		case South:
+		cout << "指向南方" << endl;
+		break;
+
+		case West:
+		cout << "指向西方" << endl;
+		break;
+
+		case East:
+		cout << "指向東方" << endl;
+		break;
+
+		default:
+		cout << "不是正確的值";
+	}
+
 	return 0;
 }

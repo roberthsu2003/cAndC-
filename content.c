41bbd51230fbeb40fc90a948316f1cfd1ad42322
Robert Hsu <roberthsu2003@gmail.com>
2021-08-23 15:24:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,14 @@
 #include <iostream>
-#define RATE 31.765
-
+#define SQUARE(x) (x) * (x)
 
 using namespace std;
-
 int main() {
-	int us;
-	float nt;
-	cout << "請輸入您有多少美金:";
-	cin >> us;
-	nt = us * RATE;
-	cout << "您有" << nt << "台幣" << endl;
+	int s, result;
+	cout << "請輸入數字:";
+	cin >> s;
+	result = SQUARE(s);
+	cout << s << "的平方值為:" << result << endl;
+	result = SQUARE(s+1);
+	cout << s+1 << "的平方值為:" << result << endl;
 	return 0;
 }

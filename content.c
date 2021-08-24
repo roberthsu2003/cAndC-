b584695d523693f6d1ed8c7f49b11d962322f4fc
Robert Hsu <roberthsu2003@gmail.com>
2021-08-24 16:47:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,21 +2,25 @@ main.cpp
 
 #include <iostream>
 #include "student.h"
-#include "time.h"
+
 
 int main() {
-	srand(time(NULL));
+	Student::begin();
 	Student stu1("robert");
 	cout << stu1.name << endl;
 	cout << stu1.chinese << endl;
 	cout << stu1.english << endl;
 	cout << stu1.math << endl;
+	cout << stu1.sum() << endl;
+	cout << stu1.average() << endl;
 
 	Student stu2("jenny");
 	cout << stu2.name << endl;
 	cout << stu2.chinese << endl;
 	cout << stu2.english << endl;
 	cout << stu2.math << endl;
+	cout << stu2.sum() << endl;
+	cout << stu2.average() << endl;
 	return 0;
 }
 
@@ -27,6 +31,7 @@ student.h
 #define _STUDENT_H_
 
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -43,11 +48,17 @@ class Student{
 			math = m;
 		}
 		//自訂的建構式(1個參數)
-		Student(string n){						
+		Student(string n){					
 			name = n;
 			chinese = rand() % 41 + 60;
 			english = rand() % 41 + 60;
 			math = rand() % 41 + 60;
+		}
+
+		//type method
+		//使用Student時，必需先執行Student::begin()
+		static void begin(){
+			srand(time(NULL));
 		}
 		
 		string name;

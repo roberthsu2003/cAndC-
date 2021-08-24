649c9554ea8f1edf4847c1ae4839ac6ce78f5cbe
Robert Hsu <roberthsu2003@gmail.com>
2021-08-24 14:51:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,8 +3,14 @@ using namespace std;
 
 class Person{
 	public:
+		//實體欄位
 		string name;
 		int tall;
+
+		//實體方法
+		void walk(int var){
+			cout << name << "每小時可以跑" << var << "公里" << endl;
+		}
 };
 
 int main() {
@@ -13,18 +19,21 @@ int main() {
 	david.tall = 180;
 
 	cout << david.name << " 身高=" << david.tall << endl;
+	david.walk(10);
 
 	Person robert;
 	robert.name = "Robert";
 	robert.tall = 170;
 
 	cout << robert.name << " 身高=" << robert.tall << endl;
+	robert.walk(15);
 
 	Person* john = new Person();
 	john->name = "John";
 	john->tall = 175;
 
 	cout << john->name << " 身高=" << john -> tall << endl;
+	john->walk(20);
 
 	return 0;
 }

6d225a947315aed62e389c9f3a961ff2b8fb0fb5
Robert Hsu <roberthsu2003@gmail.com>
2021-08-24 16:13:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,39 @@
+main.cpp
+
+#include <iostream>
+#include "student.h"
+
+int main() {
+	Student stu1("robert",78, 98, 65);
+	cout << stu1.name << endl;
+	cout << stu1.chinese << endl;
+	cout << stu1.english << endl;
+	cout << stu1.math << endl;
+	return 0;
+}
+
+
 student.h
 
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 
-#incldue <iostream>
+#include <iostream>
 using namespace std;
 
 class Student{
 	public:
+		Student(string n,int c, int e, int m){
+			name = n;
+			chinese = c;
+			english = e;
+			math = m;
+		}
 		string name;
 		int chinese;
 		int english;
 		int math;
-		
+
 		int sum(){
 			return chinese + english + math;
 		}
@@ -20,5 +41,5 @@ class Student{
 		float average(){
 			return sum() / 3.0;
 		}
-}
+};
 #endif

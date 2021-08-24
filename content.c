d6d1dc30c26e815b7659a59ff428d853e77ef0c2
Robert Hsu <roberthsu2003@gmail.com>
2021-08-24 16:23:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,7 @@ main.cpp
 #include "student.h"
 
 int main() {
-	Student stu1("robert",78, 98, 65);
+	Student stu1("robert");
 	cout << stu1.name << endl;
 	cout << stu1.chinese << endl;
 	cout << stu1.english << endl;
@@ -23,12 +23,21 @@ using namespace std;
 
 class Student{
 	public:
+		//自訂的建構式(4個參數)
 		Student(string n,int c, int e, int m){
 			name = n;
 			chinese = c;
 			english = e;
 			math = m;
 		}
+		//自訂的建構式(1個參數)
+		Student(string n){
+			name = n;
+			chinese = 60;
+			english = 60;
+			math = 60;
+		}
+
 		string name;
 		int chinese;
 		int english;

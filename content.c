7bd38493c2834293c88a67234bbaf2558dea68e1
Robert Hsu <roberthsu2003@gmail.com>
2021-08-24 14:24:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,79 +1,24 @@
-main.cpp
-
 #include <iostream>
-#include "tools.h"
-
 using namespace std;
+
+class Person{
+	public:
+		string name;
+		int tall;
+};
 
 int main() {
-	bool playagain;
-	do{
-		guessNumber();
-		string inputString;
-		cout << "請問還要繼續玩嗎?(輸入y或n)";
-		cin >> inputString;
-		playagain = (inputString == "y") ? true : false ;
-	}while(playagain);
-	
+	Person david;
+	david.name = "David";
+	david.tall = 180;
+
+	cout << david.name << " 身高=" << david.tall << endl;
+
+	Person robert;
+	robert.name = "Robert";
+	robert.tall = 170;
+
+	cout << robert.name << " 身高=" << robert.tall << endl;
+
 	return 0;
-}
-
-tools.h
-#ifndef _TOOLS_H_
-#define _TOOLS_H_
-//定義function的原型
-int add(int,int);
-
-//猜數字遊戲
-void guessNumber();
-#endif
-
-tools.cpp
-
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-
-using namespace std;
-
-int add(int a,int b){
-	return a+b;
-}
-
-void guessNumber(){
-	int guess;
-	int min = 1;
-	int max = 100;
-	int keyin;
-	int count = 0;
-	srand(time(NULL));
-	guess = rand() % max + min;
-	cout << guess << endl;
-	cout << "===============猜數字遊戲=================:\n\n";
-	do{
-		cout << "猜數字範圍" << min << "~" << max << ":";
-		cin >> keyin;
-		count++;
-
-		if(keyin >=min && keyin <=max){
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-				cout << "您猜了" << count << "次\n\n";
-				break;
-			}else if (keyin > guess){
-				max = keyin - 1 ;
-				cout << "再小一點!";
-			}else if (keyin < guess){
-				min = keyin + 1;
-				cout << "再大一點!";
-			}
-			cout << "您猜了" << count << "次\n\n";
-		}else{
-			cout << "請輸入提示範圍內的數字! \n";
-		}
-
-
-	}while(true);
-	
-	cout << "遊戲結束\n";
 }

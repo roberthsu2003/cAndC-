1f84ed7b2c54a651a6945e61c089d06e58639a78
Robert Hsu <roberthsu2003@gmail.com>
2021-08-25 15:05:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,8 +3,20 @@
 using namespace std;
 
 int main() {
+	int nums;
 	Student::begin();
-	Student stu1("robert");
-	cout << stu1.name << ",總分為" << stu1.sum() << ",平均為" << stu1.average() << endl;
+	cout << "請輸入學生人數:";
+	cin >> nums;
+	Student students[nums];
+	for(int i=0; i<nums; i+=1){
+		students[i] = Student("stu"+to_string(i+1));
+		cout << "學生姓名:" << students[i].name << endl;
+		cout << "國文分數:" << students[i].chinese << endl;
+		cout << "數學分數:" << students[i].math << endl;
+		cout << "英文分數:" << students[i].english << endl;
+		cout << "總分:" << students[i].sum() << endl;
+		cout << "平均:" << students[i].average() << endl;
+		cout << "=====================" << endl;
+	}
 	return 0;
 }

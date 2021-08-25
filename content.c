54994acd49b15dae14cb1d5e06cc47e768d1228b
Robert Hsu <roberthsu2003@gmail.com>
2021-08-25 14:45:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,34 +1,10 @@
 #include <iostream>
-#define PI 3.14159
-
-class Circle{
-	public:
-		//自訂的建構式
-		Circle(int r){
-			radius = r;
-		}
-		int radius;
-		float area(){
-			return PI * radius * radius;
-		}
-};
-
-float getCirceArea(int radius){
-	return PI * radius * radius;
-}
-
+#include "student.h"
 using namespace std;
 
 int main() {
-	Circle c1(10);
-	cout << "半徑是:" << c1.radius << ",面積是:" << c1.area() << endl;
-
-	Circle c2(20);	
-	cout << "半徑是:" << c2.radius << ",面積是:" << c2.area() << endl;
-
-	int radius;
-	cout << "請輸入半徑:";
-	cin >> radius;
-	cout << "半徑是:" << radius << ",面積是:" << getCirceArea(radius) << endl;
-	return 0; 
+	Student::begin();
+	Student stu1("robert");
+	cout << stu1.name << ",總分為" << stu1.sum() << ",平均為" << stu1.average() << endl;
+	return 0;
 }

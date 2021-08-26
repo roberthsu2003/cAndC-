cfc8641de0a414416b19913d2f2f44f53b4ca188
Robert Hsu <roberthsu2003@gmail.com>
2021-08-26 20:10:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,16 @@
 #include <iostream>
-
 using namespace std;
 int main() {
-	//c++的輸入輸出
-	int math;
-	cout << "請輸入數學分數:";
-	cin >> math;
-	cout << "您的數學分數是" << math << endl;
+	int scores[5]; //建立陣列
+	cout << "請輸入5位學生的分數" << endl;
+	//給每個元素元素值
+	for(int i=0; i<5;i+=1){
+		cout << "請輸入第" << i+1 << "學生的分數:";
+		cin >> scores[i];
+	}
+	//輸出元素值
+	for(int i=0; i<5; i+=1){
+		cout << "第" << i+1 << "位學生的分數是:" << scores[i] << endl;
+	}
 	return 0;
 }

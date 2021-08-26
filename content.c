46f6bdb22fb5e978f6a65c852003e82daa9b0e2e
Robert Hsu <roberthsu2003@gmail.com>
2021-08-26 21:52:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -18,3 +18,36 @@ int main() {
 	
 }
 
+lot.h
+
+#include <iostream>
+#include "time.h"
+
+using namespace std;
+//要執行lot()時,必需先執行一次begin()
+void begin(){
+	srand(time(NULL));
+}
+
+void lot(){
+	
+	int lot[49];
+	int num = 7;
+	int maxIndex = 48;
+	int choose[num];
+	for(int i=0; i<49; i+=1){
+		lot[i] = i+1;
+	}
+	
+	for(int i=0; i<num; i+=1){
+		int randIndex = rand() % (maxIndex+1);
+		choose[i] = lot[randIndex];
+		lot[randIndex] = lot[maxIndex];
+		maxIndex -= 1;
+	}
+
+	for(int i=0; i<num-1; i++){
+		cout << choose[i] << " ";
+	}
+	cout << "特別號" << choose[num-1] << endl;
+}

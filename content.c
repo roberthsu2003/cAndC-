33b72064d2cfbc9aec6ea6cb9db4ccacd2430b57
Robert Hsu <roberthsu2003@gmail.com>
2021-08-31 19:59:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main() {
-	int n = 10;
-	int* p = &n; //宣告指標變數p
-	*p = 20; //使用取值運算子
-	cout << "n的值是" << n << endl;
+	int a=666, b=777;
+	cout << "2數值交換" << endl;
+	int temp = a;
+	a = b;
+	b = temp;
+
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
 	return 0;
 }

cf353ca4067f5a65a5687770b9c78bb10609ec3c
Robert Hsu <roberthsu2003@gmail.com>
2021-08-31 21:05:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void addData(int s[][5],int nums){
+	for(int i=0; i<nums; i++){
+		for(int j=0; j<5; j++){
+			s[i][j] = 50;
+		}
+	}
+}
+
 int main() {
 	int nums;
 	cout << "請輸入學生的數量:";
 	cin >> nums;
 	int students[nums][5];
+	addData(students, nums);
+	for(int i=0; i<nums; i++){
+		for(int j=0; j<5; j++){
+			cout<< students[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
+	

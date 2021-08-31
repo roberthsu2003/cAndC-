3eb04e9d0308959089cf215a1d64d8a83a3c417a
Robert Hsu <roberthsu2003@gmail.com>
2021-08-31 20:34:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *n, int *m){
-	int temp = *n;
-	*n = *m;
-	*m = temp;
+void addThree(int s[],int nums){
+	for(int i=0; i<nums; i+=1){
+		s[i] += 3;
+		if(s[i] > 100){
+			s[i] = 100;
+		}
+	}
 }
 
 int main() {
-	int a=666, b=777;
-	cout << "2數值交換" << endl;
-	swap(&a, &b);
-
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
+	int scores[] = {75, 89, 69, 92, 98};
+	addThree(scores, 5);
+	for(int i=0; i<5; i+=1){
+		cout << scores[i] << endl;
+	}
 	return 0;
 }

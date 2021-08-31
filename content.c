b898d8906ceaf8978d4d7f570839ee5f034871d9
Robert Hsu <roberthsu2003@gmail.com>
2021-08-31 19:49:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,9 @@
 using namespace std;
 
 int main() {
-	int n[3] = {10, 20 ,30};
-	cout << "陣列變數內的記憶體位址是" << n << endl; 
-	cout << "n[0]的值是" << n[0] << endl;
-	cout << "&n[0]的位址是" << &n[0] << endl;
-	cout << "&n[1]的位址是" << &n[1] << endl;
-	cout << "&n[2]的位址是" << &n[2] << endl;
-	cout << "n+0的位址是" << n+0 << endl;
-	cout << "n+1的位址是" << n+1 << endl;
-	cout << "n+2的位址是" << n+2 << endl;
+	int n = 10;
+	int* p = &n; //宣告指標變數p
+	*p = 20; //使用取值運算子
+	cout << "n的值是" << n << endl;
 	return 0;
 }

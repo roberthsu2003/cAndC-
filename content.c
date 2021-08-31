8d8046fb96c8b32608512646aa87e735cd3ce872
Robert Hsu <roberthsu2003@gmail.com>
2021-08-31 20:52:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void addThree(int s[],int nums){
-	for(int i=0; i<nums; i+=1){
-		s[i] += 3;
-		if(s[i] > 100){
-			s[i] = 100;
-		}
-	}
-}
-
 int main() {
-	int scores[] = {75, 89, 69, 92, 98};
-	addThree(scores, 5);
-	for(int i=0; i<5; i+=1){
-		cout << scores[i] << endl;
-	}
-	return 0;
+	int nums;
+	cout << "請輸入學生的數量:";
+	cin >> nums;
+	int students[nums][5];
 }

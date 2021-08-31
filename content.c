5c91340416a94ea6e351a9cf3ec1a8d3bf284e0b
Robert Hsu <roberthsu2003@gmail.com>
2021-08-31 21:13:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 void addData(int s[][5],int nums){
 	for(int i=0; i<nums; i++){
 		for(int j=0; j<5; j++){
-			s[i][j] = 50;
+			s[i][j] = rand() % (100-50+1) + 50;
 		}
 	}
 }
 
 int main() {
+	srand(time(NULL));
 	int nums;
 	cout << "請輸入學生的數量:";
 	cin >> nums;

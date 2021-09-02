435a9d498b3cc6adc5f9abdd46e681a6b0d03823
Robert Hsu <roberthsu2003@gmail.com>
2021-09-02 19:46:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,32 +1,21 @@
 #include <iostream>
 using namespace std;
 
-//定義一個結構
 typedef struct student{
-	int id;
 	string name;
-	int chinese, math, english;
+	int score;
 }Student;
 
-
 int main() {
-	//建立一個結構變數
-	Student david = {99001, "David", 75, 87, 98};
-	
+	Student stu[3];
+	stu[0] = {"robert", 94};
+	stu[1] = {"david", 91};
+	stu[2] = {"alice", 94};
 
-	cout << "學生id" << david.id << endl;
-	cout << "學生姓名" << david.name << endl;
-	cout << "國文" << david.chinese << endl;
-	cout << "英文" << david.english << endl;
-	cout << "數學" << david.math << endl;
-
-	cout << "=======================" << endl;
-	Student robert = {99002, "Robert", 76, 92, 93};	
-
-	cout << "學生id" << robert.id << endl;
-	cout << "學生姓名" << robert.name << endl;
-	cout << "國文" << robert.chinese << endl;
-	cout << "英文" << robert.english << endl;
-	cout << "數學" << robert.math << endl;
-	return 0;
+	for(int i=0; i<3; i+=1){
+		Student student = stu[i];
+		cout << "學生姓名:" << student.name << endl;
+		cout << "學生分數:" << student.score << endl;
+		cout << "================" << endl;
+	}
 }

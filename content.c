3f47600a63fa9448af8cc1bfbb1c617ad897fe8e
Robert Hsu <roberthsu2003@gmail.com>
2021-09-02 19:19:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,64 +1,41 @@
-main.cpp
-
 #include <iostream>
-#include <time.h>
-#include "students.h"
 using namespace std;
 
-
-
+//定義一個結構
+struct student{
+	int id;
+	string name;
+	int chinese, math, english;
+};
 
 
 int main() {
-	srand(time(NULL));
-	int nums;
-	cout << "請輸入學生的數量:";
-	cin >> nums;
-	int students[nums][5];
-	addData(students, nums);
-	displayScores(students, nums);	
-}
-	
+	//建立一個結構變數
+	struct student david;
+	david.id = 99001;
+	david.name = "David";
+	david.chinese = 75;
+	david.english = 98;
+	david.math = 87;
 
-students.h
+	cout << "學生id" << david.id << endl;
+	cout << "學生姓名" << david.name << endl;
+	cout << "國文" << david.chinese << endl;
+	cout << "英文" << david.english << endl;
+	cout << "數學" << david.math << endl;
 
+	cout << "=======================" << endl;
+	struct student robert;
+	robert.id = 99002;
+	robert.name = "Robert";
+	robert.chinese = 76;
+	robert.english = 93;
+	robert.math = 92;
 
-
-void addData(int s[][5],int nums);
-/*----------------*/
-/*這是將陣列填滿亂數資料*/
-/*----------------*/
-
-
-
-void displayScores(int s[][5], int nums);
-/*----------------*/
-/*這是將陣列資料輸出*/
-/*----------------*/
-
-
-students.cpp
-#include <iostream>
-using namespace std;
-
-void addData(int s[][5],int nums){
-	for(int i=0; i<nums; i++){
-		for(int j=0; j<5; j++){
-			s[i][j] = rand() % (100-50+1) + 50;
-		}
-	}
-}
-
-void displayScores(int s[][5], int nums){
-	cout << "國文\t英文\t數學\t歷史\t地理\t總分\t平均" << endl;
-	for(int i=0; i<nums; i+=1){
-		int total = 0;
-		for(int j=0; j<5; j+=1){
-			cout << s[i][j] << "\t\t";
-			total += s[i][j];
-		}
-		cout << total << "\t\t";
-		cout << total/5.0 << endl;
-		
-	}
+	cout << "學生id" << robert.id << endl;
+	cout << "學生姓名" << robert.name << endl;
+	cout << "國文" << robert.chinese << endl;
+	cout << "英文" << robert.english << endl;
+	cout << "數學" << robert.math << endl;
+	return 0;
 }

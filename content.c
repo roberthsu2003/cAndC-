060649555b5a2a123e48273555bd4cb37714235b
Robert Hsu <roberthsu2003@gmail.com>
2021-09-02 21:05:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,28 @@ main.cpp
 
 #include <iostream>
 #include "student.h"
-
+#include "time.h"
+using namespace std;
 int main() {
-	Student stu1;
+	srand(time(NULL));
+	int studentCount = 50;
+	Student students[studentCount];
+	for(int i=0; i< studentCount; i+=1){
+		Student s;
+		s.name = "stu" + to_string(i+1);
+		s.chinese = rand() % (100 - 50 + 1) + 50;
+		s.english = rand() % (100 - 50 + 1) + 50;
+		s.math= rand() % (100 - 50 + 1) + 50;
+		students[i] = s;
+	}
+
+	for(int i=0; i< studentCount; i+=1){
+		Student s = students[i];
+		cout << s.name << endl;
+		cout << s.chinese << endl;
+		cout << s.english << endl;
+		cout << s.math << endl;
+	}
 }
 
 

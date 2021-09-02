e4afe4b0cc7f29b529dbafa438d5cd92fc51101e
Robert Hsu <roberthsu2003@gmail.com>
2021-09-02 20:51:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,21 @@
+main.cpp
+
+#include <iostream>
+#include "student.h"
+
+int main() {
+	Student stu1;
+}
+
+
+student.h
+
 #include <iostream>
 using namespace std;
 
-typedef struct Rectangle{
-	int width;
-	int height;
-}Rectangle;
-
-int main() {
-	Rectangle rec;
-	cout << "請輸入width:";
-	cin >> rec.width;
-	cout << "請輸入height:";
-	cin >> rec.height;
-
-	cout << "矩形的面積是" << rec.width * rec.height << endl;
-	return 0;
-}
+typedef struct student{
+	string name;
+	int chinese;
+	int english;
+	int math;
+}Student;

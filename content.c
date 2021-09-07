c07f9b3ccc8597e2efa1bfd1d89b68579a747612
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 21:19:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,29 @@
+main.cpp
+
 #include <iostream>
+#include "person.h"
 
-class Person{
+using namespace std;
 
-};
 
 int main() {
 	Person p1;
+	p1.name = "David";
+	cout << "p1的name是" << p1.name << endl;
 }
+
+
+person.h
+
+#ifndef _PERSON_H_
+#define _PERSON_H_
+
+#include <iostream>
+using namespace std;
+class Person{
+	public:
+	string name;
+};
+
+#endif
+

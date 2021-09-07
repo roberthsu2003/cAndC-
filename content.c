cf10c2f19e4c61dbaf68d0dafadcfe118b509620
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 19:45:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,11 @@
 #include <iostream>
-typedef enum direction{
-	North,
-	South,
-	East,
-	West
-}Direction;
-
 using namespace std;
+#define SQUARE(x) x * x
 
 int main() {
-	Direction direct = West;
-	cout << "方向是:";
-
-	switch(direct){
-		case North:
-		cout << "北方" << endl;
-		break;
-
-		case South:
-		cout << "南方" << endl;
-		break;
-
-		case East:
-		cout << "東方" << endl;
-		break;
-
-		case West:
-		cout << "西方" << endl;
-		break;
-
-		default:
-		cout << "無資訊" << endl;
-	}
-	return 0;
+	int s, result;
+	cout << "請輸入數字:";
+	cin >> s;
+	result = SQUARE(s);
+	cout << s << "的平方為:" << result << endl;
 }

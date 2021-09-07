0034a0c291874b0b4c728b5890280f1d3687d133
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 21:34:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -12,6 +12,7 @@ int main() {
 	p1.tall = 183;
 	cout << "p1的name是" << p1.name << endl;
 	cout << "p1的身高是" << p1.tall << endl;
+	p1.walk(20);
 
 
 	Person p2;
@@ -19,8 +20,11 @@ int main() {
 	p2.tall = 178;
 	cout << "p2的name是" << p2.name << endl;
 	cout << "p2的身高是" << p2.tall << endl;
+	p2.walk(25);
+	
 	return 0;
 }
+
 
 person.h
 
@@ -33,6 +37,9 @@ class Person{
 	public:
 	string name;
 	int tall;
+	void walk(int var){
+		cout << name << "每小時可以跑" << var << "公里" << endl;
+	}
 };
 
 #endif

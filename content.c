907e19cd7636e9852b06840b23809ab1986aa2c0
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 21:26:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,9 +9,18 @@ using namespace std;
 int main() {
 	Person p1;
 	p1.name = "David";
+	p1.tall = 183;
 	cout << "p1的name是" << p1.name << endl;
-}
+	cout << "p1的身高是" << p1.tall << endl;
 
+
+	Person p2;
+	p2.name = "Robert";
+	p2.tall = 178;
+	cout << "p2的name是" << p2.name << endl;
+	cout << "p2的身高是" << p2.tall << endl;
+	return 0;
+}
 
 person.h
 
@@ -23,7 +32,7 @@ using namespace std;
 class Person{
 	public:
 	string name;
+	int tall;
 };
 
 #endif
-

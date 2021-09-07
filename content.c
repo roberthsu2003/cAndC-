09637d0dffefb6d068d4f29aec604a2f9bcfeb11
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 21:10:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,27 +1,9 @@
-main.c
+#include <iostream>
 
-#include <stdio.h>
-#include "tools.h"
-#include "tools.h"
-#include "tools.h"
-#include "tools.h"
+class Person{
 
-int main(void) {
-  int a1 = 10;
-  int a2 = 20;
-  int result = add(a1, a2);
-  printf("加總值為%d\n", result);
-  return 0;
+};
+
+int main() {
+	Person p1;
 }
-
-
-tools.h
-
-#ifndef _TOOLS_H_
-#define _TOOLS_H_
-
-int add(int a, int b){
-	return a + b;
-}
-
-#endif

dc1f5a660d4f9e1dc0c61d9353e8f87c4f1b84b3
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 20:25:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,1 @@
-#include <iostream>
-using namespace std;
 
-#define MAC
-
-#ifndef MAC
-#define OS "WINDOW11"
-#else
-#define OS "MAC_OS_VERSION_10_0"
-#endif
-
-int main() {
-	cout << OS << endl;
-}

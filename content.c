64063929323cee340a912898f3e50e7cee6b3e66
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 20:25:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,18 @@
+main.c
 
+#include <stdio.h>
+#include "tools.h"
+
+int main(void) {
+  int a1 = 10;
+  int a2 = 20;
+  int result = add(a1, a2);
+  printf("加總值為%d\n", result);
+  return 0;
+}
+
+tools.h
+
+int add(int a, int b){
+	return a + b;
+}

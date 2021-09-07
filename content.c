c189c4beb1d95bb383f9133d023eca5ea67b5007
Robert Hsu <roberthsu2003@gmail.com>
2021-09-07 21:44:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,45 +1,33 @@
 main.cpp
 
 #include <iostream>
-#include "person.h"
+#include "human.h"
 
 using namespace std;
 
-
 int main() {
-	Person p1;
+	Human::Person p1;
 	p1.name = "David";
-	p1.tall = 183;
-	cout << "p1的name是" << p1.name << endl;
-	cout << "p1的身高是" << p1.tall << endl;
-	p1.walk(20);
-
-
-	Person p2;
-	p2.name = "Robert";
-	p2.tall = 178;
-	cout << "p2的name是" << p2.name << endl;
-	cout << "p2的身高是" << p2.tall << endl;
-	p2.walk(25);
-	
-	return 0;
+	p1.tall = 180;
 }
 
 
-person.h
 
-#ifndef _PERSON_H_
-#define _PERSON_H_
+
+human.h
+
+#ifndef _HUMAN_H_
+#define _HUMAN_H_
 
 #include <iostream>
 using namespace std;
-class Person{
-	public:
-	string name;
-	int tall;
-	void walk(int var){
-		cout << name << "每小時可以跑" << var << "公里" << endl;
+
+namespace Human{
+	class Person{
+		public:
+		string name;
+		int tall;
 	}
-};
+}
 
 #endif

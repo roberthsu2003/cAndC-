4a674618b7c6a66ffb560d2377ff2167e7f55ddc
Robert Hsu <roberthsu2003@gmail.com>
2021-09-09 20:38:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,53 +1,26 @@
 #include <iostream>
 using namespace std;
-
-class BMI{
-	private:
-	float bmi();
-
+class Car{
 	public:
-	string name;
-	unsigned short weight;
-	unsigned short height;
-	void suggestion();
+	//實體欄位
+	int speed;
+	//自訂的建構式
+	Car();
+	Car(int s);
 };
-
-float BMI::bmi(){
-	return weight / ((height/100.0) * (height/100.0));
+//實作建構式
+Car::Car(){
+	speed = 20;
 }
-
-void BMI::suggestion(){
-	float bmi = BMI::bmi();
-	cout << name + "的bmi是" << bmi << endl;
-
-	string suggest;
-	if(bmi< 18.5){
-		suggest = "體重過輕";
-	}else if(bmi<24){
-		suggest = "體重正常";
-	}else if(bmi < 27){
-		suggest = "過重";
-	}else if(bmi < 30){
-		suggest = "輕度肥胖";
-	}else if(bmi < 35){
-		suggest = "中度肥胖";
-	}else{
-		suggest = "重度肥胖";		
-	}
-	cout << "提醒:" << suggest << endl;
+Car::Car(int s){
+	speed = s;
 }
 
 int main() {
-	BMI p1;
-	cout << "請輸入姓名:";
-	cin >> p1.name;
-	cout << "請輸入身高:";
-	cin >> p1.height;
-	cout << "請輸入體重:";
-	cin >> p1.weight;
-	cout << "姓名是" << p1.name << endl;
-	cout << p1.name + "的身高是" << p1.height << endl;
-	cout << p1.name + "的體重是" << p1.weight << endl;
-	p1.suggestion();
+	Car car1(70); //呼叫建構式
+	cout << "car1的速度是" << car1.speed << endl;
+
+	Car car2; //呼叫建構式
+	cout << "car2的速度是" << car2.speed << endl; 
 	return 0;
 }

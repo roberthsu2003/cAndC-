330b01d4f99ec098b4bbcf9d57aa98624662fd71
Robert Hsu <roberthsu2003@gmail.com>
2021-09-09 21:10:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,43 +1,63 @@
 #include <iostream>
 using namespace std;
-class Student{
+
+class BMI{
+	private:
+	float bmi;
+
 	public:
-	//建構式
-	Student(string,string,int,int,int);
-	//實體欄位
+	BMI(string,unsigned short,unsigned short);
 	string name;
-	string id;
-	int chinese;
-	int english;
-	int math;
-	//實體方法
-	int sum();
-	float average();
+	unsigned short weight;
+	unsigned short height;
+	void suggestion();
 };
 
-Student::Student(string n,string d,int c,int e,int m){
+BMI::BMI(string n,unsigned short w,unsigned short h){
 	name = n;
-	id = d;
-	chinese = c;
-	english = e;
-	math = m;
+	weight = w;
+	height = h;
+	bmi = w / ((h/100.0) * (h/100.0));
 }
 
-int Student::sum(){
-	return chinese + english + math;
-}
-float Student::average(){
-	return sum()/3.0;
+
+
+void BMI::suggestion(){	
+	cout << name + "的bmi是" << bmi << endl;
+	string suggest;
+	if(bmi< 18.5){
+		suggest = "體重過輕";
+	}else if(bmi<24){
+		suggest = "體重正常";
+	}else if(bmi < 27){
+		suggest = "過重";
+	}else if(bmi < 30){
+		suggest = "輕度肥胖";
+	}else if(bmi < 35){
+		suggest = "中度肥胖";
+	}else{
+		suggest = "重度肥胖";		
+	}
+	cout << "提醒:" << suggest << endl;
 }
 
 int main() {
-	Student stu1("stu1", "A10001", 87, 96, 76);
-	cout << "學生姓名:" << stu1.name << endl;
-	cout << "學號:" << stu1.id << endl;
-	cout << "國文:" << stu1.chinese << endl;
-	cout << "英文:" << stu1.english << endl;
-	cout << "數學:" << stu1.math << endl;
-	cout << "總分:" << stu1.sum() << endl;
-	printf("平均:%.2f\n",stu1.average());
+	string name;
+	cout << "請輸入姓名:";
+	cin >> name;
+
+	unsigned short height;
+	cout << "請輸入身高:";
+	cin >> height;
+
+	unsigned short weight;
+	cout << "請輸入體重:";
+	cin >> weight;
+
+	BMI p1(name, weight, height);
+	cout << "姓名是" << p1.name << endl;
+	cout << p1.name + "的身高是" << p1.height << endl;
+	cout << p1.name + "的體重是" << p1.weight << endl;
+	p1.suggestion();
 	return 0;
 }

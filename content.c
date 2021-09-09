195ea58a047054a36603936ba58ab040aa5245b6
Robert Hsu <roberthsu2003@gmail.com>
2021-09-09 21:22:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,63 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class BMI{
-	private:
-	float bmi;
-
+class Tools{
 	public:
-	BMI(string,unsigned short,unsigned short);
-	string name;
-	unsigned short weight;
-	unsigned short height;
-	void suggestion();
+	int static add(int a, int b){
+		return a + b;
+	}
+
 };
 
-BMI::BMI(string n,unsigned short w,unsigned short h){
-	name = n;
-	weight = w;
-	height = h;
-	bmi = w / ((h/100.0) * (h/100.0));
-}
-
-
-
-void BMI::suggestion(){	
-	cout << name + "的bmi是" << bmi << endl;
-	string suggest;
-	if(bmi< 18.5){
-		suggest = "體重過輕";
-	}else if(bmi<24){
-		suggest = "體重正常";
-	}else if(bmi < 27){
-		suggest = "過重";
-	}else if(bmi < 30){
-		suggest = "輕度肥胖";
-	}else if(bmi < 35){
-		suggest = "中度肥胖";
-	}else{
-		suggest = "重度肥胖";		
-	}
-	cout << "提醒:" << suggest << endl;
-}
 
 int main() {
-	string name;
-	cout << "請輸入姓名:";
-	cin >> name;
+	int first;
+	int second;
 
-	unsigned short height;
-	cout << "請輸入身高:";
-	cin >> height;
+	cout << "請輸入第一個數值";
+	cin >> first;
 
-	unsigned short weight;
-	cout << "請輸入體重:";
-	cin >> weight;
+	cout << "請輸入第二個數值";
+	cin >> second;
 
-	BMI p1(name, weight, height);
-	cout << "姓名是" << p1.name << endl;
-	cout << p1.name + "的身高是" << p1.height << endl;
-	cout << p1.name + "的體重是" << p1.weight << endl;
-	p1.suggestion();
+	cout << "2數相加是" << Tools::add(first, second) << endl;
 	return 0;
 }

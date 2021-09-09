150072455c5cb18f162c3df7cb4a7bc99fbe2ea0
Robert Hsu <roberthsu2003@gmail.com>
2021-09-09 21:53:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,62 @@
+main.cpp
+
+#include <iostream>
+#include "student.h"
+using namespace std;
+
+int main() {
+	Student stu1("stu1", "A10001", 87, 96, 76);
+	cout << "學生姓名:" << stu1.name << endl;
+	cout << "學號:" << stu1.id << endl;
+	cout << "國文:" << stu1.chinese << endl;
+	cout << "英文:" << stu1.english << endl;
+	cout << "數學:" << stu1.math << endl;
+	cout << "總分:" << stu1.sum() << endl;
+	printf("平均:%.2f\n",stu1.average());
+	return 0;
+}
+
+student.h
+
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
 #include <iostream>
 using namespace std;
 
-class Car{
+class Student{
 	public:
-	static int total;
-	Car();
+	//建構式
+	Student(string,string,int,int,int);
+	//實體欄位
+	string name;
+	string id;
+	int chinese;
+	int english;
+	int math;
+	//實體方法
+	int sum();
+	float average();
 };
 
-int Car::total = 0;
 
-Car::Car(){
-	total += 1;
+#endif
+
+student.cpp
+
+#include "student.h"
+
+Student::Student(string n,string d,int c,int e,int m){
+	name = n;
+	id = d;
+	chinese = c;
+	english = e;
+	math = m;
 }
 
-int main() {	
-	cout << "一開始建立車的數量是" << Car::total << endl;
-	Car car1;
-	Car car2;
-	Car car3;
-	cout << "已經建立車的數量是" << Car::total << endl;
-	return 0;
+int Student::sum(){
+	return chinese + english + math;
+}
+float Student::average(){
+	return sum()/3.0;
 }

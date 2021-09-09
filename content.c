0fbce171f926cdf662cfdd900f587b2d28b1ffe6
Robert Hsu <roberthsu2003@gmail.com>
2021-09-09 19:27:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,7 +8,17 @@ class Student{
 	int chinese;
 	int english;
 	int math;
+	//實體方法
+	int sum();
+	float average();
 };
+
+int Student::sum(){
+	return chinese + english + math;
+}
+float Student::average(){
+	return sum()/3.0;
+}
 
 int main() {
 	Student stu1;
@@ -17,4 +27,13 @@ int main() {
 	stu1.chinese = 87;
 	stu1.english = 96;
 	stu1.math = 76;
+
+	cout << "學生姓名:" << stu1.name << endl;
+	cout << "學號:" << stu1.id << endl;
+	cout << "國文:" << stu1.chinese << endl;
+	cout << "英文:" << stu1.english << endl;
+	cout << "數學:" << stu1.math << endl;
+	cout << "總分:" << stu1.sum() << endl;
+	printf("平均:%.2f\n",stu1.average());
+	return 0;
 }

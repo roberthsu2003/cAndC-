d82270c67e4149b215114f708d5b4973ce81c380
Robert Hsu <roberthsu2003@gmail.com>
2021-09-09 19:15:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,40 +1,20 @@
-main.cpp
-
 #include <iostream>
-#include "human.h"
-
 using namespace std;
-using namespace Human;
+class Student{
+	public:
+	//實體欄位
+	string name;
+	string id;
+	int chinese;
+	int english;
+	int math;
+};
 
 int main() {
-	Person p1;
-	p1.name = "David";
-	p1.tall = 180;
-	p1.walk(15);
-	return 0;
+	Student stu1;
+	stu1.name = "stu1";
+	stu1.id = "A10001";
+	stu1.chinese = 87;
+	stu1.english = 96;
+	stu1.math = 76;
 }
-
-
-
-human.h
-
-#ifndef _HUMAN_H_
-#define _HUMAN_H_
-
-#include <iostream>
-using namespace std;
-
-namespace Human{
-	class Person{
-		public:
-		string name;
-		int tall;
-		void walk(int var);
-	};
-
-	void Person::walk(int var){
-		cout << name << "每小時可跑" << var << "公里" << endl;
-	}
-}
-
-#endif

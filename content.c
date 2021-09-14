02ee67e697e10da4e73c0624a75939e9ed02e554
Robert Hsu <roberthsu2003@gmail.com>
2021-09-14 20:28:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,21 @@
-#include <iostream>
+// C++ code
+//
 
-class Circle{
-	private:
-	int _radius;
+#define RED_LED 13
 
-	public:
-	Circle(int r);
-	int getRadius();
-	void setRadius(int r);	
-};
-
-Circle::Circle(int r){
-	setRadius(r);
+void setup()
+{
+  pinMode(RED_LED, OUTPUT);
 }
 
-int Circle::getRadius(){
-	return _radius;
+void loop()
+{
+  blink();
 }
 
-void Circle::setRadius(int r){
-	if(r>0){
-		_radius = r;
-	}else{
-		_radius = 1;
-	}
-}
-
-using namespace std;
-int main() {
-	Circle c1(50);	
-	cout << "c1的半徑是" << c1.getRadius() << endl;
+void blink(){
+  digitalWrite(RED_LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(RED_LED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
 }

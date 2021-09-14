cf1fd963e8384d4122226232a0826890d5900b32
Robert Hsu <roberthsu2003@gmail.com>
2021-09-14 19:37:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,9 +5,14 @@ class Circle{
 	int _radius;
 
 	public:
+	Circle(int r);
 	int getRadius();
 	void setRadius(int r);	
 };
+
+Circle::Circle(int r){
+	setRadius(r);
+}
 
 int Circle::getRadius(){
 	return _radius;
@@ -23,7 +28,6 @@ void Circle::setRadius(int r){
 
 using namespace std;
 int main() {
-	Circle c1;
-	c1.setRadius(50);
+	Circle c1(50);	
 	cout << "c1的半徑是" << c1.getRadius() << endl;
 }

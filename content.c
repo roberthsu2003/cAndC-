23d8f5533bc2489a28cc7f7b13a0b02695fb12e6
Robert Hsu <roberthsu2003@gmail.com>
2021-09-14 20:55:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,6 +6,7 @@
 void setup()
 {
   pinMode(RED_LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
@@ -15,7 +16,9 @@ void loop()
 
 void blink(){
   digitalWrite(RED_LED, HIGH);
+  Serial.println("HIGH");
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(RED_LED, LOW);
+  Serial.println("LOW");
   delay(1000); // Wait for 1000 millisecond(s)
 }

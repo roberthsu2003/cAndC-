fcaa440c95c1513cfd9248513584af88a88aed0c
Robert Hsu <roberthsu2003@gmail.com>
2021-09-14 21:55:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,12 @@
+// C++ code
+//
 #define BLUE_LED 4
 #define GREEN_LED 7
 #define RED_LED 13
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
@@ -11,22 +14,43 @@ void setup()
 
 void loop()
 {
-  blink();
+  if(Serial.available()>0){
+    char inputValue = Serial.read();
+    switch(inputValue){
+      case '1':
+      blink(1);
+      break;
+      
+      case '2':
+      blink(2);
+      break;
+      
+      case '3':
+      blink(3);
+      break;
+      
+      default:
+      break;
+    }
+  }
 }
 
-void blink(){
-  digitalWrite(RED_LED, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(RED_LED, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+void blink(int num){
+  for(int i=0;i<num; i+=1 ){
+    digitalWrite(RED_LED, HIGH);
+  	delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(RED_LED, LOW);
+  	delay(1000); // Wait for 1000 millisecond(s)
   
-  digitalWrite(GREEN_LED, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(GREEN_LED, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(GREEN_LED, HIGH);
+  	delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(GREEN_LED, LOW);                                             
+  	delay(1000); // Wait for 1000 millisecond(s)
   
-  digitalWrite(BLUE_LED, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(BLUE_LED, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(BLUE_LED, HIGH);
+  	delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(BLUE_LED, LOW);
+  	delay(1000); // Wait for 1000 millisecond(s)
+  }
+  
 }

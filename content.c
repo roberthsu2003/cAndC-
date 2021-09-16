67e3ded6835b7e87690e1fb2436261fd2b1fc1ad
Robert Hsu <roberthsu2003@gmail.com>
2021-09-16 20:00:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,56 +1,6 @@
-// C++ code
-//
-#define BLUE_LED 4
-#define GREEN_LED 7
-#define RED_LED 13
+#include <stdio.h>
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(BLUE_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-}
-
-void loop()
-{
-  if(Serial.available()>0){
-    char inputValue = Serial.read();
-    switch(inputValue){
-      case '1':
-      blink(1);
-      break;
-      
-      case '2':
-      blink(2);
-      break;
-      
-      case '3':
-      blink(3);
-      break;
-      
-      default:
-      break;
-    }
-  }
-}
-
-void blink(int num){
-  for(int i=0;i<num; i+=1 ){
-    digitalWrite(RED_LED, HIGH);
-  	delay(1000); // Wait for 1000 millisecond(s)
-  	digitalWrite(RED_LED, LOW);
-  	delay(1000); // Wait for 1000 millisecond(s)
-  
-  	digitalWrite(GREEN_LED, HIGH);
-  	delay(1000); // Wait for 1000 millisecond(s)
-  	digitalWrite(GREEN_LED, LOW);                                             
-  	delay(1000); // Wait for 1000 millisecond(s)
-  
-  	digitalWrite(BLUE_LED, HIGH);
-  	delay(1000); // Wait for 1000 millisecond(s)
-  	digitalWrite(BLUE_LED, LOW);
-  	delay(1000); // Wait for 1000 millisecond(s)
-  }
-  
+int main(void) {
+  printf("這是我的第一個C語言的程式\n");
+  return 0;
 }

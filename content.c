e016fbe97d94dade755fc5166bc764636df05e6c
Robert Hsu <roberthsu2003@gmail.com>
2021-09-16 20:28:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,6 +2,10 @@
 
 int main(void) {
 	int englishScore = 78;
-  	printf("您的英文分數是%d分\n",englishScore);
+	int chineseScore = 98;
+	int mathScore = 85; 
+  	printf("您的英文分數是%d分\n", englishScore);
+	printf("您的中文分數是%d分\n", chineseScore);
+	printf("您的數學分數是%d分\n", mathScore);
   	return 0;
 }

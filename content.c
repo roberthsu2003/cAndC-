8211d320181dc720c8a7ba2a8bb56118d4b11876
Robert Hsu <roberthsu2003@gmail.com>
2021-09-16 21:32:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-  int myInt = 56;
-  float myFloat = 67.98;
-  double myDouble = 879.12345;
-  char myChar = 'z';
-  const char* myName = "Robert";
-
-  printf("我的整數是%d\n",myInt);
-  printf("我的float是%.2f\n",myFloat);
-  printf("我的double是%.5f\n",myDouble);
-  printf("我的字元:%c\n", myChar);
-  printf("我的字串是%s\n", myName);
+  int a = 0;
+  printf("請輸入a的內容:");
+  scanf("%d",&a);
+  printf("變數a現在是內容是%d\n",a);
   return 0;
 }

7ab53d777b27850021ab7820f92d3da9913a3ea5
Robert Hsu <roberthsu2003@gmail.com>
2021-09-16 21:16:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,5 +6,11 @@ int main(void) {
   double myDouble = 879.12345;
   char myChar = 'z';
   const char* myName = "Robert";
+
+  printf("我的整數是%d\n",myInt);
+  printf("我的float是%.2f\n",myFloat);
+  printf("我的double是%.5f\n",myDouble);
+  printf("我的字元:%c\n", myChar);
+  printf("我的字串是%s\n", myName);
   return 0;
 }

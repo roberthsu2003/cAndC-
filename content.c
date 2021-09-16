f17453483703887a9dc4ba5e4dd0eeecd1cbb2b8
Robert Hsu <roberthsu2003@gmail.com>
2021-09-16 21:01:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-	int englishScore = 78;
-	int chineseScore = 98;
-	int mathScore = 85; 
-  	printf("您的英文分數是%d分\n", englishScore);
-	printf("您的中文分數是%d分\n", chineseScore);
-	printf("您的數學分數是%d分\n", mathScore);
-	printf("國文:%d,英文:%d,數學:%d\n",chineseScore,englishScore,mathScore);
-  	return 0;
+  int myInt = 56;
+  float myFloat = 67.98;
+  double myDouble = 879.12345;
+  char myChar = 'z';
+  const char* myName = "Robert";
+  return 0;
 }

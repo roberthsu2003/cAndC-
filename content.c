faec87a1565e0a40566436a324976b599e6fd13a
Robert Hsu <roberthsu2003@gmail.com>
2021-09-23 21:47:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,20 +2,8 @@
 using namespace std;
 
 int main() {
-	unsigned short chinese, english, math;
-	int sum=0;
-	float average = 0;
-
-	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;
-	cout << "請輸入英文成績(0~100):";
-	cin >> english;
-	cout << "請輸入數學成績(0~100):";
-	cin >> math;
-	
-	sum = chinese + english + math;
-	average = (float)sum / 3;
-	//cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
-	printf("您的總分為:%d分,平均為%.2f分\n",sum,average);
-
+	//建立常數
+	const float PI = 3.14159;
+	cout << "常數PI內的值是:" << PI << endl;
+	return 0;
 }

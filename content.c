4a688cb53ed822b2e9a22ee510abf9332f1e33ac
Robert Hsu <roberthsu2003@gmail.com>
2021-09-23 21:53:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,12 @@
 #include <iostream>
+//前端處理程式
+#define PI 3.14159
+
 using namespace std;
 
 int main() {
 	//建立常數
-	const float PI = 3.14159;
+	//const float PI = 3.14159;
 	cout << "常數PI內的值是:" << PI << endl;
 	return 0;
 }

ef57a2987e41b994296ac1c87e3002aeeb572756
Robert Hsu <roberthsu2003@gmail.com>
2021-09-23 21:04:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,8 @@
 using namespace std;
 
 int main() {
-	bool bolA = true;
-	bool bolB = false;
-
-	cout << bolA << endl;
-	cout << bolB << endl;
-	return 0;
+	double PI = 3.1415987;
+	int radius = 9;
+	cout << "圓面積=" << PI * radius * radius << endl;
+	printf("圓面積%.2f\n", PI * radius * radius);
 }

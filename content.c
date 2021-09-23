91fb4374052441670b2b9614256c820a36571387
Robert Hsu <roberthsu2003@gmail.com>
2021-09-23 20:49:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,17 +2,10 @@
 using namespace std;
 
 int main() {
-	//c++ 字串型別
-	string str1 = "這是c++的字串";
+	bool bolA = true;
+	bool bolB = false;
 
-	//c 字元陣列
-	const char* str2 = "這是c的字元陣列";
-
-	//使用cout輸出
-	cout << "cout輸出str1:" << str1 << endl;
-	cout << "cout輸出str2:" << str2 << endl;
-
-	//使用printf(),只可以輸出字元陣列
-	printf("printf()輸出str2:%s\n",str2);
+	cout << bolA << endl;
+	cout << bolB << endl;
 	return 0;
 }

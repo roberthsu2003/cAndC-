5bd8e0e581e55ef458fcc90c566dd1f6d78d78fc
Robert Hsu <roberthsu2003@gmail.com>
2021-09-23 20:31:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-	char chrB;
-	chrB = 'A';
-	cout << chrB << "\n";
+	//c++ 字串型別
+	string str1 = "這是c++的字串";
 
-	char chrA;
-	chrA = 97;
-	cout << chrA << "\n";
+	//c 字元陣列
+	const char* str2 = "這是c的字元陣列";
+
+	//使用cout輸出
+	cout << "cout輸出str1:" << str1 << endl;
+	cout << "cout輸出str2:" << str2 << endl;
+
+	//使用printf(),只可以輸出字元陣列
+	printf("printf()輸出str2:%s\n",str2);
+	return 0;
 }

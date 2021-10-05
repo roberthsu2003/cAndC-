8850c09906f411449e2ca36146748fbf6a937204
Robert Hsu <roberthsu2003@gmail.com>
2021-10-05 19:28:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,10 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
-
-int main() {
-	char op;
-	int num1 = 20, num2 = 4;
-	cout << "num1=20,num2=4" << endl;
-	cout << "請輸入要執行的運算(+ - * /):";
-	cin >> op;
-	switch(op){
-		case '+':
-		cout << "num1+num2=" << num1+num2 << endl;
-		break;
-
-		case '-':
-		cout << "num1-num2=" << num1-num2 << endl;
-		break;
-
-		case '*':
-		cout << "num1*num2=" << num1*num2 << endl;
-		break;
-
-		case '/':
-		cout << "num1/num2=" << num1/num2 << endl;
-		break;
-
-		default:
-		cout << "無法執行運算!";
-		break;
-	}
-	return 0;
+int main() {	
+	srand(time(NULL));
+	cout << rand() << endl;
 }

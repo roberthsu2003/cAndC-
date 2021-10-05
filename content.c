da678eba6850dedc630574dd1aba1bb4751ef17f
Robert Hsu <roberthsu2003@gmail.com>
2021-10-05 22:00:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,14 +5,19 @@ using namespace std;
 
 int main() {
 	int num;
+	int count=0;
 	cout << "請輸入一個數值:";
 	cin >> num;
 	cout << "質數有:" << endl;
+
 	for(int i=2;i<=num;i++){
 		if(isPrimeNumber(i)) {
+			count += 1;
 			cout << i << ",";
 		}
 	}
 	cout << endl;
+
+	cout << "共有" << count << "個質數" << endl;
 	return 0;
 }

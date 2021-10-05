7d5366464beacbb8522956753f91362f35c70d26
Robert Hsu <roberthsu2003@gmail.com>
2021-10-05 21:56:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,28 +1,18 @@
 #include <iostream>
 #include "tools.h"
+
 using namespace std;
 
 int main() {
-	int num;	
+	int num;
 	cout << "請輸入一個數值:";
-	cin >> num;	
-
-	string message = isPrimeNumber(num) ? "是質數" : "不是質數";
-	cout << num << message << endl;
-	return 0;
-}
-
-tools.h
-
-bool isPrimeNumber(int num){
-	bool isPrimeNumber=true;
-	for(int i=2;i < num; i++){
-		if(num % i == 0){
-			isPrimeNumber = false;
-			break;
+	cin >> num;
+	cout << "質數有:" << endl;
+	for(int i=2;i<=num;i++){
+		if(isPrimeNumber(i)) {
+			cout << i << ",";
 		}
 	}
-	return isPrimeNumber;
+	cout << endl;
+	return 0;
 }
-
-

3bf6580252443f6c60aad829e72618f1223424e4
Robert Hsu <roberthsu2003@gmail.com>
2021-10-05 19:44:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,25 @@
 #include <time.h>
 
 using namespace std;
-int main() {	
+int main() {
+	int guess;
+	int keyin;
+	int count = 0;
+
 	srand(time(NULL));
-	cout << rand() << endl;
+	guess = rand() % 10 + 1;
+	cout << guess << endl;
+	while(true){
+		count += 1;
+		cout << "猜數字範圍1~10:";
+		cin >> keyin;
+		if(keyin == guess){
+			cout << "猜對了" << endl;
+			cout << "您猜了" << count << "次" << endl;
+			break;
+		}else{
+			cout << "猜錯了" << endl;
+		}
+	};
+	return 0;
 }

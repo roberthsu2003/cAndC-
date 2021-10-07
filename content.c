bca8686b01c3d1313061635f7cf1755c30ff1730
Robert Hsu <roberthsu2003@gmail.com>
2021-10-07 21:21:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -17,6 +17,22 @@ int main() {
 	for(int i=0; i<nums; i++){
 		cout << array[i] << " ";
 	}
+	cout << endl;
 
-	//排序
+	//泡沫排序
+	for(int i=0; i<nums-1; i++){
+		for(int j=i+1; j<nums; j++){
+			if(array[i] > array[j]){
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+
+	cout << "排序後" << endl;
+	for(int i=0; i<nums; i++){
+		cout << array[i] << " ";
+	}
+	cout << endl;
 }

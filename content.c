faa23af72da54843031bbd151c3246ba249048bd
Robert Hsu <roberthsu2003@gmail.com>
2021-10-07 19:16:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,14 @@
 #include <iostream>
-#include "tools.h"
-
 using namespace std;
 
 int main() {
-	int num;
-	int count=0;
-	cout << "請輸入一個數值:";
-	cin >> num;
-	cout << "質數有:" << endl;
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
 
-	for(int i=2;i<=num;i++){
-		if(isPrimeNumber(i)) {
-			count += 1;
-			cout << i << ",";
-		}
-	}
-	cout << endl;
+	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
 
-	cout << "共有" << count << "個質數" << endl;
 	return 0;
+
 }

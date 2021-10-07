e34443e65385e6349c7a99878624612ae66782b3
Robert Hsu <roberthsu2003@gmail.com>
2021-10-07 21:08:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,22 @@
-//50個學生
-//5個分數
-
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-int randomScore(){
-	int max=100;
-	int min=60;
-	
-	return rand() % (max-min+1) + min;
-}
-
 int main() {
-	srand(time(NULL));
-	int students[50][5];
-	for(int i=0; i<50; i++){
-		cout << "第" << i+1 << "學生: ";
-		int sum = 0;
-		for(int j=0; j<5; j++){
-			students[i][j] = randomScore();
-			cout << students[i][j] << " ";
-			sum += students[i][j];
-		}
-		cout << "總分:" << sum;
-		cout << endl;
+	int nums;
+	cout << "請輸入要排序的數值個數:";
+	cin >> nums;
+
+	int array[nums];
+
+	for(int i=0; i<nums; i+=1){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
 	}
+
+	cout << "排序前" << endl;
+	for(int i=0; i<nums; i++){
+		cout << array[i] << " ";
+	}
+
+	//排序
 }

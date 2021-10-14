e482490053a583628f5d430028fec80587f8266b
Robert Hsu <roberthsu2003@gmail.com>
2021-10-14 20:22:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,14 +4,16 @@
 using namespace std;
 int main() {
 	int lot[49];
+	int minIndex = 0;
+	int maxIndex = 48;
+
 	for(int i=0; i<49; i++){
 		lot[i] = i+1;
 	}
 
-	for(int i=0; i<49; i++){
-		cout << lot[i] << " ";
-	}
+	
 	srand(time(NULL));
-	cout << rand() << endl;
+	int randIndex = rand() % (maxIndex-minIndex+1) + minIndex;
+	cout << randIndex << endl;
 	return 0;
 }

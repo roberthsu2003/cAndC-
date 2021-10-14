19e0dad9a9fe9a69effaf99096c51f6f8fc5c200
Robert Hsu <roberthsu2003@gmail.com>
2021-10-14 20:43:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,17 +3,30 @@
 
 using namespace std;
 int main() {
+	int num = 7;
 	int lot[49];
 	int minIndex = 0;
 	int maxIndex = 48;
+	int choose[num];
 
+	//建立1~49的值
 	for(int i=0; i<49; i++){
 		lot[i] = i+1;
 	}
 
 	
 	srand(time(NULL));
-	int randIndex = rand() % (maxIndex-minIndex+1) + minIndex;
-	cout << randIndex << endl;
+	//建立亂數7次，不會重覆的
+	for(int i=0; i<num; i++){
+		int randIndex = rand() % (maxIndex-minIndex+1) + minIndex;
+		choose[i] = lot[randIndex];
+		lot[randIndex] = lot[maxIndex];
+		maxIndex -= 1;
+	}
+	
+	for(int i=0; i<num; i++){
+		cout << choose[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }

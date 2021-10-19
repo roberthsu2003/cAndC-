4b67595b5c3ee95bad48cef53d291da09809a1d2
Robert Hsu <roberthsu2003@gmail.com>
2021-10-19 21:57:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,19 @@
-//多載overloading
-//建立Add()函式多載，分別處理數值和字串相加。
 #include <iostream>
 using namespace std;
+//全域變數
+int total = 0;
 
-int add(int a, int b){
-	return a + b;
-}
-
-string add(string a,string b){
-	return a + b;
+void getData(){
+	//靜態變數
+	static int a = 0;
+	a++;
+	cout << "a=" << a << endl;
+	total++;
 }
 
 int main() {
-	cout << "add(2,3)=" << add(2,3) << endl;
-	cout << "====================" << endl;
-	cout << "add(\"Nice\", \"Play\") = " << add("Nice","Play") << endl;
-	return 0;
+  getData();
+  getData();
+  getData();
+  cout << "total是" << total << endl;
 }

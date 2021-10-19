ff2c6e2786d165edb4eaeb756d266eb7195855b6
Robert Hsu <roberthsu2003@gmail.com>
2021-10-19 21:06:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void swap(int *n, int *m){
+	int temp = *n;
+	*n = *m;
+	*m = temp;
+}
+
 int main() {
 	int a=666, b=777;
 	cout << "a=" << a << ",b=" << b << endl;
-	int temp = a;
-	a = b;
-	b = temp;
+	swap(&a,&b);
 	cout << "a=" << a << ",b=" << b << endl;
 	return 0;
 }

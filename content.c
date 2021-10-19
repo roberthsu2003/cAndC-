7f9608f721b93a4b24bf7cd9fa97d693556198a4
Robert Hsu <roberthsu2003@gmail.com>
2021-10-19 19:39:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main() {
-	int xin;
-	cout << "請輸入一個整數:";
-	scanf("%d",&xin);
-	cout << "xin=" << xin << endl;
+	int n[3] = {10, 20 , 30};
+	cout << "陣列n的位址=" << n << endl;
+	cout << "陣列元素n[0]的位址=" << &n[0] << endl;
+	cout << "陣列元素n[1]的位址=" << &n[1] << endl;
+	cout << "陣列元素n[2]的位址=" << &n[2] << endl;
+	cout << "陣列元素n+0的位址=" << n+0 << endl;
+	cout << "陣列元素n+1的位址=" << n+1 << endl;
+	cout << "陣列元素n+2的位址=" << n+2 << endl;
+	return 0;
 }

5677e5c5d8e4ecbb8144aa9f8c90dee8070f2fcd
Robert Hsu <roberthsu2003@gmail.com>
2021-10-19 21:33:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,19 @@
 #include <iostream>
-using namespace std;
-//c++才有的功能,傳參考
-void swap(int &n, int &m){
-	int temp = n;
-	n = m;
-	m = temp;
-}
 
+using namespace std;
+
+int factorial(int n){
+	if(n == 0){
+		return 1;
+	}else{
+		return n * factorial(n-1);
+	}
+}
 int main() {
-	int a=666, b=777;
-	cout << "a=" << a << ",b=" << b << endl;
-	swap(a,b);
-	cout << "a=" << a << ",b=" << b << endl;
+	int n, total;
+	cout << "請輸入數字 n:";	
+	cin >> n;
+	total=factorial(n);
+	cout << n << "!=" << total << endl;
 	return 0;
 }

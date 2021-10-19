e3c39a3db91d68bbf0a2018c4850125ddd299c0f
Robert Hsu <roberthsu2003@gmail.com>
2021-10-19 20:06:08 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	/*
-	char name[] = {'R','o','b','e','r','t','\0'};
-	cout << "name:" << name << endl;
-	*/
-
-	/*
-	char name[] = "Robert";
-	cout << "name:" << name << endl;
-	//name = "davie";
-	*/
-
-	//c 語言的字串
-	const char *name = "Robert";
-	cout << "name:" << name << endl;
-	name = "David";
-	cout << "name:" << name << endl; 
-	
+	int n = 10;
+	//*是宣告指標變數
+	int *p = &n;
+	cout << "變數n的值=" << n << endl;
+	cout << "變數n的位址=" << &n << endl;
+	cout << "指標p的值=" << p << endl;
+	//*取值運算子
+	cout << "指標p指向的值是=" << *p << endl;
+	cout << "&n記憶體指向的值是=" << *(&n) << endl;
 	return 0;
 }

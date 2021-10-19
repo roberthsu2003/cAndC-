adae7529bd03168196e93130ede17521ff46fa8b
Robert Hsu <roberthsu2003@gmail.com>
2021-10-19 20:34:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
+void add(int x, int y, int *ptr){
+	*ptr = x+y;	
+}
 
 int main() {
-	int n = 10;
-	//*是宣告指標變數
-	int *p = &n;
-	cout << "變數n的值=" << n << endl;
-	cout << "變數n的位址=" << &n << endl;
-	cout << "指標p的值=" << p << endl;
-	//*取值運算子
-	cout << "指標p指向的值是=" << *p << endl;
-	cout << "&n記憶體指向的值是=" << *(&n) << endl;
-	return 0;
+  int n = 10;
+  int m = 20;
+  int sum;
+  add(n, m, &sum);
+  cout << "sum=" << sum << endl;
+  return 0;
 }

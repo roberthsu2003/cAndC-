07ad309db2a25069afa501206a8ac9339a99b32d
Robert Hsu <roberthsu2003@gmail.com>
2021-10-21 21:42:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -28,32 +28,30 @@ float average(Student s);
 
 
 data.cpp
-//要使用這個h檔,必需要先執行begin()
-#include <iostream>
 
+#include "data.h"
+#include "time.h"
 
-using namespace std;
+void begin(){
+	srand(time(NULL));
+}
 
-typedef struct student{
-	string name;
-	int chinese;
-	int english;
-	int math;
-}Student;
+Student createStudent(int num){
+	Student s;
+	s.name = "學生" + to_string(num);
+	s.chinese = rand() % (100-50+1) + 50;
+	s.english = rand() % (100-50+1) + 50;
+	s.math = rand() % (100-50+1) + 50;
+	return s;
+}
 
-/*
-//要使用這個h檔,必需要先執行begin()
-*/
-void begin();
+int sum(Student s){
+	return s.chinese + s.english + s.math;
+}
 
-//建立一個學生
-Student createStudent(int num);
-
-//算出總分
-int sum(Student s);
-
-//算出平均
-float average(Student s);
+float average(Student s){
+	return sum(s) / 3.0;
+}
 
 
 

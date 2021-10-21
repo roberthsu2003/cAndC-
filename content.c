048957d874ae27d0d7877c67db545a0ab9cd284b
Robert Hsu <roberthsu2003@gmail.com>
2021-10-21 19:33:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-struct student{
+//typedef 重新定義資料類型的名稱
+
+typedef struct student{
 	int id;
 	string name;
 	int chinese, english, math;
-};
+}Student;
 
 int main() {
-	struct student david = {99001, "David", 75, 86, 90};
+	Student david = {99001, "David", 75, 86, 90};
 	
 	cout << "學生id:" << david.id << endl;
 	cout << "學生name:" << david.name << endl;
 	cout << "學生chinese:" << david.chinese << endl;
 	cout << "學生math:" << david.math << endl;
 	cout << "學生english:" << david.english << endl;
-	}
+}

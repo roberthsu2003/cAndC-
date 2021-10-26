4ee351a99cbea3a0d39ae8e9ba2fa9292268d6a7
Robert Hsu <roberthsu2003@gmail.com>
2021-10-26 20:01:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,25 +1,14 @@
-main.cpp
-
 #include <iostream>
-#include "days.h"
+#define RATE 29.5
+
+using namespace std;
 
 int main() {
-	Day day = wendesday;
-	int color = red;
+	int us;
+	float nt;
+	cout << "請輸入您有多少美金:";
+	cin >> us;
+	nt = us * RATE;
+	cout << "您有" << nt << "台幣" << endl;
+	return 0;
 }
-
-
-days.h
-#define red 1
-#define green 2
-#define blue 3
-
-typedef enum day{
-	saturday,
-	sunday,
-	monday,
-	tuesday,
-	wendesday,
-	thursday
-	firday
-}Day;

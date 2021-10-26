051a385ffc626ffc8dc632dadf8d94105490558b
Robert Hsu <roberthsu2003@gmail.com>
2021-10-26 19:26:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,7 +10,24 @@ typedef enum direction{
 
 //typedef enum direction Direction;
 int main() {
-	Direction dest = South;
-	cout << "dest:" << dest << endl;
+	Direction dest = West;
+	//必需使用switch檢查列舉變數內容值
+	switch(dest){
+		case North:
+		cout << "North" << endl;
+		break;
+		
+		case South:
+		cout << "South" << endl;
+		break;
+
+		case East:
+		cout << "East" << endl;
+		break;
+
+		case West:
+		cout << "West" << endl;
+		break;
+	}
 	return 0;
 }

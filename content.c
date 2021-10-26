25a4b6e563f7cb703fafd8761947602f9458f7e3
Robert Hsu <roberthsu2003@gmail.com>
2021-10-26 21:05:08 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,26 @@
+main.c
 
+#include <stdio.h>
+#include "tools.h"
+#include "tools.h"
+
+int main(void) {
+	int a = 10;
+	int b = 5;
+	int result = add(a, b);
+  	printf("相加的結果是%d\n",result);
+  return 0;
+}
+
+
+tools.h
+
+//避免被重複#include
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+int add(int a,int b){
+	return a+b;
+}
+
+#endif

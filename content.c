c3674a7c519cad3c2a6a4293582e143ef2c82a25
Robert Hsu <roberthsu2003@gmail.com>
2021-10-26 19:42:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,25 @@
+main.cpp
 
+#include <iostream>
+#include "days.h"
+
+int main() {
+	Day day = wendesday;
+	int color = red;
+}
+
+
+days.h
+#define red 1
+#define green 2
+#define blue 3
+
+typedef enum day{
+	saturday,
+	sunday,
+	monday,
+	tuesday,
+	wendesday,
+	thursday
+	firday
+}Day;

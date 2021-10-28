80fe74291f35c9295839ba8d7a2cf24d1d84737f
Robert Hsu <roberthsu2003@gmail.com>
2021-10-28 19:52:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,49 +1,26 @@
-main.cpp
-
-#include <iostream>
-#include "student.h"
-using namespace std;
-
-
-int main() {
-	Student stu1;
-	stu1.name = "stu1";
-	stu1.chinese = 87;
-	stu1.english = 92;
-	stu1.math = 87;
-
-	cout << "學生總分是:" << stu1.sum() << endl;
-	cout << "學生平均是:" << stu1.average() << endl;
-	return 0;
-} 
-
-
-student.h
-
 #include <iostream>
 using namespace std;
 
-class Student{
+class BMI{
 	public:
+		BMI(string n, int h, int w);
 		string name;
-		int chinese;
-		int english;
-		int math;
-		int sum();
-		float average();
+		int height;
+		int weight;
+
 };
 
-
-student.cpp
-
-
-#include "student.h"
-
-//實作method在class的外部
-int Student::sum(){
-	return chinese + english + math;
+BMI::BMI(string n, int h, int w){
+	name = n;
+	height = h;
+	weight = w;
 }
 
-float Student::average(){
-	return sum() / 3.0;
-}
+int main() {
+
+	BMI user1("robert",175,76);
+	cout << "name=" << user1.name << endl;
+	cout << "height=" << user1.height << endl;
+	cout << "weight=" << user1.weight << endl;
+	
+} 

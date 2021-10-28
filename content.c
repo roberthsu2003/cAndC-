b3cd7f59fa825194f862b5f11538eb80aaaf80c0
Robert Hsu <roberthsu2003@gmail.com>
2021-10-28 19:16:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,42 +1,28 @@
 #include <iostream>
-#include "person.h"
 using namespace std;
 
-int main() {
-	Person p1;
-	p1.name="robert";
-	p1.height = 175;
-
-	Person p2;
-	p2.name = "david";
-	p2.height = 180;
-
-	cout << "p1的名字是:" << p1.name << endl;
-	cout << "p1的高是:" << p1.height << endl;
-	p1.walk(15);
-
-	cout << "p2的名字是:" << p2.name << endl;
-	cout << "p2的高是:" << p2.height << endl;
-	p2.walk(20);
-}
-
-
-person.h
-
-#ifndef __PERSON_H__
-#define __PERSON_H__
-
-#include <iostream>
-using namespace std;
-
-class Person{
+class Student{
 	public:
 		string name;
-		int height;
-		void walk(int var){
-			cout << name << "每小時可跑" << var << "公里" << endl;
+		int chinese;
+		int english;
+		int math;
+		int sum(){
+			return chinese + english + math;
+		}
+		float average(){
+			return sum() / 3.0;
 		}
 };
 
-#endif
+int main() {
+	Student stu1;
+	stu1.name = "stu1";
+	stu1.chinese = 87;
+	stu1.english = 92;
+	stu1.math = 87;
 
+	cout << "學生總分是:" << stu1.sum() << endl;
+	cout << "學生平均是:" << stu1.average() << endl;
+	return 0;
+} 

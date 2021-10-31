bf49eb4c694a45e0286bd2a1962ff91cfac38a52
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 14:21:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-  const char* name;
-  name = "robert";
-  printf("我的姓名是:%s\n",name);
+  double PI = 3.14159876;
+  int radius;
+  printf("請輸入整數半徑:");
+  scanf("%d",&radius);
+  //自動轉換型別
+  printf("圓面積=%.2f\n",PI * radius * radius);
   return 0;
 }

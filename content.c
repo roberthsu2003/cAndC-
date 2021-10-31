a493c7e17dd50822ed76d0b8ce7109725a065c87
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 15:28:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
+#define rate 27.83
 
 int main(void) {
-	int chinese,english,math;
-  	printf("請輸入國文成積(0-100):");
-	scanf("%d", &chinese);
-
-	printf("請輸入英文成積(0-100):");
-	scanf("%d", &english);
-
-	printf("請輸入數學成積(0-100):");
-	scanf("%d", &math);
-
-	unsigned long sum = chinese + english + math;
-	//float average = (float)sum / 3;
-	float average = sum / 3.0;
-	printf("average=%.2f\n",average);
-  return 0;
+	//const float rate = 27.83;
+	int us;
+	printf("請輸入美金總數:");
+	scanf("%d",&us);
+	printf("可換成台幣%d\n", (int)(us * rate));
+	return 0;
 }

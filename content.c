41a3b702f042c43722fd84c0f2345b2269b32a0c
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 16:11:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-	int n,m;
-  	printf("請輸入被除數(整數):");
-	scanf("%d",&n);
-
-	printf("請輸入除數(整數,不可以為零):");
-	scanf("%d",&m);
-
-	printf("商:%d,餘數:%d\n", n/m, n%m);
-  return 0;
+	int x = 1;
+	int y = x++;
+	printf("x=%d\n",x);
+	printf("y=%d\n",y);
+	return 0;
 }

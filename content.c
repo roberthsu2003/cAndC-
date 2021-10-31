c9644c4e4df3e57cfc02af021b92575cdf85d8ec
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 10:46:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,6 +2,9 @@
 
 int main(void) {
   printf("這是c語言的第一個程式\n");
-  printf("我愛C\n");
+  printf("我愛%s\n","徐國堂");
+  printf("我愛%d\n",111);
+  printf("我愛%f\n",12.3456);
+  printf("我愛%.2f-%d\n",12.3456,100);
   return 0;
 }

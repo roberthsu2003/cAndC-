4bc9da653c05bfb6eea2694bd79847ad62d3c195
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 14:34:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,9 +3,11 @@
 int main(void) {
   double PI = 3.14159876;
   int radius;
+  double area;
   printf("請輸入整數半徑:");
   scanf("%d",&radius);
   //自動轉換型別
-  printf("圓面積=%.2f\n",PI * radius * radius);
+  area = PI * radius * radius;
+  printf("圓面積=%.2f\n",area);
   return 0;
 }

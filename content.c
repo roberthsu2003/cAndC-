2b78476ff4ec7d6428120408f7821b67937f3616
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 13:59:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-	char characterA;
-	characterA = 'A';
-	printf("characterA:%c\n",characterA);
-
-	char characterB;
-	characterB = 65;
-	printf("characterB:%c\n",characterB);
-
-	unsigned char numberA = 200;
-	printf("numberA:%d\n",numberA);
-	return 0;
+  const char* name;
+  name = "robert";
+  printf("我的姓名是:%s\n",name);
+  return 0;
 }

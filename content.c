00a5c4ef19c2b2846580ced9c71d509dc0749c02
Robert Hsu <roberthsu2003@gmail.com>
2021-10-31 15:44:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#define rate 27.83
 
 int main(void) {
-	//const float rate = 27.83;
-	int us;
-	printf("請輸入美金總數:");
-	scanf("%d",&us);
-	printf("可換成台幣%d\n", (int)(us * rate));
-	return 0;
+	int n,m;
+  	printf("請輸入被除數(整數):");
+	scanf("%d",&n);
+
+	printf("請輸入除數(整數,不可以為零):");
+	scanf("%d",&m);
+
+	printf("商:%d,餘數:%d\n", n/m, n%m);
+  return 0;
 }

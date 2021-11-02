d52f64d5d44d32acc3caa8b2622c465676b8c9aa
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 21:09:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,25 @@
 #include <iostream>
 class Car{
 	private:
-		int speed=0;
+		int _speed=0;
 
 	public:
 		Car(int s);
+		int getSpeed();
 
 };
 
 Car::Car(int s){
-	speed = s;
+	_speed = s;
 }
 
+int Car::getSpeed(){
+	return _speed;
+}
+
+using namespace std;
 int main() {
 	Car car1 = Car(50);
-	
+	cout << "汽車的速度是" << car1.getSpeed() << endl;
+	return 0;
 } 

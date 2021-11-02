48becd1960a5bfe06b81e61eb8cd7a47ea37e680
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 20:18:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,15 +3,20 @@ using namespace std;
 
 class Student{
 	public:
+		//建構式
 		Student();
 		Student(string n);
 		static void begin(){
 			srandom(time(NULL));
 		}
 		string name;
+		//實體欄位
 		int chinese;
 		int english;
 		int math;
+		//實體方罄
+		int sum();
+		float average();
 };
 //建構式
 Student::Student(){
@@ -27,6 +32,13 @@ Student::Student(string n){
 	math = random() % (max-min+1) + min;
 }
 
+int Student::sum(){
+	return chinese + english + math;
+}
+float Student::average(){
+	return sum() / 3.0;
+}
+
 
 int main() {
 	int nums = 50;
@@ -35,8 +47,13 @@ int main() {
 	Student students[nums];
 	for(int i=0; i<nums; i++){
 		students[i] = Student("stu"+to_string(i+1));
-		cout << students[i].name << endl;
-		cout << students[i].chinese << endl;
+	}
+
+	cout << "學生姓名\t國文\t數學\t英文\t總分\t平均" << endl;
+	for(int i=0; i<nums; i++){
+		Student s = students[i];
+		cout << s.name << "\t\t" << s.chinese << "\t\t" << s.math << "\t\t" << s.english << "\t\t" << s.sum() << "\t\t";
+		printf("%.2f\n",s.average()); 
 	}
 	return 0;
 } 

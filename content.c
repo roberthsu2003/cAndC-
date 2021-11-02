155398f24aca2704a61d3b684b9b23c9f944b6c3
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 19:20:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(void) {
-  printf("%d\n",3==3);
-  printf("%d\n",3!=3);
-  return 0;
-}
+class Student{
+	public:
+		string name;
+		int chinese;
+		int english;
+		int math;
+};
+
+int main() {
+	Student stu1 = Student();
+	stu1.name = "robert";
+	stu1.chinese = 84;
+	stu1.english = 92;
+	stu1.math = 74;
+
+	cout <<stu1.name << "的總分" << stu1.chinese+stu1.english+stu1.math << endl;
+	return 0;
+} 

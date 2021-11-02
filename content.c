dd5eb35823303dd88998134d58e4e175b63a3880
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 21:42:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,43 +1,16 @@
 #include <iostream>
-class Car{
-	private:
-		int _speed=0;
 
+class Square{
 	public:
-		Car(int s);
-		int getSpeed();
-		void turbo(int s);
-
+		int width;
+		int getArea(){
+			return width * width;
+		}
 };
-
-Car::Car(int s){
-	if(s<0){
-		_speed = 0;
-	}else if(s > 200){
-		_speed = 200;
-	}else{
-		_speed = s;
-	}	
-}
-
-int Car::getSpeed(){
-	return _speed;
-}
-
-void Car::turbo(int s){
-	_speed += s;
-	if(_speed<0){
-		_speed = 0;
-	}else if(_speed > 200){
-		_speed = 200;
-	}
-}
 
 using namespace std;
 int main() {
-	Car car1 = Car(80);
-	car1.turbo(100);
-	car1.turbo(-200);	
-	cout << "汽車的速度是" << car1.getSpeed() << endl;
-	return 0;
+	Square sq = Square();
+	sq.width = 10;
+	cout << "正方型的面積是" << sq.getArea() << endl;
 } 

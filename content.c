4c6f616b281e653ad5bfd08087e48d6be8d53683
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 21:17:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,7 +10,13 @@ class Car{
 };
 
 Car::Car(int s){
-	_speed = s;
+	if(s<0){
+		_speed = 0;
+	}else if(s > 200){
+		_speed = 200;
+	}else{
+		_speed = s;
+	}	
 }
 
 int Car::getSpeed(){
@@ -19,7 +25,7 @@ int Car::getSpeed(){
 
 using namespace std;
 int main() {
-	Car car1 = Car(50);
+	Car car1 = Car(80);	
 	cout << "汽車的速度是" << car1.getSpeed() << endl;
 	return 0;
 } 

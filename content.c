847b79d6261f9c430364f061d4e65c70a718c951
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 21:24:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,6 +6,7 @@ class Car{
 	public:
 		Car(int s);
 		int getSpeed();
+		void turbo(int s);
 
 };
 
@@ -23,9 +24,20 @@ int Car::getSpeed(){
 	return _speed;
 }
 
+void Car::turbo(int s){
+	_speed += s;
+	if(_speed<0){
+		_speed = 0;
+	}else if(_speed > 200){
+		_speed = 200;
+	}
+}
+
 using namespace std;
 int main() {
-	Car car1 = Car(80);	
+	Car car1 = Car(80);
+	car1.turbo(100);
+	car1.turbo(-200);	
 	cout << "汽車的速度是" << car1.getSpeed() << endl;
 	return 0;
 } 

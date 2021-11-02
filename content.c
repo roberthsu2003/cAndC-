02698e5abe6aa3cc19fe91feb79e2c229740b2d1
Robert Hsu <roberthsu2003@gmail.com>
2021-11-02 19:45:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,19 +3,36 @@ using namespace std;
 
 class Student{
 	public:
+		Student();
+		Student(string n);
 		string name;
 		int chinese;
 		int english;
 		int math;
 };
 
-int main() {
-	Student stu1 = Student();
-	stu1.name = "robert";
-	stu1.chinese = 84;
-	stu1.english = 92;
-	stu1.math = 74;
+Student::Student(){
 
-	cout <<stu1.name << "的總分" << stu1.chinese+stu1.english+stu1.math << endl;
+}
+
+Student::Student(string n){
+	int max = 100;
+	int min = 60;
+	name = n;
+	chinese = random() % (max-min+1) + min;
+	english = random() % (max-min+1) + min;
+	math = random() % (max-min+1) + min;
+}
+
+
+int main() {
+	int nums = 50;
+	Student stu1 = Student();
+	Student students[nums];
+	for(int i=0; i<nums; i++){
+		students[i] = Student("stu"+to_string(i+1));
+		cout << students[i].name << endl;
+		cout << students[i].chinese << endl;
+	}
 	return 0;
 } 

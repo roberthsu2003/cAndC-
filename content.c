e733293b2c438158acadf057002cf50f1d2b9a97
Robert Hsu <roberthsu2003@gmail.com>
2021-11-07 10:55:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int num,result;
-	//printf("請輸入一個數值:");
-	cout << "請輸入一個數值:";
-	//scanf("%d",&num);
-	cin >> num;
-	result = num;
-	result *= num;
-	//printf("%d的平方是%d\n",num, result);
-	cout << num << "的平方是" << result << endl;
-	result *= num;
-	//printf("%d的立方是%d\n",num, result);
-	cout << num << "的立方是" << result << endl;
-} 
+	int in1,in2,in3;
+	int max;
+	cout << "請輸入第1個數字:";
+	cin >> in1;
+
+	cout << "請輸入第2個數字:";
+	cin >> in2;
+	max = in1 > in2 ?  in1 : in2 ;
+
+	cout << "請輸入第3個數字:";
+	cin >> in3;
+
+	max = max > in3 ? max : in3;
+
+	cout << "最大數是:" << max << endl;
+}  

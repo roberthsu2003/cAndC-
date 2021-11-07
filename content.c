ab9f89117181988d380d67f63922606392b12a2d
Robert Hsu <roberthsu2003@gmail.com>
2021-11-07 09:51:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,41 +1,8 @@
-#include <iostream>
+#include <stdio.h>
 
-class Square{
-	public:
-		int width;
-		virtual int getArea(){
-			return width * width;
-		}
-};
-
-class Rectangle:public Square{
-	public:
-		int height;
-		int getArea() override{
-			return width* height;
-		}
-};
-
-class Triangle:public Rectangle{
-	public:
-	int getArea() override{
-		return (width*height) / 2;
-	}
-};
-
-using namespace std;
-int main() {
-	Square sq = Square();
-	sq.width = 10;
-	cout << "正方型的面積是" << sq.getArea() << endl;
-
-	Rectangle rec = Rectangle();
-	rec.width = 10;
-	rec.height = 50;
-	cout << "矩型的面積是" << rec.getArea() << endl;
-
-	Triangle tri = Triangle();
-	tri.width = 10;
-	tri.height = 50;
-	cout << "三角型的面積是" << tri.getArea() << endl;
- } 
+int main(void) {
+  int n = 10;
+  n *= 10;
+  printf("n=%d",n);
+  return 0;
+}

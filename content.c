b42282511684be69dfc877b35f0e4bab29f82bce
Robert Hsu <roberthsu2003@gmail.com>
2021-11-07 14:10:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,16 +8,10 @@ int main() {
 	cout << "請輸入國文和數學的成績:(國文,數學):";
 	scanf("%d,%d",&chinese,&math);
 	//巢狀判斷
-	if(chinese == 100){
-		if(math==100){
-			bonus = 1000;
-		}else{
-			bonus = 500;
-		}
-	}else{
-		if(math==100){
-			bonus = 500;
-		}
+	if(chinese == 100 && math == 100){
+		bonus = 1000;
+	}else if(chinese == 100 || math == 100){
+		bonus = 500;
 	}
 
 	cout << "獎金是" << bonus << endl;

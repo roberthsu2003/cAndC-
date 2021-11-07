1ec252e89a3d83ff143830c633b85d9f56a99375
Robert Hsu <roberthsu2003@gmail.com>
2021-11-07 16:24:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,11 +5,14 @@ int main() {
 	int deposite = 0;
 	int count = 0;
 	int inputNum;
-	while(deposite < 53500){
+	while(true){
 		count += 1;
 		cout << "請輸入第" << count << "個月份的存款:";
 		cin >> inputNum;
 		deposite += inputNum;
+		if(deposite >= 53500){
+			break;
+		}
 	}
 
 	cout << "已經存夠了!存了" << count << "個月,總存款為:" << deposite << "元。";

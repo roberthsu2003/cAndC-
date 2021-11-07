13237039a7c24782342e5fbaa266a42910777edc
Robert Hsu <roberthsu2003@gmail.com>
2021-11-07 15:33:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int num;
-	cout << "請輸入數值:";
-	cin >> num;
-	int total=0;
-	for(int i=1; i<=num; i++){
-		total += i;
+	int n;
+	int sum = 0;
+	for(int i=1; i<=7; i++){
+		if(i == 7){
+			cout << "請輸入星期日的支出:";
+		}else{
+			cout << "請輸入星期" << i << "的支出:";
+		}
+		
+		cin >> n;
+		sum += n;
 	}
 
-	cout << "1~"<< num << "的加總是" << total << endl;
-	
+	cout << "本星期的支出為:" << sum << "元";
 } 

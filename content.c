8b192f8a06785bd0580915c4cd2c3eb6be00bac2
Robert Hsu <roberthsu2003@gmail.com>
2021-11-07 10:36:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,16 @@
 #include <iostream>
-
+using namespace std;
 int main() {
 	int num,result;
-	printf("請輸入一個數值:");
-	scanf("%d",&num);
+	//printf("請輸入一個數值:");
+	cout << "請輸入一個數值:";
+	//scanf("%d",&num);
+	cin >> num;
 	result = num;
 	result *= num;
-	printf("%d的平方是%d\n",num, result);
+	//printf("%d的平方是%d\n",num, result);
+	cout << num << "的平方是" << result << endl;
 	result *= num;
-	printf("%d的立方是%d\n",num, result);
+	//printf("%d的立方是%d\n",num, result);
+	cout << num << "的立方是" << result << endl;
 } 

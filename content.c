db4ff6341c6d864e1511cef08f8b30ea9f422fae
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 15:26:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,12 +10,16 @@ int main() {
 
 	srand(time(NULL));
 	int students[stuNum][SUBNUM];
-	cout << "國文\t英文\t數學\t歷史\t地理\t理化" << endl;	
+	cout << "國文\t英文\t數學\t歷史\t地理\t理化\t總分\t平均" << endl;	
 	for(int i=0; i<stuNum; i++){
+		int sum=0;
 		for(int j=0;j<SUBNUM;j++){
 			students[i][j] = random() % (100-50+1) + 50;
 			cout << students[i][j] << "\t\t";
+			sum += students[i][j];
 		}
+		cout << sum << "\t\t";
+		printf("%.2f",float(sum)/SUBNUM);
 		cout << endl;
 	}
 	

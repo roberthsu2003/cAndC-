b1610d25b6dffd36673b188623c58545513e81e1
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 10:17:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,27 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
-	int deposite = 0;
-	int count = 0;
 	int inputNum;
-	while(true){
-		count += 1;
-		cout << "請輸入第" << count << "個月份的存款:";
+	int num=0;
+	int sum = 0;
+	do{
+		
+		cout << "請輸入第" << num+1 << "個值:";
 		cin >> inputNum;
-		deposite += inputNum;
-		if(deposite >= 53500){
+		if(inputNum < 0){
 			break;
 		}
-	}
+		++num;
 
-	cout << "已經存夠了!存了" << count << "個月,總存款為:" << deposite << "元。";
+		if(inputNum % 2 == 1){
+			continue;
+		}
+
+		sum += inputNum;
+		
+	}while(true);
+
+	cout << "所有輸入的正偶數的加總是:" << sum << endl;
+	cout << "總共使用者輸入了" << num << "次的加總值" << endl;
 } 

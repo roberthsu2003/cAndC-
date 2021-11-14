9c11554ab4891b9d5d207d94c7819794a8fc68ef
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 14:13:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,32 +1,10 @@
 #include <iostream>
-#define NUM 6
-
 using namespace std;
-
 int main() {
-	
-	int sum = 0;
-	int failNum = 0;
-	string subjects[] = {"國文","英文","數學","歷史","地理","理化"};
-	int scores[NUM];
-
-	for(int i=0;i < NUM;i++){
-		cout << "請輸入" << subjects[i] << "分數:";
-		cin >> scores[i];
-		sum += scores[i];
-		if(scores[i] < 60){
-			failNum ++;
-		}
+	srand(time(NULL));
+	int scores[6];
+	for(int i=0; i<6; i++){
+		scores[i] = random() % (100-50+1) + 50;
+		cout << scores[i] << endl;
 	}
-	cout << endl;
-
-	cout << "同學的分數:" << endl;
-	for(int i=0;i < NUM;i++){
-		cout << subjects[i] << scores[i] << endl;
-	}
-
-	cout << "總分:" << sum << endl;
-	printf("平均為:%.2f\n",float(sum)/NUM);
-	cout << "不及格的有" << failNum << "科" << endl;
-	
 } 

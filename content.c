a83694b39336cf6b924cce40af211b22dcf611c0
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 12:14:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main() {	
-	srandom(time(NULL));
-	int max = 99;
-	int min = 1;
-	int count = 0;
-	int keyin;
-  	int guess = random() % (99-1+1) + 1;
-	cout << "===========猜數字遊戲===========" << endl;
-	while(true){
-		printf("猜數字範圍%d~%d:",min,max);
-		cin >> keyin;
-		count += 1;
-		if(keyin >= min && keyin <= max){
-			if(keyin == guess){
-				cout << "恭喜!猜對了!" << endl;
-				cout << "您猜了" << count << "次" << endl;
-				break;
-			}else if(keyin > guess){
-				cout << "再小一點" << endl;
-				max = keyin - 1;
-			}else if(keyin < guess){
-				cout << "再大一點" << endl;
-				min = keyin + 1;
-			}
-			cout << "您已經猜了" << count << "次" << endl;
-			cout << endl;
-		}else{
-			cout << "超出範圍,請重新輸入" << endl;
-		}
-
-	}
-
-	cout << "遊戲結束" << endl;
+int main() {
+	int scores[6];
+	scores[0] = 96;
+	scores[1] = 50;
+	scores[2] = 76;
+	scores[3] = 81;
+	scores[4] = 57;
+	scores[5] = 80;
+	cout << "同學的分數:" << endl;
+	cout << "國文" << scores[0] << endl;
+	cout << "英文" << scores[1] << endl;
+	cout << "數學" << scores[2] << endl;
+	cout << "歷史" << scores[3] << endl;
+	cout << "地理" << scores[4] << endl;
+	cout << "理化" << scores[5] << endl;
 } 

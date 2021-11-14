ee5b2c6490a0ee5ee6503733b0dd974a74df1804
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 12:27:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
+	string subjects[] = {"國文","英文","數學","歷史","地理","理化"};
 	int scores[6];
 	scores[0] = 96;
 	scores[1] = 50;
@@ -9,10 +10,8 @@ int main() {
 	scores[4] = 57;
 	scores[5] = 80;
 	cout << "同學的分數:" << endl;
-	cout << "國文" << scores[0] << endl;
-	cout << "英文" << scores[1] << endl;
-	cout << "數學" << scores[2] << endl;
-	cout << "歷史" << scores[3] << endl;
-	cout << "地理" << scores[4] << endl;
-	cout << "理化" << scores[5] << endl;
+	for(int i=0;i<=5;i++){
+		cout << subjects[i] << scores[i] << endl;
+	}
+	
 } 

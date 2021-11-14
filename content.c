777ff8ce61be30dd2d2486bc0ecf8222fe3208fc
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 16:20:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,49 @@
 #include <iostream>
 using namespace std;
 
-#include "tools.h"
-
-int main() {
-	int stuNum;	
-	cout << "請輸入學生數:";
-	cin >> stuNum;
-
-	studentScore(stuNum);
-	return 0;
-	
-} 
-
-
-tools.h
-
-#include <iostream>
-using namespace std;
-void studentScore(int stuNum){
-	const int SUBNUM = 6;
-	srand(time(NULL));
-	int students[stuNum][SUBNUM];
-	cout << "國文\t英文\t數學\t歷史\t地理\t理化\t總分\t平均" << endl;	
-	for(int i=0; i<stuNum; i++){
-		int sum=0;
-		for(int j=0;j<SUBNUM;j++){
-			students[i][j] = random() % (100-50+1) + 50;
-			cout << students[i][j] << "\t\t";
-			sum += students[i][j];
+void playGame(int minume, int maxmum){
+	srandom(time(NULL));
+	int max = maxmum;
+	int min = minume;
+	int count = 0;
+	int keyin;
+  	int guess = random() % (99-1+1) + 1;
+	cout << "===========猜數字遊戲===========" << endl;
+	while(true){
+		printf("猜數字範圍%d~%d:",min,max);
+		cin >> keyin;
+		count += 1;
+		if(keyin >= min && keyin <= max){
+			if(keyin == guess){
+				cout << "恭喜!猜對了!" << endl;
+				cout << "您猜了" << count << "次" << endl;
+				break;
+			}else if(keyin > guess){
+				cout << "再小一點" << endl;
+				max = keyin - 1;
+			}else if(keyin < guess){
+				cout << "再大一點" << endl;
+				min = keyin + 1;
+			}
+			cout << "您已經猜了" << count << "次" << endl;
+			cout << endl;
+		}else{
+			cout << "超出範圍,請重新輸入" << endl;
 		}
-		cout << sum << "\t\t";
-		printf("%.2f",float(sum)/SUBNUM);
-		cout << endl;
+
 	}
 }
+
+int main() {	
+	while(true){
+		playGame(20,80);
+		char yn;
+		cout << "好玩嗎?再來一盤吧?(y or n)):";
+		cin >> yn;
+		if(yn=='n'){
+			break;
+		}
+	}
+	cout << "遊戲結束!" << endl;
+	return 0;	
+} 

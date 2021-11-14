f083bf2b6ec38cb2092a7ba0c31fa5745fd24934
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 13:39:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
 	string subjects[] = {"國文","英文","數學","歷史","地理","理化"};
 	int scores[6];
-	scores[0] = 96;
-	scores[1] = 50;
-	scores[2] = 76;
-	scores[3] = 81;
-	scores[4] = 57;
-	scores[5] = 80;
+
+	for(int i=0;i<=5;i++){
+		cout << "請輸入" << subjects[i] << "分數:";
+		cin >> scores[i];
+	}
+	
 	cout << "同學的分數:" << endl;
 	for(int i=0;i<=5;i++){
 		cout << subjects[i] << scores[i] << endl;

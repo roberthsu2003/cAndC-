7a405132c286636cfc49e97cc653479c6f7038ff
Robert Hsu <roberthsu2003@gmail.com>
2021-11-14 14:36:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main() {	
+	int stuNum;
+	int subjectNum;
+	cout << "請輸入學生數:";
+	cin >> stuNum;
+	cout << "請輸入科目數:";
+	cin >> subjectNum;
+
 	srand(time(NULL));
-	int students[35][6];	
-	for(int i=0; i<35; i++){
-		for(int j=0;j<6;j++){
+	int students[stuNum][subjectNum];	
+	for(int i=0; i<stuNum; i++){
+		for(int j=0;j<subjectNum;j++){
 			students[i][j] = random() % (100-50+1) + 50;
 			cout << students[i][j] << " ";
 		}

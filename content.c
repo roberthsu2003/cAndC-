ad99817977b9e3654986eeb8ba37babfe2dcc014
徐國堂 <roberthsu2003@gmail.com>
2021-12-07 21:53:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,20 @@
 
 int main(void) {
 	int first;
+	int second;
+	int total;
+
   	printf("請輸入第一個數值:");
 	scanf("%d",&first);
 	printf("first變數的內容是%d\n",first);
+
+	printf("請輸入第2個數值:");
+	scanf("%d",&second);
+	printf("second變數是內容是:%d\n",second);
+
+	total = first * second;
+
+	printf("%d * %d = %d\n",first,second,total);
+
   	return 0;
 }

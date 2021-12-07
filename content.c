99d991934ae051343d56cf539e537aad4f6e79f5
徐國堂 <roberthsu2003@gmail.com>
2021-12-07 21:39:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-	// 建立a1,a2 2個變數
-	int a1;
-	int a2;
-
-	//a1,a2給值
-	a1 = 521;
-	a2 = 281;
-
-	//建立變數
-	int a3;
-
-	a3 = a1 * a2;
-
-	//輸出
-
-	printf("%d*%d=%d\n",a1,a2,a3);
-
-
-	return 0;
+	int first;
+  	printf("請輸入第一個數值:");
+	scanf("%d",&first);
+	printf("first變數的內容是%d\n",first);
+  	return 0;
 }

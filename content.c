e8e9c1e9f59e7dd232bf7006e35097d80b882a6e
徐國堂 <roberthsu2003@gmail.com>
2021-12-09 20:41:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
-
 int main(void) {
   double PI = 3.1415915987;
   printf("PI=%f\n",PI);
   printf("PI=%.2f\n",PI);
   printf("PI=%.10f\n",PI);
+
+  unsigned int a = 20;
+  printf("a=%ud\n",a);
+
+  long long int b = 100;
+  printf("b=%lld\n",b);
+
+
   return 0;
 }

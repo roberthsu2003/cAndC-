1a06acb3873ef55d94cb32273b7cb94b20cf689e
徐國堂 <roberthsu2003@gmail.com>
2021-12-09 20:12:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("姓名\t\t座號\t電話\n");
-  printf("李\"振\"聲\t11\t\t0229435437\n");
-  printf("張四聲\t\t13\t\t0229435467\n");
+  char charB;
+  charB = 'a';
+  printf("%c\n",charB);
+
+  char charC;
+  charC = 97;
+  printf("%c\n",charC);
+
+  char charD;
+  charD = 97;
+  printf("%d\n",charD);
   return 0;
 }

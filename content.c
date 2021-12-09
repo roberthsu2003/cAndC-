1671132a99b9a364d15c8c957f6f6c0e20008bb5
徐國堂 <roberthsu2003@gmail.com>
2021-12-09 21:57:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
-  int chinese, english, math;
-  int sum;
-  printf("請輸入國文成績:");
-  scanf("%d",&chinese);
+int main() {
+	int chinese, english, math;
+	int sum;
+	printf("請輸入國文成績:");
+	scanf("%d",&chinese);
 
-  printf("請輸入英文成績:");
-  scanf("%d",&english);
+	printf("請輸入英文成績:");
+	scanf("%d",&english);
 
-  printf("請輸入數學成績:");
-  scanf("%d",&math);
+	printf("請輸入數學成績:");
+	scanf("%d",&math);
 
-  sum = chinese + english + math;
-  printf("3科總分是%d",sum);
-  //強制轉換為float
-  printf("3科平均是%.2f\n",(float)sum/3);
+	sum = chinese + english + math;
+	printf("3科總分是%d\n",sum);
+	//強制轉換為float
+	// printf("3科平均是%.2f\n",(float)sum/3);
+	printf("3科平均是%.2f\n",sum/3.0);
 
-  return 0;
-}
+	return 0;
+} 

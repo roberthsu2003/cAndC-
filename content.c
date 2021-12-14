7360ef0c3cfffa35986d05212371cedb57078838
徐國堂 <roberthsu2003@gmail.com>
2021-12-14 20:16:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,24 +2,15 @@
 using namespace std;
 
 int main() {
-	int x = 5;
-	x = x + 1;
-	cout << "x:"<< x << endl;
+	int x = 1;
+	int y;
+	y = x++;
 
-	int y = 1;
-	y++; 
-	cout << "y:" <<y << endl;
+	cout << "x=" << x << ",y=" << y << endl;
 
-
-	int y1 = 1;
-	++y1;
-	cout << "y1:" << y1 << endl;
 
 	int n = 1;
-	n--;
-	cout << "n:" << n  << endl;
-
-	int n1 = 1;
-	--n1;
-	cout << "n1:" << n1 << endl;
+	int m;
+	m = ++n;
+	cout << "n=" << n << ",m=" << m << endl;
 } 

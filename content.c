93566e9e332a408192dd988f1c976a403e05f293
徐國堂 <roberthsu2003@gmail.com>
2021-12-14 19:38:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,26 +2,13 @@
 using namespace std;
 
 int main() {
-	int chinese, english, math;
-	int sum;
-	
-	cout << "請輸入國文成績:";
-	cin >> chinese;
+	int n,m;
+	cout << "請輸入除數(整數):";
+	cin >> n;
 
-	
+	cout << "請輸入除數(整數,不可以為0):";
+	cin >> m;
 
-	cout << "請輸入英文成績:";
-	cin >> english;
+	cout << "商:" << n/m << ",餘數:" << n%m << endl;
 
-	
-
-	cout << "請輸入數學成績:";
-	cin >> math;
-
-	sum = chinese + english + math;
-	
-	cout << "3科總分是" << sum << endl;
-	//cout << "3科平均是" << sum/3.0 << endl;
-	printf("3科平均是%.2f\n", sum/3.0);
-	return 0;
 } 

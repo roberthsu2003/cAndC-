66df8a79d17a7385e37fc60281c67a90eb47b756
徐國堂 <roberthsu2003@gmail.com>
2021-12-16 19:26:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,24 @@
+/*
+成績及格判斷
+請使用者輸入成績，若成績大於等於60分，則顯示及格，否則顯示不及格。
+
+顯示:
+請輸入成績:78
+及格!(不及格!)
+*/
+
 #include <iostream>
 using namespace std;
+
 int main() {
-	string password;
-	cout << "請輸入密碼:";
-	cin >> password;
-
-	if(password == "1234"){
-		//true的程式區塊
-		cout << "歡迎光臨!\n";
+	int score;
+	cout << "請輸入成績:";
+	cin >> score;
+	cout << "分數:" << score << endl;	
+	if(score >= 60){		
+		cout << "及格!\n";
 	}else{
-		//false的程式區塊
-		cout << "密碼錯誤!\n";
-		cout << "請重新輸入!\n";
+		cout << "不及格\n";
 	}
-
 	return 0;
 } 

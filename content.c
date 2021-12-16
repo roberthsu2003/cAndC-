398de4ec506746423fa996d73cb4520a5f9bf2f1
徐國堂 <roberthsu2003@gmail.com>
2021-12-16 19:54:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,23 @@
-/*
-成績及格判斷
-請使用者輸入成績，若成績大於等於60分，則顯示及格，否則顯示不及格。
-
-顯示:
-請輸入成績:78
-及格!(不及格!)
-*/
-
 #include <iostream>
 using namespace std;
 
 int main() {
-	int score;
-	cout << "請輸入成績:";
-	cin >> score;
-	cout << "分數:" << score << endl;	
-	if(score >= 60){		
-		cout << "及格!\n";
+	int money;
+	int payMoney;
+	cout << "請輸入購買金額:";
+	cin >> money;
+	if(money >= 100000){
+		payMoney = money * 0.8;
+	}else if(money >= 50000){
+		payMoney = money * 0.85;
+	}else if(money >= 30000){
+		payMoney = money * 0.9;
+	}else if(money >= 10000){
+		payMoney = money * 0.95;
 	}else{
-		cout << "不及格\n";
+		payMoney = money;
 	}
+
+	cout << "實付金額是:" << payMoney << "元\n";
 	return 0;
 } 

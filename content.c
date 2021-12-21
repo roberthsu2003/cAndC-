5f613304f8750067a0576188be35b1ad37e61dc3
徐國堂 <roberthsu2003@gmail.com>
2021-12-21 19:59:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,15 +4,18 @@ using namespace std;
 int main() {
 	int score;
 	int sum=0;
-	do{
-		cout << "請輸入學生的成績:";
+	int num = 0;
+	do{		
+		cout << "請輸入第" << num+1 << "位學生的成績:";
 		cin >> score;
 		if(score<0){
 			break; //直接跳出迴圈
 		}
+		num += 1;
 		sum += score;
 	}while(score >= 0);
 
+	cout << "學生人數是" << num << endl;
 	cout << "全班總成績為:" << sum << endl;
 	return 0;
 } 

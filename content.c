287ba0c6158f026051ac832325f9a4019353bfa7
徐國堂 <roberthsu2003@gmail.com>
2021-12-21 21:50:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,16 +8,18 @@ int main() {
 	int guessValue;
 	int max = 99;
 	int min = 1;
+	int count = 0;
 	srandom(time(NULL));
-	int randomValue = random() % (max-min+1) + min;
-	cout << randomValue << endl;
+	int randomValue = random() % (max-min+1) + min;	
 	cout << "============猜數字遊戲================\n\n";
 	while(true){
 		printf("猜數字範圍%d~%d:",min,max);
 		cin >> guessValue;
+		count += 1;
 		if(guessValue >= min && guessValue <= max){
 			if(guessValue == randomValue){
 				cout << "賓果!猜對了,答案是" << guessValue << endl;
+				cout << "您猜了" << count << "次" << endl;
 				break;
 			}else if(guessValue > randomValue){
 				max = guessValue - 1;
@@ -26,6 +28,7 @@ int main() {
 				min = guessValue + 1;
 				cout << "再大一點!";
 			}
+			cout << "您已經猜了" << count << "次" << endl;
 		}else{
 			cout << "請輸入提示範圍內的數字!\n";
 		}

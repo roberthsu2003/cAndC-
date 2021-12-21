6dabd7314d6a4ff1d0ee1bc96643dca137129ab6
徐國堂 <roberthsu2003@gmail.com>
2021-12-21 20:31:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,21 +2,20 @@
 using namespace std;
 
 int main() {
-	int score;
-	int sum=0;
 	int num = 0;
-	
-	while(true){		
-		cout << "請輸入第" << num+1 << "位學生的成績:";
-		cin >> score;
-		if(score<0){
-			break; //直接跳出迴圈
+	int inputValue;
+	int sum = 0;
+	while(true){
+		cout << "請輸入第" << ++num << "個數值:";
+		cin >> inputValue;
+		if(inputValue < 0){
+			break;
+		}else if(inputValue % 2 == 1){
+			continue;
 		}
-		num += 1;
-		sum += score;
+
+		sum += inputValue;
 	}
 
-	cout << "學生人數是" << num << endl;
-	cout << "全班總成績為:" << sum << endl;
-	return 0;
+	cout << "所有輸入的正偶數的加總是:" << sum << endl;
 } 

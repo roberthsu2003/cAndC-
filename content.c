6c3212f656ae9fa99904abadec9b795bafcf7052
徐國堂 <roberthsu2003@gmail.com>
2021-12-23 20:22:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include "tool.h"
 
@@ -7,21 +5,18 @@ using namespace std;
 int main() {
 	int fvalue;
 	float result;
-	cout << "請輸入攝氏溫度:";
-	cin >> fvalue;
-	result = temperature(fvalue); //呼叫function
-	cout << "華氏溫度=" << result << endl;
+	char doagain;
+	
+
+	do{
+		cout << "請輸入攝氏溫度:";
+		cin >> fvalue;
+		result = temperature(fvalue); //呼叫function
+		cout << "華氏溫度=" << result << endl;
+		
+		cout << "請要繼續輸入嗎?(y or n))";
+		cin >> doagain;
+	}while(doagain == 'y');
+	cout << "應用程式結束" << endl;
 	return 0;
 } 
-
-
-tool.h
-
-//定義function
-float temperature(int fvalue){
-	/*
-	float cvalue = 1.8 * fvalue + 32;
-	return cvalue;
-	*/
-	return 1.8 * fvalue + 32;
-}

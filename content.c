e24d6d830ece619f610b51a9554ad95a36512b6d
徐國堂 <roberthsu2003@gmail.com>
2021-12-23 21:26:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,61 +1,16 @@
-main.cpp
-
 #include <iostream>
-#include "game.h"
-
 using namespace std;
 
 int main() {
-	while(true){
-		number_game();
-		char doagain;
-		cout << "還要繼續嗎?(y,n)";
-		cin >> doagain;
-		if(doagain == 'y'){
-			continue;			
-		}else{		
-			cout << "遊戲結束" << endl;
-			break;
-		}
-	}
+	//宣告陣列變數
+	int scores[3];
+
+	//給元素值
+	scores[0] = 78;
+	scores[1] = 89;
+	scores[2] = 91;
+
+	//取出元素值
+	cout << scores[0] <<","<< scores[1] << "," << scores[2] << endl;
+	printf("%d,%d,%d\n",scores[0],scores[1],scores[2]);
 } 
-
-game.h
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-
-using namespace std;
-
-void number_game(){
-	int guessValue;
-	int max = 99;
-	int min = 1;
-	int count = 0;
-	srandom(time(NULL));
-	int randomValue = random() % (max-min+1) + min;	
-	cout << "============猜數字遊戲================\n\n";
-	while(true){
-		printf("猜數字範圍%d~%d:",min,max);
-		cin >> guessValue;
-		count += 1;
-		if(guessValue >= min && guessValue <= max){
-			if(guessValue == randomValue){
-				cout << "賓果!猜對了,答案是" << guessValue << endl;
-				cout << "您猜了" << count << "次" << endl;
-				break;
-			}else if(guessValue > randomValue){
-				max = guessValue - 1;
-				cout << "再小一點!";
-			}else{
-				min = guessValue + 1;
-				cout << "再大一點!";
-			}
-			cout << "您已經猜了" << count << "次" << endl;
-		}else{
-			cout << "請輸入提示範圍內的數字!\n";
-		}
-		
-
-	}
-}

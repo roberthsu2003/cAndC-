3b751fdcbf9c586bad357d4b2735991d270d2444
徐國堂 <roberthsu2003@gmail.com>
2021-12-25 10:19:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,13 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-	srandom(time(NULL));
-	//宣告陣列變數
-	int count = 100;
-	int scores[count];
-	for(int i=0; i<count; i++){		 	
-		scores[i] = random() % (100-50+1) + 50;
-	}
-
-	
-
-	//取出元素值
-	for(int i=0;i<count;i++){
-		cout << "第" << i+1 << "個分數是:" << scores[i] << endl;
-	}
-} 
+int main(void) {
+  int x = 1;
+  int y = ++x;
+  printf("x=%d,y=%d\n",x,y);
+  
+  int m = 1;
+  int n = m++;
+  printf("m=%d,n=%d\n",m,n);
+  
+  return 0;
+}

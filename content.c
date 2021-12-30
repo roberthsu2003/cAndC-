c39f6687228618b018be863d2cda9e3786b863fa
徐國堂 <roberthsu2003@gmail.com>
2021-12-30 21:26:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
-	int scores[3];
+	int num = 4;
+	int scores[num];
 	scores[0] = 87;
 	scores[1] = 75;
 	scores[2] = 90;
+	scores[3] = 45;
+
+	for(int i=0;i<num-1;i++){		
+		for(int j=i+1;j<num;j++){
+			cout << "i=" << i << "比對" << "j=" << j << endl;
+			cout << scores[i] << "比對" << scores[j] << endl;
+		}
+		cout << "================\n";
+	}	
 } 

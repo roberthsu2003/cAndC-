e92f8995f8589ec200e0423d91b2e787c8a2824b
徐國堂 <roberthsu2003@gmail.com>
2021-12-30 20:50:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,18 +6,4 @@ int main() {
 	scores[0] = 87;
 	scores[1] = 75;
 	scores[2] = 90;
-
-	//c++的輸出語法
-	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
-
-
-	//c的輸出語法
-	printf("%d,%d,%d\n",scores[0],scores[1],scores[2]);
-
-	
-	cout << "=========================" << endl;
-	//使用for做traveling
-	for(int i=0;i<3;i++){
-		cout << scores[i] << endl;
-	}
- } 
+} 

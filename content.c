2932075ee41f64841af7f71f42249a697662bc92
徐國堂 <roberthsu2003@gmail.com>
2022-01-01 09:52:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,40 +2,31 @@
 using namespace std;
 
 int main() {
-	int num = 4;
-	int scores[num];
-	scores[0] = 87;
-	scores[1] = 75;
-	scores[2] = 90;
-	scores[3] = 45;
-	
-	//使用泡沫排序,讓所有的元素值都有相互比對
-	for(int i=0;i<num-1;i++){		
-		for(int j=i+1;j<num;j++){
-			cout << "i=" << i << "比對" << "j=" << j << endl;
-			cout << scores[i] << "比對" << scores[j] << endl;
-			//由大到小排序
-			/*
-			if(scores[i] < scores[j]){
-				//2數對調
-				int temp = scores[i];
-				scores[i] = scores[j];
-				scores[j] = temp;
-			}
-			*/
-			//由小到大排序
-			if(scores[i] > scores[j]){
-				//2數對調
-				int temp = scores[i];
-				scores[i] = scores[j];
-				scores[j] = temp;
-			}
+	int chinese,math;
+	int bouns;
+	cout << "請輸入國文分數:";
+	cin >> chinese;
+	cout << "請輸入數學分數:";
+	cin >> math;
 
+	if(chinese == 100){
+		if(math == 100){
+			bouns = 1000;
+		}else{
+			bouns = 500;
 		}
-		cout << "================\n";
+
+	}else{
+
+		if(math == 100){
+			bouns = 5000;
+		}else{
+			bouns = 0;
+		}
+
 	}
 
-	for(int i=0; i<num; i++){
-		cout << scores[i] << endl;
-	}	
+	cout << "獎金是:" << bouns << endl;
+
+
 } 

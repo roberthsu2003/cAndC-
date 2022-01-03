8a9cd1a8a0538f6a3d92912beea5b6092ca2ad59
徐國堂 <roberthsu2003@gmail.com>
2022-01-03 20:12:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,23 +2,11 @@
 using namespace std;
 
 int main() {
-	int num = 0;
-	int inputNum;
-	int sum = 0;
-	while(true){
-		
-		cout << "請輸入第" << num + 1 << "個數值:";
-		cin >> inputNum;
-		if(inputNum < 0){
-			break;
-		}else if(inputNum % 2 == 1){
-			num += 1;
-			continue;
-			
-		}
-		sum += inputNum;
-		num += 1;
-	}
+	double PI = 3.1415915987;
+	int radius = 9;
+	double area;
+	area = PI * radius * radius;
+	cout << "圓面積=" << area << endl;
 
-	cout << "共加總" << num << "次,偶數加總的值是" << sum << endl;
+	return 0;
 } 

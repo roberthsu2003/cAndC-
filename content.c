f9832e345dcde0e62674394a8e6ea8be558308c3
徐國堂 <roberthsu2003@gmail.com>
2022-01-04 20:43:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,13 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-	int n=10;
-	//建立指標變數p,p是儲存n變數的記憶體位址
-	int* p = &n;
+//定義function
+int add(int n, int m){
+	return n + m;
+}
 
-	cout << *p << endl;
+int main() {
+	//呼叫function
+	cout << add(699,971) << endl;
 } 

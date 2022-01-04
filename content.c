d18fd74f72bffce8b43b815c250020f122779e1e
徐國堂 <roberthsu2003@gmail.com>
2022-01-04 19:40:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,10 +8,16 @@ int main() {
 	srandom(time(NULL));
 	int scores[50][5];
 	for(int i=0;i<50;i++){
+		int sum = 0;
+
 		for(int j=0;j<5;j++){
 			scores[i][j] = random() % 100;
+			sum += scores[i][j];
 			cout << scores[i][j] << "\t";
 		}
+
+		cout << sum << "\t";
+		cout << sum / 5 << "\t";
 		cout << endl;
 	}
 

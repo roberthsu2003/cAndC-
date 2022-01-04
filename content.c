125f18b041b93bd33cd9035fae0ef75cf419ced1
徐國堂 <roberthsu2003@gmail.com>
2022-01-04 20:05:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,17 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-
 using namespace std;
-//使用2維陣列，儲存50個學生，每個學生5個分數
+
 int main() {
-	srandom(time(NULL));
-	int scores[50][5];
-	cout << "國文\t英文\t數學\t社會\t歷史\t總分\t平均" << endl;
-	for(int i=0;i<50;i++){
-		int sum = 0;
+	double m = 5;
+	int n = 10;
 
-		for(int j=0;j<5;j++){
-			scores[i][j] = random() % (100-60+1) + 60;
-			sum += scores[i][j];
-			cout << scores[i][j] << "\t\t";
-		}
+	cout << "變數m的值=" << m << endl;
+	cout << "變數n的值=" << n << endl;
 
-		cout << sum << "\t\t";
-		cout << sum / 5 << "\t";
-		cout << endl;
-	}
+	cout << "變數m的位址=" << &m << endl;
+	cout << "變數n的位址=" << &n << endl;
 
+	cout << "變數m的記憶體=" << sizeof(m) << endl;
+	cout << "變數n的記憶體=" << sizeof(n) << endl;
 
 } 

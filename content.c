8228c44d7f41af4b906457714bd302552c422cdc
徐國堂 <roberthsu2003@gmail.com>
2022-01-04 19:18:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,15 @@
-// Name        : ladder.cpp
-//讓使用者輸入梯形的上底、下底及高，程式會計算梯形的面積(上底加下底乘以高除以2)
-
 #include <iostream>
 using namespace std;
 
 int main() {
-	float top, bottom, height, area;
-	cout << "請輸入梯形的上底(公分):";
-	cin >> top;
+	int scores[50][5];
+	for(int i=0;i<50;i++){
+		for(int j=0;j<5;j++){
+			scores[i][j] = 0;
+			cout << scores[i][j] << " ";
+		}
+		cout << endl;
+	}
 
-	cout << "請輸入梯形的下底(公分):";
-	cin >> bottom;
 
-	cout << "請輸入梯形的高(公分):";
-	cin >> height;
-
-	area = (top + bottom) * height /2;
-	cout << "梯形的面積:" << area << "平方公分\n";
-	return 0;
-}
+} 

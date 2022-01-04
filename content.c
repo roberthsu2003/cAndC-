7e30ba2a08c099780f01ecd045d7a1d08e71d0b8
徐國堂 <roberthsu2003@gmail.com>
2022-01-04 19:29:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 int main() {
+	srandom(time(NULL));
 	int scores[50][5];
 	for(int i=0;i<50;i++){
 		for(int j=0;j<5;j++){
-			scores[i][j] = 0;
-			cout << scores[i][j] << " ";
+			scores[i][j] = random() % 100;
+			cout << scores[i][j] << "\t";
 		}
 		cout << endl;
 	}

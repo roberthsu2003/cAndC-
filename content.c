56f8ef0d2ea990c3879f84fc1a3b54edb347c45d
徐國堂 <roberthsu2003@gmail.com>
2022-01-06 21:53:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <math.h>
+#include <string.h>
 
 using namespace std;
+
 
 int main() {
 	int value;
@@ -10,4 +12,9 @@ int main() {
 	cout << value << "開根號的值是" << sqrt(value) << endl;
 	cout << max(10,5) << endl;
 	cout << min(10,5) << endl;
+	cout << pow(13,3) << endl;
+
+	for(int i=1; i<=50; i++){
+		cout << "學號" + to_string(i) << endl;
+	}
 } 

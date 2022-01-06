77d0515bc0c52fddce9b8fe0f0a3fa6abe05d05c
徐國堂 <roberthsu2003@gmail.com>
2022-01-06 19:21:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void showArray(int *n){
-	for(int i=0; i<3; i++){
-		n[i] *= 3;
+void sortedArray(int num, int scores[]){
+	for(int i=0;i<num-1;i++){		
+		for(int j=i+1;j<num;j++){			
+			//由小到大排序
+			if(scores[i] < scores[j]){
+				//2數對調
+				int temp = scores[i];
+				scores[i] = scores[j];
+				scores[j] = temp;
+			}
+
+		}		
 	}
 }
 
 int main() {
-	int nums[3] ={111,222,333};
-	cout << nums << endl;
-
-	showArray(nums);
-
-	for(int i=0; i<3; i++){
-		cout << nums[i] << endl;
+	int nums[] = {5, 7, 9, 24, 93}; //建立陣列
+	sortedArray(5, nums); //排序陣列
+	for(int i=0; i<5; i++){ //輸出陣列
+		cout << nums[i] << " ";
 	}
+	cout << endl;
 } 

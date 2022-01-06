d5b63c63b496df6e3601b48efbf528181d30b5cb
徐國堂 <roberthsu2003@gmail.com>
2022-01-06 19:35:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void sortedArray(int num, int scores[]){
+void sortedArrayAscending(int num, int *scores){
+	for(int i=0;i<num-1;i++){		
+		for(int j=i+1;j<num;j++){			
+			//由小到大排序
+			if(scores[i] > scores[j]){
+				//2數對調
+				int temp = scores[i];
+				scores[i] = scores[j];
+				scores[j] = temp;
+			}
+
+		}		
+	}
+}
+
+void sortedArrayDescending(int num, int *scores){
 	for(int i=0;i<num-1;i++){		
 		for(int j=i+1;j<num;j++){			
 			//由小到大排序
@@ -18,7 +33,15 @@ void sortedArray(int num, int scores[]){
 
 int main() {
 	int nums[] = {5, 7, 9, 24, 93}; //建立陣列
-	sortedArray(5, nums); //排序陣列
+	//由小到大
+	sortedArrayAscending(5, nums); //排序陣列
+	for(int i=0; i<5; i++){ //輸出陣列
+		cout << nums[i] << " ";
+	}
+	cout << endl;
+
+	//由大到小
+	sortedArrayDescending(5, nums); //排序陣列
 	for(int i=0; i<5; i++){ //輸出陣列
 		cout << nums[i] << " ";
 	}

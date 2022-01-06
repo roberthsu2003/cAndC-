738dfd471a44c6bc387d74abba65b027ffb58241
徐國堂 <roberthsu2003@gmail.com>
2022-01-06 21:16:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,22 @@
+//全域變數，區塊變數，區域變數
+
 #include <iostream>
 using namespace std;
+int n; //全域變數
 
-int add(int a, int b){
-	return a + b;
-}
-
-//overloading
-double add(double a, double b){
-	return a + b;
-}
-
-//overloading
-string add(string a, string b){
-	return a + b;
+void showNum(){
+	int value = n; //區域變數
+	cout << "======================" << endl;
+	for(int n=value; n >= 1; n--){
+		cout << "區塊變數n=" << n << endl;
+	}
+	cout << "======================" << endl;
+	cout << "全域變數n=" << n << endl;
 }
 
 int main() {
-	cout << add(5,6) << endl;
-	cout << add(5.5, 7.1) << endl;
-	cout << add("Hello! ", "World") << endl;
+	cout << "請輸入全域變數 n=";
+	cin >> n;
+	showNum();
+	return 0;
 } 

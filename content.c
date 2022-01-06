1f019f19e27062c0e6d0b6f456d2ff844bad5694
徐國堂 <roberthsu2003@gmail.com>
2022-01-06 20:28:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,72 +1,20 @@
-main.cpp
-
+//自鍵盤輸入一個數字n, 利用遞迴來計算n階層(n!)
 #include <iostream>
-#include "mysort.h"
-
 using namespace std;
+
+int factorial(int n){
+	if (n ==0){
+		return 1;
+	}else{
+		return n * factorial(n-1);
+	}
+	
+}
+
 int main() {
-	int nums[] = {5, 7, 9, 24, 93}; //建立陣列
-	//由小到大
-	sortedArrayAscending(5, nums); //排序陣列
-	for(int i=0; i<5; i++){ //輸出陣列
-		cout << nums[i] << " ";
-	}
-	cout << endl;
-
-	//由大到小
-	sortedArrayDescending(5, nums); //排序陣列
-	for(int i=0; i<5; i++){ //輸出陣列
-		cout << nums[i] << " ";
-	}
-	cout << endl;
+	int n,total;
+	cout << "請輸入數字n:";
+	cin >> n;
+	total = factorial(n);
+	cout << n << "!是" << total << endl;
 } 
-
-
-mysort.h
-
-/*-------------------------------
-sortedArrayAscending(int num, int *scores)
-這是由小到大排序，num是陣列元素的數量，scores是陣列
-*/
-void sortedArrayAscending(int, int*);
-
-/*-------------------------------
-void sortedArrayDescending(int num, int *scores)
-這是由大到小排序，num是陣列元素的數量，scores是陣列
-*/
-void sortedArrayDescending(int , int*);
-
-
-mysort.cpp
-
-#include "mysort.h"
-
-void sortedArrayAscending(int num, int *scores){
-	for(int i=0;i<num-1;i++){		
-		for(int j=i+1;j<num;j++){			
-			//由小到大排序
-			if(scores[i] > scores[j]){
-				//2數對調
-				int temp = scores[i];
-				scores[i] = scores[j];
-				scores[j] = temp;
-			}
-
-		}		
-	}
-}
-
-void sortedArrayDescending(int num, int *scores){
-	for(int i=0;i<num-1;i++){		
-		for(int j=i+1;j<num;j++){			
-			//由小到大排序
-			if(scores[i] < scores[j]){
-				//2數對調
-				int temp = scores[i];
-				scores[i] = scores[j];
-				scores[j] = temp;
-			}
-
-		}		
-	}
-}

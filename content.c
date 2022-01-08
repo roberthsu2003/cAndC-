b44b80429c7487b429df87d4b667f654c56c2467
徐國堂 <roberthsu2003@gmail.com>
2022-01-08 16:36:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,18 +2,14 @@
 using namespace std;
 
 int main() {
-	int payout;
-	int total=0;
-	for(int i=1; i<=7; i++){
-		if (i == 7){
-			cout << "請輸入星期日的支出:";
-		}else{
-			cout << "請輸入星期" << i << "的支出:";
-		}
-		
-		cin >> payout;
-		total += payout;
+	int deposit=0;
+	int inputValue;
+	int month = 0;
+	while(deposit < 30000){
+		month++;
+		cout << "請輸入第" << month << "存款:";
+		cin >> inputValue;
+		deposit += inputValue;
 	}
-
-	cout << "本星期的支出為:" << total << endl;
+	cout << "存夠了,共存了" << deposit << endl;
 } 

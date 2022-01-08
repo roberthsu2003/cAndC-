242bf6f84c4a3ec1aa921b7cae753b1e845a7889
徐國堂 <roberthsu2003@gmail.com>
2022-01-08 15:12:21 +0800

    Create content.c

--- content.c
+++ content.c
@@ -2,30 +2,18 @@
 using namespace std;
 
 int main() {
-	char op;
-	int num1 = 20, num2 = 4;
-	cout << "請輸入要執行的運算(+-*/):";
-	cin >> op;
-
-	switch(op){
-		case '+':
-		cout << "num1+num2=" << num1 + num2 << endl;	
-		break;
-
-		case '-':
-		cout << "num1-num2=" << num1 - num2 << endl;	
-		break;
-
-		case '*':
-		cout << "num1*num2=" << num1 * num2 << endl;	
-		break;
-
-		case '/':
-		cout << "num1/num2=" << num1 / num2 << endl;	
-		break;
-
-		default:
-		cout << "輸入不正確的字元" << endl;
-
+	int payout;
+	int total=0;
+	for(int i=1; i<=7; i++){
+		if (i == 7){
+			cout << "請輸入星期日的支出:";
+		}else{
+			cout << "請輸入星期" << i << "的支出:";
+		}
+		
+		cin >> payout;
+		total += payout;
 	}
+
+	cout << "本星期的支出為:" << total << endl;
 } 

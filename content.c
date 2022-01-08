617933b0ff3ec10657ca6948628ec6454e6a8fd4
徐國堂 <roberthsu2003@gmail.com>
2022-01-08 16:51:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,10 @@
 using namespace std;
 
 int main() {
-	int deposit=0;
-	int inputValue;
-	int month = 0;
-	while(deposit < 30000){
-		month++;
-		cout << "請輸入第" << month << "存款:";
-		cin >> inputValue;
-		deposit += inputValue;
-	}
-	cout << "存夠了,共存了" << deposit << endl;
+	string password;	
+	do{
+		cout << "請輸入密碼:";
+		cin >> password;
+	}while (password != "1234");
+	cout << "密碼正確";
 } 

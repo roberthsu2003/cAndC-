cb37b4c70c8cead3de84519a1ed765fb2e1734c3
徐國堂 <roberthsu2003@gmail.com>
2022-01-11 19:22:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
 
+struct student{
+	string id;
+	string name;
+	int chinese,math,english;
+};
+
 int main() {
-	string password;	
-	do{
-		cout << "請輸入密碼:";
-		cin >> password;
-	}while (password != "1234");
-	cout << "密碼正確";
+	struct student david;
+	david.id = "99001";
+	david.name = "David";
+	david.chinese = 85;
+	david.math = 86;
+	david.english = 90;
+
+	struct student robert = {"99002","Robert",76, 95, 73};
+	cout << "name:" << robert.name << endl;
+	cout << "id:" << robert.id << endl;
+	cout << "chinese:" << robert.chinese << endl;
+	cout << "math:" << robert.math << endl;
+	cout << "english:" << robert.english << endl;
 } 

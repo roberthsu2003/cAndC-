36361736046d6c49c0848f99a9c2d9c1acb3b0a4
徐國堂 <roberthsu2003@gmail.com>
2022-01-11 20:09:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,11 +2,29 @@ main.cpp
 
 #include <iostream>
 #include "data.h"
+#include "time.h"
+
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-} 
+	srandom(time(NULL));
+	Student students[50];
+	for(int i=0; i<50; i++){
+		students[i].name = "stu" + to_string(i+1);
+		students[i].chinese = random() % (100-60+1) + 60;
+		students[i].english = random() % (100-60+1) + 60;
+		students[i].math = random() % (100-60+1) + 60;
+	}
 
+	for(int i=0; i< 50; i++){
+		cout << students[i].name << endl;
+		cout << students[i].chinese << endl;
+		cout << students[i].english << endl;
+		cout << students[i].math << endl;
+		cout << "===================" << endl;
+	}
+} 
 
 
 

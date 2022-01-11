9c674f0c3a21ebc75e898e107c8ee4ec21852b9f
徐國堂 <roberthsu2003@gmail.com>
2022-01-11 21:42:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,18 @@
 #include <iostream>
-//建立marco 
-#define SQUARE(x) (x) * (x)
-
-
 using namespace std;
-int main() {
-	int s, result;
-	cout << "請輸入數字:";
-	cin >> s;
-	result = SQUARE(s);
-	cout << s << "的平方為:" << result << endl;
 
-	result = SQUARE(s+1);
-	cout << s+1 << "的平方為:" << result << endl;
+#define W 
+
+#ifdef MAC
+
+#define OS "MAC_OS_VERSION"
+
+#else 
+
+#define OS "Window10"
+
+#endif
+
+int main() {
+  cout << OS << endl;
 } 

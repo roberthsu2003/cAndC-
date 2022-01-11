2f45d2b22fb419653bc06a8b2b3af037429aefd7
徐國堂 <roberthsu2003@gmail.com>
2022-01-11 19:49:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,23 @@
+main.cpp
+
 #include <iostream>
-using namespace std;
-//定義矩形結構
-typedef struct rectangle{
-	int width;
-	int height;
-}Rectangle;
-
-typedef struct point{
-	int x;
-	int y;
-}Point;
-
+#include "data.h"
 
 int main() {
-
-Rectangle rec1 = {76, 45};
-Point po1 = {5, 10};
-cout << "矩形的寬是:" << rec1.width << endl;
-cout << "矩形的高是:" << rec1.height << endl;
-cout << "矩形的面績是:" << rec1.width * rec1.height <<  endl;
-
+  std::cout << "Hello World!\n";
 } 
+
+
+
+
+data.h
+
+#include <iostream>
+using namespace std;
+
+typedef struct student{
+	string name;
+	int chinese;
+	int english;
+	int math;
+}Student;

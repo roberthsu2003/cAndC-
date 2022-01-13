a2238c96606871791e1074590f1e409ada59ae49
徐國堂 <roberthsu2003@gmail.com>
2022-01-13 20:57:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,35 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Student{
+class Tools{
 	public:
-	string name;
-	string id;
-	int chinese;
-	int english;
-	int math;
-
-	//自訂的建構式
-	Student(string n,string i,int ch, int en, int ma){
-		name = n;
-		id = i;
-		chinese = ch;
-		english = en;
-		math = ma;
-	}
-	
-	int sum(){
-		return chinese + english + math;
+	static float circleArea(int radius){
+		return radius * radius * 3.14159;
 	}
 
-	float average(){
-		return sum() / 3.0;
+	static int add(int a, int b){
+		return a + b;
+	}
+
+	static float rectangleArea(int width, int height){
+		return width * height;
 	}
 };
 
 int main() {
-	Student stu1("stu1", "A100001", 87, 97, 67);
-	cout << stu1.name << endl;
-	cout << "總分:" << stu1.sum() << endl;
-	cout << "平均:" << stu1.average() << endl;
+	int radius;
+	cout << "請輸入半徑:";
+	cin >> radius;
+	cout << "圓面積是:" <<  Tools::circleArea(radius) << endl;
+
+	cout << "矩形面積是:" << Tools::rectangleArea(100, 150) << endl;
 } 

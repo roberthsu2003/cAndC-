ce4e1d641f2150bf5b37ce825a3380b83e7ebeaa
徐國堂 <roberthsu2003@gmail.com>
2022-01-13 20:01:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -12,8 +12,15 @@ class Circle{
 
 int main() {
 	cout << "請輸入圓的半徑";
+	/* 參考變數
 	Circle oneCircle;
 	cin >> oneCircle.radius;
 	cout << "圓的半徑是" << oneCircle.radius << ",面積是" << oneCircle.area() << endl;
+	*/
+
+	//指標變數操控實體
+	Circle *twoCircle = new Circle();
+	cin >> twoCircle -> radius;
+	cout << "圓的半徑是" << twoCircle->radius << ",面積是" << twoCircle -> area() << endl;
 	
 } 

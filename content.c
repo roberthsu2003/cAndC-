b871f0bce7fe9772bc339300a17f448e4d1e6874
徐國堂 <roberthsu2003@gmail.com>
2022-01-13 21:07:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,33 @@
+main.cpp
+
 #include <iostream>
+#include "tool.h"
 using namespace std;
+
+
+
+int main() {
+	int radius;
+	cout << "請輸入半徑:";
+	cin >> radius;
+	cout << "圓面積是:" <<  Tools::circleArea(radius) << endl;
+
+	cout << "矩形面積是:" << rectangleArea(100, 150) << endl;
+} 
+
+tools.h
+// #define
+// function
+// 類別
+
+
+float circleArea(int radius){
+		return radius * radius * 3.14159;
+}
+
+float rectangleArea(int width, int height){
+		return width * height;
+}
 
 class Tools{
 	public:
@@ -15,12 +43,3 @@ class Tools{
 		return width * height;
 	}
 };
-
-int main() {
-	int radius;
-	cout << "請輸入半徑:";
-	cin >> radius;
-	cout << "圓面積是:" <<  Tools::circleArea(radius) << endl;
-
-	cout << "矩形面積是:" << Tools::rectangleArea(100, 150) << endl;
-} 

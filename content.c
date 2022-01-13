39b77066c5da16cbb7d2998b90732c477016f910
徐國堂 <roberthsu2003@gmail.com>
2022-01-13 21:41:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,12 +6,19 @@ main.cpp
 int main() {
 	BMI stu1("Robert",178, 78);
 	cout << stu1.name << endl;
-	
+	cout <<  "BMI是:" << stu1.getBMI() << endl;
+	cout << "提醒:" << stu1.suggestion() << endl;
+
+	BMI stu2("Alice",170, 49);
+	cout << stu2.name << endl;
+	cout <<  "BMI是:" << stu2.getBMI() << endl;
+	cout << "提醒:" << stu2.suggestion() << endl;
 } 
 
 bmi.h
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class BMI{
@@ -25,6 +32,32 @@ class BMI{
 		this -> name = name;
 		this -> height = height;
 		this -> weight = weight;
+	}
+	//實體方法
+	float getBMI(){
+		return weight / pow(height/100.0,2);
+	}
+
+	//實體方法
+	string suggestion(){
+		float bmi = getBMI();
+		string message;
+
+		if(bmi<18.5){
+			message = "體重過輕";
+		}else if(bmi < 24){
+			message = "正常範圍";
+		}else if(bmi < 27){
+			message = "過重";
+		}else if(bmi < 30){
+			message = "輕度肥胖";
+		}else if(bmi < 35){
+			message = "中度肥胖";
+		}else{
+			message = "重度肥胖";
+		}
+
+		return message;
 	}
 
 };

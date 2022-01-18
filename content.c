bf45dad28220e12ea484e64854779c63b78b0b59
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 21:09:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,14 +7,19 @@ using namespace std;
 
 int main() {
 	Circle c1(10);
+	c1.setRadius(120);
+	c1.setRadius(-10);
+	c1.setRadius(50);
 	cout << c1.getRadius() << endl;
 } 
-
 
 circle.h
 
 #ifndef __CIRCLE_H__
 #define __CIRCLE_H__
+
+#include <iostream>
+using namespace std;
 
 #define PI 3.14159
 class Circle{
@@ -23,6 +28,7 @@ class Circle{
 	public:
 		Circle(int);
 		int getRadius(void);
+		void setRadius(int);
 };
 
 Circle::Circle(int r){
@@ -31,6 +37,18 @@ Circle::Circle(int r){
 
 int Circle::getRadius(void){
 	return _radius;
+}
+
+void Circle::setRadius(int r){
+	if(r > 100){
+		_radius = 100;
+		cout << "radius被設為100" << endl;
+	}else if(r<=0){
+		_radius = 1;
+		cout << "radius被設為1" << endl;
+	}else{
+		_radius = r;
+	}
 }
 
 #endif

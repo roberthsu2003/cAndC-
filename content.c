69c5e4de6c1c83a854eaf182bb5fb77148eb8f35
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 20:03:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,27 +1,36 @@
-// C++ code
-//
+#include <iostream>
+using namespace std;
+
 class Student{
+	public:
+		string name;
+		string id;
+		int chinese;
+		int english;
+		int math;
+		Student(string n,string id,int c, int e, int m);
+		int sum();
+		float average();
 };
 
+Student::Student(string n,string id,int c, int e, int m){
+			name = n;
+			this -> id = id;
+			chinese = c;
+			english = e;
+			math = m;
+		}
 
-void setup()
-{
-  Serial.begin(9600);
-  int englishScore = 60;
-  Serial.println("input english score:");
-  
-  while(true){
-    if(Serial.available()>0){
-      englishScore = Serial.parseInt();
-      break;
-    }
-  }
-
-  Serial.print("Hello! your english is:");
-  Serial.println(englishScore);
+int Student::sum(){
+	return chinese + english + math;
 }
 
-void loop()
-{
-  
+float Student::average(){
+	return sum() / 3.0;
 }
+
+int main() {
+	Student stu1("robert","A10001",78,93,78);
+	cout << "學生總分為:" << stu1.sum() << endl;
+	cout << "學生平均為:" << stu1.average() << endl;
+} 

eab5775c898a98fbcf260ebb13dc3c48bd48ed66
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 19:33:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,17 +3,15 @@
 class Student{
 };
 
-#define LED 13
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
+  Serial.begin(9600);  
+  Serial.println("Hello,Arduino");
 }
 
 void loop()
 {
-  digitalWrite(LED, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  Serial.println("Hello,Arduino");
+  delay(1000);
 }

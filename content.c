91222df55f01530e12be8aab793c0716848e2562
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 19:45:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,12 +6,22 @@ class Student{
 
 void setup()
 {
-  Serial.begin(9600);  
-  Serial.println("Hello,Arduino");
+  Serial.begin(9600);
+  int englishScore = 60;
+  Serial.println("input english score:");
+  
+  while(true){
+    if(Serial.available()>0){
+      englishScore = Serial.parseInt();
+      break;
+    }
+  }
+
+  Serial.print("Hello! your english is:");
+  Serial.println(englishScore);
 }
 
 void loop()
 {
-  Serial.println("Hello,Arduino");
-  delay(1000);
+  
 }

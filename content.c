284004defa70a17ce2f123d404dd5ced07ab11fb
徐國堂 <roberthsu2003@gmail.com>
2022-01-22 10:35:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int n[3] = {10, 20, 30};
-	
-	//陣列變數操作
-	for(int i=0; i<3; i++){
-		cout << n[0] << endl;
-	}	
-	cout << "===========" << endl;
-	//指標變數操作
-	for(int i=0; i<3; i++){
-		cout << *(n+i) << endl; 
-	}
 
+int main() {
+	int* m = new int(10);
+	cout << "*m指向的值是:" << *m << endl;
+
+	double* n = new double(20.5);
+	cout << "*n指向的值是:" << *n << endl;
+	//cout << "*m + *n" << *m + *n << endl;
+	double total = *m + *n;
+	cout << "totol一般變數的值是" << total << endl;
+
+	double *totalptr = &total;
+	cout << "*totol指標變數指向的值是" << *totalptr << endl;
 } 

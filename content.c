f924ef79265105a4760602ee4c96247d61cc4fff
徐國堂 <roberthsu2003@gmail.com>
2022-01-22 10:15:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int n = 10;
-	int* p = &n;
-	cout << "一般變數:" << n << endl;
-	cout << "指標變數:" << *p << endl;
+	int n[3] = {10, 20, 30};
+	cout << "變數n的值:" << n << endl;
+	cout << "n[0]=" << n[0] << endl;
+	cout << "n[0]的記憶體位址=" << &n[0] << endl;
+	cout << "n[1]=" << n[1] << endl;
+	cout << "n[1]的記憶體位址=" << &n[1] << endl;
+	cout << "n[2]=" << n[2] << endl;
+	cout << "n[2]的記憶體位址=" << &n[2] << endl;
+	cout << "*n=" << *n << endl;
+	cout << "n+0=" << n+0 << endl;
+	cout << "*(n+0)=" << *(n+0) << endl;
+	cout << "*(n+1)=" << *(n+1) << endl;
+	cout << "*(n+2)=" << *(n+2) << endl;
 } 

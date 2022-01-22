be6ba262e138a7179d1da99be7be715b6fc17fcd
徐國堂 <roberthsu2003@gmail.com>
2022-01-22 14:27:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int add(int n, int m){
-	int total = n + m;
-	return total;
-}
-
-void swap(int n, int m){
-	int temp = n;
-	n = m;
-	m = temp;
-}
-
 int main() {
-	int n = 10;
-	int m = 20;
-	cout << "n=" << n << endl;
-	cout << "m=" << m << endl;
-	cout << "2數相加後的值是;" << endl;
-	int total = add(n,m);
-	cout << "2數相加後的值是;" << total << endl;
+	int scores[5];
+	scores[0] = 87;
+	scores[1] = 78;
+	scores[2] = 87;
+	scores[3] = 95;
+	scores[4] = 76;
 
-	cout << "2數交換後的值是:" << endl;
-	swap(n,m);
-	cout << "n=" << n << endl;
-	cout << "m=" << m << endl;
-
+	for(int i=0;i<5;i++){
+		cout << scores[i] << endl;
+	}
 } 

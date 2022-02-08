cb4592d03a06245e717cf75ead8f5b21ac44550f
徐國堂 <roberthsu2003@gmail.com>
2022-02-08 19:57:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,6 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-	srandom(time(NULL));
-	int nums = 7;
-	int loto[49];
-	int choose[nums];
-	int minIndex = 0;
-	int maxIndex = 49-1;
-	for(int i=0; i<49; i++){
-		loto[i] = i+1;
-	}
-
-
-	for(int i=0; i<nums; i++){
-		int randomIndex = random() % (maxIndex - minIndex - 1) + minIndex;
-		choose[i] = loto[randomIndex];
-		loto[randomIndex] = loto[maxIndex];
-		maxIndex--;
-	}
-
-	for(int i=0;i<nums;i++){
-		cout << choose[i] << " ";
-	}
-
-
-
-
+int main(void) {
+  printf("這是我的第一個c語言程式\n");
+  return 0;
 }

25dfb87dc3ec04f36a8dcd5534f2593114e1a327
徐國堂 <roberthsu2003@gmail.com>
2022-02-08 20:40:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-	int score = 95;
-	printf("我的分數是%d\n",score);
+	int chinese; //建立變數
+	chinese = 78; //給變數值
+
+	int math;
+	math = 97;
+
+	printf("我的國文分數是%d\n", chinese); //輸出字串和變數內容
+	printf("我的數學分數是%d\n", math); //輸出字串和變數內容
 	return 0;
 }

60e7c9108877fd152949c092712f67cd96810065
徐國堂 <roberthsu2003@gmail.com>
2022-02-08 20:16:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("這是我的第一個c語言程式\n");
-  printf("這是第一節課程\n");
-  printf("printf()是輸出\n");
-  return 0;
+	int score = 95;
+	printf("我的分數是%d\n",score);
+	return 0;
 }

d39fcc0b317de9b401a0814731ce766b1b32866d
徐國堂 <roberthsu2003@gmail.com>
2022-02-10 21:25:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-  //使用c++ 的輸入和輸出
-  int chinese;
-  int english;
-  int math;
-  int sum;
-  float average;
-
-  cout << "請輸入國文成績:"; //提示使用者輸入  
-  cin >> chinese;
-
-  cout << "請輸入英文成績:"; //提示使用者輸入  
-  cin >> english;
-
-  cout << "請輸入數學成績:"; //提示使用者輸入  
-  cin >> math;
-
-  sum = chinese + english + math;
-  //average = (float)sum / 3;
-  average = sum / 3.0;
-  
-  cout << "國文成績是" << chinese << endl;  
-  cout << "英文成績是" << english << endl;  
-  cout << "數學成績是" << math << endl;
-  cout << "====================\n";
-  
-  cout << "總分是" << sum << endl;
-  //cout << "平均是" << average << endl;
-  printf("平均是%.2f\n", average);
-  return 0;
+	int years;
+	cout << "出生民國年份:";
+	cin >> years;
+	int westernYear = years + 1911;
+	int age = 2022 - westernYear;
+	cout << "對應西元年份是:" << westernYear << endl;
+	cout << "今年" << age << "歲" << endl;
+	return 0;
 }

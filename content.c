de9cb9143347f90907546c509169daf5f0954987
徐國堂 <roberthsu2003@gmail.com>
2022-02-10 20:10:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-  int chinese = 95;
+  int chinese;
+  printf("請輸入國文成績:"); //提示使用者輸入
+  scanf("%d",&chinese); //接收使用者輸入的值
   printf("國文成績是%d\n",chinese);
-  printf("chinese變數的記憶體位址是%x\n",&chinese);
   return 0;
 }

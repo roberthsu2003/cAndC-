83e856453c38ab74c84710c6ebe71a7aa8ccbee6
徐國堂 <roberthsu2003@gmail.com>
2022-02-10 19:44:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,6 +3,7 @@
 int main(void) {
   double PI = 3.14159787;
   int radius = 9;
-  printf("圓面積:%.2f\n",radius * radius * PI); //自動轉換型別
+  double area = radius * radius * PI;
+  printf("圓面積:%.2f\n",area); 
   return 0;
 }

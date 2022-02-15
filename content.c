334e6becdcc17d90a6249f0ca7f907b45ddf1453
徐國堂 <roberthsu2003@gmail.com>
2022-02-15 20:32:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main() {
-	int num;
-	int result;
-	cout << "請輸入任意數:";
-	cin >> num;
-	result = num;
-	result *= num;
-	cout << "此數的平方是:" << result << "\n";
-	result *= num;
-	cout << "此數的立方值是:" << result << endl;
-	return 0;
+	float value;
+	float result = 0.0;
+    cout << "請輸入第一個數:";
+	cin >> value;
+	result += value;
+
+	cout << "請輸入第二個數:";
+	cin >> value;
+	result += value;
+
+	cout << "請輸入第三個數:";
+	cin >> value;
+	result += value;
+
+	cout << "三個數的總和為:" << result << endl;
 }

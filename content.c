948646dd5e92565388fba62f902363286c160a2c
徐國堂 <roberthsu2003@gmail.com>
2022-02-15 20:17:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main() {
-	int n = 5;
-	cout << (n == 5) << endl;
-	cout << (n != 5) << endl;
+	int num;
+	int result;
+	cout << "請輸入任意數:";
+	cin >> num;
+	result = num;
+	result *= num;
+	cout << "此數的平方是:" << result << "\n";
+	result *= num;
+	cout << "此數的立方值是:" << result << endl;
 	return 0;
 }

1a4617ca438c6b645de8355815c55f39bde6abef
徐國堂 <roberthsu2003@gmail.com>
2022-02-17 20:28:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,10 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int money;
-	int payMoney;
+	int chinese;
+	int math;
 
-	cout << "請輸入購買金額:";
-	cin >> money;
-
-	if(money >= 100000){
-		payMoney = money * 0.8;
-	}else if(money >= 50000){
-		payMoney = money * 0.85;
-	}else if(money >= 30000){
-		payMoney = money * 0.9;
-	}else if(money >= 10000){
-		payMoney = money * 0.95;
-	}else{
-		payMoney = money;
-	}
-
-	cout << "實付金額是:" << payMoney << "元\n";
+	cout << "請輸入國文成績和數學成績(國文,數學):";
+	scanf("%d,%d",&chinese, &math);
+	cout << chinese << "," << math << endl;
 }

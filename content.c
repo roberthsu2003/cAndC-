901445d4949eddcbc4239ac2caf29048051d288f
徐國堂 <roberthsu2003@gmail.com>
2022-02-17 21:05:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,21 +7,16 @@ int main() {
 
 	cout << "請輸入國文成績和數學成績(國文,數學):";
 	scanf("%d,%d",&chinese, &math);
-	cout << chinese << "," << math << endl;
-
-	if(chinese == 100){
-		if(math == 100){
-			bonus = 1000;
-		}else{
-			bonus = 500;
-		}
+	
+	if(chinese == 100 && math == 100){
+		bonus = 1000;
+	}else if(chinese == 100 || math == 100){
+		bonus = 500;
 	}else{
-		if(math == 100){
-			bonus = 500;
-		}else{
-			bonus = 0;
-		}
+		bonus = 0;
 	}
+
+	
 
 	cout << "獎金是:" << bonus << endl;
 }

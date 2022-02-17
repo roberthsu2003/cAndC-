98a69316696ac248d9e4c4b471d3f68c16818603
徐國堂 <roberthsu2003@gmail.com>
2022-02-17 19:53:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,21 +2,9 @@
 using namespace std;
 
 int main() {
-	int score;
-	cout << "請輸入學生分數:";
-	cin >> score;
-	
-	if(score >= 90){
-		cout << "優等" << endl;
-	}else if(score >= 80){
-		cout << "甲等" << endl;
-	}else if(score >= 70){
-		cout << "乙等" << endl;		
-	}else if(score >= 60){
-		cout << "丙等" << endl;
-	}else{
-		cout << "丁等" << endl;
-	}
-	
-	return 0;
+	//如果是c++,操控unsigned char,不要使用cin,cout
+	unsigned char score;
+	cout << "請輸入分數:";
+	scanf("%hhu", &score);
+	printf("%hhu\n", score);
 }

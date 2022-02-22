7cc71c8c69919ddaff35ea4f4979b922c432e6da
徐國堂 <roberthsu2003@gmail.com>
2022-02-22 21:50:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,13 +6,19 @@ int main() {
 	int sum = 0;
 	int num = 0;
 	while(true){
-		cout << "請輸入第" << num+1 << "學生成績:";
+		cout << "請輸入第" << num+1 << "個數值:";
 		cin >> score;
 		if(score < 0){
 			break;
 		}
-		sum += score;
+		
 		num += 1;
+
+		if(score % 2 == 1){
+			continue;
+		}
+		sum += score;
+		
 	};
-	cout << "全班總成績為:" << sum << ",平均分數為" << (float)sum/num << endl;
+	cout << "所有正偶數的加總是:" << sum << endl;
 }

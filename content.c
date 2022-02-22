57a55d7df1fad1d689d42fd055593c6fb7baaf0b
徐國堂 <roberthsu2003@gmail.com>
2022-02-22 20:32:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	string password;	
-	do{
-		cout << "請輸入密碼";
-		cin >> password;
-	}while(password != "5678");
-	cout << "恭喜!您的密碼正確!請進" << endl;
+	int height;
+	int weight;
+	float bmi;
+	cout << "=======計算BMI==========\n";
+	cout << "請輸入身高(公分):";
+	cin >> height;
+	cout << "請輸入體重(公斤):";
+	cin >> weight;
+	bmi = weight / ((height/100.0) * (height/100.0));
+	printf("您的BMI是%.2f\n", bmi);
 	return 0;
-
 }

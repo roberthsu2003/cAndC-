35f723e0a12cdd342b623183a9234a39308cb81e
徐國堂 <roberthsu2003@gmail.com>
2022-02-22 20:56:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -11,5 +11,19 @@ int main() {
 	cin >> weight;
 	bmi = weight / ((height/100.0) * (height/100.0));
 	printf("您的BMI是%.2f\n", bmi);
+
+	if(bmi < 18.5){
+		cout << "狀態:體重過輕" << endl;
+	}else if(bmi < 24){
+		cout << "狀態:正常範圍" << endl;
+	}else if(bmi < 27){
+		cout << "狀態:過重" << endl;
+	}else if(bmi < 30){
+		cout << "狀態:輕度肥胖" << endl; 
+	}else if(bmi < 35){
+		cout << "狀態:中度肥胖" << endl; 
+	}else{
+		cout << "狀態:重度肥胖" << endl; 
+	}
 	return 0;
 }

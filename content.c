02f4bc2861fd2b58a934241aee8f7cd80be2a9d9
徐國堂 <roberthsu2003@gmail.com>
2022-02-22 20:00:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	for(int i=1; i<=9; i++){
-		for(int j=1; j<=9; j++){
-			cout << i << "*" << j << "=" << i * j << "\t";
-		}
-		cout << endl;
+	int deposit=0;
+	int num = 0;
+	int inputNum;
+	//不明確要執行的次數，請用while迴圈
+	while(deposit < 30000){
+		cout << "請輸入第" << ++num << "個月份的存款:";
+		cin >> inputNum;
+		deposit += inputNum;
 	}
+
+	cout << "恭喜!已經存夠了，存了" << num << "個月的總存款為:" << deposit << endl;
 }

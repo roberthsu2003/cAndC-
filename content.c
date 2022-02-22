9d49ffe2e312215bf34dc353ad3dbb599c6fd49b
徐國堂 <roberthsu2003@gmail.com>
2022-02-22 20:18:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int deposit=0;
-	int num = 0;
-	int inputNum;
-	//不明確要執行的次數，請用while迴圈
-	while(deposit < 30000){
-		cout << "請輸入第" << ++num << "個月份的存款:";
-		cin >> inputNum;
-		deposit += inputNum;
-	}
 
-	cout << "恭喜!已經存夠了，存了" << num << "個月的總存款為:" << deposit << endl;
+int main() {
+	string password;	
+	do{
+		cout << "請輸入密碼";
+		cin >> password;
+	}while(password != "5678");
+	cout << "恭喜!您的密碼正確!請進" << endl;
+	return 0;
+
 }

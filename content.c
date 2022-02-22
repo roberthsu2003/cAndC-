ea1d0cd4597816a59db98f47a24d6c9205c961e2
徐國堂 <roberthsu2003@gmail.com>
2022-02-22 19:21:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
-	for(int i=0; i<10; i++){
-		cout << i << endl;
+	int n;
+	int sum = 0;
+	for(int i=1; i<=7; i++){
+		if(i == 7){
+			cout << "請輸入星期日的支出:";
+		}else{
+			cout << "請輸入星期" << i << "的支出:";
+		}
+		
+		cin >> n;
+		sum += n;
 	}
+
+	cout << "本星期的支出為:" << sum << "元\n";
+	return 0;
 }

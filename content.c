fa38ea71623a4fee084661ee6f7cdf6e9b29386c
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 19:13:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,10 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
-
 int main() {
-	int score = 0;
-	int sum = 0;
-	int num = 0;
-	while(true){
-		cout << "請輸入第" << num+1 << "個數值:";
-		cin >> score;
-		if(score < 0){
-			break;
-		}
-		
-		num += 1;
-
-		if(score % 2 == 1){
-			continue;
-		}
-		sum += score;
-		
-	};
-	cout << "所有正偶數的加總是:" << sum << endl;
+	srand(time(NULL));
+	cout << random() << endl;
 }

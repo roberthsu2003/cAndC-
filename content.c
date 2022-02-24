d4deea535ba0b6c168c0167a63cbd9ad4a8a4a98
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 20:21:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,10 @@
+#include <iostream>
 using namespace std;
 
 int main() {
-	int scores[3];
-	scores[0] = 70;
-	scores[1] = 80;
-	scores[2] = 90;
-
-	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
+	int scores[3] = {70, 80, 90};
+	for(int i=0; i < 3; i++){
+		cout << scores[i] << endl;
+	}
+	return 0;
 }

0ea54c248a1dec640c1aeaa87dfba977f730b1ff
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 20:59:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,10 +5,13 @@ int main() {
 	srandom(time(NULL));
 	int min = 60;
 	int max = 100;
-	int nums = 5;
+	int nums = 250;
 	int scores[nums];
 	for(int i=0; i<nums; i++){
 		scores[i] = random() % (max-min+1) + min;
-		cout << scores[i] << endl;
+		cout << scores[i] << " ";
+		if ((i+1) % 5 == 0){
+			cout << endl;
+		}		
 	}	
 }

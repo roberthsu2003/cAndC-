5a5850dab940ccbb7ce37f17072c5017ef66e669
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 20:13:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,10 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 using namespace std;
+
 int main() {
-	srand(time(NULL));
-	int max = 99;
-	int min = 1;
-	int keyin;
-	int count = 0;
-	int guess = random() % (max-min+1) + min;
-	cout << "=========猜數字遊戲==============\n\n";
-	while(true){
-		printf("猜數字範圍%d~%d:",min,max);
-		cin >> keyin;
-		count += 1;
-		if(keyin >= min && keyin <= max){
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-				cout << "您猜了" << count << "次" << endl;
-				break;
-			}else if(keyin > guess){
-				max = keyin;
-				cout << "再小一點!";
-			}else if(keyin < guess){
-				min = keyin;
-				cout << "再大一點!";
-			}
-			cout << "您已經猜了" << count << "次" << endl;
-		}else{
-			cout << "請輸入提示範圍內的數字!\n";
-		}
-	}
-	cout << "遊戲結束" << endl;
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
+
+	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
 }

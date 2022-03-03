24dfc8b965c55ec6e0b2212545026e43df3c646e
徐國堂 <roberthsu2003@gmail.com>
2022-03-03 21:38:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,80 +1,44 @@
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 using namespace std;
 
+//建立function
+void guessGame(){
+	srand(time(NULL));	
+	int max = 99;
+	int min = 1;
+	int keyin;
+	int count = 0;
+	int guess = random() % (max-min+1) + min;
+	cout << guess << endl;
+	cout << "=========猜數字遊戲==============\n\n";
+	while(true){
+		printf("猜數字範圍%d~%d:",min,max);
+		cin >> keyin;
+		count += 1;
+		if(keyin >= min && keyin <= max){
+			if(keyin == guess){
+				cout << "賓果!猜對了,答案是" << guess << endl;
+				cout << "您猜了" << count << "次" << endl;
+				break;
+			}else if(keyin > guess){
+				max = keyin;
+				cout << "再小一點!";
+			}else if(keyin < guess){
+				min = keyin;
+				cout << "再大一點!";
+			}
+			cout << "您已經猜了" << count << "次" << endl;
+		}else{
+			cout << "請輸入提示範圍內的數字!\n";
+		}
+	}
+}
+
 int main() {
-	srandom(time(NULL));
-	int num;
-	cout << "請輸元素的數量:";
-	cin >> num;
-	int scores[num];
-	cout << num << "個元素陣列scores已經建立" << endl;
-	//給scores的元素值
-	for(int i=0; i < num; i++){
-		scores[i] = random() % 200;
-	}
-	
-	//輸出陣列元素的值
-	for(int i=0; i < num; i++){
-		cout << scores[i] << "\t";
-	}
-	cout << endl;
-
-	int max = scores[0];
-	int min = scores[0];
-
-	for(int i=1; i < num; i++){
-		if (max < scores[i]){
-			max = scores[i];
-		}
-
-		if (min > scores[i]){
-			min = scores[i];
-		}
-	}
-
-	cout << "最大的元素值是:" << max << endl;
-	cout << "最小的元素值是:" << min << endl;
-
-	//泡沫排序
-	for(int i=0; i < num-1; i++){
-		for(int j=i+1; j < num; j++){
-			if(scores[i] > scores[j]){
-				//2數對換
-				int temp = scores[i];
-				scores[i] = scores[j];
-				scores[j] = temp;
-			}
-		}
-	}
-
-	cout << "由小到大的排序是:" << endl;	
-	//輸出陣列元素的值
-	for(int i=0; i < num; i++){
-		cout << scores[i] << "\t";
-	}
-	cout << endl;
-
-	//泡沫排序
-	for(int i=0; i < num-1; i++){
-		for(int j=i+1; j < num; j++){
-			if(scores[i] < scores[j]){
-				//2數對換
-				int temp = scores[i];
-				scores[i] = scores[j];
-				scores[j] = temp;
-			}
-		}
-	}
-
-
-	cout << "由大到小的排序是:" << endl;	
-	//輸出陣列元素的值
-	for(int i=0; i < num; i++){
-		cout << scores[i] << "\t";
-	}
-	cout << endl;
-	
+	//呼叫function
+	guessGame();	
+	cout << "遊戲結束" << endl;
 }

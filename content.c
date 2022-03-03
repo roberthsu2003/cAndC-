e7b894b5e64043f2e917840aabc0739e18ebb700
徐國堂 <roberthsu2003@gmail.com>
2022-03-03 20:12:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,9 +2,20 @@
 using namespace std;
 
 int main() {
+	srandom(time(NULL));
 	int num;
 	cout << "請輸元素的數量:";
 	cin >> num;
 	int scores[num];
 	cout << num << "個元素陣列scores已經建立" << endl;
+	//給scores的元素值
+	for(int i=0; i < num; i++){
+		scores[i] = random() % 200;
+	}
+	
+	//輸出陣列元素的值
+	for(int i=0; i < num; i++){
+		cout << scores[i] << endl;
+	}
+	
 }

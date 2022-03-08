fa1cafca5049f96ca5a8d1a4da1721e9004d818d
徐國堂 <roberthsu2003@gmail.com>
2022-03-08 21:37:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
+void multiplyTen(int *m){ //call by reference
+	for (int i=0; i<3; i++){
+		m[i] *= 10;
+	}
+}
+
 int main() {
 	int n[3] = {10, 20, 30};
-	cout << "陣列n的內容是:" << n << endl;
-	cout << "陣列n[0]的位址是:" << &n[0] << endl;
-	cout << "陣列n[0]的位址是:" << n+0 << endl;
+	multiplyTen(n);
+	for(int i=0; i<3; i++){
+		cout << n[i] << endl;
+	}
 }

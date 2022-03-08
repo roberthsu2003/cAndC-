aa3d42d314034f969d4ab2d69519b23ee64609f8
徐國堂 <roberthsu2003@gmail.com>
2022-03-08 19:42:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,16 @@
+main.cpp
+
+#include <iostream>
+#include "weather.h"
+using namespace std;
+
+int main(){
+	onefunc();
+	return 0;
+}
+
+
+weather.h
 #include <iostream>
 using namespace std;
 
@@ -5,12 +18,11 @@ float temperature(int value){
      return 1.8 * value + 32;
 }
 
-int main(){
+void onefunc(){
 	int cValue;
 	float fValue;
 	cout << "請輸入攝氏溫度:";
 	cin >> 	cValue;
 	fValue = temperature(cValue);
 	cout << "華氏溫度=" << fValue << endl;
-	return 0;
 }

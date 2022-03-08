2b34d8f5e8b8736fc3b0e40eab1b81f18d675667
徐國堂 <roberthsu2003@gmail.com>
2022-03-08 20:37:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int addValueFun(int value){ //call by value,如果有改變內容,要傳回值
+	value *= 1.1;
+	value = (value > 500)? 500 : value;
+	return value;
+}	
+
 int main() {
 	int scores;
 	int addValue;
@@ -9,8 +15,7 @@ int main() {
 	
 	if(addValue){
 		cout << "加分" << endl;
-	}else{
-		cout << "不加分" << endl;
+		scores = addValueFun(scores);
 	}
 
 	cout << "學生的分數是:" << scores << endl;

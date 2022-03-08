36373f3f3abc5d3249b2bf8273a99efcb5be9d8d
徐國堂 <roberthsu2003@gmail.com>
2022-03-08 21:12:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	double m = 5;
-	int n = 10;
-	cout << "變數m的值=" << m << endl;
-	cout << "變數n的值=" << n << endl;
-	cout << "變數m的位址值=" << &m << endl;
-	cout << "變數n的位址值=" << &n << endl;
+	int n[3] = {10, 20, 30};
+	cout << "陣列n的內容是:" << n << endl;
+	cout << "陣列n[0]的位址是:" << &n[0] << endl;
+	cout << "陣列n[0]的位址是:" << n+0 << endl;
 }

e638da8d6e03c09312b6ea72a2a0c7914abd3f96
徐國堂 <roberthsu2003@gmail.com>
2022-03-10 21:17:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
+float circle_area(float radius);
+float circle_area(int radius);
+
 int main() {
-	cout << M_PI << endl;
+	int radius;
+	float area;
+	cout << "請輸內圓半徑:";
+	cin >> radius;
+	area = circle_area(radius);
+	cout << "圓面積是:" << area << endl;
+}
+
+float circle_area(float radius){
+	return M_PI * pow(radius,2); //內建的function
+}
+
+float circle_area(int radius){
+	return M_PI * pow(radius,2); //內建的function
 }

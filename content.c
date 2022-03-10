03eb9bc69feca6aa5b5bd6d57620e08087c369d4
徐國堂 <roberthsu2003@gmail.com>
2022-03-10 19:23:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
-
-void sort(int *array,int nums){
+void sort(int array[],int nums){
 	int temp;
 	for(int i=0; i < nums-1; i++){
 		for(int j=i+1; j < nums; j++){
@@ -14,11 +15,19 @@ void sort(int *array,int nums){
 	}
 }
 
-int main() {	
+int main() {
+	srandom(time(NULL));
+	int nums;
 	cout << "請輸入要排序的數值個數:";
-	int n[3] = {20, 10, 50};
-	sort(n,3);
-	for(int i=0; i<3; i++){
+	cin >> nums;
+	int n[nums];
+	//給陣列值
+	for(int i=0; i<nums; i++){
+		n[i] = 	random() % 100 + 1;
+	}
+	//排序
+	sort(n,nums);	
+	for(int i=0; i<nums; i++){
 		cout << n[i] << "  ";
 	}
 	cout << endl;

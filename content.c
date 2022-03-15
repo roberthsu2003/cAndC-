5b06131167a1c8ff262712ce5e5e146b0f48f32a
徐國堂 <roberthsu2003@gmail.com>
2022-03-15 21:11:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,9 +8,28 @@ typedef struct student{
 	int math;
 }Student;
 
+Student createStudent(int num){
+	Student s;
+	s.name = "stu" + to_string(num+1);
+	s.chinese = random() % (100-50+1) + 50;
+	s.english = random() % (100-50+1) + 50;
+	s.math = random() % (100-50+1) + 50;
+	return s;
+}
+
 int main() {
+	srandom(time(NULL));
 	int nums;
 	cout << "請輸入學生人數:";
 	cin >> nums;
 	Student students[nums];
+	for (int i=0; i<nums; i++){
+		students[i] = createStudent(i);
+	}
+
+	cout << students[0].name << endl;
+	cout << students[0].chinese << endl;
+	cout << students[0].english << endl;
+	cout << students[0].math << endl;
+	
 }

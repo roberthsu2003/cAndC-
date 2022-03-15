ce0f28f83ae94f9991869f2a990821e7696cd0ad
徐國堂 <roberthsu2003@gmail.com>
2022-03-15 19:28:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void swap(int* x, int* y);
+
 int main() {
- 	int n=10;
-	int* p = &n;
-	*p = 15;
+	int n = 666, m = 777;
+	swap(&n,&m);
 	cout << "n=" << n << endl;
+	cout << "m=" << m << endl;
 }
+
+void swap(int* x, int* y){
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+

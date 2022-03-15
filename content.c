396965031133a182cd34253f38b3ba4c2185bb3c
徐國堂 <roberthsu2003@gmail.com>
2022-03-15 20:21:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,9 +10,22 @@ typedef struct student{
 	int english;
 } Student;
 
+int sum(Student stu){
+	return stu.chinese + stu.english + stu.math;
+}
+
+float average(Student stu){
+	return sum(stu) / 3.0;
+}
+
 int main() {
 	Student stu1 = {99001,"robert",75,86,90};
 	Student stu2={99002,"jenny",85,76,97};
 	cout << stu1.name << endl;
+	cout << "stu1的總分" << sum(stu1) << endl;
+	cout << "stu1的平均" << average(stu1) << endl;
+	
 	cout << stu2.name << endl;
+	cout << "stu2的總分" << sum(stu2) << endl;
+	cout << "stu2的平均" << average(stu2) << endl;
 }

19c910eb158e618970ba3000f2eaf220fcaaffae
徐國堂 <roberthsu2003@gmail.com>
2022-03-15 20:09:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,29 +2,17 @@
 using namespace std;
 
 //定義一個變生的結構
-struct student{
+typedef struct student{
 	int id;
 	string name;
 	int chinese;
 	int math;
 	int english;
-};
+} Student;
 
 int main() {
-	struct student stu1;
-	stu1.id = 99001;
-	stu1.name = "robert";
-	stu1.chinese = 75;
-	stu1.math = 86;
-	stu1.english = 90;
-
-	struct student stu2;
-	stu2.id = 99002;
-	stu2.name = "jenny";
-	stu2.chinese = 85;
-	stu2.math = 76;
-	stu2.english = 97;
-
+	Student stu1 = {99001,"robert",75,86,90};
+	Student stu2={99002,"jenny",85,76,97};
 	cout << stu1.name << endl;
 	cout << stu2.name << endl;
 }

c6504ecf0f7d42ae182b352c81105972dd6fb67c
徐國堂 <roberthsu2003@gmail.com>
2022-03-15 19:13:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void change_value(){
-	static int n = 0; //靜態變數
-	n++;
-	cout << "n的值是:" << n << endl;
-}
-
 int main() {
-	change_value();
-	change_value();
-	change_value();
+ 	int n=10;
+	int* p = &n;
+	*p = 15;
+	cout << "n=" << n << endl;
 }

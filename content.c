9ed5fd32cb243715e18f40ced7e8c562515e18a0
徐國堂 <roberthsu2003@gmail.com>
2022-03-15 20:29:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,8 @@
+main.cpp
+
 #include <iostream>
+#include "student.h"
 using namespace std;
-
-//定義一個變生的結構
-typedef struct student{
-	int id;
-	string name;
-	int chinese;
-	int math;
-	int english;
-} Student;
-
-int sum(Student stu){
-	return stu.chinese + stu.english + stu.math;
-}
-
-float average(Student stu){
-	return sum(stu) / 3.0;
-}
-
 int main() {
 	Student stu1 = {99001,"robert",75,86,90};
 	Student stu2={99002,"jenny",85,76,97};
@@ -29,3 +14,4 @@ int main() {
 	cout << "stu2的總分" << sum(stu2) << endl;
 	cout << "stu2的平均" << average(stu2) << endl;
 }
+

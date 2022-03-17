daa28413a4f4204d962ddb96f8c5f02da5491990
徐國堂 <roberthsu2003@gmail.com>
2022-03-17 21:13:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,24 @@
 #include <iostream>
+using namespace std;
+
 class Rectangle{
 	public:
 		int width;
 		int height;
+		
 		int area(){
 			return width * height;
 		}
 };
 
-using namespace std;
+
 int main() {
 	Rectangle rec1;
-	rec1.width = 50;
-	rec1.height = 100;
+	cout << "請輸入矩形的寬:";
+	cin >> rec1.width;
+	cout << "請輸入矩形的高:";
+	cin >> rec1.height;		
 	cout << "面積是" << rec1.area() << endl;
 
-	Rectangle rec2;
-	rec2.width = 10;
-	rec2.height = 30;
-	cout << "面積是" << rec2.area() << endl;
+	
 }

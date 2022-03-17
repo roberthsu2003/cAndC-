ea12a41b2d98f652e1ae34e2181b3f2bfa500682
徐國堂 <roberthsu2003@gmail.com>
2022-03-17 21:19:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -13,12 +13,12 @@ class Rectangle{
 
 
 int main() {
-	Rectangle rec1;
+	Rectangle* rec1 = new Rectangle();//使用指標變數控制實體
 	cout << "請輸入矩形的寬:";
-	cin >> rec1.width;
+	cin >> rec1 -> width;
 	cout << "請輸入矩形的高:";
-	cin >> rec1.height;		
-	cout << "面積是" << rec1.area() << endl;
+	cin >> rec1 -> height;		
+	cout << "面積是" << rec1 -> area() << endl;
 
 	
 }

ef7e98f122f1ba57f7fa412e8acd9b5bbb7dec41
徐國堂 <roberthsu2003@gmail.com>
2022-03-22 20:33:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,6 +3,7 @@
 using namespace std;
 
 class BMI{
+	public:
 	string name;
 	int weight;
 	int height;
@@ -20,6 +21,26 @@ float BMI::getBMI(){
 	return weight / pow(height/100.0, 2);
 }
 
+float getBMI(int weight, int height){
+	return weight / pow(height/100.0, 2);
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+	
+	string name;
+	int weight;
+	int height;
+	cout << "input your name:";
+	cin >> name;
+	cout << "input weight(kg):";
+	cin >> weight;
+	cout << "input height(cm):";
+	cin >> height;
+	
+	/*
+	BMI p1(name,weight,height);
+	cout << p1.getBMI() << endl;
+	*/
+
+	cout << getBMI(weight,height) << endl;
 }

4a748d12159e75a142223ec847d33bcbc3b798d3
徐國堂 <roberthsu2003@gmail.com>
2022-03-22 20:56:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,6 +9,7 @@ class BMI{
 	int height;
 	BMI(string name, int weight, int height);
 	float getBMI();
+    string suggestion();
 };
 
 BMI::BMI(string name, int weight, int height){
@@ -21,9 +22,26 @@ float BMI::getBMI(){
 	return weight / pow(height/100.0, 2);
 }
 
-float getBMI(int weight, int height){
-	return weight / pow(height/100.0, 2);
+string BMI::suggestion(){
+    float bmi = getBMI();
+    string message;
+    if(bmi<18.5){
+       message = "體重過輕";
+	}else if(bmi < 24){
+       message = "正常範圍";
+	}else if(bmi < 27){
+       message = "過重";
+	}else if(bmi < 30){
+       message = "輕度肥胖";
+	}else if(bmi < 35){
+       message = "中度肥胖";
+	}else{
+       message = "重度肥胖";
+	}
+    return message;
 }
+
+
 
 int main() {
 	
@@ -37,10 +55,11 @@ int main() {
 	cout << "input height(cm):";
 	cin >> height;
 	
-	/*
+	
 	BMI p1(name,weight,height);
+    cout << p1.name << endl;
 	cout << p1.getBMI() << endl;
-	*/
+	cout << p1.suggestion() << endl;
 
-	cout << getBMI(weight,height) << endl;
+	
 }

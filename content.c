dd0625b67a282f21c8a04ebeeb0b625ec5849cfa
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 14:53:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int chinese;
-	cout << "請輸入國文分數:";
-	cin >> chinese;
-	cout << "您的國文分數是" << chinese << endl;
+	int year;
+	int westernYear;
+	
+	cout << "請輸入出生民國年份:";
+	cin >> year;
+	westernYear = 1911 + year;
+	cout << "您的西元年份是" << westernYear << endl;
 }

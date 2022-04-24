74d63331b4b9b0a41bdeb21e02101869e527078b
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 16:15:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main() {
-	string name;
+	string name,sex;
+	int year;
+	
 	cout << "請輸入您的姓名:";
 	cin >> name;
+	cout << "請輸入您的性別:";
+	cin >> sex;
+	cout << "請輸入您的出生(民國年):";
+	cin >> year;
+
+	cout << endl;
 	cout << "您的姓名是" << name << endl;
+	cout << "您的性別是" << sex << endl;
+	cout << "您的出生(西元):" << year + 1911 << endl;
+	cout << "您" << 2022 - (year + 1911)<< "歲" << endl;
 }

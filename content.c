9049f94201f5ac6f6206eb1227b5b107dab9b501
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 12:09:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,13 @@
 
 int main(void) {
 	int chinese;
-  	printf("請輸入國文分數:");
-	scanf("%d",&chinese);
-	printf("您輸入是國文分數是%d\n",chinese);
+	int english;
+	int math;
+  	printf("請輸入分數(國文,英文,數學):");
+	scanf("%d,%d,%d",&chinese,&english,&math);
+
+	int total;
+	total = chinese + english + math;
+	printf("總分為是%d\n",total);
   	return 0;
 }

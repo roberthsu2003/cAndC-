ae680cd1dfa5df4c50deaf8bd94d052fbd686a2a
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 14:42:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
 	int chinese;
-	std::cout << "請輸入國文分數:";
-	std::cin >> chinese;
-	std::cout << "您的國文分數是" << chinese << std::endl;
+	cout << "請輸入國文分數:";
+	cin >> chinese;
+	cout << "您的國文分數是" << chinese << endl;
 }

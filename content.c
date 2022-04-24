fe5a7a57a33e0bf401d578408f7427f11eeeb062
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 15:34:28 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
-	float PI = 3.14159;
-	float radius;
-	cout << "請輸半徑(可以有小數點):";
-	cin >> radius;
-	cout << "圓面積是:" <<  radius * radius * PI << endl;
-	
+	char charA = 'a';
+	cout << "charA的內容是" << charA << endl;
+	printf("charA的內容是%c\n",charA);
+	printf("charA的內容是%d\n",charA);
+
+	char charB = 97;
+	cout << "charB的內容是" << charB << endl;
+	printf("charB的內容是%c\n",charB);
+	printf("charB的內容是%d\n",charB);
 }

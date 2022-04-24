d92d5c1fd57cdc78d302669e61fdad22e7421f29
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 14:28:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,8 @@
 #include <iostream>
-#include <stdio.h>
 
-int main(void) {
+int main() {
 	int chinese;
-	int english;
-	int math;
-  	printf("請輸入分數(國文,英文,數學):");
-	scanf("%d,%d,%d",&chinese,&english,&math);
-
-	int total;
-	total = chinese + english + math;
-	printf("國文是%d\n",chinese);
-	printf("英文是%d\n",english);
-	printf("數學是%d\n",math);	
-	printf("總分為是%d\n",total);
-  	return 0;
+	std::cout << "請輸入國文分數:";
+	std::cin >> chinese;
+	std::cout << "您的國文分數是" << chinese << std::endl;
 }

7e0414570bf3b2ac9f556ab263548bd711fe233a
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 11:36:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-  	int chinese = 86;
-	int math = 85;
+  	int chinese = 86;	
+	int math = 85;	
 	int english = 92;
-
-	int total;
+	int total;	
 	total = chinese + math + english;
+	
+	printf("國文是%d\n",chinese);
+	printf("英文是%d\n",english);
+	printf("數學是%d\n",math);
 	printf("總分是%d\n",total);
   return 0;
 }

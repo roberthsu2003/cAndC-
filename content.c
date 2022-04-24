cd81b376022f6fe5860d7bac95ea62abae981deb
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 15:16:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main() {
-	int year;
-	int westernYear;
+	float PI = 3.14159;
+	float radius;
+	cout << "請輸半徑(可以有小數點):";
+	cin >> radius;
+	cout << "圓面積是:" <<  radius * radius * PI << endl;
 	
-	cout << "請輸入出生民國年份:";
-	cin >> year;
-	westernYear = 1911 + year;
-	cout << "您的西元年份是" << westernYear << endl;
 }

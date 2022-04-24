f5b5d4cf1f92012cf88bd37664d8ec2a994b9b30
徐國堂 <roberthsu2003@gmail.com>
2022-04-24 16:03:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,13 +2,8 @@
 using namespace std;
 
 int main() {
-	char charA = 'a';
-	cout << "charA的內容是" << charA << endl;
-	printf("charA的內容是%c\n",charA);
-	printf("charA的內容是%d\n",charA);
-
-	char charB = 97;
-	cout << "charB的內容是" << charB << endl;
-	printf("charB的內容是%c\n",charB);
-	printf("charB的內容是%d\n",charB);
+	string name;
+	cout << "請輸入您的姓名:";
+	cin >> name;
+	cout << "您的姓名是" << name << endl;
 }

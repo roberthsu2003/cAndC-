4dad6cf477b509b2330e73bcfaf6c8590fdfaee6
徐國堂 <roberthsu2003@gmail.com>
2022-05-01 16:34:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
-
+//請計算1~某一個正整數的總合
 int main() {
-	for(int i=0;i < 10; i++){
-		cout << "i=" << i << endl;
+	int sum=0;
+	int someone=0;
+	cout << "請輸入某一正整數:";
+	cin >> someone;
+	
+	for(int i=1;i <= someone; i++){
+		sum += i;
 	}
+	cout << "1~" << someone <<"的總合是:" << sum << endl;
 }

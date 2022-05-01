8c9569256ee760fb149d713f205e9a4d89853780
徐國堂 <roberthsu2003@gmail.com>
2022-05-01 14:46:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,38 +1,6 @@
 #include <iostream>
-//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
-using namespace std;
+//讓使用者輸入成績, 若成績在90分以上就顯示「優等」, 80-89分顯示「甲等」，70-79分顯示「乙等」，60-69分顯示「丙等」，60分以下顯示「丁等」。
 
 int main() {
-	int money;
-	int payMoney;
-	cout << "請輸入購買金額:";
-	cin >> money;
-	
-	/*----------由大到小------------
-	if(money >= 100000){
-		payMoney = money * 0.8;
-	}else if(money >= 50000){
-		payMoney = money * 0.85;
-	}else if(money >= 30000){
-		payMoney = money * 0.9;
-	}else if(money >= 10000){
-		payMoney = money * 0.95;
-	}else{
-		payMoney = money;
-	}
-	*/
 
-	if(money < 10000){
-		payMoney = money;
-	}else if(money < 30000){
-		payMoney = money * 0.95;
-	}else if(money < 50000){
-		payMoney = money * 0.9;
-	}else if(money < 100000){
-		payMoney = money * 0.85;
-	}else{
-		payMoney = money * 0.8;
-	}
-
-	cout << "實付金額是:" << payMoney << "元\n";
 }

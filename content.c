5fed9987fc5d4ac05175c6126e0f45c8e7b07001
徐國堂 <roberthsu2003@gmail.com>
2022-05-01 15:23:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,17 @@
 #include <iostream>
-using namespace std;
 
+//國文100同時數學100,獎金1000
+//國文100或者數學100,獎金500
+
+using namespace std;
 int main() {
-	int age;
-	cout << "請輸入您的age:";
-	cin >> age;
-	//使用邏輯判斷
-	if(age>=20 && age<=50){
-		cout << "壯年" << endl;
-	}else{
-		cout << "不是壯年" << endl;
-	}
+	int chinese;
+	int math;
+	int bonus = 0;
+	cout << "請輸入國文分數:";
+	cin >> chinese;
+	cout << "請輸入數學分數:";
+	cin >> math;
 	
+	cout << "獎金是:" << bonus << endl;
 }

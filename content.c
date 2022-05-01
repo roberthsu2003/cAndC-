204cd6fff62f88e1459e424c1721497da11e76ed
徐國堂 <roberthsu2003@gmail.com>
2022-05-01 11:30:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,23 @@
 #include <iostream>
+//請以(複合指定運算子)設計程式,讓用者輸入三個任意數，程式會顯示3數相加的總和(float)
 using namespace std;
 
 int main() {
-	int x = 5;
-	x += 5;
-	x -= 3;
-	x *= 3;
-	cout << x << endl;
+	float inputValue;	
+	float sum=0;
+	
+	cout << "請輸入第一個數:";
+	cin >> inputValue;
+	sum += inputValue;
+	
+	cout << "請輸入第二個數:";
+	cin >> inputValue;
+	sum += inputValue;
+	
+	cout << "請輸入第三個數:";
+	cin >> inputValue;
+	sum += inputValue;
+
+	cout << "三個數的總和為:" << sum << endl;
+	
 }

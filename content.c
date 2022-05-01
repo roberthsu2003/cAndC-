734de565f6f2516875b7d5409ee3ffc6ddf6ef39
徐國堂 <roberthsu2003@gmail.com>
2022-05-01 15:14:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,16 +5,11 @@ int main() {
 	int age;
 	cout << "請輸入您的age:";
 	cin >> age;
-	//巢狀判斷
-	if(age >= 30){
-		if(age <= 50){
-			cout << "壯年" << endl;
-		}else{
-			cout << "不是壯年" << endl;
-		}	
+	//使用邏輯判斷
+	if(age>=20 && age<=50){
+		cout << "壯年" << endl;
 	}else{
 		cout << "不是壯年" << endl;
 	}
-
 	
 }

4dd5468b1e696e48b11338da5866f51f3c1feab4
徐國堂 <roberthsu2003@gmail.com>
2022-05-01 15:04:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,20 @@
 #include <iostream>
-//讓使用者輸入成績, 若成績在90分以上就顯示「優等」, 80-89分顯示「甲等」，70-79分顯示「乙等」，60-69分顯示「丙等」，60分以下顯示「丁等」。
 using namespace std;
 
 int main() {
-  int score;
-  cout << "請輸入成績:";
-  cin >> score;
-  if(score >=90){
-    cout << "優等" << endl; 
-  }else if(score >=80){
-    cout << "甲等" << endl; 
-  }else if(score >=70){
-    cout << "乙等" << endl;
-  }else if(score >=60){
-    cout << "丙等" << endl;
-  }else{
-    cout << "丁等" << endl;
-  }
-  return 0;
+	int age;
+	cout << "請輸入您的age:";
+	cin >> age;
+	//巢狀判斷
+	if(age >= 30){
+		if(age <= 50){
+			cout << "壯年" << endl;
+		}else{
+			cout << "不是壯年" << endl;
+		}	
+	}else{
+		cout << "不是壯年" << endl;
+	}
+
+	
 }

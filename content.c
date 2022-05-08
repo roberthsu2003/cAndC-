2be7d89eda6d6e468bba54b54919ef97a5ee9403
徐國堂 <roberthsu2003@gmail.com>
2022-05-08 16:19:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
+
 int main() {
 	srand(time(NULL));
-	//1個學生的分數
-	int scores[5]; //建立陣列
+	int studentNum = 50;
+	int scoresNum = 5;
+	int totols = studentNum * scoresNum;
+	//50個學生每個學生5個分數
+	int scores[totols]; //建立陣列
 	//給值
-	for(int i=0; i<5; i++){
+	for(int i=0; i<totols; i++){
 		scores[i] = rand() % (100-60+1) + 60;
 	}
 
 	//輸出值
-	for(int i=0; i<5; i++){
-		cout << scores[i];
-		if(i==4){
-			cout << "\n";
-		}else{
-			cout << ",";
-		}
+	for(int i=0; i<<totols; i++){
+		cout << scores[i] << endl;
 	}
 }

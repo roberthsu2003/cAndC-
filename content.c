7f25be41f0f1066517a101afa17d18e2ad7631da
徐國堂 <roberthsu2003@gmail.com>
2022-05-08 15:32:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,40 +1,18 @@
 #include <iostream>
-#include <time.h>
-
 using namespace std;
-
 int main() {
-	int keyin;
-	int guess;
-	srand(time(NULL));
-	//1~99
-	int max = 99;
-	int min = 1;
-	int count = 0;
-	guess = rand() % (max - min + 1) + min;
-	//cout << guess << endl;
-	cout << "==========猜數字遊戲================\n\n";
-	while(true){
-		cout << "猜數字範圍" << min << "~" << max << ":";
-		cin >> keyin;
-		count++;
-		if(keyin >= min && keyin <= max){
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-				cout << "您猜了" << count << "次" << endl;
-				break;
-			}else if(keyin > guess){
-				max = keyin;
-				cout << "再小點" << endl;				
-			}else if(keyin < guess){
-				min = keyin;
-				cout << "再大一點" << endl;
-			}
-			cout << "您已經猜了" << count << "次" << "\n\n";
-		}else{
-			cout << "請輸入提示範圍內的數字!\n" << endl;
-		}
-		
-	}
-	cout << "遊戲結束" << endl;
+	int scores[5]; //建立陣列
+	//給值
+	scores[0] = 93;
+	scores[1] = 75;
+	scores[2] = 94;
+	scores[3] = 65;
+	scores[4] = 82;
+
+	//輸出值
+	cout << scores[0] << ",";
+	cout << scores[1] << ",";
+	cout << scores[2] << ",";
+	cout << scores[3] << ",";
+	cout << scores[4] << "\n";
 }

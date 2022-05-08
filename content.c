313dfd03ebaec795566f5619d7f98c18d49d5ab4
徐國堂 <roberthsu2003@gmail.com>
2022-05-08 12:28:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,9 @@
 #include <iostream>
-//請設計一個程式，讓使用者輸入數值，只有加總正偶數值，不加總正奇數值，如果輸入負數，結束程式。
+#include <time.h>
+
 using namespace std;
 
 int main() {
-	int inputValue;
-	int sum = 0;
-	int num = 0;
-	while(true){
-		cout << "請輸入第" << ++num << "個數值:";
-		cin >> inputValue;
-		if(inputValue < 0){
-			break;
-		}
-
-		if(inputValue % 2 == 1){
-			continue;
-		}
-		sum += inputValue;
-	}
-	cout << "所有輸入的正偶數的加總是:" << sum << endl;
+	srand(time(NULL));
+  	cout << rand() << endl;
 }

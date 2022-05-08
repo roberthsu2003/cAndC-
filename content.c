8f5bfdba475d64fe03724ec0fb6d88383d99e0db
徐國堂 <roberthsu2003@gmail.com>
2022-05-08 10:18:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,10 @@
 #include <iostream>
+// while迴圈明確知道執行幾次
 using namespace std;
-
 int main() {
-	for(int i=1; i<=9; ++i){		
-		for(int j=1; j<=9; j++){
-			cout << i << "*" << j << "=" << i * j << "\t";
-		}
-		cout << "\n";
+	int i=1;
+	while(i<=9){
+		cout << i << endl;
+		i++;
 	}
 }

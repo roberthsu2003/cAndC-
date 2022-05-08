192b4147c44c38ab6d5b49bfeaae7c2ad0172107
徐國堂 <roberthsu2003@gmail.com>
2022-05-08 16:48:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
 	srand(time(NULL));
-	int studentNum = 20;
-	int scoresNum = 10;
+	int studentNum = 50;
+	int scoresNum = 5;
 	int totols = studentNum * scoresNum;
 	//50個學生每個學生5個分數
 	int scores[totols]; //建立陣列
@@ -17,9 +17,12 @@ int main() {
 	//輸出值
 	for(int i=0; i<studentNum; i++){
 		cout << "第" << i+1 << "位學生  ";
+		int sum = 0;
 		for(int j=0;j<scoresNum;j++){
 			cout << scores[i*scoresNum + j] << " ";
+			sum += scores[i*scoresNum + j];
 		}
+		cout << "總分:" << sum;
 		cout << "\n";
 	}
 }

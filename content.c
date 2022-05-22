6f7215c5194b59742de77a9923b0ea5aaec0d3f3
徐國堂 <roberthsu2003@gmail.com>
2022-05-22 11:35:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,12 +3,19 @@
 using namespace std;
 
 int main() {
-	srand(time(NULL));	
-	int scoresNum = 250;
-	int scores[scoresNum];
-	for(int i=0; i<scoresNum; i++){
-		scores[i] = rand() % (100-60+1) + 60;
-		cout << scores[i] << endl;
-	}
+	srand(time(NULL));
+	int studentsNum = 50; //學生人數
+	int scoresNum = 5; //每個學生的分數
+	int scores[studentsNum][scoresNum]; //建立所有學生分數的2維陣列
+
+	for(int f=0;f < studentsNum; f++){
+		cout << "第" << f+1 << "學生:";
+		for(int l=0; l < scoresNum; l++){
+			scores[f][l] = rand() % (100-60+1) + 60;
+			cout << scores[f][l] << "   ";
+		}
+		cout << endl;
+	}	
+	
 	
 }

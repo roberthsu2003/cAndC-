62af66d9dbd30ed9c3ceea21d748369671d17ec8
徐國堂 <roberthsu2003@gmail.com>
2022-05-22 14:58:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,46 @@
+// Name        : guess.cpp
+//猜數字遊戲
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
 using namespace std;
-
-float temperature(int value){
-	return value * 1.8 + 32;
-}
-
 int main() {
-	int fvalue;
-  	float result = temperature(10); 
-	cout << "攝氏10度轉換為華氏為:" << result << endl;
-	cout << "請輸入攝氏溫度:";
-	cin >> fvalue;
-	result = temperature(fvalue);
-	cout << "攝氏溫度:" << fvalue << " 華氏溫度為:" << result << endl;
+	int guess;
+	int keyin;
+	int count = 0;
+	int max = 99;
+	int min = 1;
+
+	srand(time(NULL));
+	guess = rand() % (max-min+1) + min;
+	cout << guess << endl;
+	cout << "==============猜數字遊戲===============\n\n";
+	while(true){
+		printf("猜數字範圍%d~%d:",min,max);
+		cin >> keyin;
+		count += 1;
+
+		if(keyin >= min && keyin <= max){
+			//輸入的範圍正確
+			if(keyin == guess){
+				cout << "賓果!猜對了,答案是" << guess << endl;
+				cout << "您猜了" << count << "次" << endl;
+				break;
+			}else if(keyin > guess){
+				max = keyin - 1;
+				cout<< "再小一點!";
+			}else if(keyin < guess){
+				min = keyin + 1;
+				cout << "再大一點!";			}
+			cout << "您已經猜了" << count << "次" << endl;
+		}else{
+			//輸入的範圍不正確
+			cout << "請輸入提示範圍內的數字!\n";
+			continue;
+		}
+		
+	};
 	return 0;
 }

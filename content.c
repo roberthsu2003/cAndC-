295ccc477013e61b40917a7fcca0e13d3c4af9e9
徐國堂 <roberthsu2003@gmail.com>
2022-05-22 11:00:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
 	srand(time(NULL));	
-	int scoresNum = 5;
+	int scoresNum = 250;
 	int scores[scoresNum];
 	for(int i=0; i<scoresNum; i++){
 		scores[i] = rand() % (100-60+1) + 60;
 		cout << scores[i] << endl;
-	}	
+	}
 	
 }

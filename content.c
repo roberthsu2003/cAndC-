41e6b037ae8c4f574f76ff05b61e9fc66d1cac59
徐國堂 <roberthsu2003@gmail.com>
2022-05-22 16:46:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,41 +1,20 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-void getLots(){
-	int num = 7;
-   	int choose[num];
-   	int lot[49];
-	int maxIndex = 48;
+long long int factorial(int n){
+	if(n==0){
+		return 1;
+	}else{
+		return n * factorial(n-1);
+	}
 	
-	for(int i=0; i<49; i++){
-		lot[i] = i+1;
-	}
-
-	
-	for(int i=0; i<num; i++){
-		int randIndex = rand() % (maxIndex+1);
-		choose[i] = lot[randIndex];
-		lot[randIndex] = lot[maxIndex];	
-		maxIndex--;
-	}
-
-	cout << "本期大樂透電腦選號號碼如下:\n\n";
-	for(int i=0; i<num; i++){
-		cout << choose[i] << " ";
-	}
-
-	cout << "\n\n特別號:" << choose[num-1] << "\n\n";
 }
 
 int main() {
-	srand(time(NULL));
-	int nums;
-   	cout << "請輸入電腦選號的組數:";
-	cin >> nums;
-	for(int i=1; i<=nums; i++){
-		cout << "第" << i << "組號碼是:" << endl;
-		getLots();
-		cout << "\n\n";
-	}
+	int n;
+	long long int result;
+	cout << "請輸入階層:";
+	cin >> n;
+	result = factorial(n);
+	cout << n << "!=" << result << endl;
 }

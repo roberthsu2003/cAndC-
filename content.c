86edd437e58fd32fbcaffbe5021e32d821c81e29
徐國堂 <roberthsu2003@gmail.com>
2022-05-22 14:30:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,12 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
+//定義function
+void sayHello(){
+	cout << "歡迎光臨!" << endl;
+}
+
 int main() {
-	srand(time(NULL));
-	int nums;
-	cout << "請輸入要排序的數值個數:";
-	cin >> nums;
-	cout << "排序前:" << endl;
-	int array[nums];
-	for (int i=0; i<nums; i++){
-		array[i] = rand() % (999-1+1) + 1;
-		cout << array[i] << " ";
-	}
-	cout << endl;
-
-	//泡沫排序
-	for(int i=0;i < nums-1; i++){
-		for(int j=i+1;j<nums;j++){
-			if(array[i] > array[j]){
-				//交換2數
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-
-	cout << "排序後:" << endl;
-	for (int i=0; i<nums; i++){		
-		cout << array[i] << " ";
-	}
-
-	cout << endl;
-	
+  	sayHello(); //呼叫function
+	return 0;
 }

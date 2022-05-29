b90a40ce7c45e43badda048353717618ea762b32
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 10:18:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
-int add(int a, int b){
+
+
+//array是陣列變數call by Reference
+int add(int array[]){
 	int total;
-	total = a + b;
+	total = array[0] + array[1];
 	return total;
 }
 
 int main() {
-	int total,chinese,english;
+	int total;
+	int scores[2];
 	cout << "請輸入國文成績:";
-	cin >> chinese;
+	cin >> scores[0];
 	cout << "請輸入英文成績:";
-	cin >> english;		
-	total = add(chinese, english);
+	cin >> scores[1];		
+	total = add(scores);
 	cout << "總分是:" << total << endl;
 }

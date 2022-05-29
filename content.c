f94c5d037581f0d94d8e6b12f06c6212eb61ff87
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 16:58:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,6 +4,19 @@ main.cpp
 #include "tools.h"
 using namespace std;
 
+void rank_student(Student *s, int num){
+	Student temp;
+	for(int i=0; i < num-1; i++){
+		for(int j=i+1; j < num; j++){
+			if (s[i].average < s[j].average){
+				temp = s[i];
+				s[i] = s[j];
+				s[j] = temp;
+			}
+		}
+	}
+}
+
 int main() {
 	//建立陣列
 	int num;
@@ -14,11 +27,13 @@ int main() {
 
 	//將陣列內所有的結構給值
 	add_student_info(students,num);
+
+	//排陣列順序
+	rank_student(students,num);
 	
 	//輸出學生的資料
 	print_students(students, num);
 }
-
 
 
 tools.h

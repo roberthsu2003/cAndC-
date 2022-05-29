0e79da589f9898acd2d21a63c0ce71701a4ef4fd
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 15:56:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 typedef struct student{
@@ -9,11 +10,12 @@ typedef struct student{
 }Student;
 
 void add_student_info(Student *s,int num){
+	srand(time(NULL));
 	for(int i=0; i<num; i++){
-		s[i].name= "robert";
-		s[i].chinese = 89;
-		s[i].english = 90;
-		s[i].math = 65;
+		s[i].name= "stu" + to_string(i+1);
+		s[i].chinese = rand() % (100-60+1) + 60;
+		s[i].english = rand() % (100-60+1) + 60;
+		s[i].math = rand() % (100-60+1) + 60;
 	}
 }
 
@@ -21,10 +23,13 @@ void add_student_info(Student *s,int num){
 
 int main() {
 	//建立陣列
-	Student students[50];
+	int num;
+	cout << "請輸入學生數量:";
+	cin >> num;
+	Student students[num];
 
 	//將陣列內所有的結構給值
-	add_student_info(students,50);
+	add_student_info(students,num);
 	
 	//輸出第一位學生的資料
 	cout << students[0].name << endl;

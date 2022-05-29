145535f6e1f149c9ba556fc9807d87f27742f857
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 12:12:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,6 +2,12 @@
 #include <time.h>
 
 using namespace std;
+void create_scores(int scores[],int num){
+	for(int i=0; i<num; i++){
+		scores[i] = rand() % (100-60+1) + 60;
+		cout << scores[i] << " ";
+	}
+}
 
 int main() {
 	srand(time(NULL));
@@ -9,9 +15,7 @@ int main() {
 	cout << "請輸入學生考試科目是數量:";
 	cin >> num;
 	int scores[num];
-	for(int i=0; i<num; i++){
-		scores[i] = rand() % (100-60+1) + 60;
-		cout << scores[i] << " ";
-	}
+	//建立學生分數
+	create_scores(scores,num);
 	cout << endl;
 }

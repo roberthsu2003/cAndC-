8726aec3112e6f495fb13867f86aff4d92c74584
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 14:39:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void print_array(int *array,int count){
+	for(int i=0;i < count; i++){
+		cout << array[i] << " ";
+	}
+	cout << endl;
+
+	for(int i=0;i < count; i++){
+		cout << *(array+i) << " ";
+	}
+	cout << endl;
+}
+
 int main() {
 	int array[] = {100, 200, 300};
-	//透過陣列變數取出值
-	for(int i=0; i<3; i++){
-		cout<< array[i] << " ";
-	}
-	cout << endl;
-
-	//透過陣列變數取出元素記憶體位置
-	for(int i=0; i<3; i++){
-		cout << &array[i] << " ";
-	}
-	cout << endl;
-
-	//透過指標變數取出記憶體位置
-	int *ptr_array = array;
-	for(int i=0; i<3; i++){
-		cout << ptr_array + i << " ";
-	}
-	cout << endl;
-
-	//透過指標變數取出元素值
-	
-	for(int i=0; i<3; i++){
-		cout << *(ptr_array + i) << " ";
-	}
-	cout << endl;
+	print_array(array,3);
 }

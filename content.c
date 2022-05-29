d10e960fbb02df4f534d5b374e5e7e7da31de54a
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 16:28:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,6 +7,8 @@ typedef struct student{
 	int chinese;
 	int math;
 	int english;
+	int sum;
+	float average;
 }Student;
 
 void add_student_info(Student *s,int num){
@@ -16,22 +18,28 @@ void add_student_info(Student *s,int num){
 		s[i].chinese = rand() % (100-60+1) + 60;
 		s[i].english = rand() % (100-60+1) + 60;
 		s[i].math = rand() % (100-60+1) + 60;
+		s[i].sum = s[i].chinese + s[i].english + s[i].math;
+		s[i].average = s[i].sum / 3.0;
 	}
 }
 
 void print_students(Student *s,int num){
 	cout << "姓名" << "\t";
 	cout << "國文" << "\t";
-	cout << "英文" << "\t";
-	cout << "數學" << endl;
-
+	cout << "英文" << "\t";	
+	cout << "數學" << "\t";
+	cout << "總分" << "\t";
+	cout << "平均" << endl;
+	
 	for(int i=0; i<num; i++){
 		Student stu = s[i];
 		cout << stu.name << "\t";
 		cout << stu.chinese << "\t\t";
 		cout << stu.english << "\t\t";
-		cout << stu.math << "\t";
-		cout << endl;
+		cout << stu.math << "\t\t";
+		cout << stu.sum << "\t\t";
+		cout << stu.average << endl;
+		
 	}
 	
 }

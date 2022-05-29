e0e15e18b5353d2e8fa02359ece445b260c02951
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 14:54:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void print_array(int *array,int count){
-	for(int i=0;i < count; i++){
-		cout << array[i] << " ";
-	}
-	cout << endl;
-
-	for(int i=0;i < count; i++){
-		cout << *(array+i) << " ";
-	}
-	cout << endl;
-}
-
 int main() {
-	int array[] = {100, 200, 300};
-	print_array(array,3);
+	char word[] = {'H','e','l','l','o'};
+	cout << word << endl;
+	
+	//就是c語言建立的字串(字元陣列)
+	const char *word1 = "Hello";
+	cout << word1 << endl;
+	word1 = "World";
+	cout << word1 << endl;
+
+	//c++語言 string
+	string word2 = "Hello";
+	cout << word2 << endl;
+	word2 = "World";
+	cout << word2 << endl;
 }

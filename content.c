489e19b3babc2cc33da380fb24d40f718e9bd233
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 12:23:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,7 +7,19 @@ void create_scores(int scores[],int num){
 		scores[i] = rand() % (100-60+1) + 60;
 		cout << scores[i] << " ";
 	}
+	cout << endl;
 }
+
+void sum_average(int scores[], int num){
+	int sum=0;
+	for(int i=0; i<num; i++){
+		sum += scores[i];
+	}	
+	cout << "學生總分是:" << sum << endl;
+	cout << "學生平均是:" << (float)sum/num << endl;
+}
+
+
 
 int main() {
 	srand(time(NULL));
@@ -17,5 +29,6 @@ int main() {
 	int scores[num];
 	//建立學生分數
 	create_scores(scores,num);
-	cout << endl;
+	sum_average(scores,num);
+	
 }

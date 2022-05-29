c823c5e49fcbc5d4dbdb35219b046b124529b023
徐國堂 <roberthsu2003@gmail.com>
2022-05-29 15:13:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	char word[] = {'H','e','l','l','o'};
-	cout << word << endl;
-	
-	//就是c語言建立的字串(字元陣列)
-	const char *word1 = "Hello";
-	cout << word1 << endl;
-	word1 = "World";
-	cout << word1 << endl;
+typedef struct student{
+	string name;
+	int chinese;
+	int math;
+	int english;
+}Student;
 
-	//c++語言 string
-	string word2 = "Hello";
-	cout << word2 << endl;
-	word2 = "World";
-	cout << word2 << endl;
+int main() {
+	Student stu1;
+	stu1.name = "Robert";
+	stu1.chinese = 78;
+	stu1.english = 85;
+	stu1.math = 76;
+
+	cout << stu1.name << endl;
+	cout << stu1.chinese << endl;
+	cout << stu1.english << endl;
+	cout << stu1.math << endl;
 }

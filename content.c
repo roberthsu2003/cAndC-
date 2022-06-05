839ebbdcf47dfd70b8ad2bde344fecdc7ebe6db8
徐國堂 <roberthsu2003@gmail.com>
2022-06-05 15:12:39 +0800

    Update content.c

--- content.c
+++ content.c
@@ -12,11 +12,32 @@ class BMI{
 		BMI(float weight, float Height);
 		float getHeight();
 		float getWeight();
+		void setHeight(float height);
+		void setWeight(float weight);
 };
 
+void BMI::setHeight(float height){
+	if(height < 50){
+		this->height = 50;
+	}else if(height > 250){
+		this->height = 250;
+	}else{
+		this->height = height;
+	}
+}
+void BMI::setWeight(float weight){
+	if(weight < 30){
+		this->weight = 30;
+	}else if(weight > 200){
+		this->weight = 200;
+	}else{
+		this->weight = weight;
+	}
+}
+
 BMI::BMI(float weight, float height){
-	this -> weight = weight;
-	this -> height = height;
+	setWeight(weight);
+	setHeight(height);
 }
 
 float BMI::getHeight(){
@@ -31,12 +52,13 @@ float BMI::getWeight(){
 
 
 main.cpp
+
 #include <iostream>
 #include "BMI.h"
 
 using namespace std;
 int main() {
-	BMI bmi1(78, 170);
+	BMI bmi1(210, 270);
 	cout << "bmi1的身高是" << bmi1.getHeight() << endl;
 	cout << "bmi1的體重是" << bmi1.getWeight() << endl;
 }

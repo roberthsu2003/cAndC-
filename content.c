43b4722a5f68558145e8a36e07b97ef62e169ce2
徐國堂 <roberthsu2003@gmail.com>
2022-06-05 14:38:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,43 +1,9 @@
-tools.h
-
-#ifndef __TOOLS_H__
-#define __TOOLS_H__
-
-class Square{
-	public:
-	int width;
-	Square();
-	Square(int width);
-	int getArea();
-
-};
-
-Square::Square(){
-	width = 0;
-}
-Square::Square(int width){
-	this->width = width;
-}
-int Square::getArea(){
-	return width * width;
-}
-#endif
-
-main.cpp
-
 #include <iostream>
 #include "tools.h"
 
 using namespace std;
 int main() {
-	Square s1(10);
-	cout << "s1的寬是" << s1.width << endl;
-	cout << "s1面積是:" << s1.getArea() << endl;
-
-	cout << "請輸入正方形的一邊:";
-	Square s2;
-	cin >> s2.width;
-	cout << "s2的寬是" << s2.width << endl;
-	cout << "s2面積是:" << s2.getArea() << endl;
-	
+	Square *s1 = new Square(10);
+	cout << "s1的寬是:" << s1->width << endl;
+	cout << "s1的面積是:" << s1->getArea() << endl;
 }

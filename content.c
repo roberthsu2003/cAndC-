9b95186797b737c8844a637dee06861bf1e253e0
徐國堂 <roberthsu2003@gmail.com>
2022-06-05 11:47:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,31 @@
+main.h
+
+#include <iostream>
+#include "tools.h"
+
+People p1;
+People p2;
+using namespace std;
+
+int main() {
+	p1.name = "robert";
+	p1.height = 170;
+	p1.weight = 76;
+
+	cout << p1.name << endl;
+	cout << p1.height << endl;
+	cout << p1.weight << endl;
+
+	p2.name = "john";
+	p2.height = 180;
+	p2.weight = 80;
+
+	cout << p2.name << endl;
+	cout << p2.height << endl;
+	cout << p2.weight << endl;
+}
+
+tools.h
 #ifndef __TOOLS_H__
 #define __TOOLS_H__
 

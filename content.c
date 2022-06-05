dd6151734e9b1dc1ffe2414c1659c0869cf2e59b
徐國堂 <roberthsu2003@gmail.com>
2022-06-05 11:36:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,42 +1,14 @@
-main.h
-
-#include <iostream>
-#include "tools.h"
-
-People p1;
-People p2;
-using namespace std;
-
-int main() {
-	cout << m << endl;
-}
-
-tools.h
 #ifndef __TOOLS_H__
 #define __TOOLS_H__
 
-#define PI 3.14159
-#define SQUARE(x) (x) * (x)
-
-int m = 10;
-
-int add(int a, int b){
-	return a + b;
-}
-
-int substract(int a, int b){
-	return a - b;
-}
-
-typedef enum direction{
-	North,
-	East,
-	South,
-	West
-}Direction;
+#include <iostream>
+using namespace std;
 
 class People{
-	
+	public:
+		string name;
+		int height;
+		int weight;
 };
 
 

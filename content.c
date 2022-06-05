dd4fa91ba2e3ea4e271d4f86c48fb4bbdbec195b
徐國堂 <roberthsu2003@gmail.com>
2022-06-05 14:14:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,60 +1,33 @@
-main.h
-
-#include <iostream>
-#include "tools.h"
-
-#include <iostream>
-#include "tools.h"
-
-People p1("robert",170, 76);
-People p2("john", 180, 80);
-using namespace std;
-
-int main() {	
-
-	cout << p1.name << endl;
-	cout << p1.height << endl;
-	cout << p1.weight << endl;
-	cout << p1.bmi() << endl;
-	
-
-	cout << p2.name << endl;
-	cout << p2.height << endl;
-	cout << p2.weight << endl;
-	cout << p2.bmi() << endl;
-}
-
 tools.h
+
 #ifndef __TOOLS_H__
 #define __TOOLS_H__
 
-#include <iostream>
-#define SQUARE(x) ((x)*(x))
-using namespace std;
-
-class People{
+class Square{
 	public:
-		//實體欄位
-		string name;
-		int height;
-		int weight;
-		
-		//實體方法的原型
-		float bmi();
-		//自訂建構式原型
-		People(string n, int h, int w);
+	int width;
+	Square(int width);
+	int getArea();
+
 };
 
-//實作原型
-float People::bmi(){
-			return weight / SQUARE(height/100.0);
+Square::Square(int width){
+	this->width = width;
 }
-
-//實作建構式原型
-People::People(string n, int h, int w){
-			name = n;
-			height = h;
-			weight = w;
+int Square::getArea(){
+	return width * width;
 }
-
 #endif
+
+
+main.cpp
+
+#include <iostream>
+#include "tools.h"
+
+using namespace std;
+int main() {
+	Square s1(10);
+	cout << "s1的寬是" << s1.width << endl;
+	cout << "面積是:" << s1.getArea() << endl;
+}

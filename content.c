9492f588c9df2b78b84fcb65a20f187b7745e481
徐國堂 <roberthsu2003@gmail.com>
2022-06-05 10:16:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,11 @@
 #include <iostream>
 #define PI 3.14159
+#define SQUARE(x) (x) * (x)
 
 using namespace std;
 int main() {
 	int r;
 	cout << "請輸入半徑:" << endl;
 	cin >> r;
-	cout << "面積是:" << PI * r * r << endl;
+	cout << "面積是:" << PI * SQUARE(r) << endl;
 }

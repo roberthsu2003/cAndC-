6a25b0d455d5b5b3d2b44c43da463fbb0be62d93
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 14:43:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
 	//c 語言的字串
 	const char* name = "徐國堂";
 	printf("我的名字是%s\n",name);
 	
-	std::string englishName = "robert";
-	std::cout << englishName << std::endl;
+	string englishName = "robert";
+	cout << englishName << endl;
 }

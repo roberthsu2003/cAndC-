a2c419c54c8782cbda4a8de14b4b6fc3eae66e66
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 16:22:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,10 @@
 #include <iostream>
+#define MYPI 3.14159
+
 using namespace std;
+
 int main() {
-	int chinese=0,english=0,math=0;
-	cout << "請輸入國文成績(0~100):";
-	cin >> chinese;
-
-	cout << "請輸入英文成績(0~100):";
-	cin >> english;	
-
-	cout << "請輸入數學成績(0~100):";
-	cin >> math;
-
-	int sum = chinese + english + math;
-	//強制轉換型別
-	float average = (float)sum / 3;
-
-	cout << "您的總分為:" << sum << "分,平均為:" << average << "分\n";
-	
+	const float PI = 3.14159;
+	cout << PI << endl;
+	cout << MYPI << endl;
 }

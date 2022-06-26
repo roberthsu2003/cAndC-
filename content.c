60da19c684bd5e2c02519d44a345a5d18474388b
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 15:07:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int x,y,total;
-	cout << "請輸入x的值:";
-	cin >> x;
-
-	cout << "請輸入y的值:";
-	cin >> y;
-
-	total = x + y;
-	cout << x << "+" << y << "=" << total << endl;
-	
+	cout << "姓名\t座號\t電話\n";
+	cout << "李振聲\t11\t\t0229435437\n";
+	cout << "張四聲\t12\t\t0229435400\n";
 }

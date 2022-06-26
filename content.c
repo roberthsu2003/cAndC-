92ec63b8d08f870bb6f2c668a8ff51ac1a6c8eac
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 16:09:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	double PI = 3.1415915987;
-	int radius = 9;
-	int area = PI * radius * radius;
-	cout << "圓面積=" << area << endl;
-	printf("圓面積=%d\n", area);
+	int chinese=0,english=0,math=0;
+	cout << "請輸入國文成績(0~100):";
+	cin >> chinese;
+
+	cout << "請輸入英文成績(0~100):";
+	cin >> english;	
+
+	cout << "請輸入數學成績(0~100):";
+	cin >> math;
+
+	int sum = chinese + english + math;
+	//強制轉換型別
+	float average = (float)sum / 3;
+
+	cout << "您的總分為:" << sum << "分,平均為:" << average << "分\n";
+	
 }

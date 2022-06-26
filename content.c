ca9baab66b6e4fdac1960efd99a19e5b0a9cc87a
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 12:05:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-	float myPi = 3.14159;
-	double myDouble = 3.141596743;
-	printf("myPi=%.2f\n",myPi);
-	printf("myDoube=%.9f\n",myDouble);
-	return 0;
+	int x;
+	printf("請輸入一個整數:");
+	scanf("%d",&x);
+	printf("使用者輸入的是:%d\n",x);
+  	return 0;
 }

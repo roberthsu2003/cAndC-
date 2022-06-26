65e16e56092b10f0611553dc3abed87336b2a9a9
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 12:23:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-	int x;
-	printf("請輸入一個整數:");
+	int x,y,total;
+	printf("請輸入x的值:");
 	scanf("%d",&x);
-	printf("使用者輸入的是:%d\n",x);
-  	return 0;
+
+	printf("請輸入y的值:");
+	scanf("%d",&y);
+
+	total = x + y;
+
+	printf("%d+%d=%d\n",x,y,total);
+	return 0;
 }

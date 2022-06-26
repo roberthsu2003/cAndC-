98513f1727fa841fbba3027fb8905fdf8f9bdd61
徐國堂 <roberthsu2003@gmail.com>
2022-06-26 15:16:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "姓名\t座號\t電話\n";
-	cout << "李振聲\t11\t\t0229435437\n";
-	cout << "張四聲\t12\t\t0229435400\n";
+	bool bolA = true;
+	bool bolB = false;
+
+	cout << bolA << endl;
+	cout << bolB << endl;
 }

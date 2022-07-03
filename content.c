6226aa8e2e24589d356d0f5213e8d7edfbd45528
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 14:40:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,7 +3,7 @@ using namespace std;
 /*
 國文100同時數學100,獎金1000
 國文100或者數學100,獎金500
-巢狀判斷
+邏輯判斷
 */
 
 int main() {
@@ -12,18 +12,13 @@ int main() {
 	int bonus;
 	cout << "請輸入國文成績和數學成績(國文,數學):";
 	scanf("%d,%d",&chinese,&math);
-	if(chinese == 100){
-		if(math == 100){
-			bonus = 1000;
-		}else{
-			bonus = 500;
-		}
+	
+	if(chinese == 100 && math == 100){
+		bonus = 1000;
+	}else if(chinese == 100 || math == 100){
+		bonus = 500;
 	}else{
-		if(math == 100){
-			bonus = 500;
-		}else{
-			bonus = 0;
-		}
+		bonus = 0;
 	}
 
 	cout << "獎金是:" << bonus << endl;

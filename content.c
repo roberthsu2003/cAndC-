3e8ce893195f9fb7f0217aaeee2e8cf25d05a67c
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 12:26:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,19 @@
 #include <iostream>
-//成績及格判斷
-//請使用者輸入成績，若成績大於等於60分，則顯示及格，否則顯示不及格。
+//讓使用者輸入成績, 若成績在90分以上就顯示「優等」, 80-89分顯示「甲等」，70-79分顯示「乙等」，60-69分顯示「丙等」，60分以下顯示「丁等」。
 using namespace std;
 int main() {
 	int score;
 	cout << "請輸入成績:";
 	cin >> score;
-	if (score >= 60){
-		cout << "您的分數是" << score << "及格\n"; 
+	if(score >= 90){
+		cout << "優等\n";
+	}else if (score >= 80){
+		cout << "甲等\n";
+	}else if (score >= 70){
+		cout << "乙等\n";
+	}else if (score >= 60){
+		cout << "丙等\n";
 	}else{
-		cout << "您的分數是" << score << "不及格\n";
+		cout << "丁等\n";
 	}
 }

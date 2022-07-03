eabf65f12a7e2f85f70bd7bb6a9ac396b1d552e1
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 14:29:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,30 @@
 #include <iostream>
-//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
 using namespace std;
+/*
+國文100同時數學100,獎金1000
+國文100或者數學100,獎金500
+巢狀判斷
+*/
 
 int main() {
-	int money;
-	int payMoney;
-	cout << "請輸入購買金額:";
-	cin >> money;
-
-	if(money < 10000){
-		payMoney = money;
-	}else if(money < 30000){
-		payMoney = money * 0.95;
-	}else if(money < 50000){
-		payMoney = money * 0.9;
-	}else if(money < 100000){
-		payMoney = money * 0.85;
+	int chinese;
+	int math;
+	int bonus;
+	cout << "請輸入國文成績和數學成績(國文,數學):";
+	scanf("%d,%d",&chinese,&math);
+	if(chinese == 100){
+		if(math == 100){
+			bonus = 1000;
+		}else{
+			bonus = 500;
+		}
 	}else{
-		payMoney = money * 0.8;
+		if(math == 100){
+			bonus = 500;
+		}else{
+			bonus = 0;
+		}
 	}
 
-	cout << "實付金額是:" << payMoney << "元\n";
+	cout << "獎金是:" << bonus << endl;
 }

f5c7334730451b4cc7a0af990a8b2383f1ecce26
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 10:07:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int x = 10;
-	int y = 20;
-	x = x + 10;
-	cout << "x=" << x << endl;
-
-	y += 10;
-	cout << "y=" << y << endl;
+	//請使用者輸入一個任意數，程式會顯示此數的平方值及立方值
+	float num,result;
+	cout << "請輸入任意數:";
+	cin >> num;
+	result = num;
+	result *= num;
+	cout << "此數的平方是:" << result << "\n";
+	result *= num;
+	cout << "此數的立方是:" << result << "\n";
 }

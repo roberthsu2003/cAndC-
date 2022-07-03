571e3b7db8f65fe6261909027719128b7ee05818
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 12:01:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,14 @@
 #include <iostream>
+//成績及格判斷
+//請使用者輸入成績，若成績大於等於60分，則顯示及格，否則顯示不及格。
 using namespace std;
-//讓使用者輸入密碼,如果輸入的密碼正確(1234), 要顯示「密碼正確!歡迎光臨!」。如果不正確就不做任何事
 int main() {
-	string password;
-	cout << "請輸入密碼:";
-	cin >> password;
-	//單項選擇
-	if (password == "1234"){
-		//true的程式區塊
-		cout << "密碼正確!歡迎光臨!" << endl;
+	int score;
+	cout << "請輸入成績:";
+	cin >> score;
+	if (score >= 60){
+		cout << "您的分數是" << score << "及格\n"; 
 	}else{
-		//false程式區塊
-		cout << "密碼錯誤!" << endl;
+		cout << "您的分數是" << score << "不及格\n";
 	}
-
 }

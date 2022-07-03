7d293da45d237c07fbcb0ac9be9c0513a6c3930b
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 15:28:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,10 @@
 #include <iostream>
+//計算1 - 10所有偶數的總和
 using namespace std;
-
 int main() {
-	for(int i=0;i<10;i++){
-		cout << i << endl;
+	int sum = 0;
+	for(int i=2; i<=100; i+=2){
+		sum += i;
+		cout << "第" << i/2 << "次迴圈 i=" << i << ",總和為" << sum << endl;
 	}
 }

c6e95786c8f8ede3f193918807f91186c58fa492
徐國堂 <roberthsu2003@gmail.com>
2022-07-03 15:52:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,19 @@
 #include <iostream>
-//計算1 - 10所有偶數的總和
+//計算固定中的支出，媽媽每天會將家裡的花費記錄下來，並且計算本週的花費總和
 using namespace std;
+
 int main() {
-	int sum = 0;
-	for(int i=2; i<=100; i+=2){
-		sum += i;
-		cout << "第" << i/2 << "次迴圈 i=" << i << ",總和為" << sum << endl;
+	int n, sum=0;
+	for(int i=1; i<=7; i++){
+		if(i == 7){
+			cout << "請輸入星期日的支出:";
+		}else{
+			cout << "請輸入星期" << i << "的支出:";
+		}
+		
+		cin >> n;
+		sum += n;
 	}
+	cout << "本星期的支出為:" << sum << "元\n";
+	
 }

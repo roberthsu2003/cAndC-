1a1f0d6aa26fcd54778d4b782d628660064d07da
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 14:42:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,15 +3,23 @@ using namespace std;
 
 int main() {
 	//建立一個學生的分數
-	int scores[5];
-	scores[0] = 78;
-	scores[1] = 92;
-	scores[2] = 74;
-	scores[3] = 91;
-	scores[4] = 83;
+	//宣告的同時給值
+	//由使用者手動輸入分數
+	int num = 5;
+	int scores[num];
+	int sum = 0;	
 
-	for(int i=0; i<5; i++){
+	for(int i=0; i<num;i++){
+		cout << "請輸入第" << i+1 << "學生的分數:";
+		cin >> scores[i];		
+	}	
+	
+	for(int i=0; i<num; i++){
 		cout << scores[i] << endl;
+		sum += scores[i];
 	}
+
+	cout << "學生總分是:" << sum << endl;
+	cout << "學生平均分數是" << (float)sum / num << endl;
 	
 }

5c91f752f7e3555ed076bf6d0111db538a41cfbd
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 15:04:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,8 +8,14 @@ int main() {
 	//由亂數建立分數
 	srandom(time(NULL));
 	int num;
+	int subjectNum;
+	int studentNum;
+	cout << "請輸入學生人數:";
+	cin >> studentNum;	
 	cout << "請輸入學生的科目數量";
-	cin >> num;
+	cin >> subjectNum;
+
+	num = studentNum * subjectNum;
 	
 	int scores[num];
 	int sum = 0;	
@@ -17,13 +23,14 @@ int main() {
 	for(int i=0; i<num;i++){		
 		scores[i] = random() % (100-50+1) + 50; //50~100的亂數 	
 	}	
-	
+
 	for(int i=0; i<num; i++){
-		cout << scores[i] << endl;
+		cout << scores[i] << "\t";
 		sum += scores[i];
 	}
+	cout << endl;
 
-	cout << "學生總分是:" << sum << endl;
-	cout << "學生平均分數是" << (float)sum / num << endl;
+	cout << "所有學生總分是:" << sum << endl;
+	cout << "所有學生平均分數是" << (float)sum / num << endl;
 	
 }

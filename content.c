ec7e65a655b653c6b6789455385c5c9c89b4c4bb
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 11:40:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,5 +8,6 @@ int main() {
 	cout << "亂數值:"<< random_value << endl;
 	cout << "亂數值0~99:"<< random_value % 100 << endl;
 	cout << "亂數值1~100:"<< random_value % 100 + 1 << endl;
+	cout << "亂數值50~60:" << random_value % (60-50+1) + 50 << endl;
 }
 

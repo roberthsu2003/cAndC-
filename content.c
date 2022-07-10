e7b073582fe5de99bdd6092d1b03bfdff3a5a694
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 10:30:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,15 +5,15 @@ int main() {
 	int score;
 	int sum=0;
 	int num=0;
-	do{		
+	while(true){		
 		cout << "請輸入第" << num+1 << "位學生成績:";
 		cin >> score;
-		if(score >= 0){
-			sum += score;
-			num += 1;
+		if(score < 0){
+			break;
 		}		
-		
-	}while(score >= 0);
+		sum += score;
+		num += 1;			
+	}
 
 	cout << "全班總成績為:" << sum << ",平均分數是" << (float)sum / num << endl;
 	

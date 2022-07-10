2a00433deba480e80d1f3dee87c811747b7f93fb
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 11:49:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,12 +2,12 @@
 #include <time.h>
 
 using namespace std;
-int main() {	
+int main() {
+	int guess;
+	int max = 99;
+	int min = 1;
 	srandom(time(NULL));
-	long random_value = random();
-	cout << "亂數值:"<< random_value << endl;
-	cout << "亂數值0~99:"<< random_value % 100 << endl;
-	cout << "亂數值1~100:"<< random_value % 100 + 1 << endl;
-	cout << "亂數值50~60:" << random_value % (60-50+1) + 50 << endl;
+	guess = random() % (max-min+1) + min;
+	cout << "================猜數字遊戲=================\n\n";
+	cout << "數字:" << guess << endl;
 }
-

fd88d1eabb7a381df036281c33888059397d0bfe
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 14:23:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	//建立陣列變數的語法
-	int numbers[10];
-	double scores[10];
-	char ascii[10];
+	//建立一個學生的分數
+	int scores[5];
+	scores[0] = 78;
+	scores[1] = 92;
+	scores[2] = 74;
+	scores[3] = 91;
+	scores[4] = 83;
+
+	for(int i=0; i<5; i++){
+		cout << scores[i] << endl;
+	}
+	
 }

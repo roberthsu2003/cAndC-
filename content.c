e0030db94bef29766bc8f787c8dfa8cfd20e87b3
徐國堂 <roberthsu2003@gmail.com>
2022-07-10 16:31:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,39 +1,39 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
-
 int main() {
-	//建立一個學生的分數
-	//宣告的同時給值
-	//由亂數建立分數
-	srandom(time(NULL));
-	int num;
-	int subjectNum;
-	int studentNum;
-	cout << "請輸入學生人數:";
-	cin >> studentNum;	
-	cout << "請輸入學生的科目數量";
-	cin >> subjectNum;
-
-	num = studentNum * subjectNum;
+	int nums = 4;
+	int array[nums];
 	
-	int scores[num];
-		
-	
-	for(int i=0; i<num;i++){		
-		scores[i] = random() % (100-50+1) + 50; //50~100的亂數 	
-	}	
-
-	for(int i=0; i<studentNum; i++){
-		cout << "學生" << i+1 << "分數:" << endl;
-		int sum = 0;
-		for(int j=0;j<subjectNum;j++){
-			int scores_index = i * subjectNum + j;
-			cout << scores[scores_index] << "\t";
-			sum += scores[scores_index];
-		}
-		cout << sum << "\t";
-		cout << (float)sum / subjectNum << endl;
+	for(int i=0; i<nums; i++){
+		cout << "請輸入第" << i+1 << "個數值:";
+		cin >> array[i];
 	}
-	
+
+	//排序後的順序
+	cout << "排序前:" << endl;
+	for(int i=0; i<nums; i++){
+		cout << array[i] << "\t";
+	}
+	cout << endl;
+
+	//開始排序(泡沫排序法)
+	for(int i=0; i<nums-1; i++){
+		for(int j=i+1; j<nums; j++){
+			int head = array[i];
+			int end = array[j];
+			if(head > end){
+				int temp = head;
+				array[i] = end;
+				array[j] = temp;
+			}
+		}
+		
+	}
+
+	//排序後的順序
+	cout << "排序後:" << endl;
+	for(int i=0; i<nums; i++){
+		cout << array[i] << "\t";
+	}
+	cout << endl;
 }

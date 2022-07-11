7359a7748eb371a3f98d75ed86ccccd77ebb1c55
徐國堂 <roberthsu2003@gmail.com>
2022-07-11 14:50:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,6 @@
-#include <iostream>
-//讓使用者輸入加、減、乘、除運算子, 就會顯示運算結果
-using namespace std;
-int main() {
-	char op;
-	int num1 = 20, num2 = 4;
-	cout << "num1=20,num2=4\n";
-	cout << "請輸入要執行的運算(+-*/):";
-	cin >> op;
-	switch(op){
-		case '+':
-		cout << "num1+num2=" << num1+num2 << "\n";
-		break;
+#include <stdio.h>
 
-		case '-':
-		cout << "num1-num2=" << num1-num2 << "\n";
-		break;
-
-		case '*':
-		cout << "num1*num2=" << num1*num2 << "\n";
-		break;
-
-		case '/':
-		cout << "num1/num2=" << num1/num2 << "\n";
-		break;
-
-		default:
-		cout << "無法執行運算!\n";
-		
-	}
+int main(void) {
+  printf("Hello 這是我第一個c語言程式\n");
+  return 0;
 }

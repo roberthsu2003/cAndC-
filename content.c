6871f84e9d1f28a1e716d15bd9ed13d97c9475d8
徐國堂 <roberthsu2003@gmail.com>
2022-07-11 16:52:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-	//c語言的輸入輸出
+  //c語言的輸入輸出
 	int chinese,english;
-	printf("請輸入中文成績:");
-	scanf("%d", &chinese);
-	printf("請輸入英文成績:");
-	scanf("%d",&english);
-
+	printf("請輸入中文和英文成績(國文,英文):");
+	scanf("%d,%d",&chinese,&english);
 	printf("您的中文分數是%d,英文分數是%d\n",chinese,english);
+
+	int total = chinese + english;
+	int average = total / 2;
+	printf("總分是%d,平均是%d\n",total, average);
+	return 0;
 	
-  return 0;
 }

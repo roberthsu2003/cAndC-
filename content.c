c71de281d8dfae85bb8444d48010a68cc5d0e0b4
徐國堂 <roberthsu2003@gmail.com>
2022-07-11 16:09:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,9 +7,21 @@ int main(void) {
 	//單行註解
 	//現在要建立變數b
 	int b;
-	b = 123456;
+	b = 123450;
 	printf("b=%d\n",b);
-	printf("a+b=%d\n", a+b);
+
+	int c;
+	c = a + b;
+	printf("a+b=%d\n", c);
+	
+	c = a - b;
+	printf("a-b=%d\n",c);
+
+	c = a * b;
+	printf("a*b=%d\n",c);
+
+	c = b / a;
+	printf("b/10=%d\n",c);
 	
   return 0;
 }

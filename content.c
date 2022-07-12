e6a9d3fba45f90c9fc5e4c211c46dc6c19910133
徐國堂 <roberthsu2003@gmail.com>
2022-07-12 16:20:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
-
+//數值型別自動轉換
 int main() {
 	double PI = 3.1415962;
 	int radius;
+	int area;
 	cout << "請輸入半徑:";
 	cin >> radius;
-	cout << "圓面積=" << PI * radius * radius << endl;
+	area = PI * radius * radius;
+	cout << "圓面積=" << area << endl;
 	printf("圓面積=%.2f\n",PI * radius * radius);
 }

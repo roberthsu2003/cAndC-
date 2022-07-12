98e9b9ea160d5012b7350ef162d04b52461554c4
徐國堂 <roberthsu2003@gmail.com>
2022-07-12 15:29:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int year;
-	cout << "請輸入出生民國年份:";
-	cin >> year;
-	int westernYear = year + 1911;
-	int age = 2022 - westernYear;
-	cout << "西元年份:" << westernYear << "今年" << age << "歲" << endl;
+	char characterA;
+	characterA = 65;
+	cout << characterA << endl;
+
+	char characterB;
+	characterB = 'A';
+	cout << characterB << endl;
 }

2d6f7a0c8800a6ad85fd4cd456c9c3b8bbafb394
徐國堂 <roberthsu2003@gmail.com>
2022-07-12 16:35:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
-//數值型別自動轉換
 int main() {
-	double PI = 3.1415962;
-	int radius;
-	int area;
-	cout << "請輸入半徑:";
-	cin >> radius;
-	area = PI * radius * radius;
-	cout << "圓面積=" << area << endl;
-	printf("圓面積=%.2f\n",PI * radius * radius);
+	int chinese,english,math;
+	
+	cout << "請輸入國文成績(0~100):";
+	cin >> chinese;
+
+	cout << "請輸入英文成績(0~100):";
+	cin >> english;
+
+	cout << "請輸入數學成績(0~100):";
+	cin >> math;
+
+	int sum = chinese + english + math;
+	//強制轉換
+	float average = (float)sum / 3;
+	cout << "您的總分為" << sum << "分,平均為" << average << "分\n";
+	
 }

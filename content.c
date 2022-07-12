ae3c57b2340ff651244367317d2b50365165a10f
徐國堂 <roberthsu2003@gmail.com>
2022-07-12 15:39:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	char characterA;
-	characterA = 65;
-	cout << characterA << endl;
-
-	char characterB;
-	characterB = 'A';
-	cout << characterB << endl;
+	//c語言的字串
+	const char* str1 = "這是c語言的字串";
+	//c++
+	string str2 = "這是c++的字串";
+	cout << str1 << endl;
+	cout << str2 << endl;
 }

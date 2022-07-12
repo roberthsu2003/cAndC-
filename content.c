a2deac946bb22b368f3549b17e5cbf315f0d3c62
徐國堂 <roberthsu2003@gmail.com>
2022-07-12 14:50:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,28 @@
 #include <iostream>
 
+using namespace std;
 int main() {
 	unsigned char myChar = 100;
 	printf("myChar:%u\n",myChar);
+	cout << "myChar:" << myChar << "\n";
 
 	unsigned short myShort = 60000;
 	printf("myShort:%u\n", myShort);
+	cout << "myShort:" << myShort << endl;
 
 	int myInt = 210000000;
 	printf("myInt:%d\n",myInt);
+	cout << "myInt:" << myInt << endl;
 
 	long long myLong = 21000000000000000;
 	printf("myLong:%lld\n", myLong);
+	cout << "myLong:" << myLong << endl;
 
 	float myFloat = 3.145;
 	printf("myFloat:%.2f\n", myFloat);
+	cout << "myFloat:" << myFloat << endl;
 
 	double myDouble = 35689.34567;
 	printf("myDouble:%.4f\n", myDouble);
+	cout << "myDouble:" << myDouble << endl;
 }

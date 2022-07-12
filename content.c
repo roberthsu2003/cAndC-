9491f39721cec7d7d600fe60a9b1ffb18a408f6b
徐國堂 <roberthsu2003@gmail.com>
2022-07-12 16:09:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	//c語言的字串
-	const char* str1 = "這是c語言的字串";
-	//c++
-	string str2 = "這是c++的字串";
-	cout << str1 << endl;
-	cout << str2 << endl;
+	double PI = 3.1415962;
+	int radius;
+	cout << "請輸入半徑:";
+	cin >> radius;
+	cout << "圓面積=" << PI * radius * radius << endl;
+	printf("圓面積=%.2f\n",PI * radius * radius);
 }

d4e8d87c3e268d62f02f06d11aa7c4e0b126ceda
徐國堂 <roberthsu2003@gmail.com>
2022-07-13 15:19:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,11 @@
 #include <iostream>
-//讓使用者輸入梯形的上底、下底及高，程式會計算梯形的面積(上底加下底乘以高除以2)
-
 using namespace std;
-
 int main() {
-	int top, bottom , height;
-	float area;
-	cout << "請輸入梯形的上底(公分):";
-	cin >> top;
+	const double PI = 3.141592653;
+	float radius;
+	cout << "請輸入半徑(公分):";
+	cin >> radius;
 
-	cout << "請輸入梯形的下底(公分):";
-	cin >> bottom;
-
-	cout << "請輸入梯形的高(公分):";
-	cin >> height;
-
-	area = (top + bottom) * height / 2.0;
-
-	cout << "梯形的面積:" << area << "平方公分\n";
-	
+	cout << "圓周長為:" << 2 * PI * radius << endl;
+	cout << "圓面積是:" << PI * radius * radius << endl;
 }

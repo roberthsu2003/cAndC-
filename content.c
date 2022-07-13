11b97161a9134cb9882c4c59f899841a2f782816
徐國堂 <roberthsu2003@gmail.com>
2022-07-13 16:55:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,20 @@
-#include <iostream>
-//成績及格判斷
-//請使用者輸入成績，若成績大於等於60分，則顯示及格，否則顯示不及格。
-
+//國文100同時數學100,獎金1000
+//國文100或者數學100,獎金500
 using namespace std;
+
 int main() {
-	int scores;	
-	cout << "請輸入成績:";
-	cin >>  scores;
-	if(scores>=60){
-		cout << "及格" << endl;
-	}else{
-		cout << "不及格" << endl;
+	int chinese, math, bonus = 0;
+	cout << "請輸入國文成績:";
+	cin >> chinese;
+	cout << "請輸入數學成績:";
+	cin >> math;
+
+	if(math==100 && chinese==100){
+		bonus = 1000;
+	}else if(math == 100 || chinese == 100){
+		bonus = 500;
 	}
+
+	cout << "獎金是:" << bonus << endl;
+	
 }

6670327ec5a72a5b908ba3146f419a970b03875d
徐國堂 <roberthsu2003@gmail.com>
2022-07-13 15:36:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
-int main() {
-	const double PI = 3.141592653;
-	float radius;
-	cout << "請輸入半徑(公分):";
-	cin >> radius;
+//讓使用者輸入三個任意數，程式會顯示三數中的最大數。
 
-	cout << "圓周長為:" << 2 * PI * radius << endl;
-	cout << "圓面積是:" << PI * radius * radius << endl;
+int main() {
+	double input1, input2, input3, max;
+	cout << "請輸入第一個數:";
+	cin >> input1;
+
+	cout << "請輸入第二個數:";
+	cin >> input2;
+
+	max = input1 > input2 ? input1 : input2;
+
+	cout << "請輸入第三個數:";
+	cin >> input3;
+
+	max = max > input3 ? max : input3;	
+
+	cout << "輸入三個數中最大的數為:" << max << endl;
 }

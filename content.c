3f43d9506407088a2c4d27beca999967dacb0b44
徐國堂 <roberthsu2003@gmail.com>
2022-07-14 14:43:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
-
+//計算0 - 10所有偶數的總和
 int main() {
-	for(int i=0; i<5; i++){
-		cout << "執行第" << i+1 << "次, ";
-		cout << "i=" << i << endl;
+	int sum = 0;
+	for(int i=0; i<=10; i+=2){
+		sum += i;
+		cout << "執行第" << (i/2)+1 << "次";		
+		cout << ",i=" << i ;
+		cout << ", sum=" << sum << endl;
 	}
 }

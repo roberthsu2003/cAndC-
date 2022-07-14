47cfcc09e306815b722fdd96da2e652e14e78255
徐國堂 <roberthsu2003@gmail.com>
2022-07-14 15:56:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <math.h>
+//小明想要存錢買一輛機車,機車每輛30000元，他將每月存的錢輸入，當存款足夠買機車時，就顯示提示訊息告知。
 using namespace std;
 int main() {
-	const double PI = 3.141592653;
-	
-	for(int deg=0; deg<=180; deg+=15){
-		cout << "角度:" << deg;
-		cout << ",弳度是" << deg * PI / 180;
-		cout << ",sin(角度" << deg << ")=";
-		printf("%.2f\n",sin(deg * PI / 180));
+	int deposit = 0;
+	int num=0;
+	int inputNum;
+	while(deposit < 30000){
+		num++;
+		cout << "請輸入第" << num << "個月分的存款:";
+		cin >> inputNum;
+	  	deposit += inputNum;
 	}
+	cout << "存夠了!存了" << num << "個月, 已經存了" << deposit << endl;
 }

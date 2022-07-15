57e5f5e50e9a975e86fea16a1e7484d70327056b
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 15:15:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <time.h>
-using namespace std;
 
-int main() {	
+using namespace std;
+int main() {
 	srandom(time(NULL));
-	long random_value = random(); 	
-	cout << "亂數總值:"<< random_value << endl;
-	cout << "建立0~99的亂數" << random_value % 100 << endl;
+	int min = 50;
+	int max = 60;
+	cout << min << "~" << max << "的亂數是:" << random() % (max-min+1) + min << endl;
 }

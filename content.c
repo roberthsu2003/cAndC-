20a07388900b2432b70c7173ccb2c0c64eabd6bf
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 14:42:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,5 +6,20 @@ int main() {
 	cout << "請輸入一個數值:";
 	cin >> end_value;
 	cout << "2~" << end_value << "的質數有:" << endl;
-	cout << "2,3,5..... " << endl;
+	for(int i=2;i<=end_value;i++){
+		//取出要檢查的數值
+		int check_value = i;
+		bool is_prime = true;
+		for(int j=2;j<check_value;j++){
+			//檢查是否為值數
+			if(check_value % j == 0){
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime){
+			cout << check_value << "\t";
+		}
+		
+	}
 }

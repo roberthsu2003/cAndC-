e8ae86cff0e61493317efa5ab6d12a5717bacdd7
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 16:45:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,12 +2,24 @@
 using namespace std;
 
 int main() {
-	//建立陣列變數,同時給值
-	int arr[] = {78, 90, 73, 92, 72};
+	//手動給分數
+	int nums = 5;
+	int scores[nums];
+	for(int i=0; i<nums; i++){
+		cout << "請輸入第" << i+1 << "個分數:";
+		cin >> scores[i];
+	}
+	
 	
 
 	//取出陣列內的值
-	for(int i=0;i<5;i++){
-		cout << arr[i] << endl;
+	int sum=0;
+	cout << "學生分數是:" << endl;
+	for(int i=0;i<nums;i++){
+		cout << scores[i] << "\t";
+		sum += scores[i];
 	}
+	cout << endl;
+	cout << "總分為:" << sum << endl;
+	cout << "平均是" << (float)sum / nums << endl;
 }

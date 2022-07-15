19a82e61251282094fbd920e2c0875d0f6b515dd
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 16:28:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,39 +1,18 @@
 #include <iostream>
-#include <time.h>
-
 using namespace std;
+
 int main() {
-	srandom(time(NULL));
-	int min = 1;
-	int max = 99;
-	int guess_value = random() % (max-min+1) + min;
-	int keyin;
-	int count = 0;
-	//cout << min << "~" << max << "的亂數是:" << guess_value << endl;
-	cout << "==============猜數字遊戲===================\n\n";
-	while(true){
-		printf("猜數字範圍%d~%d:",min, max);
-		cin >> keyin;
-		count += 1;
-		if(keyin >= min && keyin <= max){
-			if(keyin == guess_value){
-				cout << "猜中了" << endl;
-				cout << "您共猜了" << count << "次" << endl;
-				break;
-			}else if(keyin > guess_value){
-				cout << "再小一點" << endl;
-				max = keyin;
-				
-			}else if(keyin < guess_value){
-				cout << "再大一點" << endl;
-				min = keyin;
-			}
-			cout << "您已經猜了" << count << "次" << endl;
-		}else{
-			cout << "超出min~max的範圍" << endl;
-			cout << "您已經猜了" << count << "次" << endl;
-		}
-		
+	//建立陣列變數
+	int arr[5];
+	//給陣列變數內容值
+	arr[0] = 78;
+	arr[1] = 90;
+	arr[2] = 73;
+	arr[3] = 92;
+	arr[4] = 72;
+
+	//取出陣列內的值
+	for(int i=0;i<5;i++){
+		cout << arr[i] << endl;
 	}
-	cout << "遊戲結束\n";
 }

0468fd0cb79dc8f3a904dde50e943abc3491781c
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 16:35:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,9 @@
 using namespace std;
 
 int main() {
-	//建立陣列變數
-	int arr[5];
-	//給陣列變數內容值
-	arr[0] = 78;
-	arr[1] = 90;
-	arr[2] = 73;
-	arr[3] = 92;
-	arr[4] = 72;
+	//建立陣列變數,同時給值
+	int arr[] = {78, 90, 73, 92, 72};
+	
 
 	//取出陣列內的值
 	for(int i=0;i<5;i++){

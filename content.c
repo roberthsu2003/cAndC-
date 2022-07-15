f3557a7089d25405d915a1f8b2dc192dfb5c0e01
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 15:28:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,25 @@
 using namespace std;
 int main() {
 	srandom(time(NULL));
-	int min = 50;
-	int max = 60;
-	cout << min << "~" << max << "的亂數是:" << random() % (max-min+1) + min << endl;
+	int min = 1;
+	int max = 99;
+	int guess_value = random() % (max-min+1) + min;
+	int keyin;
+	cout << min << "~" << max << "的亂數是:" << guess_value << endl;
+	cout << "==============猜數字遊戲===================\n\n";
+	while(true){
+		printf("猜數字範圍%d~%d:",min, max);
+		cin >> keyin;
+		if(keyin >= min && keyin <= max){
+			if(keyin == guess_value){
+				cout << "猜中了" << endl;
+				break;
+			}else{
+				cout << "猜錯了" << endl;
+			}
+		}else{
+			cout << "超出min~max的範圍";
+		}
+		
+	}
 }

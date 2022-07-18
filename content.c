6ed89fba69a7452548203526992d026eb7337023
徐國堂 <roberthsu2003@gmail.com>
2022-07-18 14:10:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,13 @@ using namespace std;
 
 int main() {
 	srandom(time(NULL));
-	int nums = 250;
+	int students;
+	int score_num;
+	cout << "請輸入學生人數:";
+	cin >> students;
+	cout << "每位學生的科目數:";
+	cin >> score_num;
+	int nums = students * score_num;
 	int scores[nums];
 	for(int i=0; i<nums; i++){		
 		scores[i] = random() % (100-60+1) + 60;

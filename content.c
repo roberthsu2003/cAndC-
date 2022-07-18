0183104d7200dc45c29e2ff577eb9c2c817d52ac
徐國堂 <roberthsu2003@gmail.com>
2022-07-18 15:32:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,8 +6,24 @@ using namespace std;
 int main() {
 	srandom(time(NULL));
 	int nums[20];
+	int lucky_num;
+	int match_num=0;
 	for(int i=0;i<20;i++){
 		nums[i] = random() % (99-1+1) + 1;
 		cout << nums[i] << "\t"; 
 	}
+	cout << endl;
+
+	cout << "請輸入一個幸運號碼:";
+	cin >> lucky_num;
+	//循序搜尋
+	for(int i=0; i<20; i++){
+		if(lucky_num == nums[i]){
+			match_num += 1;
+		}
+	}
+
+	cout << "符合號碼的有" << match_num << "個" << endl;
+	
+	
 }

99737ceec9375e7e12a9e499ec6d4845d95c4b33
徐國堂 <roberthsu2003@gmail.com>
2022-07-18 16:34:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,26 +3,34 @@
 using namespace std;
 
 int main() {
-	int lot[49];
-	int choose[7];
-	int max_index = 48;
-	for(int i=0; i<49; i++){
-		lot[i] = i+1;
-	}
-
 	srandom(time(NULL));
-	for(int i=0; i<7; i++){
-		int randIndex = random() % (max_index+1);
-		choose[i] = lot[randIndex];
-		lot[randIndex] = lot[max_index];
-		max_index -= 1;
-	}
+	int groups;
+	cout << "請輸入大樂透電腦選號組數:";
+	cin >> groups;
 
-	cout << "本期大樂透電腦選號號碼如下:\n\n";
-	for(int i=0; i<7;i++){
-		cout << choose[i] << " ";
+	for(int j=0; j<groups; j++){
+		int lot[49];
+		int choose[7];
+		int max_index = 48;
+		for(int i=0; i<49; i++){
+			lot[i] = i+1;
+		}
+	
+		
+		for(int i=0; i<7; i++){
+			int randIndex = random() % (max_index+1);
+			choose[i] = lot[randIndex];
+			lot[randIndex] = lot[max_index];
+			max_index -= 1;
+		}
+	
+		cout << "本期大樂透電腦選號第" << j+1 << "組\n";
+		for(int i=0; i<7;i++){
+			cout << choose[i] << " ";
+		}
+		cout << "\n\n\n";
 	}
-	cout << endl;
+	
 
 	
 }

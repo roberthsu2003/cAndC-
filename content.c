d3a4c1326d50719c206f622efa6ad5c515ba02d4
徐國堂 <roberthsu2003@gmail.com>
2022-07-18 14:56:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -21,10 +21,13 @@ int main() {
 	//取出陣列內的值
 	for(int i=0;i<students;i++){
 		cout << "學生" << i+1 << "\t";
+		int sum = 0;		
 		for(int j=0; j<score_num; j++){
 			int index = i*score_num + j;
 			cout << scores[index] << "\t";
+			sum += scores[index];
 		}
+		cout << sum << "\t";
 		cout << endl;
 	}
 }

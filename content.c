b99ac71ae73a4381c4bf4cef234af8b0a7747002
徐國堂 <roberthsu2003@gmail.com>
2022-07-19 16:30:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,38 +1,19 @@
 #include <iostream>
 #include <time.h>
-
 using namespace std;
 
 int main() {
-	int max = 100;
-	int min = 0;	
 	srandom(time(NULL));
-	int guess = random() % (max-min+1) + min;
-	int keyin;
-	int count = 0;
-	//cout << guess << endl;
-
-	cout <<  "============猜數字遊戲================\n\n";
-	while(true){
-		cout << "猜數字範圍(不包含)" << min << "~" << max << ":";
-		cin >> keyin;
-		count += 1;
-		if(keyin > min && keyin < max){	
-			//輸入範圍正確的
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-				cout << "您猜了" << count << "次" << endl;
-				break;
-			}else if(keyin > guess){
-				cout << "再小一點";
-				max = keyin;
-			}else if(keyin < guess){
-				cout << "再大一點";
-				min = keyin;
-			}
-			cout << "您已經猜了" << count << "次" << endl;
-		}else{
-			cout << "請輸入提示範圍內的數字!\n";
-		}
+	int student_num,subject_num;
+	cout << "請輸入學生人數:";
+	cin >> 	student_num;
+	cout << "請輸入科目數:";
+	cin >> 	subject_num;
+	int num = student_num * subject_num;
+	int scores[num];
+	
+	for (int i=0;i<num;i++){
+		scores[i] = random() % (100-60+1) + 60;
+		cout << scores[i] << endl;
 	}
 }

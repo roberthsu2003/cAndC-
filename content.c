7d018fe0734da8ecedbe8476206c7068cf050b6e
徐國堂 <roberthsu2003@gmail.com>
2022-07-19 14:05:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,37 +1,12 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
-int main() {
-	srandom(time(NULL));
-	int nums;
-	int temp;
-	cout << "請輸入要排序的數值個數:";
-	cin >> nums;
-	int array[nums];
-
-	cout << "排序前:\n";
-	for(int i=0; i<nums; i++){
-		array[i] = random() % (99-1+1) + 1;
-		cout << array[i] << " ";
-	}
-	cout << endl;
-
-	//陣列排序
-	for(int i=0; i<nums-1; i++){
-		for(int j=i+1;j<nums;j++){
-			if(array[i] > array[j]){
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
+int main() {	
+	for(int i=1; i<10; i++){		
+		
+		for(int j=1; j<10; j++){
+			cout << i << "*" << j << "=" << i*j << "\t";
 		}
+		cout << endl;
 		
 	}
-
-	cout << "排序後:\n";
-	for(int i=0; i<nums; i++){		
-		cout << array[i] << " ";
-	}
-	cout << endl;
-	
 }

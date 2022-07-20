49aa9f68db363631b7b4f070658c363fe156df1f
徐國堂 <roberthsu2003@gmail.com>
2022-07-20 16:07:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,12 @@
 #include <iostream>
-
 using namespace std;
-//自訂的function
-double circle_area(int radius){
-	return  3.141592653 * radius * radius;
+int fun1(int x){
+	return x*x + 2*x + 1;
 }
 
 int main() {
-	int radius;
-	cout << "請輸入半徑:";
-	cin >> radius;
-	double my_area = circle_area(radius);
-	cout << "半徑" << radius << "的圓面積是" << my_area << endl;
+	for(int i=0; i<=10; i++){
+		int result = fun1(i);
+		cout << "如果x=" << i << "得到的值是" << result << endl; 
+	}
 }

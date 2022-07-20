773959592a005b2489f02b6599c52e71815139e1
徐國堂 <roberthsu2003@gmail.com>
2022-07-20 17:00:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-double degree_to_radiam(int degree){
+//建立一個function,輸入弳度，傳出sin(弳度)
+double display_sine(double radian){
+	//return sin(radian)
+	return sin(radian);
+}
+
+double degree_to_radian(int degree){
 	double pi = 3.141592653;
 	return pi / 180 * degree;
 }
@@ -10,7 +17,8 @@ int main() {
 	int degree;
   	cout << "請輸入degree:";
 	cin >> degree;
-	double radian = degree_to_radiam(degree);
+	double radian = degree_to_radian(degree);
 	cout << "角度" << degree << ",弳度是" << radian << endl;
-
+	cout << "角度" << degree << "sin(弳度)=" << display_sine(radian) << endl;
+	return 0;
 }

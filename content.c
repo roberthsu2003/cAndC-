e13dd4b68f66350104f3409c595249465e9acaac
徐國堂 <roberthsu2003@gmail.com>
2022-07-20 15:30:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,12 @@
 #include <iostream>
-#include <time.h>
+
 using namespace std;
+//自訂的function
+double circle_area(int radius){
+	return  3.141592653 * radius * radius;
+}
+
 int main() {
-	srandom(time(NULL));
-	cout << random() << endl;
+	double my_area = circle_area(10);
+	cout << "半徑10的圓面積是" << my_area << endl;
 }

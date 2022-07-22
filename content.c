fafaddd69d98d555075320c409e5ea63137e2782
徐國堂 <roberthsu2003@gmail.com>
2022-07-22 14:39:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
-int main() {
-  int *pa = new int(); //動態建立int的記憶體空間和傳出記憶體位址
-  int *pb = new int();
-  cout << "請輸入a的值:";
-  cin >> *pa;
-  cout << "請輸入b的值:";
-  cin >> *pb;
 
-  int c = *pa + *pb;
-  cout << "2數相加的結果是" << c << endl;
+int main() {
+	int a = 333;
+	int b = 666;
+	int temp = a;
+	a = b;
+	b = temp;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
 }

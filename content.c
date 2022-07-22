23a5802c1686c39bff59d739af71f49955bf66ff
徐國堂 <roberthsu2003@gmail.com>
2022-07-22 15:22:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void swap(int mya, int myb) {
-  int temp = mya;
-  mya = myb;
-  myb = temp;
+void swap(int *mya, int *myb) {
+  int temp = *mya;
+  *mya = *myb;
+  *myb = temp;
 }
 
 int main() {
   int a = 333;
   int b = 666;
-  swap(a, b);
+  swap(&a, &b);
   cout << "a=" << a << endl;
   cout << "b=" << b << endl;
 }

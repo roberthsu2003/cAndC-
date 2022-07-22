44c97c7ad25faed6f036920dffaf55bb3e63097e
徐國堂 <roberthsu2003@gmail.com>
2022-07-22 16:21:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void changToZero1(int array[]) {
+  array[0] = 0;
+  array[1] = 0;
+  array[2] = 0;
+  array[3] = 0;
+  array[4] = 0;
+}
+
 int main() {
   int scores[] = {78, 92, 63, 95, 91};
   for (int i = 0; i < 5; i++) {
@@ -9,9 +17,7 @@ int main() {
   cout << endl;
 
   //內容更改為0
-  for (int i = 0; i < 5; i++) {
-    scores[i] = 0;
-  }
+  changToZero1(scores);
 
   //重新輸出
   for (int i = 0; i < 5; i++) {

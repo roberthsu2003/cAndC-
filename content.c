eb3deffe0be310df2620b05a8895e964abe41149
徐國堂 <roberthsu2003@gmail.com>
2022-07-22 14:51:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,13 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
-	int a = 333;
-	int b = 666;
-	int temp = a;
-	a = b;
-	b = temp;
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
+  int *pa = new int(333);
+  int *pb = new int(666);
+  int temp = *pa;
+  *pa = *pb;
+  *pb = temp;
+
+  cout << "pa內容是:" << *pa << endl;
+  cout << "pb內容是:" << *pb << endl;
 }

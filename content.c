18b83dd3145d3ffe0db8b7845de50f59b293777d
徐國堂 <roberthsu2003@gmail.com>
2022-07-22 15:56:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *mya, int *myb) {
-  int temp = *mya;
-  *mya = *myb;
-  *myb = temp;
-}
-
 int main() {
-  int a = 333;
-  int b = 666;
-  swap(&a, &b);
-  cout << "a=" << a << endl;
-  cout << "b=" << b << endl;
+	const char* cName = "cRobert";
+	cout << cName << endl;
+
+	string name = "c++Robert";
+	cout << name << endl;
+
+	cName = "cJenny_Hsu";
+	cout << cName << endl;
+
+	int id = 10;
+	cout << name + to_string(id) << endl;
 }

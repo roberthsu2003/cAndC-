f8f9debb21ba05cd72b398d0c95533ed670dd148
徐國堂 <roberthsu2003@gmail.com>
2022-07-23 14:27:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,18 +9,13 @@ int main() {
   cin >> student_num;
   cout << "請輸入科目數:";
   cin >> subject_num;
-  int num = student_num * subject_num;
-  int scores[num];
-  for (int i = 0; i < num; i++) {
-    scores[i] = random() % (100 - 60 + 1) + 60;
-    // cout << scores[i] << endl;
-  }
-
+  int students[student_num][subject_num];
   for (int i = 0; i < student_num; i++) {
-    cout << "學生" << i + 1 << endl;
+    cout << "學生" << i+1 << ":"
+         << "\t";
     for (int j = 0; j < subject_num; j++) {
-      int index = i * subject_num + j;
-      cout << scores[index] << "\t";
+      students[i][j] = random() % (100 - 60 + 1) + 60;
+      cout << students[i][j] << "\t";
     }
     cout << endl;
   }

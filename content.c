6d227a3e795e079fccc815cfdcd4743725ae9de2
徐國堂 <roberthsu2003@gmail.com>
2022-07-24 10:35:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,7 +2,11 @@
 #include <time.h>
 using namespace std;
 int main() {
+	int max = 49;
+	int min = 1;
 	srandom(time(NULL));	
 	long random_value = random();
 	cout << random_value << endl;
+	//1~49
+	cout << random_value % (max - min + 1) + min << endl;
 }

aa6923f81f8a37f5e2c7524dfa691b37deca5c72
徐國堂 <roberthsu2003@gmail.com>
2022-07-24 10:50:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,9 +4,17 @@ using namespace std;
 int main() {
 	int max = 49;
 	int min = 1;
+	int loto[7];
 	srandom(time(NULL));	
-	long random_value = random();
-	cout << random_value << endl;
-	//1~49
-	cout << random_value % (max - min + 1) + min << endl;
+	for(int i=0; i<7; i++){
+		//i = 0~6
+		loto[i] = random() % (max-min+1) + min;
+	}
+
+	cout << "本期大樂透電腦選號號碼如下:\n\n";
+	for(int i=0; i<7; i++){
+		cout << loto[i] << "\t";
+	}
+	cout << endl;	
+	
 }

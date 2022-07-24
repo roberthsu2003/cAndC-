5131ddaa7fd90e3cd1d29edb91c9fd6984057029
徐國堂 <roberthsu2003@gmail.com>
2022-07-24 15:06:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,7 @@ using namespace std;
 
 double degree_to_radian(int degree) {
   const double PI = 3.141592653;
-  
+
   return PI / 180 * degree;
 }
 
@@ -16,8 +16,7 @@ double get_sine(int degree) {
 int main() {
   int degrees[] = {0, 30, 45, 60, 90, 180, 360};
   int length = sizeof(degrees) / sizeof(degrees[0]);
-  for (int i = 0; i < length; i++) {
-    cout << "角度:" << degrees[i] << " sin()值為:" << get_sine(degrees[i])
-         << endl;
+  for (int i = 0; i < length; i++) {    
+    printf("度:%d sin()值為:%.2f\n", degrees[i], get_sine(degrees[i]));
   }
 }

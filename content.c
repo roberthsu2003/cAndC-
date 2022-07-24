9bfd5104fdf01da8a3010ad784b8977ab597b792
徐國堂 <roberthsu2003@gmail.com>
2022-07-24 16:15:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,60 +1,18 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 using namespace std;
 
-void play_game(){
-  int guess;
-  int keyin;
-  int count = 0;
-  int max = 99;
-  int min = 1;
-
-  srand(time(NULL));
-  guess = rand() % (max - min + 1) + min;
-  cout << guess << endl;
-  cout << "==============猜數字遊戲===============\n\n";
-  while (true) {
-    printf("猜數字範圍%d~%d:", min, max);
-    cin >> keyin;
-    count += 1;
-
-    if (keyin >= min && keyin <= max) {
-      //輸入的範圍正確
-      if (keyin == guess) {
-        cout << "賓果!猜對了,答案是" << guess << endl;
-        cout << "您猜了" << count << "次" << endl;
-        break;
-      } else if (keyin > guess) {
-        max = keyin - 1;
-        cout << "再小一點!";
-      } else if (keyin < guess) {
-        min = keyin + 1;
-        cout << "再大一點!";
-      }
-      cout << "您已經猜了" << count << "次" << endl;
-    } else {
-      //輸入的範圍不正確
-      cout << "請輸入提示範圍內的數字!\n";
-      continue;
-    }
-  };
+// recursive遞迴
+double factorial(double n) {
+  if (n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-
 int main() {
-	while(true){
-		char play_again;
-		play_game();
-		cout << "請問還要繼續嗎?:y(yes),n(no)";
-		cin >> play_again;
-		if(play_again == 'n'){
-			break;
-		}
-	}
-
-	cout << "遊戲結束" << endl;
-  	
-  return 0;
+  double b, total;
+  cout << "請輸入數字 n:";
+  cin >> b;
+  total = factorial(b);
+  printf("%.0f!是%20.4f\n", b, total);
 }

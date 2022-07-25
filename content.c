13a7a470145822fdcb3816ad8be099c8cecd0bf0
徐國堂 <roberthsu2003@gmail.com>
2022-07-25 15:14:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,64 +1,16 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include "loto.h"
-
 using namespace std;
-
+void swap_address(int *n, int *m){
+	int temp;
+	temp = *n;
+	*n = *m;
+	*m = temp;
+}
 
 int main() {
-  int groups;
-  srand(time(NULL));
-  cout << "請輸入所需要的大樂透組數:";
-  cin >> groups;
-  for (int i = 0; i < groups; i++) {
-    int num = 7;
-    int choose[num];
-    //建立一組7個不重複號碼的樂透
-    created_loto(choose, num);
-    //建立一組7個不重複號碼的樂透
-    created_loto(choose, num);
-    //排序
-    sorted(choose, num);
-
-    cout << "大樂透電腦選號號碼-第" << i + 1 << "組如下:\n";
-    for (int i = 0; i < num; i++) {
-      cout << choose[i] << " ";
-    }
-    cout << "\n\n";
-  }
+	int a=666,b=888;
+	swap_address(&a,&b);
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	
 }
-
-
-
-
-
-loto.h
-void sorted(int array[], int nums) {
-  //陣列排序
-  int temp;
-  for (int i = 0; i < nums - 1; i++) {
-    for (int j = i + 1; j < nums; j++) {
-      if (array[i] > array[j]) {
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-    }
-  }
-}
-
-void created_loto(int array[], int nums) {
-  int lot[49];
-  int maxIndex = 48;
-  for (int i = 0; i < 49; i++) {
-    lot[i] = i + 1;
-  }
-  for (int i = 0; i < nums; i++) {
-    int randIndex = rand() % (maxIndex + 1);
-    array[i] = lot[randIndex];
-    lot[randIndex] = lot[maxIndex];
-    maxIndex--;
-  }
-}
-

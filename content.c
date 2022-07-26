cac2fb56dff5e2d2bcaaf2cb30dc41688beb455b
徐國堂 <roberthsu2003@gmail.com>
2022-07-26 15:39:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,32 +1,17 @@
 #include <iostream>
-using namespace std;
 
-typedef enum direction{
-	North,
-	South,
-	East,
-	West
-}Direction;
+using namespace std;
+typedef enum day {
+  saturday = 10,
+  sunday,
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday
+} Day;
 
 int main() {
-	Direction destination;
-	destination = East;
-
-	switch(destination){
-		case North:
-		cout << "北" << endl;
-		break;
-
-		case South:
-		cout << "南" << endl;
-		break;
-
-		case East:
-		cout << "東" << endl;
-		break;
-
-		case West:
-		cout << "西" << endl;
-		break;
-	}
+  Day today = wednesday;
+  cout << wednesday << endl;
 }

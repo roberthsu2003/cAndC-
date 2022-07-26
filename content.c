240bdf064a61728e941b7366bbd32ccd543a02d4
徐國堂 <roberthsu2003@gmail.com>
2022-07-26 14:44:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,29 +2,31 @@
 #include <time.h>
 using namespace std;
 
-typedef struct student{
-	string name;
-	int chinese;
-	int english;
-	int math;
-}Student;
+typedef struct student {
+  string name;
+  int chinese;
+  int english;
+  int math;
+} Student;
+
+Student createStudent(int student_id) {
+  Student s;
+  s.name = "學生" + to_string(student_id);
+  s.chinese = 50 + random() % 51;
+  s.math = 50 + random() % 51;
+  s.english = 50 + random() % 51;
+  return s;
+}
 
 int main() {
-	srandom(time(NULL));
-	
-	Student stu1;
-	stu1.name = "robert";
-	stu1.chinese = 50 + random() % 51;
-	stu1.math = 50 + random() % 51;
-	stu1.english = 50 + random() % 51;
+  srandom(time(NULL));
 
+  Student stu1 = createStudent(1);
+  Student stu2 = createStudent(2);
 
-	Student stu2;
-	stu2.name = "jenny";
-	stu2.chinese = 50 + random() % 51;
-	stu2.english = 50 + random() % 51;
-	stu2.math = 50 + random() % 51;
+  cout << stu1.name << endl;
+  cout << stu1.chinese << endl;
 
-	cout << stu1.chinese << endl;
-	cout << stu2.chinese << endl;
+  cout << stu2.name << endl;
+  cout << stu2.chinese << endl;
 }

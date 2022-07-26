dea24a3e479ff49e1d66c232b77cdaffda73c677
徐國堂 <roberthsu2003@gmail.com>
2022-07-26 14:23:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,5 +8,16 @@ typedef struct student{
 }Student;
 
 int main() {
-  std::cout << "Hello World!\n";
+	Student stu1;
+	stu1.name = "robert";
+	stu1.chinese = 78;
+	stu1.math = 76;
+	stu1.english = 82;
+
+
+	Student stu2;
+	stu2.name = "jenny";
+	stu2.chinese = 98;
+	stu2.english = 80;
+	stu2.math = 85;
 }

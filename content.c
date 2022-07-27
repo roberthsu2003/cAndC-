51234e7647c4268acd5bd8ed1aa36855e1a98a43
徐國堂 <roberthsu2003@gmail.com>
2022-07-27 15:28:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,24 +7,16 @@ public:
   int area() { return width * width; }
 };
 
+//(Square s)會copy整個實體過來
+//(Square &s)會copy參考過來
+void changeSquare(Square &s) { 
+	s.width = 20; 
+}
+
 int main() {
   Square s1; //建立實體
   s1.width = 10;
+  changeSquare(s1);
   cout << "s1:" << s1.width << endl;
   cout << "s1面積:" << s1.area() << endl;
-
-  Square s2; //建立實體
-  s2.width = 20;
-  cout << "s2:" << s2.width << endl;
-  cout << "s2面積:" << s2.area() << endl;
-
-  Square s3; //建立實體
-  s3.width = 30;
-  cout << "s3:" << s3.width << endl;
-  cout << "s3面積:" << s3.area() << endl;
-
-  Square s4; //建立實體
-  s4.width = 40;
-  cout << "s4:" << s4.width << endl;
-  cout << "s4面積:" << s4.area() << endl;
 }

b89657f93cca81a0d5d3642bc5317738eb1726e4
徐國堂 <roberthsu2003@gmail.com>
2022-07-27 15:47:52 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,16 +7,45 @@ public:
   int area() { return width * width; }
 };
 
-//(Square s)會copy整個實體過來
-//(Square &s)會copy參考過來
-void changeSquare(Square &s) { 
-	s.width = 20; 
-}
+class Rectangle{
+	public:
+		int width = 0;
+		int height = 0;
+		int area(){
+			return width * height;			
+		}
+};
+
+class Trapezoid{
+	public:
+		int top = 0;
+		int height = 0;
+		int bottom = 0;
+		double area(){
+			return (top + bottom) * height / 2.0;
+		}
+};
+
+
 
 int main() {
-  Square s1; //建立實體
-  s1.width = 10;
-  changeSquare(s1);
-  cout << "s1:" << s1.width << endl;
-  cout << "s1面積:" << s1.area() << endl;
+	Square s; //建立實體
+	s.width = 10;  
+	cout << "正形:" << s.width << endl;
+	cout << "正形面積:" << s.area() << endl;
+
+	Rectangle r;
+	r.width = 30;
+	r.height = 20;
+	cout << "矩形的寬:" << r.width << "高:" << r.height << endl;
+	cout << "矩形的面積:" << r.area() << endl;
+
+	Trapezoid t;
+	t.top = 10;
+	t.bottom = 20;
+	t.height = 15;
+	cout << "梯形的上底:" << t.top << ",下底:" << t.bottom << ",高" << t.height << endl;
+
+	cout << "梯形的面積:" << t.area() << endl;
+	
 }

9eca967b1c9a84f7652e2da94a6c3c967343767d
徐國堂 <roberthsu2003@gmail.com>
2022-07-27 16:15:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,1 +1,68 @@
+main.cpp
+
+#include <iostream>
+#include "tools.h"
+#include "tools.h"
+using namespace std;
+
+//定義Square
+
+
+
+
+int main() {
+	Square s; //建立實體
+	s.width = 10;  
+	cout << "正形:" << s.width << endl;
+	cout << "正形面積:" << s.area() << endl;
+
+	Rectangle r;
+	r.width = 30;
+	r.height = 20;
+	cout << "矩形的寬:" << r.width << "高:" << r.height << endl;
+	cout << "矩形的面積:" << r.area() << endl;
+
+	Trapezoid t;
+	t.top = 10;
+	t.bottom = 20;
+	t.height = 15;
+	cout << "梯形的上底:" << t.top << ",下底:" << t.bottom << ",高" << t.height << endl;
+
+	cout << "梯形的面積:" << t.area() << endl;
+	
+}
+
+
+tools.h
+
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+class Square {
+public:
+  int width = 0;
+  int area() { return width * width; }
+};
+
+class Rectangle{
+	public:
+		int width = 0;
+		int height = 0;
+		int area(){
+			return width * height;			
+		}
+};
+
+class Trapezoid{
+	public:
+		int top = 0;
+		int height = 0;
+		int bottom = 0;
+		double area(){
+			return (top + bottom) * height / 2.0;
+		}
+};
+
+#endif
+
 

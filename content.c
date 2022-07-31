c5cd51c39bbe897d9a92758521e2bed8ccbab1a3
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 12:28:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,21 @@
 #include <iostream>
-using namespace std;
 
-void swap(int* n, int* m){
-	int temp = *n;
-	*n = *m;
-	*m = temp;
+using namespace std;
+void zero(int arr[], int length){
+	for(int i=0; i<length; i++){
+		arr[i] = 0;
+	}
 }
 
 int main() {
- 	int a = 666;
-	int b = 888;
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
-	cout << "2數對調" << endl;
-	swap(&a,&b);
-	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
+	int scores[5] = {78, 95, 64, 30, 20};
+	cout << "將所有分數變為0\n";
+	
+	zero(scores, 5);
+	
+	for(int i=0; i<5; i++){
+		cout << scores[i] << "\t";
+	}
+	cout << endl;
+	
 }

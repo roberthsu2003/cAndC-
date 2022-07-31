acc781c1042d5f52fc5f0624b64e93242a1f946d
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 12:18:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void swap(int* n, int* m){
+	int temp = *n;
+	*n = *m;
+	*m = temp;
+}
+
 int main() {
  	int a = 666;
 	int b = 888;
 	cout << "a=" << a << endl;
 	cout << "b=" << b << endl;
 	cout << "2數對調" << endl;
-	int temp = a;
-	a = b;
-	b = temp;
+	swap(&a,&b);
 	cout << "a=" << a << endl;
 	cout << "b=" << b << endl;
 }

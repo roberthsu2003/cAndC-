1bfb98087642c06ca49acb449f31b3fe6fc5839f
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 16:26:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,17 +9,21 @@ typedef struct bmi {
 } BMI;
 
 int main() {
-  BMI b1;
+  int nums;
   cout << "========BMI計算==========" << endl;
-  cout << "請輸入姓名:";
-  cin >> b1.name;
-  cout << "請輸入身高(公分):";
-  cin >> b1.height;
-  cout << "請輸入體重(公斤):";
-  cin >> b1.weight;
+  cout << "請輸入要計算的人數:";
+  cin >> nums;
+  BMI bmis[nums];
+  for (int i = 0; i < nums; i++) {
+    BMI* one = &bmis[i];
+    cout << "請輸入第" << i+1 << "位的姓名,身高,體重(中間要空隔):";
+    cin >> one->name >> one->height >> one->weight;    
+  }
 
-  double bmi = b1.weight / pow(b1.height / 100.0, 2);
-
-  cout << b1.name << endl;
-  printf("您的BMI是:%.2f\n", bmi);
+  for (int i = 0; i < nums; i++) {
+    BMI *one = &bmis[i];
+    double bmi = one->weight / pow(one->height / 100.0, 2);
+    cout << one->name << endl;
+    printf("您的BMI是:%.2f\n\n", bmi);
+  }
 }

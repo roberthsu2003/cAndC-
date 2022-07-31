0e4c81d9e3bf4cb85763435cd7492ee696e84ef9
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 10:39:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int n[3] = {10, 20 , 30};
-	cout << "陣列n的位址=" << n << endl;	
-	cout << "陣列n[0]的位址=" << &n[0] << endl;
-	cout << "陣列n[1]的位址=" << &n[1] << endl;
-	cout << "陣列n[2]的位址=" << &n[2] << endl;
-	cout << "陣列n[0]的位址=" << n+0 << endl;
-	cout << "陣列n[1]的位址=" << n+1 << endl;
-	cout << "陣列n[2]的位址=" << n+2 << endl;
+  int n = 10;
+  cout << "變數n的值" << n << endl;
+	cout << "變數n的address:" << &n << endl;
+	int* p; //建立指標變數,記憶位址會指向int資料類型的記憶體空間
+	p = &n;	//將n的記憶體空間位址給指標變數p
+	cout << "指標變數指向的記憶體空間的值是:" << *p << endl;
+
+	cout << "改變n的值為100" << endl; 	
+	n = 100;
+	cout << "指標變數指向的記憶體空間的新值是:" << *p << endl;
 }

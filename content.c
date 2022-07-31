723bfaeceb11b684f6fc9a4f95a96087aab90ad6
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 14:17:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,18 @@
-#include <iostream>
-
 using namespace std;
-void zero(int arr[], int length){
-	for(int i=0; i<length; i++){
-		arr[i] = 0;
-	}
-}
-
 int main() {
-	int scores[5] = {78, 95, 64, 30, 20};
-	cout << "將所有分數變為0\n";
-	
-	zero(scores, 5);
-	
-	for(int i=0; i<5; i++){
-		cout << scores[i] << "\t";
-	}
+  int array1[] = {10, 20, 30};
+  int array2[] = {90, 80, 70};
+
+  int *ptr;
+	ptr = array1;
+  for (int i = 0; i < 3; i++) {
+		cout << *(ptr+i) << "\t";
+  }
 	cout << endl;
-	
+
+	ptr = array2;
+	for (int i = 0; i < 3; i++) {
+		cout << *(ptr+i) << "\t";
+  }
+	cout << endl;
 }

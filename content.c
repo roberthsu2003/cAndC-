cd5a5a70a70d990170c67d8477b9c8ca49f1ec48
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 10:55:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
-int main() {
-  int n = 10;
-  cout << "變數n的值" << n << endl;
-	cout << "變數n的address:" << &n << endl;
-	int* p; //建立指標變數,記憶位址會指向int資料類型的記憶體空間
-	p = &n;	//將n的記憶體空間位址給指標變數p
-	cout << "指標變數指向的記憶體空間的值是:" << *p << endl;
 
-	cout << "改變n的值為100" << endl; 	
-	n = 100;
-	cout << "指標變數指向的記憶體空間的新值是:" << *p << endl;
+int main() {
+	//動態配置記憶體
+  int *n;
+  int *m;
+  n = new int();
+  m = new int();
+  cout << "請輸入n的值:";
+  cin >> *n;
+
+  cout << "請輸入m的值:";
+  cin >> *m;
+
+  int sum;
+  sum = *n + *m;
+  cout << "n和m加總為:" << sum << endl;
 }

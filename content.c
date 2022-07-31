4873ffffbff35fec2894ae930955ef00b7b54f57
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 11:47:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	//建立連續的記憶體空間
-	int* par = new int[5];
-	cout << "請輸入5個數值(中間空白):";
-	for(int i=0; i<5; i++){
-		cin >> *(par+i);
-	}
+void print(int *par, int length) {
+  for (int i = 0; i < length; i++) {
+    cout << *(par + i) << "\t";
+  }
+  cout << endl;
+}
 
-	//輸出
-	for(int i=0; i<5; i++){
-		cout << *(par+i) << "\t";
-	}
-	cout << endl;
+int main() {
+  //建立連續的記憶體空間
+  int *par = new int[5];
+  cout << "請輸入5個數值(中間空白):";
+  for (int i = 0; i < 5; i++) {
+    cin >> *(par + i);
+  }
+
+  //輸出
+  print(par, 5);
 }

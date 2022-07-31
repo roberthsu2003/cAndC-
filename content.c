fd949f9d058bbcbee4cd66c70c7d54e3d8e4cb57
徐國堂 <roberthsu2003@gmail.com>
2022-07-31 09:48:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,52 +1,13 @@
-main.cpp
-
 #include <iostream>
-#include "person.h"
-
-
 using namespace std;
+
 int main() {
-	string name;
-	int weight,height;
-	cout << "請輸入姓名:";
-	cin >> name;
-
-	cout << "請輸入身高:";
-	cin >>	height;
-
-	cout << "請輸入體重:";
-	cin >> weight;
-	
-	Person p1(name, height, weight);
-	cout << "name:" << p1.name << endl;
-	cout << "bmi是:" << p1.bmi() << endl;
+	double m = 5;
+	int n = 10;
+	cout << "變數m的值=" << m << endl;
+	cout << "變數n的值=" << n << endl;
+	cout << "變數m的記憶體位址:" << &m << endl;
+	cout << "變數n的記憶體位址:" << &n << endl;
+	cout << "變數m的記憶體大小:" << sizeof(m) << endl;
+	cout << "變數n的記憶體大小:" << sizeof(n) << endl;
 }
-
-
-
-person.h
-
-#ifndef __PERSON_H__
-#define __PERSON_H__
-
-#include <cmath>
-#include <iostream>
-using namespace std;
-
-class Person {
-public:
-  string name;
-  int height;
-  int weight;
-	//自訂的建構式
-  Person(string n,int h, int w){
-	  name = n;
-	  height = h;
-	  weight = w;
-  }
-
-  double bmi() { 
-	  return weight / pow(height / 100.0, 2); 
-  }
-};
-#endif

0c3cc7acd4429d49d746fb4067f0830935b1f1ed
徐國堂 <roberthsu2003@gmail.com>
2022-08-01 14:40:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int* ptr_n = new int(10);
-	cout << "ptr_n指向的值:" << *ptr_n << endl;
-
-	int* ptr_m = new int();
-	*ptr_m = 20;
-
-	int sum = *ptr_n + *ptr_m;
-
-	cout << sum << endl;
-	
+	int a = 666;
+	int b = 888;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	cout << "2數對調" << endl;
+	int temp = a;
+	a = b;
+	b = temp;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
 }

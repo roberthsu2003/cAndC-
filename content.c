2f55a128006218806f2983386bfbb354dc5866fb
徐國堂 <roberthsu2003@gmail.com>
2022-08-01 15:57:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,20 @@
 #include <iostream>
 using namespace std;
-void multiply10(int x[],int size){
-	for(int i=0; i<size; i++){
-		x[i] *= 10;
-	}
-}
 
-void division10(int *x,int size){
-	for(int i=0; i<size; i++){
-		*(x+i) /= 10;
-	}
-}
-
-void print(int *x, int size){
-	for(int i=0; i<3; i++){
-		cout << x[i] << "\t";
-	}
-	cout << endl;
-}
-
-int main() {
-	int nums = 3;
-	int* n = new int[nums];
+int* create_sequence_space(){
+	int* n = new int[3];
 	n[0] = 10;
 	n[1] = 20;
-	n[2] = 30;	
-	multiply10(n,nums);
-	print(n,nums);
-	division10(n, nums);
-	print(n,nums);
-	delete[] n;	
+	n[2] = 30;
+	return n;
+}
+
+
+
+int main() {
+	int* y = create_sequence_space();
+	for(int i=0; i<3; i++){
+		cout << y[i] << "\t";
+	}
+	cout << endl;
 }

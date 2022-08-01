e06b9df1d1b5584c1981ae6cd0398a1c74608c65
徐國堂 <roberthsu2003@gmail.com>
2022-08-01 14:22:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n[3];
-	cout << "陣列n的address:" << n << endl;
-	cout << "n[0]address:" << &n[0] << endl;
-	cout << "n[1]address:" << &n[1] << endl;
-	cout << "n[2]address:" << &n[2] << endl;
-	cout << "n+0 address:" << n+0 << endl;
-	cout << "n+1 address:" << n+1 << endl;
-	cout << "n+2 address:" << n+2 << endl;
+	int n = 10;
+	int *p;
+	p = &n;
+	cout << "p的內容:"<< p << endl;
+	cout << "n的value:" << n << endl;
+	cout << "*p的value:" << *p << endl;
+	n = 20;	
+	cout << "n的value:" << n << endl;
+	cout << "*p的value:" << *p << endl;
+	*p = 10;
+	cout << "n的value:" << n << endl;
+	cout << "*p的value:" << *p << endl;
+
+	int **ptr = &p;
+	cout << *ptr << endl;
+	cout << **ptr << endl;
+	cout << ptr << endl;
+	
 }

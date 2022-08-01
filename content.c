8973b6e53c6d6994fb7d6b1cfe6652866636d4b1
徐國堂 <roberthsu2003@gmail.com>
2022-08-01 16:31:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,16 @@
 #include <iostream>
 using namespace std;
+
+typedef struct point{
+	double x;
+	double y;
+}Point;
+
 int main() {
-	int array1[] = {10, 20, 30};
-	int array2[] = {70, 80, 90};
-	int *x = array1;
-	for(int i=0; i<3; i++){
-		cout << x[i] << "\t";
-	}
-	cout << endl;
+	Point p1;
+	p1.x = 10.0;
+	p1.y = 20.0;
 
-	x = array2;
-	for(int i=0; i<3; i++){
-		cout << x[i] << "\t";
-	}
-	cout << endl;
-
-	
+	cout << p1.x << endl;
+	cout << p1.y << endl;
 }

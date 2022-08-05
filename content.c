f58cce3e8c8a537e9e5385a4e2ab5371f8d5c1f2
徐國堂 <roberthsu2003@gmail.com>
2022-08-05 14:22:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -16,6 +16,7 @@ typedef struct rect {
 
 using namespace std;
 /*
+call by value
 Rect modifyRect(Rect rec,int x, int y, float width, float
  height){
         rec.p.x = x;
@@ -25,7 +26,8 @@ Rect modifyRect(Rect rec,int x, int y, float width, float
         return rec;
  }
 */
-
+/*
+call by address
 void modifyRect(Rect* rec,int x, int y, float width, float height) {
 	rec -> p.x = x;
 	rec -> p.y = y;
@@ -33,6 +35,17 @@ void modifyRect(Rect* rec,int x, int y, float width, float height) {
 	rec -> s.height = height;
 	
 }
+*/
+/*
+call by reference
+void modifyRect(Rect& rec,int x, int y, float width, float height) {
+	rec.p.x = x;
+	rec.p.y = y;
+	rec.s.width = width;
+	rec.s.height = height;
+	
+}
+	*/
 int main() {
   Rect r1;
   r1.p = {4.5, 5.6};
@@ -44,7 +57,7 @@ int main() {
   cout << "r1的height座標:" << r1.s.height << endl;
 
   //修改結構
-  modifyRect(&r1, 0, 0, 100, 100);
+  modifyRect(r1 , 0, 0, 100, 100);
   cout << "r1的x座標:" << r1.p.x << endl;
   cout << "r1的y座標:" << r1.p.y << endl;
   cout << "r1的width座標:" << r1.s.width << endl;

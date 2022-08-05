12ce82cf5d95d576e09d2a1687f3949fd41a5e99
徐國堂 <roberthsu2003@gmail.com>
2022-08-05 16:24:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,40 @@
 #include <iostream>
+#include <time.h>
 #include <vector>
 
 using namespace std;
-void print(vector<int>& list) {
-  for (int &elem : list) {
-    cout << elem << "\t";
+
+typedef struct student {
+  string name;
+  int chinese;
+  int english;
+  int math;
+} Student;
+
+void created_students(vector<Student> &students) {
+  for (int i = 0; i < students.size(); i++) {
+    Student *s_info = &students[i];
+    s_info->name = "學生" + to_string(i + 1);
+    s_info->chinese = random() % (100 - 50 + 1) + 50;
+    s_info->english = random() % (100 - 50 + 1) + 50;
+    s_info->math = random() % (100 - 50 + 1) + 50;
   }
-	cout << endl;
+}
+
+void print_students(const vector<Student> &students) {
+  cout << "學生姓名"  << "\t"  << "國文"  << "\t" << "英文"  << "\t"  << "數學" << endl;
+  for (Student s : students) {
+    cout << s.name << "\t\t";
+    cout << s.chinese << "\t\t";
+    cout << s.english << "\t\t";
+    cout << s.math << "\t";
+    cout << endl;
+  }
 }
 
 int main() {
-  vector<int> list;
-  // vector的實體有實體方法,size(),push_back(),pop_back()
-  list.push_back(10);
-  list.push_back(20);
-  list.push_back(30);
-	list.push_back(40);
-	print(list);
-	
-	cout << "==============" << endl;
-	list.pop_back();
-	print(list);
+  srandom(time(NULL));
+  vector<Student> students(30);
+  created_students(students);
+  print_students(students);
 }

67c6059d8db7846065782bc7b22735939bc08aec
徐國堂 <roberthsu2003@gmail.com>
2022-08-05 15:49:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,15 +2,23 @@
 #include <vector>
 
 using namespace std;
+void print(vector<int>& list) {
+  for (int &elem : list) {
+    cout << elem << "\t";
+  }
+	cout << endl;
+}
 
 int main() {
-  vector<double> vec(5);
-  cout << "請輸入5個數字:";
-  for (double &elem : vec) {
-    cin >> elem;
-  }
-
-  for (double elem : vec) {
-    cout << elem << endl;
-  }
+  vector<int> list;
+  // vector的實體有實體方法,size(),push_back(),pop_back()
+  list.push_back(10);
+  list.push_back(20);
+  list.push_back(30);
+	list.push_back(40);
+	print(list);
+	
+	cout << "==============" << endl;
+	list.pop_back();
+	print(list);
 }

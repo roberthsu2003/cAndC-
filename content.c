3de21153dfe761af881ba9cdad4038d8ff36915d
徐國堂 <roberthsu2003@gmail.com>
2022-08-05 15:32:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,22 +3,14 @@
 
 using namespace std;
 
-void setToZero(vector<double> &d) {
-  for (int i = 0; i < d.size(); i++) {
-    d[i] = 0;
-  }
-}
-
-void print(const vector<double> &d) {
-  for (int i = 0; i < d.size(); i++) {
-    cout << d[i] << "\t";
-  }
-  cout << endl;
-}
-
 int main() {
-  vector<double> vec_d{10, 20, 30, 40, 50};
-  print(vec_d);
-  setToZero(vec_d);
-  print(vec_d);
+  vector<double> vec(5);
+  cout << "請輸入5個數字:";
+  for (double &elem : vec) {
+    cin >> elem;
+  }
+
+  for (double elem : vec) {
+    cout << elem << endl;
+  }
 }

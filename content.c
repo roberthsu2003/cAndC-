e08e90ce9a2c3d4860bc1b011d3dfc6bcc1a0815
徐國堂 <roberthsu2003@gmail.com>
2022-08-05 13:59:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -15,6 +15,15 @@ typedef struct rect{
 }Rect;
 
 using namespace std;
+
+Rect modifyRect(Rect rec,int x, int y, float width, float 
+ height){
+	rec.p.x = x;
+	rec.p.y = y;
+	rec.s.width = width;
+	rec.s.height = height;
+	return rec;
+ }
 int main() {
 	Rect r1;
 	r1.p = {4.5, 5.6};
@@ -24,5 +33,13 @@ int main() {
 	cout << "r1的y座標:" << r1.p.y << endl;
 	cout << "r1的width座標:" << r1.s.width << endl;
 	cout << "r1的height座標:" << r1.s.height << endl;
+
+	//修改結構
+	r1=modifyRect(r1, 0, 0, 100, 100);
+	cout << "r1的x座標:" << r1.p.x << endl;
+	cout << "r1的y座標:" << r1.p.y << endl;
+	cout << "r1的width座標:" << r1.s.width << endl;
+	cout << "r1的height座標:" << r1.s.height << endl;
+	
 	
 }

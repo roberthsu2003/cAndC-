ed648c77f536e5601e4a5739ba146a31be50a470
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 15:02:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -16,6 +16,9 @@ void description_square(Square s) {
   cout << "area:" << s.area() << endl;
   cout << "==================" << endl;
 }
+void change_width(Square& s) { 
+	s.width += 10; 
+}
 
 int main() {
   Square s1;
@@ -23,5 +26,12 @@ int main() {
   s1.width = 10;
   s2.width = 20;
   description_square(s1);
+  description_square(s2);
+
+  //寬加10
+	change_width(s1);
+	change_width(s2);
+
+	description_square(s1);
   description_square(s2);
 }

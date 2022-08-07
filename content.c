9efb7e0f132ec6c0d92231d725b73b47b1040f35
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 16:09:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,15 +2,29 @@
 using namespace std;
 
 class Student {
-  string name;
-  int chinese;
-  int english;
-  int math;
+	public:	
+	  string name;
+	  int chinese;
+	  int english;
+	  int math;	
+
+		
 };
+
+void print(Student& stu){
+	cout << stu.name << endl;
+}
 
 int main() {
   int nums;
   cout << "請輸入學生人數:";
   cin >> nums;
-  Student students[nums];
+	Student students[nums];
+	
+	for(int i=0; i<nums; i++){
+		Student s = students[i];
+		s.name = "學生" + to_string(i+1);
+		print(s);
+	}
+  
 }

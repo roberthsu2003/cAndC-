a3fbb5b5a228b6a4fa3e0cc64b278be39abb9360
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 11:54:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,9 @@
 #include <iostream>
-//美金換台幣的匯率(動態的marco)
-#define RATE(x) (x) * 32.78
+#define min(a,b) (a) >= (b) ? (b) : (a)
+#define max(a,b) (a) >= (b) ? (a) : (b)
 using namespace std;
-
 int main() {
-	double us,nt;
-	cout << "請輸入您有多少美金:";
-	cin >> 	us;
-	nt = RATE (us+1);
-	cout << "您有" << nt << "台幣" << endl;
+	int x=3,y=8;
+	cout << "2數較小值是:"<< (min(x,y)) << endl;
+	cout << "2數較大值是:"<< (max(x,y)) << endl;
 }

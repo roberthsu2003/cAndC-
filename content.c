0ab8252ae975cc443f4f8518e9c75b0aea58d218
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 16:19:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,37 @@
 #include <iostream>
+#include <time.h>
+
 using namespace std;
 
 class Student {
-	public:	
-	  string name;
-	  int chinese;
-	  int english;
-	  int math;	
-
-		
+public:
+  string name;
+  int chinese;
+  int english;
+  int math;
 };
 
-void print(Student& stu){
-	cout << stu.name << endl;
+void print(Student &stu) {
+  cout << stu.name << "\t";
+  cout << stu.chinese << "\t";
+  cout << stu.english << "\t";
+  cout << stu.math << "\t";
+  cout << endl;
 }
 
 int main() {
+  srandom(time(NULL));
   int nums;
   cout << "請輸入學生人數:";
   cin >> nums;
-	Student students[nums];
-	
-	for(int i=0; i<nums; i++){
-		Student s = students[i];
-		s.name = "學生" + to_string(i+1);
-		print(s);
-	}
-  
+  Student students[nums];
+
+  for (int i = 0; i < nums; i++) {
+    Student s = students[i];
+    s.name = "學生" + to_string(i + 1);
+    s.chinese = random() % (100 - 50 + 1) + 50;
+    s.english = random() % (100 - 50 + 1) + 50;
+    s.math = random() % (100 - 50 + 1) + 50;
+    print(s);
+  }
 }

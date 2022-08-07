6aefd19e9c86fc9f8d86c2f931b676f71d575527
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 16:58:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -15,13 +15,26 @@ public:
   double average() { return sum() / 3.0; }
 };
 
-void print(Student &stu) {
+void sorted_students(Student students[],int nums){
+	for(int i=0; i< nums-1; i++){
+		for(int j=i+1; j < nums; j++){
+			if(students[i].sum() < students[j].sum()){
+				Student temp = students[i];
+				students[i] = students[j];
+				students[j] = temp;
+			}
+		}
+	}
+}
+
+void print(Student &stu,int i) {
   cout << stu.name << "\t";
   cout << stu.chinese << "\t\t";
   cout << stu.english << "\t\t";
   cout << stu.math << "\t\t";
   cout << stu.sum() << "\t\t";
-  printf("%.2f\t", stu.average());
+  printf("%.2f\t\t", stu.average());
+	cout << i << "\t\t";
   cout << endl;
 }
 
@@ -31,13 +44,26 @@ int main() {
   cout << "請輸入學生人數:";
   cin >> nums;
   Student students[nums];
-	cout << "姓名\t" << "國文\t" << "英文\t" << "數學\t" << "總分\t" << "平均" << endl;
+  cout << "姓名\t"
+       << "國文\t"
+       << "英文\t"
+       << "數學\t"
+       << "總分\t"
+       << "平均\t"
+       << "名次" << endl;
   for (int i = 0; i < nums; i++) {
-    Student s = students[i];
+    Student& s = students[i];
     s.name = "學生" + to_string(i + 1);
     s.chinese = random() % (100 - 50 + 1) + 50;
     s.english = random() % (100 - 50 + 1) + 50;
-    s.math = random() % (100 - 50 + 1) + 50;
-    print(s);
+    s.math = random() % (100 - 50 + 1) + 50; 
+		
   }
+	//排序
+	sorted_students(students,nums);
+	
+	for(int i=0; i < nums; i++){
+		Student& s = students[i];
+		print(s,i+1);
+	}
 }

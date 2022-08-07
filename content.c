d8c4a60162e9aaf8469bfb815ad3c62e39f96dcd
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 09:49:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,14 @@
 #include <iostream>
 using namespace std;
-void multiply10(int x[],int size){
-	for(int i=0; i<size; i++){
-		x[i] *= 10;
-	}
-}
+int main() {
+	int num = 3;
+	int* n = new int[3];
+	*(n+0) = 10;
+	*(n+1) = 20;
+	*(n+2) = 30;
 
-void division10(int *x,int size){
-	for(int i=0; i<size; i++){
-		*(x+i) /= 10;
-	}
-}
-
-void print(int *x, int size){
-	for(int i=0; i<3; i++){
-		cout << x[i] << "\t";
+	for(int i=0; i<num; i++){
+		cout << *(n+i) << "\t";
 	}
 	cout << endl;
-}
-
-int main() {
-	int nums = 3;
-	int* n = new int[nums];
-	n[0] = 10;
-	n[1] = 20;
-	n[2] = 30;	
-	multiply10(n,nums);
-	print(n,nums);
-	division10(n, nums);
-	print(n,nums);
-	delete[] n;	
 }

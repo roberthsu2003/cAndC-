b05c5e819da06e26c061b7286aad4f89e68e1ec0
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 15:55:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,16 @@
 #include <iostream>
-class Student {
+using namespace std;
 
+class Student {
+  string name;
+  int chinese;
+  int english;
+  int math;
 };
 
-int main() { 
-	Student students[50]; 
+int main() {
+  int nums;
+  cout << "請輸入學生人數:";
+  cin >> nums;
+  Student students[nums];
 }

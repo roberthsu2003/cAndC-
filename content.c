4e0d397d666acadeefc3329e14d5c365fd887623
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 10:00:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int num = 3;
-	int* n = new int[3];
-	*(n+0) = 10;
-	*(n+1) = 20;
-	*(n+2) = 30;
 
-	for(int i=0; i<num; i++){
-		cout << *(n+i) << "\t";
-	}
-	cout << endl;
+void print(int x[]) {
+  for (int i = 0; i < 3; i++) {
+    cout << x[i] << "\t";
+  }
+  cout << endl;
+}
+
+int main() {
+  int num = 3;
+  int *n = new int[3];
+  *(n + 0) = 10;
+  *(n + 1) = 20;
+  *(n + 2) = 30;
+
+  /*
+  for(int i=0; i<num; i++){
+          cout << *(n+i) << "\t";
+  }
+
+  cout << endl;
+  */
+  print(n);
 }

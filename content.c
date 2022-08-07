9c821608ee99c1c1525169bdece7401ee89b3ca1
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 11:44:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,12 @@
 #include <iostream>
-//美金換台幣的匯率
-#define RATE 32.78
+//美金換台幣的匯率(動態的marco)
+#define RATE(x) (x) * 32.78
 using namespace std;
 
 int main() {
 	double us,nt;
 	cout << "請輸入您有多少美金:";
 	cin >> 	us;
-	nt = RATE * us;
+	nt = RATE (us+1);
 	cout << "您有" << nt << "台幣" << endl;
 }

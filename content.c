628c16505e95452f375ada2877106b91607ad6ab
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 14:41:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,7 +3,13 @@
 class Square {
 public:
   int width; // field,欄位
-  double area() { return width * width; }
+  double area() { 
+		return width * width; 
+	}
+
+	int perimeter(){
+		return 4 * width;
+	}	
 };
 
 using namespace std;
@@ -14,5 +20,8 @@ int main() {
   s2.width = 20;
 
   cout << "s1的面積是:" << s1.area() << endl;
+	cout << "s1的周長是:" << s1.perimeter() << endl;
+	
   cout << "s2的面積是:" << s2.area() << endl;
+	cout << "s2的周長是:" << s2.perimeter() << endl;
 }

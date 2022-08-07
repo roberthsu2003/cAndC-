eb04917b0f72481928db4394e25be5fa3546bc74
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 15:29:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,41 +1,32 @@
 #include <iostream>
-//將實體傳送給function使用
+
+class Rectangle{
+	public:
+		int width;
+		int height;
+
+		Rectangle(int w, int h){
+			width = w;
+			height = h;
+		}
+		int perimeter(){
+			return width*2 + height*2;
+		}
+
+		double area(){
+			return width * height;
+		}
+		
+	
+};
 using namespace std;
 
-class Square {
-public:
-  int width; // field,欄位
-	Square(int w){ // 自訂建構式
-		width = w;
-	}
-  double area() { return width * width; } //method,方法
-
-  int perimeter() { return 4 * width; }//method,方法
-};
-
-void description_square(Square s) {
-  cout << "width:" << s.width << endl;
-  cout << "perimeter:" << s.perimeter() << endl;
-  cout << "area:" << s.area() << endl;
-  cout << "==================" << endl;
-}
-void change_width(Square& s) { 
-	s.width += 10; 
-}
-
-
-
 int main() {
-  Square s1(10);
-	Square s2(20);
- 
-  description_square(s1);
-  description_square(s2);
+	Rectangle r1(10,20);
+	Rectangle r2(50,60);
+	cout << "r1周長:" << r1.perimeter() << endl;
+	cout << "r1面積:" << r1.area() << endl;
 
-  //寬加10
-	change_width(s1);
-	change_width(s2);
-
-	description_square(s1);
-  description_square(s2);
+	cout << "r2周長:" << r2.perimeter() << endl;
+	cout << "r2面積:" << r2.area() << endl;
 }

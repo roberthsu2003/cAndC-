7630610835b4d822ea37399a6314c3f9afcbe847
徐國堂 <roberthsu2003@gmail.com>
2022-08-07 11:03:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,35 @@
 #include <iostream>
-typedef enum direction{
+typedef enum direction {
 	North,
 	South,
 	East,
-	West
-}Direction;
+	West 
+} Direction;
 
 using namespace std;
 int main() {
-	Direction destination = South;
-	cout << destination << endl;
+  Direction destination = West;
+  cout << destination << endl;
+
+  //檢查列舉變數的內容
+  switch (destination) {
+  case North:
+    cout << "North" << endl;
+    break;
+
+  case South:
+    cout << "South" << endl;
+    break;
+
+  case East:
+    cout << "East" << endl;
+    break;
+
+  case West:
+    cout << "West" << endl;
+    break;
+
+  default:
+    break;
+  }
 }

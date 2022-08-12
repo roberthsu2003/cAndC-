9f2aaa30905ca00bdebc087b62d9a97557267efe
徐國堂 <roberthsu2003@gmail.com>
2022-08-12 15:56:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,38 +2,23 @@
 
 using namespace std;
 class Person {
+private:
+  string _name; //instance field
+
 public:
-  string name;
+  string name() { return _name; }
+
+  void setName(string n) { //instance method
+    if (n == "") {
+      _name = "沒有名字";
+    } else {
+      _name = n;
+    }
+  }
 };
 
-Person changeName(Person p,string newName){ //call by value
-	p.name = newName;
-	return p;
-}
-
-void changeName1(Person& p,string newName){ //call by reference
-	p.name = newName;
-}
-
-void changeName2(Person *p,string newName){
-	p->name = newName;
-}
 int main() {
-  Person p1; //預設建構式
-  p1.name = "robert";
-	p1 = changeName(p1, "Professor Robert");
-	//changeName1(p1, "Professor Robert");
-
-  Person p2;
-  p2.name = "Ting";
-	//p2 = changeName(p2, "Doctor Ting");
-	changeName1(p2, "Doctor Ting");
-	
-  cout << "p1.name:" << p1.name << endl;
-  cout << "p2.name:" << p2.name << endl;
-
-	Person *p3 = new Person();
-	p3 -> name = "john";
-	changeName2(p3, "Hello John");
-	cout << "p3的name:" << p3->name << endl;
+  Person p1;
+  p1.setName("");
+  cout << p1.name() << endl;
 }

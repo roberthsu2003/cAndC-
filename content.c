2a028a249a705df2704f02ef7286171deb7dbc10
徐國堂 <roberthsu2003@gmail.com>
2022-08-12 13:54:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,10 @@
 #include <iostream>
-#define RATE 29.1234
+#define RATE(x) (x) * 29.1234
 using namespace std;
-
 int main() {
   double us, nt;
   cout << "請輸入您有多少美金:";
   cin >> us;
-  nt = us * RATE;
+  nt = RATE(us + 1);
   cout << "您有" << nt << "台幣" << endl;
 }

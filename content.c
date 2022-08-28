015d040c341c44ce983f88edd10b4b3eca602f3a
徐國堂 <roberthsu2003@gmail.com>
2022-08-28 15:24:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,11 +4,11 @@ int main(void) {
   //計算圓面積
 	double PI = 3.141592653;
 	int radius;
-	double area;
+	int area;
 	printf("請輸入半徑:");
 	scanf("%d",&radius);
 	//數值型別的自動轉換
 	area = radius * radius * PI;
-	printf("圓面積=%.2f\n",area);
+	printf("圓面積=%d\n",area);
   return 0;
 }

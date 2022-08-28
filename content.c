fd7ed7ea8e9fc1950884a2696e04f0c615c6e6ec
徐國堂 <roberthsu2003@gmail.com>
2022-08-28 11:52:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("這是我的第一個了解語法的程式\n");
-	printf("這是整數%d\n",13);
-	printf("這是浮點數%.2f\n",3.14159);
-	printf("這是字串%s\n","13.0");
-	printf("%d + %d = %d\n",3,3,3+3);
-	printf("%.1f + %.1f = %.2f\n",5.2,6.2, 5.2 + 6.2);	
+  int x;
+  x = 20;
+
+  int y;
+  y = 50;
+
+  int total;
+  total = x + y;
+
+  printf("%d + %d = %d\n", x, y, total);
   return 0;
 }

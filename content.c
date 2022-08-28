67f5f810b3eb685c8ef8439d58550f526155aac4
徐國堂 <roberthsu2003@gmail.com>
2022-08-28 11:36:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,6 +5,7 @@ int main(void) {
 	printf("這是整數%d\n",13);
 	printf("這是浮點數%.2f\n",3.14159);
 	printf("這是字串%s\n","13.0");
-	
+	printf("%d + %d = %d\n",3,3,3+3);
+	printf("%.1f + %.1f = %.2f\n",5.2,6.2, 5.2 + 6.2);	
   return 0;
 }

cdb9ab3001d6de542987e16007daf97235cd4635
徐國堂 <roberthsu2003@gmail.com>
2022-08-28 16:34:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,16 +1,20 @@
 //直角三角型
 
+#include <math.h>
 #include <stdio.h>
 
 int main(void) {
-  int x, y;
+  double x, y;
+  double z;
   printf("請輸入x:");
-  scanf("%d", &x);
+  scanf("%lf", &x);
 
   printf("請輸入y:");
-  scanf("%d", &y);
+  scanf("%lf", &y);
 
-  printf("直角三角型,2邊為%d公分,%d公分\n", x, y);
-  printf("斜邊為");
+  z = sqrt(pow(x, 2) + pow(y, 2));
+
+  printf("直角三角型,2邊為%.2f公分,%.2f公分\n", x, y);
+  printf("斜邊為%.2f\n",z);
   return 0;
 }

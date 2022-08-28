1c9e7f617e1264f4072410f8f7945b2c4ffa9720
徐國堂 <roberthsu2003@gmail.com>
2022-08-28 14:34:51 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  char a1 = 'z';
-  printf("a1的內容是%c\n", a1);
-  unsigned char a2 = 255;
-  printf("a2的內容是%u\n", a2);
+  unsigned char born;
+  printf("出生民國年份:");
+  scanf("%u", &born);
+
+  unsigned short year;
+  year = born + 1911;
+
+  unsigned char age;
+  age = 2022 - year;
+
+  printf("對應西元年份:%d,今年%u歲\n", year, age);
   return 0;
 }

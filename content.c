d123c7140fb772a72793d6f87a869842322b6823
徐國堂 <roberthsu2003@gmail.com>
2022-08-28 14:36:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,7 +3,7 @@
 int main(void) {
   unsigned char born;
   printf("出生民國年份:");
-  scanf("%u", &born);
+  scanf("%hhu", &born);
 
   unsigned short year;
   year = born + 1911;
@@ -11,6 +11,6 @@ int main(void) {
   unsigned char age;
   age = 2022 - year;
 
-  printf("對應西元年份:%d,今年%u歲\n", year, age);
+  printf("對應西元年份:%d,今年%hhu歲\n", year, age);
   return 0;
 }

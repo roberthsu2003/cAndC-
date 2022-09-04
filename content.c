12c97466a1b0267eb38e9ff5ad442b949af482fc
徐國堂 <roberthsu2003@gmail.com>
2022-09-04 12:28:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,6 +9,12 @@ int main() {
 	cin >> money;
 	if(money >= 100000){
 		payMoney = money * 0.8;
+	}else if(money >= 50000){
+		payMoney = money * 0.85;
+	}else if(money >= 30000){
+		payMoney = money * 0.9;
+	}else if(money >= 10000){
+		payMoney = money * 0.95;
 	}else{
 		payMoney = money;
 	}

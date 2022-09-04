e0a883d02a3b99a25771841cb27178fb737e83e9
徐國堂 <roberthsu2003@gmail.com>
2022-09-04 15:17:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,20 @@
+//計算固定中的支出，媽媽每天會將家裡的花費記錄下來，並且計算本週的花費總和
 #include <iostream>
 using namespace std;
+
 int main() {
-  for (int i = 0; i < 7; i++) {
-    cout << "i的值是" << i << endl;
+  int n;
+  int sum = 0;
+  for (int i = 1; i <= 7; i++) {
+    if (i == 7) {
+      cout << "請輸入星期日的支出:";
+    } else {
+      cout << "請輸入星期" << i << "的支出:";
+    }
+
+    cin >> n;
+    sum += n;
   }
+
+  cout << "本星期的支出為:" << sum << "元\n";
 }

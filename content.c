5f0b8130509e95796fe751ef763c66c2d65aa21d
徐國堂 <roberthsu2003@gmail.com>
2022-09-04 14:35:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -7,25 +7,18 @@ using namespace std;
 int main() {
   int chinese;
   int math;
-	int bonus;	
+  int bonus;
   cout << "請輸入國文成績和數學成績(國文 數學):";
   //使用cin一次輸入2個值
   cin >> chinese >> math;
-	if(chinese == 100){
-		//國文100
-		if(math == 100){
-			//數學100
-			bonus = 1000;
-		}else{
-			bonus = 500;
-		}
-	}else{
-		if(math == 100){
-			bonus = 500;
-		}else{
-			bonus = 0;
-		}
-	}
 
-	cout << "獎金是:" << bonus << endl;
+  if (chinese == 100 && math == 100) {
+    bonus = 1000;
+  } else if (chinese == 100 || math == 100) {
+    bonus = 500;
+  } else {
+    bonus = 0;
+  }
+
+  cout << "獎金是:" << bonus << endl;
 }

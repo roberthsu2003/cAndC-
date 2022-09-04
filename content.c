26b9a29ce95d088620bf046b0d79100946035d1b
徐國堂 <roberthsu2003@gmail.com>
2022-09-04 15:52:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
   for (int i = 1; i <= 9; i++) {
-    cout << "i的值" << i << endl;
+    
     for (int j = 1; j <= 9; j++) {
-      cout << "j=" << j << "\t";
+      cout << i << "*" << j << "=" << i*j << "\t";
     }
     cout << endl;
   }

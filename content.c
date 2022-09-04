358d5fbfc790f49f8fde8a77acfb63dd21870db5
徐國堂 <roberthsu2003@gmail.com>
2022-09-04 10:42:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,11 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int sum = 0;
-	int score;
-	cout << "請輸入國文分數:";
-	cin >>	score;
-	sum += score;
+	int x = 1;
+	int y = ++x;
+	cout << "x=" << x << ",y=" << y << endl;
 
-	cout << "請輸入數學分數:";
-	cin >>	score;
-	sum += score;
-
-	cout << "請輸入英文分數:";
-	cin >>	score;
-	sum += score;
-
-	cout << "三科總分為:" << sum << endl;
+	int n = 1;
+	int m = n++;
+	cout << "n=" << n << ",m=" << m << endl;
 }

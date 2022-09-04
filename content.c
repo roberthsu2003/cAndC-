08e180c5535676687ee51c6ce084122056b2db4e
徐國堂 <roberthsu2003@gmail.com>
2022-09-04 15:06:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,30 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-  int num1 = 20;
-  int num2 = 4;
-  char op;
-  cout << "請輸入要執行的運算(+,-,*,/):";
-  cin >> op;
-  switch (op) {
-  case '+':
-    cout << "num1 + num2 = " << num1 + num2 << endl;
-    break;
-
-  case '-':
-    cout << "num1 - num2 = " << num1 - num2 << endl;
-    break;
-
-  case '*':
-    cout << "num1 * num2 = " << num1 * num2 << endl;
-    break;
-
-  case '/':
-    cout << "num1 / num2 = " << num1 / num2 << endl;
-    break;
-
-  default:
-    cout << "無法執行運算" << endl;
+  for (int i = 0; i < 7; i++) {
+    cout << "i的值是" << i << endl;
   }
 }

34a6f7c526258ad3067200caf7e2f6f81ff7731f
徐國堂 <roberthsu2003@gmail.com>
2022-09-11 16:09:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,23 @@
 #include <time.h>
 using namespace std;
 
-void sorted(int arr[], int nums) {
+void sorted(int arr[], int nums, bool small) {
   for (int f = 0; f < nums - 1; f++) {
     for (int b = f + 1; b < nums; b++) {
-      if (arr[f] > arr[b]) {
-        // 2數對調
-        int temp = arr[f];
-        arr[f] = arr[b];
-        arr[b] = temp;
+      if (small) {
+        if (arr[f] > arr[b]) {
+          // 2數對調
+          int temp = arr[f];
+          arr[f] = arr[b];
+          arr[b] = temp;
+        }
+      } else {
+        if (arr[f] < arr[b]) {
+          // 2數對調
+          int temp = arr[f];
+          arr[f] = arr[b];
+          arr[b] = temp;
+        }
       }
     }
   }
@@ -30,10 +39,17 @@ int main() {
   }
   cout << endl;
   //呼叫排序
-  sorted(array, nums);
+  sorted(array, nums, true);
   cout << "由小到大的排序後:" << endl;
   for (int i = 0; i < nums; i++) {
     cout << array[i] << "\t";
   }
+	cout << endl;
+  cout << "由大到小的排序後:" << endl;
+  sorted(array, nums, false);
+  for (int i = 0; i < nums; i++) {
+    cout << array[i] << "\t";
+  }
+
   cout << endl;
 }

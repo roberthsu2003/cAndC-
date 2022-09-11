99c34e2c1067102a11895008e910ed71aca6b10d
徐國堂 <roberthsu2003@gmail.com>
2022-09-11 12:08:58 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,53 +1,15 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-void playGame(){
-	srandom(time(NULL));
-  int min = 1;
-  int max = 100;
-	int keyin;
-	int count = 0;
-  int guess = random() % (max - min + 1) + min;
-	cout << guess << endl;
-	cout << "============猜數字遊戲================" << endl;
-	while(true){
-		cout << "猜數字的範圍" << min << "~" << max << ":";
-		cin >> keyin;
-		count += 1;
-		if(keyin >=min && keyin <= max){
-			if(keyin == guess){
-				cout << "賓果!猜對了,答案是" << guess << endl;
-				cout << "您猜了" << count << "次" << endl;
-				break;
-			}else if(keyin > guess){
-				cout << "再小一點!";
-				max = keyin - 1;	
-				
-			}else if(keyin < guess){
-				cout << "再大一點!";
-				min = keyin + 1;				
-			}
-			cout << "您已經猜了" << count << "次" << endl;
-		}else{
-			cout << "請輸入提示範圍內的數字!\n";
-			continue;
-		}
-	}	
-}
-
-
 int main() {
-	char playagain;
-  while(true){
-		playGame();
-		cout << "請問還要繼續遊戲嗎?(y,n):";
-		cin >> playagain;
-		if(playagain == 'n'){
-			break;
-		}
+	int stu1[5];
+	stu1[0] = 78;
+	stu1[1] = 97;
+	stu1[2] = 65;
+	stu1[3] = 59;
+	stu1[4] = 85;
+
+	for(int i=0;i < 5;i++){
+		cout << "stu1[" << i << "]的值是" << stu1[i] << endl;
 	}
-	cout << "遊戲結束" << endl;
-	
-	
 }

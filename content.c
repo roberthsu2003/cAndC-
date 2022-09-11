eb0dc8a61b0558f4644d44e4f550837e0a13c47a
徐國堂 <roberthsu2003@gmail.com>
2022-09-11 14:11:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,19 +3,24 @@
 
 using namespace std;
 
-int get_score() { 
-	return random() % (100 - 50 + 1) + 50; 
-}
+int get_score() { return random() % (100 - 50 + 1) + 50; }
 
 int main() {
   srandom(time(NULL));
-	int stu_num = 50;
+  int stu_num = 50;
   int score_num = 5;
-	int total_nums = stu_num * score_num;
+  int total_nums = stu_num * score_num;
   int scores[total_nums];
 
   for (int i = 0; i < total_nums; i++) {
     scores[i] = get_score();
-    cout << "scores[" << i << "]的值是" << scores[i] << endl;
+  }
+
+  for (int i = 0; i < stu_num; i++) {
+    cout << "第" << i + 1 << "學生: ";
+		for(int j=0; j < score_num; j++){
+			cout << scores[i * score_num + j] << "\t";
+		}
+		cout << endl;
   }
 }

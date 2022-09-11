1073662021a2c7e169aac412af95ba787aa72e0a
徐國堂 <roberthsu2003@gmail.com>
2022-09-11 10:36:27 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   srandom(time(NULL));
   int min = 1;
-  int max = 10;
+  int max = 100;
 	int keyin;
 	int count = 0;
   int guess = random() % (max - min + 1) + min;
@@ -19,7 +19,15 @@ int main() {
 				cout << "賓果!猜對了,答案是" << guess << endl;
 				cout << "您猜了" << count << "次" << endl;
 				break;
+			}else if(keyin > guess){
+				cout << "再小一點!";
+				max = keyin - 1;	
+				
+			}else if(keyin < guess){
+				cout << "再大一點!";
+				min = keyin + 1;				
 			}
+			cout << "您已經猜了" << count << "次" << endl;
 		}else{
 			cout << "請輸入提示範圍內的數字!\n";
 			continue;

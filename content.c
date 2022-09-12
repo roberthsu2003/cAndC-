46cb46e52825df0f7d2cfca2193fa1c1fc482073
徐國堂 <roberthsu2003@gmail.com>
2022-09-12 21:39:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-  int x;
-	int y;
-	printf("請輸入x的值:");
-	scanf("%d",&x);
-	printf("x的值是:%d\n",x);
-
-	printf("請輸入y的值:");
-	scanf("%d",&y);
-	printf("y的值是:%d\n",y);
+	int chinese;
+	int english;
+	int math;
+	printf("請輸入國文分數:");
+	scanf("%d",&chinese);
+	printf("請輸入英文分數:");
+	scanf("%d",&english);
+	printf("請輸入數學分數:");
+	scanf("%d",&math);
+	printf("國文分數是%d\n",chinese);
+	printf("英文分數是%d\n",english);
+	printf("數學分數是%d\n",math);
+	printf("總分是%d\n",chinese+english+math);
+	printf("平均是%.2f\n",(chinese+english+math)/3.0);
   return 0;
 }

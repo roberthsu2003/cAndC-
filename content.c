5b6c3a7be5164dade27213bf6e60257d4b31de62
徐國堂 <roberthsu2003@gmail.com>
2022-09-12 21:49:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	int chinese;
-	int english;
-	int math;
-	printf("請輸入國文分數:");
-	scanf("%d",&chinese);
-	printf("請輸入英文分數:");
-	scanf("%d",&english);
-	printf("請輸入數學分數:");
-	scanf("%d",&math);
-	printf("國文分數是%d\n",chinese);
-	printf("英文分數是%d\n",english);
-	printf("數學分數是%d\n",math);
-	printf("總分是%d\n",chinese+english+math);
-	printf("平均是%.2f\n",(chinese+english+math)/3.0);
+	int bottom;
+	int height;
+	
+	printf("請輸入三角形的底(cm):");
+	scanf("%d",&bottom);
+	printf("請輸入三角形的高(cm):");
+	scanf("%d",&height);
+
+	printf("3角形的面積是%.2f\n", bottom * height / 2.0);
   return 0;
 }

19106816d9429e89fc1d7c0ec332bfd72f4d667b
徐國堂 <roberthsu2003@gmail.com>
2022-09-12 20:23:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,7 +2,8 @@
 
 int main(void) {
   printf("我的國文成績是%d\n",89);
-  printf("我的英文成績是%d\n",92);
-  printf("數學分數是%d,社會分數是%d\n",92,85);	
+	printf("我的英文成績是%d\n",92);
+	printf("數學分數是%d,社會分數是%d\n",92,85);
+	printf("我的平均是%.2f\n",89.7);
   return 0;
 }

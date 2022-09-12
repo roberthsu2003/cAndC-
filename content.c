b127584f9da61a874aa2961c8818088ba4933622
徐國堂 <roberthsu2003@gmail.com>
2022-09-12 19:47:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,58 +1,7 @@
-tools.h
+#include <stdio.h>
 
-#include <iostream>
-using namespace std;
-
-void sorted(int arr[], int nums, bool small) {
-  for (int f = 0; f < nums - 1; f++) {
-    for (int b = f + 1; b < nums; b++) {
-      if (small) {
-        if (arr[f] > arr[b]) {
-          // 2數對調
-          int temp = arr[f];
-          arr[f] = arr[b];
-          arr[b] = temp;
-        }
-      } else {
-        if (arr[f] < arr[b]) {
-          // 2數對調
-          int temp = arr[f];
-          arr[f] = arr[b];
-          arr[b] = temp;
-        }
-      }
-    }
-  }
-}
-
-void printArray(int arr[], int nums){
-	for (int i = 0; i < nums; i++) {
-    cout << arr[i] << "\t";
-  }
-	cout << endl;
-}
-
-void random_value(int arr[], int nums){
-	for(int i=0; i<nums; i++){
-		arr[i] = random() % (100-1+1) + 1;
-	}
-}
-
-
-main.cpp
-
-#include <iostream>
-#include "tools.h"
-#include <time.h>
-
-using namespace std;
-int main() {
-	srandom(time(NULL));
-	int a[10];
-	random_value(a,10);
-	cout << "排序前:" << endl;
-	printArray(a, 10);
-	cout << "排序後:" << endl;	
-	sorted(a,10,true);
-	printArray(a, 10);
+int main(void) {
+  printf("Hello c語言\n");
+	printf("這是我學習C語言的第一個程式\n");	
+  return 0;
 }

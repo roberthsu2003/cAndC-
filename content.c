32becaaba50101354efd2a6f85ab2144bdba8923
徐國堂 <roberthsu2003@gmail.com>
2022-09-14 21:58:46 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,6 +5,7 @@ int main() {
 	double PI = 3.14159;
 	int radius;
 	cout << "請輸入圓半徑:";
-	cout << "圓面積是:" << endl;
-	cout << "圓周長是:" << endl;
+	cin >> radius;
+	cout << "圓面積是:" << radius*radius*PI << endl;
+	cout << "圓周長是:" << 2*radius*PI << endl;
 }

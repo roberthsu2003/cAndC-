1bdf175e0f70bcf0aaffd92df6adef7b130f343c
徐國堂 <roberthsu2003@gmail.com>
2022-09-14 21:30:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
- 	cout << 5 + 8 << endl;
-	cout << 5.0 + 8 << endl;
-	cout << 3/2 << endl;
-	cout << 3/2.0 << endl;
-
-	int sum;
-	sum = 3 / 2.0;
-	cout << sum << endl;
+	int chinese, english, math;
+	cout << "請輸入國文,英文,數學的分數:(分數和分數之間空1格):";
+	cin >> chinese >> english >> math;
+	cout << "國文成績是" << chinese << endl;
+	cout << "英文成績是" << english << endl;
+	cout << "數學成績是" << math << endl;
 }

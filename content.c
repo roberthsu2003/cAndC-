1aa699f07a2cb9e8083159c489b82bc8273e1200
徐國堂 <roberthsu2003@gmail.com>
2022-09-14 21:49:54 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int chinese, english, math;
-	cout << "請輸入國文,英文,數學的分數:(分數和分數之間空1格):";
-	cin >> chinese >> english >> math;
-	cout << "國文成績是" << chinese << endl;
-	cout << "英文成績是" << english << endl;
-	cout << "數學成績是" << math << endl;
-	int sum = chinese + english + math;
-	double average = sum / 3.0;
-	cout << "您的總分為:" << sum << "分,平均為" << average << "分\n";
+	double PI = 3.14159;
+	int radius;
+	cout << "請輸入圓半徑:";
+	cout << "圓面積是:" << endl;
+	cout << "圓周長是:" << endl;
 }

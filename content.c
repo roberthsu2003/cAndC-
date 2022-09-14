66b27025b03b0dec27d789d0954bbb14ac37adb7
徐國堂 <roberthsu2003@gmail.com>
2022-09-14 21:11:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-	string name;	
-	cout << "請輸入姓名:";
-	cin >> name;
-	cout << "您的姓名是:" << name << endl;
-
-	const char *smallName = "robert";
-	cout << "您的小名是" << smallName << endl;
-	
-	
+ 	cout << 5 + 8 << endl;
+	cout << 5.0 + 8 << endl;
+	cout << 3/2 << endl;
+	cout << 3/2.0 << endl;
 }

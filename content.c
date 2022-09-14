125b63beee511ccc584a886863ec2a1a7475a9b3
徐國堂 <roberthsu2003@gmail.com>
2022-09-14 19:15:21 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-	int bottom;
-	int height;
-	
-	printf("請輸入三角形的底(cm):");
-	scanf("%d",&bottom);
-	printf("請輸入三角形的高(cm):");
-	scanf("%d",&height);
+  int i;
+  printf("請輸入i的值:");
+  scanf("%d", &i);
 
-	printf("3角形的面積是%.2f\n", bottom * height / 2.0);
+  printf("i的值為%d\n", i);
   return 0;
 }

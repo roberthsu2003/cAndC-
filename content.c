9f138c69ac7b1072f13bc30e0c9c48c9a1911c48
徐國堂 <roberthsu2003@gmail.com>
2022-09-14 20:33:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	short s = 100;
-	cout << "s=" << s << endl;
-	int i = 100;
-	cout << "i=" << i << endl;
-	long long int l = 100;
-	cout << "l=" << l << endl;
+	char characterB;
+	characterB = 65;
+	cout << characterB << endl;
+	printf("%d\n",characterB);
 
-	float f = 3.14;
-	cout << "f=" << f << endl;
 
-	double d = 3.1415962;
-	//cout << "d=" << d << endl;
-	printf("d=%.7f\n", d);
+	char characterA;
+	characterA = 'A';
+	cout << characterA << endl;
+	printf("%d\n",characterA);
 }

540d16d60e45445f1344880af4767527694487b6
徐國堂 <roberthsu2003@gmail.com>
2022-09-16 19:16:08 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,10 +2,14 @@
 using namespace std;
 
 int main() {
-	double PI = 3.14159;
-	int radius;
-	cout << "請輸入圓半徑:";
-	cin >> radius;
-	cout << "圓面積是:" << radius*radius*PI << endl;
-	cout << "圓周長是:" << 2*radius*PI << endl;
+  //複合指定運算子
+  int n = 5;
+  n += 5;
+  cout << n << endl;
+  n -= 5;
+  cout << n << endl;
+  n *= 10;
+  cout << n << endl;
+  n /= 10;
+  cout << n << endl;
 }

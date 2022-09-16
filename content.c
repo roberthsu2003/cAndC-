d058b32932e33766f157e68e34a79913a82a8d88
徐國堂 <roberthsu2003@gmail.com>
2022-09-16 19:54:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    int n = 5;
-    n--;
-    cout << "n=" << n << endl; 
+int main() {
+	int n = 1;
+	int m = ++n;
+	cout << "n=" << n << endl;
+	cout << "m=" << m << endl;
 
-    return 0;
+	int x = 1;
+	int y = x++;
+	cout << "x=" << x << endl;
+	cout << "y=" << y << endl;
 }

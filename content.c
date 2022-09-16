6b1d5ed0c324da580e5498d5b0bf0469b678543b
徐國堂 <roberthsu2003@gmail.com>
2022-09-16 21:37:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
 //讓使用者輸入成績, 若成績在90分以上就顯示「優等」,
-//80-89分顯示「甲等」，70-79分顯示「乙等」，60-69分顯示「丙等」，60分以下顯示「丁等」。
-int main() {}
+// 80-89分顯示「甲等」，70-79分顯示「乙等」，60-69分顯示「丙等」，60分以下顯示「丁等」。
+
+int main() {
+  int scores;
+  cout << "請輸入學生分數:";
+  cin >> scores;
+  if (scores >= 90) {
+    cout << "優等" << endl;
+  } else if (scores >= 80) {
+    cout << "甲等" << endl;
+  } else if (scores >= 70) {
+    cout << "乙等" << endl;
+  } else if (scores >= 60) {
+    cout << "丙等" << endl;
+  } else {
+    cout << "丁等" << endl;
+  }
+}

6c1be0b4ed1b25e2e090c4802ab7dd0fd9fbd3dc
徐國堂 <roberthsu2003@gmail.com>
2022-09-16 20:56:37 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,8 +2,12 @@
 //試用if-else敘述，選寫一個判斷輸入的數字為奇數或偶數的程式
 using namespace std;
 int main() {
-	int values;
-	cout << "請輸入一個數值:";
-	cin >> values;
-	
+  int values;
+  cout << "請輸入一個數值:";
+  cin >> values;
+  if (values % 2 == 0) {
+    cout << "偶數" << endl;
+  } else {
+    cout << "奇數" << endl;
+  }
 }

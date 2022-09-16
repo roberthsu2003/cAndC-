0d21d748ae9e6a01913bb03a276fb464302aaf51
徐國堂 <roberthsu2003@gmail.com>
2022-09-16 20:28:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,24 @@
 #include <iostream>
+//學生符合加分條件就加5%分,如果超過300分,就以300為主。
+//單項選擇
 using namespace std;
 int main() {
-	int a = 2, b = 2;
-	cout << "a==b:" << (a==b) << endl;
+  int scores;
+  bool add;
+  cout << "請輸入學生的分數:";
+  cin >> scores;
 
-	int n = 2, m = 1;
-	cout << "n==m:" << (n==m) << endl;
+  cout << "學生符合加分條件嗎?yes請輸入1,no請輸入0:";
+  cin >> add;
+
+	if(add){
+		//有加分
+		scores *= 1.05;
+		if(scores > 300){
+			scores = 300;
+		}
+	}
+
+	cout << "學生分數是:" << scores << endl;		
+	
 }

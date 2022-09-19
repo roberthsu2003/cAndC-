9ca3d86143fc3ba9018a31548eca251a53d0ea5f
徐國堂 <roberthsu2003@gmail.com>
2022-09-19 19:14:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,29 @@
-/*
-//========================================================
-
-//請輸入購買金額:130000
-//實付金額是:104000元
-
-//========================================================
-
-*/
-//輸入顧客購買金額，若金額在100000元打8折，50000打85折，30000打9折，10000打95折
+// 2個以上的條件
+//巢狀判斷
+//國文100同時數學100,獎金1000
+//國文100或者數學100,獎金500
 
 #include <iostream>
 using namespace std;
+int main() { 
+	int chinese, math;
+	int bonus;
+	cout << "請輸入國文成績和數學成績(國文 數學):";
+	cin >> chinese >> math;
 
-int main() {
-	int money;
-	int payMoney;
-  std::cout << "Hello World!\n";
+	if(chinese == 100){
+		if(math == 100){
+			bonus = 1000;
+		}else{
+			bonus = 500;
+		}
+	}else{
+		if(math == 100){
+			bonus = 500;
+		}else{
+			bonus = 0;
+		}
+	}
+
+	cout << "獎金是" << bonus << endl;
 }

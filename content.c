354d864a177cba0c978b7f6b079c68189075b418
徐國堂 <roberthsu2003@gmail.com>
2022-09-19 21:06:11 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-	for(int i=1; i<=9; i++){
-		cout << "i=" << i << endl;
+	for(int i=1; i<=9; i++){		
 		for(int j=1; j<=9; j++){			
-			cout << "j=" << j << " ";
+			cout << i << "*" << j << "=" << i * j << "\t";
 		}
 		cout << endl;
 	}

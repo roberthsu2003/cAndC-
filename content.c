5cb01b10203e06a4b85517ad6c768884b25b27a6
徐國堂 <roberthsu2003@gmail.com>
2022-09-19 20:19:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,33 +1,8 @@
-//讓使用者輸入加、減、乘、除運算子, 就會顯示運算結果
-//比對值是否相等
-
 #include <iostream>
+
 using namespace std;
 int main() {
-	char op;
-	int num1=20,num2=4;
-	cout << "請輸入要執行的運算(+,-,*,/):";
-	cin >> op;
-
-	switch(op){
-		case '+':
-		cout << "num1+num2=" << num1 + num2 << endl;
-		break;
-
-		case '-':
-		cout << "num1-num2=" << num1 - num2 << endl;
-		break;
-
-		case '/':
-		cout << "num1/num2=" << num1 / num2 << endl;
-		break;
-
-		case '*':
-		cout << "num1*num2=" << num1 * num2 << endl;
-		break;
-
-		default:
-		cout << "無法執行運算!" << endl;
+	for(int i=0; i<7; i++){
+		cout << i << endl;
 	}
-	
 }

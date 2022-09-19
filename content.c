ba74ffdf6c10f5b37c62a6d15b7567a6128f2bea
徐國堂 <roberthsu2003@gmail.com>
2022-09-19 21:57:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,14 @@
-//請你以while迴圈方式為小美設計一個輸入成績的程式，如果輸入負數表示成績輸入結束，在輸入成績結束後顯示班上總成績及平均成績
 #include <iostream>
-
 using namespace std;
 int main() {
-	int value;	
-  while(true){
-		//break可以跳出迴圈
-		cout << "請輸入一個數值:";
-		cin >> value;
-		if(value==-1){
+	string password;
+	while(true){
+		cout << "請輸入密碼:";
+		cin >> password;
+		if(password == "5678"){
 			break;
 		}
-		cout << "輸入的值是:" << value << endl;
 	}
 
-	cout << "程式結束" << endl;
+	cout << "恭喜!您的密碼正確!請進" << endl;
 }

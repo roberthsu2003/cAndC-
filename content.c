9eefe943d6782be9a540ffb29274d89b67d6849b
徐國堂 <roberthsu2003@gmail.com>
2022-09-21 21:20:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,12 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
-int main() {
-  int min = 1;
-  int max = 100;
-  int keyin;
-  int count = 0;
-  srandom(time(NULL));
-  int guess = random() % (max - min + 1) + min;
 
-  cout << "============猜數字遊戲================\n\n";
-  while (true) {
-    cout << "猜數字範圍" << min << "~" << max << ":";
-    cin >> keyin;
-    count += 1;
-    if (keyin >= min && keyin <= max) {
-      if (keyin == guess) {
-        cout << "賓果!猜對了,答案是" << guess << endl;
-        cout << "您猜了" << count << "次" << endl;
-        break;
-      } else if (keyin > guess) {
-        cout << "再小一點!";
-        max = keyin - 1;
-      } else if (keyin < guess) {
-        cout << "再大一點!";
-        min = keyin + 1;
-      }
-      cout << "您已經猜了" << count << "次" << endl;
-    } else {
-      cout << "請輸入提示範圍內的數字!\n";
-      continue;
-    }
-  }
-	cout << "遊戲結束" << endl;	
+//沒有傳出值和參數的function
+void sayHello() { 
+	cout << "歡迎光臨!" << endl; 
+}
+
+int main() {
+  sayHello();
+  return 0;
 }

ae0e5af590660e9240488ce4a1b6974ecbb510b0
徐國堂 <roberthsu2003@gmail.com>
2022-09-21 20:06:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
-int main() {	
+int main() {
+	int min = 5;
+	int max = 10;
 	srandom(time(NULL));	
-	std::cout << random() << endl; 
+	std::cout << random() % (max-min+1) + min << endl; 
 }

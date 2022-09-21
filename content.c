3b707370777545e4ddbf774bc893b4cd4caef8a5
徐國堂 <roberthsu2003@gmail.com>
2022-09-21 19:25:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,20 @@
+// while迴圈方式為小美設計一個輸入成績的程式，如果輸入負數表示成績輸入結束，在輸入成績結束後顯示班上總成績及平均成績
 #include <iostream>
 using namespace std;
-int main() {
-	string password;
-	while(true){
-		cout << "請輸入密碼:";
-		cin >> password;
-		if(password == "5678"){
-			break;
-		}
-	}
 
-	cout << "恭喜!您的密碼正確!請進" << endl;
+int main() {
+  int num = 0;
+  int score;
+  int sum = 0;
+  while (true) {    
+    cout << "請輸入第" << num+1 << "學生的成績:";
+    cin >> score;
+    if (score < 0) {
+      break;
+    }
+    sum += score;
+		num += 1;
+  }
+
+  cout << "全班總成績為:" << sum << "平均分數為:" << (float)sum/num << endl; 
 }

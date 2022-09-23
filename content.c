ffb8e04721c7f0d1c8f3b117ae2d86e4f470ed40
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 21:07:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,22 +3,26 @@
 #include <time.h>
 using namespace std;
 
-int main() {
-	srandom(time(NULL));	
-	int nums[250];
-	int keyin;
-	int count = 0;
-	
-	for(int i=0; i<250; i++){
-		nums[i] = random() % (50 - 1 + 1) + 1;
-	}
-	cout << "請輸入搜尋的數字(1~50):";
-	cin >> keyin;
-	for(int i=0; i<250; i++){
-		if(keyin == nums[i]){
-			count += 1;
-		}
-	}
+void search(int n[], int elements, int key) {
+  int count = 0;
+  for (int i = 0; i < elements; i++) {
+    if (key == n[i]) {
+      count += 1;
+    }
+  }
 
-	cout << keyin << "出現的次數是" << count << "次\n";
+  cout << key << "出現的次數是" << count << "次\n";
+}
+
+int main() {
+  srandom(time(NULL));
+  int nums[250];
+  int keyin;
+
+  for (int i = 0; i < 250; i++) {
+    nums[i] = random() % (50 - 1 + 1) + 1;
+  }
+  cout << "請輸入搜尋的數字(1~50):";
+  cin >> keyin;
+  search(nums, 250, keyin);
 }

474ded9b78795c77858c77350804a4d0f0542ca5
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 21:26:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,29 +1,30 @@
-//搜尋
 #include <iostream>
 #include <time.h>
+
 using namespace std;
 
-void search(int n[], int elements, int key) {
-  int count = 0;
+void printArray(int n[], int elements) {
   for (int i = 0; i < elements; i++) {
-    if (key == n[i]) {
-      count += 1;
-    }
+    cout << n[i] << " ";
   }
-
-  cout << key << "出現的次數是" << count << "次\n";
+  cout << endl;
 }
 
 int main() {
   srandom(time(NULL));
-  int nums[250];
-  int keyin;
+  int nums;
+  cout << "請輸入要排序的數值個數:";
+  cin >> nums;
 
-  for (int i = 0; i < 250; i++) {
-    nums[i] = random() % (50 - 1 + 1) + 1;
+  int array[nums];
+  for (int i = 0; i < nums; i++) {
+    array[i] = random() % (100 - 1 + 1) + 1;
   }
+  cout << "排序前" << endl;
+  printArray(array, nums);
 
-	for(int i=1; i<=50; i++){
-		search(nums,250,i);
-	}
+  //泡沫排序法
+
+  cout << "排序後" << endl;
+  printArray(array, nums);
 }

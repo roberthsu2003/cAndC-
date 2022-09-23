b4946144065f6df16e49fc5b18f980c67d70b8e1
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 20:19:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -12,10 +12,11 @@ int main() {
 	for(int i=0; i<totals; i++){
 		scores[i] = random() % (100-50+1) + 50;
 	}
-
+	cout << "姓名 國文 英文 數學 地理 社會\n"; 
 	for(int i=0; i<students; i++){
+		cout << "學生" << i+1 << " ";
 		for(int j=0; j<subjects; j++){
-			cout << scores[i*subjects + j] << " ";
+			cout << scores[i*subjects + j] << "  ";
 		}
 		cout << endl;
 	}	

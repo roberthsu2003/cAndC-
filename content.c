dbe3486aecb50dbb257370be0bcee656a258e74a
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 19:32:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,11 @@
+#include <iostream>
+using namespace std;
+
 int main() {
-  int numbers[5];
-  numbers[0] = 78;
-  numbers[1] = 85;
-  numbers[2] = 92;
-  numbers[3] = 83;
-  numbers[4] = 64;
+  int numbers[] = {78, 92, 69, 85, 75};  
 
   for(int i=0; i<5; i++){
 		cout << numbers[i] << " ";
 	}
 	cout << endl;
+}

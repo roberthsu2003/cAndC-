3d7cfb3c592da71fd3335be53bef3ca36d02e42d
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 19:46:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int main() {
+  srandom(time(NULL));
   int num;
-	cout << "請輸入學生數:";
-	cin >> num;	
-	
+  cout << "請輸入學生數:";
+  cin >> num;
   int numbers[num];
+	
   for (int i = 0; i < num; i++) {
-    cout << "請輸入第" << i + 1 << "學生的分數:";
-    cin >> numbers[i];
+    numbers[i] = random() % (100 - 50 + 1) + 50;
   }
 
   for (int i = 0; i < num; i++) {

cddbca77e1c512c6f5749b7ec269a205a4036af4
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 21:43:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -24,6 +24,16 @@ int main() {
   printArray(array, nums);
 
   //泡沫排序法
+  int temp;
+  for (int i = 0; i < nums - 1; i++) {
+    for (int j = i + 1; j < nums; j++) {
+      if (array[i] > array[j]) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
 
   cout << "排序後" << endl;
   printArray(array, nums);

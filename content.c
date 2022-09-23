4592470ce10be813af6335c4b36b16cfe5d9848b
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 19:41:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,18 @@
 using namespace std;
 
 int main() {
-  int numbers[5];
-	for(int i=0;i<5;i++){
-		cout << "請輸入第" << i+1 << "學生的分數:";
-		cin >> numbers[i];		
-	}
+  int num;
+	cout << "請輸入學生數:";
+	cin >> num;	
+	
+  int numbers[num];
+  for (int i = 0; i < num; i++) {
+    cout << "請輸入第" << i + 1 << "學生的分數:";
+    cin >> numbers[i];
+  }
 
-  for(int i=0; i<5; i++){
-		cout << numbers[i] << " ";
-	}
-	cout << endl;
+  for (int i = 0; i < num; i++) {
+    cout << numbers[i] << " ";
+  }
+  cout << endl;
 }

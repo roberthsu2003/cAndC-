89a7ce7502bde2e540f5c8e5a44241b7bae18e68
徐國堂 <roberthsu2003@gmail.com>
2022-09-23 20:34:19 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,21 +3,17 @@
 using namespace std;
 
 int main() {
-	srandom(time(NULL));
-	int students = 50;
-	int subjects = 5;
-	int totals = students * subjects;
-	int scores[totals];
+  srandom(time(NULL));
 
-	for(int i=0; i<totals; i++){
-		scores[i] = random() % (100-50+1) + 50;
-	}
-	cout << "姓名 國文 英文 數學 地理 社會\n"; 
-	for(int i=0; i<students; i++){
-		cout << "學生" << i+1 << " ";
-		for(int j=0; j<subjects; j++){
-			cout << scores[i*subjects + j] << "  ";
-		}
-		cout << endl;
-	}	
+  int students = 50;
+  int subjects = 5;
+  int scores[students][subjects];
+  for (int i = 0; i < students; i++) {
+    cout << "學生" << i + 1 << "\t";
+    for (int j = 0; j < subjects; j++) {
+      scores[i][j] = random() % (100 - 50 + 1) + 50;
+      cout << scores[i][j] << "\t";
+    }
+    cout << endl;
+  }
 }

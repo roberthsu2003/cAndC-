bfe13774bc791f246898e701853aa6d0d71da4a8
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 16:40:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,12 @@
 #include <iostream>
 using namespace std;
-void change(int n[], int e) {
-  cout << "n的記憶體位址:" << n << endl;
-  for (int i = 0; i < e; i++) {
-    n[i] *= 10;
-  }
+
+void functionA(int* n){
+	*n *= 10;
 }
 
 int main() {
-  //陣列變數
-  int nums[] = {10, 20, 30, 40, 50, 60, 70, 80};
-  int elements = sizeof(nums) / sizeof(nums[0]);
-  cout << "nums的記憶體位址" << nums << endl; // 陣列變數儲存的是記憶體位置
-  change(nums, elements);
-
-  for (int i = 0; i < elements; i++) {
-    cout << nums[i] << endl;
-  }
+	int a = 10;
+	functionA(&a);
+	cout << "a的內容是" << a << endl;
 }

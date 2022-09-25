7b2e7fcebbc5a20863beb4478ff2dab5df24a297
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 14:31:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int main() {
-
+  srandom(time(NULL));
   int lot[49];
 
   for (int i = 0; i < 49; i++) {
     lot[i] = i + 1;
-    cout << lot[i] << " ";
   }
   int num = 7;
   int choose[num];
   int maxIndex = 48;
+  for (int i = 0; i < num; i++) {
+    int randIndex = random() % (maxIndex + 1);
+    choose[i] = lot[randIndex];
+    lot[randIndex] = lot[maxIndex];
+    maxIndex--;
+  }
+
+  for (int i = 0; i < num-1; i++) {
+    cout << choose[i] << " ";
+  }
+  cout << endl;
+	cout << "特別號:" << choose[num-1] << endl;
 }

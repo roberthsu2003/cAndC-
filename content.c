e57e01d023b49cb2f58f9fc6b3b5a26d82121c07
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 10:13:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	printf("Hello! World!\n");
-	std::cout << "這是c++專案" << std::endl;
+  printf("Hello! World!\n");
+	cout << "這是c++專案" << endl;
 	
 }

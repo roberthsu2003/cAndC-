dbec7d600e19b040954658b0dc547fa0d82ba080
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 12:09:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,6 +9,35 @@ void print_array(int n[], int elements) {
   cout << endl;
 }
 
+void sort_to_big(int n[], int elements) {
+  for (int i = 0; i < elements - 1; i++) {
+    for (int j = i + 1; j < elements; j++) {
+      //前面的比較大,要2數交換
+      if (n[i] > n[j]) {
+        int temp;
+        temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
+      }
+    }
+  }
+}
+
+void sort_to_small(int n[], int elements) {
+  for (int i = 0; i < elements - 1; i++) {
+    for (int j = i + 1; j < elements; j++) {
+      //前面的比較大,要2數交換
+      if (n[i] < n[j]) {
+        int temp;
+        temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
+      }
+    }
+  }
+}
+
+
 int main() {
   srandom(time(NULL));
   int elements;
@@ -19,5 +48,14 @@ int main() {
     arr[i] = random() % (100 - 1 + 1) + 1;
   }
   cout << "排序前的內容:" << endl;
+  print_array(arr, elements);  
+ 
+
+  cout << "由小到大排序:" << endl;
+	sort_to_big(arr, elements);
   print_array(arr, elements);
+
+	cout << "由大到小的排序:" << endl;
+	sort_to_small(arr, elements);
+	print_array(arr, elements);
 }

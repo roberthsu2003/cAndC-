23da921292c00ad573f5fcda10784dd9d68a19cd
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 11:09:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -8,6 +8,14 @@ int main() {
   int arr[elements];
   for (int i = 0; i < elements; i++) {
     arr[i] = random() % (50 - 1 + 1) + 1;
-    cout << arr[i] << " ";
   }
+
+  int search_value = 3;
+  int repeation = 0;
+  for (int i = 0; i < elements; i++) {
+    if (arr[i] == search_value) {
+      repeation += 1;
+    }
+  }
+  cout << search_value << "的數量有" << repeation << endl;
 }

ac4ece71f786585ab2dbc37f5443b50ca9036678
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 14:14:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-  int nums[] = {10, 20, 30, 40, 50};
-  cout << "nums全部使用記憶體大小是:" << sizeof(nums) << endl;
-  cout << "元素0的記憶體大小是:" << sizeof(nums[0]) << endl;
-  cout << "nums的元素數量是:" << sizeof(nums) / sizeof(nums[0]) << endl;
 
-  float f1 = 34.5;
-  cout << "f1的變數佔的記憶體大小是:" << sizeof(f1) << endl;
+  int lot[49];
 
-  double d1 = 34.5;
-  cout << "d1的變數佔的記憶體大小是:" << sizeof(d1) << endl;
-
-  char c1 = 'a';
-  cout << "c1的變數佔的記憶體大小是:" << sizeof(c1) << endl;
-
-  short s1 = 45;
-  cout << "s1的變數佔的記憶體大小是:" << sizeof(s1) << endl;
-
-  int i1 = 45;
-  cout << "i1的變數佔的記憶體大小是:" << sizeof(i1) << endl;
-
-  long long int ll1 = 45;
-  cout << "ll1的變數佔的記憶體大小是:" << sizeof(ll1) << endl;
+  for (int i = 0; i < 49; i++) {
+    lot[i] = i + 1;
+    cout << lot[i] << " ";
+  }
+  int num = 7;
+  int choose[num];
+  int maxIndex = 48;
 }

6317d810032f41653b93f3a8c24764c6c14a3c8b
徐國堂 <roberthsu2003@gmail.com>
2022-09-25 12:26:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,61 +1,26 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
-
-void print_array(int n[], int elements) {
-  for (int i = 0; i < elements; i++) {
-    cout << n[i] << " ";
-  }
-  cout << endl;
-}
-
-void sort_to_big(int n[], int elements) {
-  for (int i = 0; i < elements - 1; i++) {
-    for (int j = i + 1; j < elements; j++) {
-      //前面的比較大,要2數交換
-      if (n[i] > n[j]) {
-        int temp;
-        temp = n[i];
-        n[i] = n[j];
-        n[j] = temp;
-      }
-    }
-  }
-}
-
-void sort_to_small(int n[], int elements) {
-  for (int i = 0; i < elements - 1; i++) {
-    for (int j = i + 1; j < elements; j++) {
-      //前面的比較大,要2數交換
-      if (n[i] < n[j]) {
-        int temp;
-        temp = n[i];
-        n[i] = n[j];
-        n[j] = temp;
-      }
-    }
-  }
-}
-
-
 int main() {
-  srandom(time(NULL));
-  int elements;
-  cout << "請輸入陣列元素的數量:";
-  cin >> elements;
-  int arr[elements];
-  for (int i = 0; i < elements; i++) {
-    arr[i] = random() % (100 - 1 + 1) + 1;
-  }
-  cout << "排序前的內容:" << endl;
-  print_array(arr, elements);  
- 
+  int nums[] = {10, 20, 30, 40, 50};
+  cout << "nums全部使用記憶體大小是:" << sizeof(nums) << endl;
+  cout << "元素0的記憶體大小是:" << sizeof(nums[0]) << endl;
+  cout << "nums的元素數量是:" << sizeof(nums) / sizeof(nums[0]) << endl;
 
-  cout << "由小到大排序:" << endl;
-	sort_to_big(arr, elements);
-  print_array(arr, elements);
+  float f1 = 34.5;
+  cout << "f1的變數佔的記憶體大小是:" << sizeof(f1) << endl;
 
-	cout << "由大到小的排序:" << endl;
-	sort_to_small(arr, elements);
-	print_array(arr, elements);
+  double d1 = 34.5;
+  cout << "d1的變數佔的記憶體大小是:" << sizeof(d1) << endl;
+
+  char c1 = 'a';
+  cout << "c1的變數佔的記憶體大小是:" << sizeof(c1) << endl;
+
+  short s1 = 45;
+  cout << "s1的變數佔的記憶體大小是:" << sizeof(s1) << endl;
+
+  int i1 = 45;
+  cout << "i1的變數佔的記憶體大小是:" << sizeof(i1) << endl;
+
+  long long int ll1 = 45;
+  cout << "ll1的變數佔的記憶體大小是:" << sizeof(ll1) << endl;
 }

e054b38387dbbf37d904bea81940ce3e2714fd11
徐國堂 <roberthsu2003@gmail.com>
2022-10-02 14:36:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,39 +1,12 @@
-main.cpp
-
-//巢狀結構
-#include <iostream>
-#include "data.h"
-
 using namespace std;
 
-int main() {
-  Rect r1;
-  r1.p = {20.5, 10.5};
-  r1.s = {10.0, 20.1};
-  cout << "r1的x座標:" << r1.p.x << endl;
-  cout << "r1的y座標:" << r1.p.y << endl;
-  cout << "r1的寬:" << r1.s.width << endl;
-  cout << "r1的高:" << r1.s.height << endl;
+typedef struct student {
+  string name;
+  int chinese;
+  int english;
+  int math;
+} Student;
+
+int main() { 
+	Student students[50]; 
 }
-
-
-
-data.h
-
-typedef struct point {
-  float x;
-  float y;
-} Point;
-
-typedef struct size {
-  float width;
-  float height;
-} Size;
-
-typedef struct rect {
-  Point p;
-  Size s;
-} Rect;
-
-
-

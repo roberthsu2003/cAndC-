9a906b40947da066216125d0ec40cf9dbbcfdf32
徐國堂 <roberthsu2003@gmail.com>
2022-10-02 15:20:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -22,6 +22,10 @@ int sum(Student s) {
 	return s.chinese + s.english + s.math; 
 }
 
+float average(Student s){
+	return 0.0;
+}
+
 int main() {
   srandom(time(NULL));
   Student students[50];
@@ -31,13 +35,14 @@ int main() {
   }
 
   //輸出學生資料
-  cout << "姓名\t國文\t英文\t數學\t總分\n";
+  cout << "姓名\t國文\t英文\t數學\t總分\t平均\n";
   for (int i = 0; i < 50; i++) {
     Student s = students[i];
     cout << s.name << "\t";
     cout << s.chinese << "\t\t";
     cout << s.english << "\t\t";
     cout << s.math << "\t\t";
-    cout << sum(s) << endl;
+    cout << sum(s) << "\t\t";
+		cout << average(s) << endl;
   }
 }

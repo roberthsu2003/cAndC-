ea3c2af383cf020c2bb7a99e4cd36a90c3797f1d
徐國堂 <roberthsu2003@gmail.com>
2022-10-02 10:13:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
-int add(int x, int y){ // call value
-	int sum = x + y;
-	return sum;
+int add(int x, int y) { // call value
+  int sum = x + y;
+  return sum;
+}
+
+void add1(int x, int y, int *sum) { 
+	*sum = x + y; 
 }
 
 int main() {
-	int n = 10; //區域變數n
-	int m = 20;
-	int total = add(n, m);
-	cout << "total=" << total << endl;
-	return 0;
+  int n = 10; //區域變數n
+  int m = 20;
+  int sum;
+  int total = add(n, m);
+  add1(n, m, &sum);
+
+  cout << "total=" << total << endl;
+  cout << "sum=" << sum << endl;
+  return 0;
 }

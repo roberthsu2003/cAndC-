5d735aa0a03ec6fcb0cb35d7a04da84ee66a5793
徐國堂 <roberthsu2003@gmail.com>
2022-10-02 16:26:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,6 +6,27 @@ typedef enum direction {
 } Direction;
 
 int main() {
-  Direction destination = West;
+  Direction destination = North;
   cout << "目標=" << destination << endl;
+	switch (destination){
+		case North:
+			cout << "向北" << endl;
+			break;
+
+		case South:
+			cout << "向南" << endl;
+			break;
+
+		case East:
+			cout << "向東" << endl;
+			break;
+
+		case West:
+			cout << "向西" << endl;
+			break;
+
+		default:
+			break;
+		 
+	}
 }

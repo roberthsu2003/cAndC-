459ae51e81f5d5143e11028b3316b35585d13504
徐國堂 <roberthsu2003@gmail.com>
2022-10-02 12:25:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,18 @@
-//結構struct
 #include <iostream>
 using namespace std;
 
-struct Rectangle{
-	int width;
-	int height;
-};
+typedef struct Point {
+  float x;
+  float y;
+} Point;
 
 int main() {
-	struct Rectangle rec;
-	rec.width = 10;
-	rec.height = 20;
+  //一次要給結構值
+  Point p1 = {3.5, 2.0};
+  cout << "p1的x是:" << p1.x << endl;
+  cout << "p1的y是:" << p1.y << endl;
 
-	cout << "矩形的寬是:" << rec.width << endl;
-	cout << "矩形的高是:" << rec.height << endl;
+	Point p2 = {3.8, 4.5};
+  cout << "p2的x是:" << p2.x << endl;
+  cout << "p2的y是:" << p2.y << endl;
 }

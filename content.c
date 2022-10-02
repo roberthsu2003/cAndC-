cee7caeaf22f9e08c963e9df04e8d103ada07434
徐國堂 <roberthsu2003@gmail.com>
2022-10-02 14:48:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <time.h>
 using namespace std;
 
 typedef struct student {
@@ -7,6 +9,21 @@ typedef struct student {
   int math;
 } Student;
 
-int main() { 
-	Student students[50]; 
+Student createStudent(int num){
+	Student s;
+	s.name = "學生" + to_string(num);
+	s.chinese = random() % (100-50+1) + 50;
+	s.english = random() % (100-50+1) + 50;
+	s.math = random() % (100-50+1) + 50;
+	return s;
+}
+
+int main() {
+	srandom(time(NULL));
+	Student students[50];
+	for(int i=0; i<50; i++){
+		students[i] = createStudent(i+1);
+	}
+	
+	
 }

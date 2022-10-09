db1457be8c6dd92d3ca7dc411152a07cd0d4edf4
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 10:40:26 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,42 @@
-main.c 
+main.cpp
 
-#include <stdio.h>
-#include "a1.h"
-#include "a1.h"
-#include "a1.h"
-#include "a1.h"
+#include "const.h"
+#include <iostream>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+using namespace std;
+int main() { 
+	cout << week1 << endl;
+	cout << week2 << endl;
+	cout << week3 << endl;
+	cout << week4 << endl;
+	cout << week5 << endl;
+	cout << week6 << endl;
+	cout << week7 << endl;
+
+	cout << "3+4=" << add(3,4) << endl;
 }
 
-a1.h
-#ifndef __A1_H__
-#define __A1_H__
+const.h
+
+//可以定義
+//#define
+//function
+//class
+//結構
+//例舉
+#ifndef __CONST_H__
+#define __CONST_H__
+
+#define week1 1
+#define week2 2
+#define week3 3
+#define week4 4
+#define week5 5
+#define week6 6
+#define week7 7
 
 int add(int a, int b){
-	return a+b;
+	return a + b;
 }
 
 #endif

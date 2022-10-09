3275c9602667cd7bf94165cbdf2f3ab75d08e75c
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 11:36:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,6 +10,7 @@ public:
 
 using namespace Human;
 int main() {
+	//一般變數
   Person p1;
   p1.name = "robert";
   p1.age = 30;
@@ -17,18 +18,12 @@ int main() {
   cout << "p1的name:" << p1.name << endl;
   cout << "p1的age:" << p1.age << endl;
 
-	Person p2;
-  p2.name = "alice";
-  p2.age = 23;
+	//指標變數
+	Person* p2 = new Person();
+	p2 -> name = "alice";
+	p2 -> age = 23;
 
-  cout << "p2的name:" << p2.name << endl;
-  cout << "p2的age:" << p2.age << endl;
-
-	Person p3 = p2;
-	p3.name = "jenny";
-  p3.age = 34;
+	cout << "p2的name:" << p2->name << endl;
+  cout << "p2的age:" << p2->age << endl;
 	
-	
-	cout << "p3的name:" << p3.name << endl;
-  cout << "p3的age:" << p3.age << endl;
 }

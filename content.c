92263f9b2f398050b5f9777698ccc1bc22b76fc9
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 14:30:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,18 +2,13 @@
 #include <vector>
 
 using namespace std;
-
 int main() {
 	vector<int> list(3);
-	list[0] = 5;
-	list[1] = -3;
-	list[2] = 12;
-
-	for(int i=0; i<3; i++){
-		cout << list[i] << endl;
+	for(int &elem :list){
+		elem = 10;
 	}
-	cout << "=================" << endl;
-	for (int elem :list){
+
+	for(int elem:list){
 		cout << elem << endl;
-	}	
+	}
 }

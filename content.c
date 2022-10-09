8680bc661a434cfa7b31af7b0c5ce45ab0259eeb
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 09:40:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,11 @@
 #include <iostream>
-//marco巨集
-#define RATE(x) (x) * 34.54   
+#define RATE 34.5
 
 using namespace std;
-
 int main() {
-  double us, nt;
-  cout << "請輸入您有多少美金:";
-  cin >> us;
-  nt = RATE(us+1);
-  cout << "您有" << nt << "台幣" << endl;
+	double us, nt;
+	cout << "請輸入您有多少美金:";
+	cin >> us;
+	nt = us * RATE;
+	cout << "您有" << nt << "台幣" << endl;
 }

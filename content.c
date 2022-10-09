1ad913a452ddf8e6e9384034df43fca0861d4d8b
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 16:33:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,52 +1,7 @@
-main.cpp
-
 #include "size.h"
 #include <iostream>
 
-//實體配合function的使用,使用call by reference
-void addTwo(Size &s) {
-  s.x += 2;
-  s.y += 2;
-}
-using namespace std;
+
 int main() {
-  Size tri1(10, 20);
-  Size tri2(25, 30);
-  addTwo(tri1);
-  addTwo(tri2);
-  cout << "tri1-" << tri1.x << "-" << tri1.y << endl;
-  cout << "tri1的面積是" << tri1.area() << endl;
-	cout << "tri1的斜邊是" << tri1.edge() << endl;
-  cout << "tri2-" << tri2.x << "-" << tri2.y << endl;
-  cout << "tri2的面積是" << tri2.area() << endl;
-	cout << "tri2的斜邊是" << tri2.edge() << endl;
+  std::cout << "Hello World!\n";
 }
-
-size.h
-
-#ifndef __SIZE_H__
-#define __SIZE_H__
-
-#include <math.h>
-
-class Size {
-public:
-  unsigned int x;
-  unsigned int y;
-  Size(int n, int m) {
-    x = n;
-    y = m;
-  }
-
-	//實體方法
-  double area() {
-		return x * y / 2;
-	}
-
-	//實體方法
-	double edge(){
-		return sqrt(pow(x,2) + pow(y,2));
-	}
-};
-
-#endif

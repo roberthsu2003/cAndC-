ee7ccdfeabbbb84690577b0f7a4921008007a603
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 15:53:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,17 @@
-main.cpp
-
 #include "size.h"
 #include <iostream>
 
+//實體配合function的使用,使用call by reference
+void addTwo(Size &s){
+	s.x += 2;
+	s.y += 2;
+}
+using namespace std;
 int main() {
   Size tri1(10, 20);
   Size tri2(25, 30);
+	addTwo(tri1);
+	addTwo(tri2);
+	cout << "tri1-" << tri1.x << "-" << tri1.y << endl;
+	cout << "tri2-" << tri2.x << "-" << tri2.y << endl;
 }
-
-
-size.h
-#ifndef __SIZE_H__
-#define __SIZE_H__
-
-class Size{
-	public:
-	unsigned int x;
-	unsigned int y;
-	Size(int n, int m){
-		x = n;
-		y = m;
-	}
-};
-
-#endif

dfe4565ba65799d583432c1616749be3b4584b3a
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 14:11:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,43 +1,15 @@
-main.cpp
-
 #include <iostream>
-#include "bmi.h"
+#include <vector>
+
 using namespace std;
 
 int main() {
-	BMI robert;
-	robert.name = "徐國堂";
-	robert.height = 178.0;
-	robert.weight = 78;
-	cout << robert.name << "的bmi是" << robert.bmi() << endl;
+	vector<int> list(3);
+	list[0] = 5;
+	list[1] = -3;
+	list[2] = 12;
 
-	BMI jenny;
-	jenny.name = "王慧質";
-	jenny.height = 160;
-	jenny.weight = 51;
-	cout << jenny.name << "的bmi是" << jenny.bmi() << endl;
-}
-
-
-bmi.h
-
-#ifndef __BMI_H__
-#define __BMI_H__
-
-#include <math.h>
-#include <iostream>
-using namespace std;
-
-class BMI{
-	public:
-	string name;
-	float height;
-	float weight;
-	//實體的方法
-	float bmi(){
-		return weight / pow((height / 100),2);
+	for(int i=0; i<3; i++){
+		cout << list[i] << endl;
 	}
-};
-
-
-#endif
+}

a142b96a75a7d1c86c66f76e6c425907b0699130
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 11:26:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,34 @@
 #include <iostream>
 using namespace std;
-namespace Human{
-	class Person{
-		public:
-			string name;
-			int age;
-	};
-}
+namespace Human {
+class Person {
+public:
+  string name;
+  int age;
+};
+} // namespace Human
 
 using namespace Human;
 int main() {
-	Person p1;
-	p1.name = "robert";
-	p1.age = 30;
+  Person p1;
+  p1.name = "robert";
+  p1.age = 30;
 
-	cout << p1.name << endl;
-	cout << p1.age << endl;
+  cout << "p1的name:" << p1.name << endl;
+  cout << "p1的age:" << p1.age << endl;
+
+	Person p2;
+  p2.name = "alice";
+  p2.age = 23;
+
+  cout << "p2的name:" << p2.name << endl;
+  cout << "p2的age:" << p2.age << endl;
+
+	Person p3 = p2;
+	p3.name = "jenny";
+  p3.age = 34;
 	
+	
+	cout << "p3的name:" << p3.name << endl;
+  cout << "p3的age:" << p3.age << endl;
 }

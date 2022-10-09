7ddc6005506085241277e3327fe6ebc2ae217fb6
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 12:08:36 +0800

    Update content.c

--- content.c
+++ content.c
@@ -10,16 +10,16 @@ public:
 
 using namespace Human;
 
-// call by reference
-void addData(Person &person, string name, int age) {
-  person.name = name;
-  person.age = age;
+// call by address
+void addData(Person *person, string name, int age) {
+  person->name = name;
+  person->age = age;
 }
 
 int main() {
-  //一般變數
-  Person p1;
+  //指標變數
+  Person *p1 = new Person();
   addData(p1, "robert", 30);
-  cout << "p1的name:" << p1.name << endl;
-  cout << "p1的age:" << p1.age << endl;
+  cout << "p1的name:" << p1->name << endl;
+  cout << "p1的age:" << p1->age << endl;
 }

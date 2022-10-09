13fcaa1555a12e592371181d8f4d59e92f035307
徐國堂 <roberthsu2003@gmail.com>
2022-10-09 14:44:18 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,13 +2,26 @@
 #include <vector>
 
 using namespace std;
+
 int main() {
-	vector<int> list(3);
-	for(int &elem :list){
-		elem = 10;
+	vector<int> list;
+	for(int i=1; i<=10; i++){
+		list.push_back(i);
 	}
 
-	for(int elem:list){
+	for(int elem : list){
 		cout << elem << endl;
 	}
+
+	cout << "==============" << endl;
+	list.pop_back();
+	list.pop_back();
+	
+	for(int elem : list){
+		cout << elem << endl;
+	}
+	cout << "index 0:" << list.operator[](0) << endl;
+	cout << "index 0:" << list[0] << endl;
+
+	cout << "size:" << list.size() << endl;
 }

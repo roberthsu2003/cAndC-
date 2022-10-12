e46cc50da932163d282b0b951682c4b70bf9bf4b
徐國堂 <roberthsu2003@gmail.com>
2022-10-12 19:25:18 +0800

    lesson1_1
    介紹c++的cout

--- content.c
+++ content.c
@@ -1,55 +1,6 @@
-main.cpp
-
-#include "size.h"
 #include <iostream>
 
-using namespace std;
 int main() {
-	Size tri1(10, 20);
-	cout << "tri1面積:" << tri1.area() << endl;
-	cout << "tri1斜邊:" << tri1.edge() << endl;
+	printf("Hello! C語言\n");
+	std::cout << "Hello! C++語言" << std::endl;
 }
-
-size.h
-
-#ifndef __SIZE_H__
-#define __SIZE_H__
-
-#include <math.h>
-
-class Size {
-public:
-  unsigned int x;
-  unsigned int y;
-  Size(int n, int m);
-
-	//實體方法
-  double area(); //傳出面積
-
-	//實體方法
-	double edge(); //傳出斜邊
-		
-
-};
-
-#endif
-
-
-size.cpp
-
-#include "size.h"
-
-Size::Size(int n, int m) {
-    x = n;
-    y = m;
-}
-
-double Size::area() {
-		return x * y / 2;
-		//1000行
-}
-
-double Size::edge(){ //傳出斜邊
-		//1000行
-		return sqrt(pow(x,2) + pow(y,2));
-	}

b2f9ad40ced2f676453213142d9b027318dcba34
徐國堂 <roberthsu2003@gmail.com>
2023-02-14 16:13:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int isPlay;
-    do{
+    while(true){
 			
 			System.out.println("請輸入欲選購的電影票種類:");
 			System.out.print("1.全票(300元) 2.優待票(270元) 3.早場票(240)：");
@@ -40,7 +40,7 @@ class Main {
 			isPlay = sc.nextInt();
 			if(isPlay == 2) break;
 				
-		}while(true);
+		}
 		System.out.println("程式結束");
   }
 }

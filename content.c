9e3b18e249c9a13099edf97f0e1231328130b020
徐國堂 <roberthsu2003@gmail.com>
2023-07-18 15:11:02 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,46 +1,33 @@
-import java.util.Scanner;
+#include <iostream>
+#include <time.h>
+using namespace std;
 
-class Main {
-  public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int isPlay;
-    while(true){
+int main() {
+	srandom(time(NULL));
+	int min = 1;
+	int max = 10;
+	int keyin;
+	int count = 0;
+	int target = random() % (max-min+1) + min;
+	cout << "==========猜數字遊戲============\n\n";
+	while(true){
+		cout << "猜數字範圍" << min << "~" << max << ":";
+		cin >> keyin;
+		count++;
+		if(keyin >= min && keyin <= max){
+			if(keyin == target){
+				cout << "賓果!猜對了,答案是" << target << endl;
+				cout << "您一共猜了" << count << "次" << endl;
+				break;
+			}else{
+				cout << "猜錯了" << endl;
+				cout << "您已經猜了" << count << "次" << endl;
+			}
 			
-			System.out.println("請輸入欲選購的電影票種類:");
-			System.out.print("1.全票(300元) 2.優待票(270元) 3.早場票(240)：");
-			int ticket = sc.nextInt();
-			int price=0;
-			boolean isError = true;
-			switch(ticket){
-				case 1:
-					isError = false;
-					price = 300;
-					break;
-				case 2:
-					isError = false;
-					price = 270;
-					break;
-				case 3:
-					isError = false;
-					price = 240;
-					break;
-			}
-			if(isError){
-				System.out.println("輸入錯誤");
-				break;
-			}
-			System.out.print("請輸入預購張數：");
-			int num = sc.nextInt();
-			if(num > 10){
-				System.out.println("超過10張");
-				break;
-			}
-			System.out.println("總價" + (num * price));			
-			System.out.print("還要繼續運算嗎?1.繼續2.結束");
-			isPlay = sc.nextInt();
-			if(isPlay == 2) break;
-				
+		}else{
+			cout << "不正確的範圍" << endl;
+			continue;
 		}
-		System.out.println("程式結束");
-  }
+	}
+	cout << "Game Over!\n";
 }

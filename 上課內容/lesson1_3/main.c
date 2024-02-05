6651e594025305bb54077121b2e50019fa6b7870
徐國堂 <roberthsu2003@gmail.com>
2024-02-05 21:22:18 +0800

    Create main.c

---
+++ 上課內容/lesson1_3/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("2數相加為%d\n",5 + 10);
+	printf("%d + %d = %d\n", 5, 10, 5 + 10);
+	printf("%.1lf + %.1lf = %.1lf\n", 5.0, 10.0, 5.0 + 10.0);
+	printf("%d + %.1lf = %.1lf\n", 5, 10.0, 5 + 10.0);
+  return 0;
+}

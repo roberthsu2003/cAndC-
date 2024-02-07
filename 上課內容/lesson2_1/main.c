e0e4d16f0a743c9f408de7c0a8f91ed6d5100f19
徐國堂 <roberthsu2003@gmail.com>
2024-02-07 19:23:51 +0800

    Create main.c

---
+++ 上課內容/lesson2_1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+	n = 10;
+
+	double m = 10.0;
+
+	double x = n + m;
+
+	printf("n=%d\n",n);
+	printf("m=%.2lf\n",m);
+	printf("2數相加為%.2lf\n",x);
+	
+  return 0;
+}

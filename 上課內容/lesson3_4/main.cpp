291cc950f010baf772387a3f12ec477f10887b50
徐國堂 <roberthsu2003@gmail.com>
2024-02-16 20:34:05 +0800

    Create main.cpp

---
+++ 上課內容/lesson3_4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 0;
+	//單獨使用遞增
+	n++;
+	cout << "n=" << n << endl;
+	++n;
+	cout << "n=" << n << endl;
+	n--;
+	cout << "n=" << n << endl;
+	--n;
+	cout << "n=" << n << endl;
+	
+}

c4a0db16efc735442c10118d9497d9f2e253d49d
徐國堂 <roberthsu2003@gmail.com>
2024-02-16 19:36:30 +0800

    Create main.cpp

---
+++ 上課內容/lesson3_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "請輸入姓名:";
+	cin >> name;
+	cout << "您輸入的姓名是:" << name << endl;
+	printf("身高是:%d\n",172);
+	printf("BMI:%lf\n",25.17895);
+}

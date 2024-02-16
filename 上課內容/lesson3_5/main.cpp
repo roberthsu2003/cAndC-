05245e2b9e8452aea03a3ae2fa012e84a2e224f8
徐國堂 <roberthsu2003@gmail.com>
2024-02-16 20:58:12 +0800

    Create main.cpp

---
+++ 上課內容/lesson3_5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 1;
+	int y = ++x; //前置遞增
+	cout << "x=" << x << endl;
+	cout << "y=" << y << endl;
+
+	int m = 1;
+	int n = m++; //後置遞增
+	cout << "m=" << m << endl;
+	cout << "n=" << n << endl;
+}

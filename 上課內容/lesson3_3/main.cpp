cb217424bdb77bcdd14435ffa6c5bc39ebc31268
徐國堂 <roberthsu2003@gmail.com>
2024-02-16 20:19:41 +0800

    Create main.cpp

---
+++ 上課內容/lesson3_3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//讓使用者輸入被除數(整數)及除數(整數，不可以是零)，程式會顯示兩數相除的商及餘數。
+
+int main() {
+	int n,m;
+	cout << "請輸入被除數(整數):";
+	cin >> n;
+
+	cout << "請輸入除數(整數,不可以為0):";
+	cin >> m;
+
+	cout << "商:" << n/m;
+	cout << "餘數:" << n%m << endl;
+}

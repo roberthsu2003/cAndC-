a0b7e3ea8f69c1cea2d61e5ace61535ee51b38af
徐國堂 <roberthsu2003@gmail.com>
2024-02-19 19:41:40 +0800

    Create main.cpp

---
+++ 上課內容/lesson4_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//學生符合加分條件就加5%分,如果超過300分,就以300為主。
+
+int main() {
+  int score;
+  bool add;
+  cout << "請輸入學生總分:";
+  cin >> score;
+  cout << "學生符合加分條件嗎? yes請輸入1,no請輸入0:";
+  cin >> add;
+  if (add) { //單項選擇
+    score *= 1.05;
+  }
+
+  cout << "學生分數是" << score << endl;
+}

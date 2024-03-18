cc9207b57be7c2a9775d637de96b893e85e374e8
徐國堂 <roberthsu2003@gmail.com>
2024-03-18 20:38:31 +0800

    Create main.cpp

---
+++ 上課內容/lesson1_2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//配置記憶體給陣列變數n
+  int n[3];
+  //儲存元素值
+  n[0] = 10;
+  n[1] = 20;
+  n[2] = 30;
+
+  //取出元素值
+  cout << "第1個元素:" << n[0] << endl;
+  cout << "第2個元素:" << n[1] << endl;
+  cout << "第3個元素:" << n[2] << endl;
+}

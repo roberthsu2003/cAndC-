51d9df97e7d02703009970a679a3c15dea81fbea
徐國堂 <roberthsu2003@gmail.com>
2024-03-22 20:51:19 +0800

    Update main.cpp

--- 上課內容/lesson3_3/main.cpp
+++ 上課內容/lesson3_3/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void change_n(int n_value){
-	n_value += 10;
+void change_n(int* ptr_n){ //宣告指標變數
+	cout << "在ptr_n內的值是:"<< ptr_n << endl; 
+	cout << "在main function內的變數n的值是:" << *ptr_n << endl; //*是取值運算子
+	*ptr_n = *ptr_n + 10;
 }
 
 int main() {
 	int n = 5;
 	//n內容+10
-	change_n(n);
+	cout << "n的記憶體位址:" << &n << endl;
+	change_n(&n);
 	cout << "現在n的內容是:" << n << endl;
 }

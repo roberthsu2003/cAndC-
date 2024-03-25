86e97cc03ea1bd26cc6c2fb643fddd0265fada9c
徐國堂 <roberthsu2003@gmail.com>
2024-03-25 21:53:52 +0800

    Update main.cpp

--- 上課內容/lesson4_6/main.cpp
+++ 上課內容/lesson4_6/main.cpp
@@ -1,16 +1,21 @@
+#include <iostream>
 using namespace std;
 class Person {
 public:
   string name;
-	Person(string n){ //自訂的建構式
+	double us;
+	Person(string n,double money){ //自訂的建構式
 		name = n;
+		us = money;
 	}
 };
 
 int main() {
-  Person p1("徐國堂");
+  Person p1("徐國堂",10);
   cout << p1.name << endl;
+	cout << p1.us << endl;
 
-  Person p2("張xx");
+  Person p2("張xx",20);
   cout << p2.name << endl;
+	cout << p2.us << endl;
 }

611a9fe64ac3077f63b32b8ba448abf6d8273605
徐國堂 <roberthsu2003@gmail.com>
2024-03-25 19:31:20 +0800

    Update main.cpp

--- 上課內容/lesson4_0/main.cpp
+++ 上課內容/lesson4_0/main.cpp
@@ -15,17 +15,17 @@ void change_value(int* array){
 }
 */
 
-void change_value(int *array) {
-  *(array + 0) *= 10;
-  *(array + 1) *= 10;
-  *(array + 2) *= 10;
+void change_value(int *array, int n) {
+  for (int i = 0; i < n; i++) {
+    *(array + i) *= 10;
+  }
 }
 
 int main() {
   int nums[] = {10, 20, 30};
-  change_value(nums);
-
-  for (int i = 0; i < 3; i++) {
+  int count = sizeof(nums) / sizeof(nums[0]);
+  change_value(nums, count);
+  for (int i = 0; i < count; i++) {
     cout << nums[i] << endl;
   }
 }

88e9d164d9c75af3cc6347c10fb8ef699831625f
徐國堂 <roberthsu2003@gmail.com>
2024-03-27 20:56:13 +0800

    Update main.cpp

--- 上課內容/lesson5_1/main.cpp
+++ 上課內容/lesson5_1/main.cpp
@@ -21,10 +21,27 @@ public:
     cout << "姓名:" << name << endl;
     cout << "身高:" << height << "cm" << endl;
     cout << "體重:" << weight << "kg" << endl;
-		cout << "BMI:" << bmi() << endl;
+    cout << "BMI:" << bmi() << endl;
+		cout << "您的狀況:" << status(bmi()) << endl;
   }
 
   double bmi() { return weight / pow(height / 100.0, 2); }
+
+	string status(double bmi){
+		if(bmi < 18.5){
+			return "體重過輕";
+		}else if(bmi < 24){
+			return "正常範圍";
+		}else if(bmi < 27){
+			return "過重";
+		}else if(bmi < 30){
+			return "輕度肥胖";
+		}else if(bmi < 35){
+			return "中度肥胖";
+		}else{
+			return "重度肥胖";
+		}
+	}
 };
 
 int main() {
@@ -33,10 +50,8 @@ int main() {
   p1.height = 183;
   p1.weight = 81;
   p1.profile();
-  
 
   cout << "===================\n";
   People p2("張xx", 69, 170);
   p2.profile();
-  
 }

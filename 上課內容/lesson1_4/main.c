07bb83e8b4035ae538076f7a1bda14b2a5ff722f
徐國堂 <roberthsu2003@gmail.com>
2024-10-21 21:28:31 +0800

    Update main.c

--- 上課內容/lesson1_4/main.c
+++ 上課內容/lesson1_4/main.c
@@ -2,7 +2,9 @@
 
 int main(void) {
   //如何使用printf()輸出數值
-  printf("這是整數:%d\n",15);
-  printf("這是有小數點的數值(double):%lf\n",9.9999);
+  printf("這是整數:%d\n", 15);
+  printf("這是有小數點的數值(double):%lf\n", 9.9999);
+  printf("這是加法運算,5+5=%d\n", 5 + 5);
+  printf("這是加法運算,5.0+10.0:%.2lf\n", 5.0 + 10.0);
   return 0;
 }

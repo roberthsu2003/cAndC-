9e9250025975b5e0a44675542fe9158fedb4ac9b
徐國堂 <roberthsu2003@gmail.com>
2024-10-23 20:18:30 +0800

    Create main.c

---
+++ 上課內容/lesson2_3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int top = 2;
+  printf("變數top的值:%d\n", top);
+  printf("變數top的記憶體位址:%p\n", &top);
+
+  int bottom = 3;
+  printf("變數bottom的值:%d\n", bottom);
+  printf("變數bottom的記憶體位址:%p\n", &bottom);
+
+  int height = 3;
+  printf("變數height的值:%d\n", height);
+  printf("變數height的記憶體位址:%p\n", &height);
+  return 0;
+}

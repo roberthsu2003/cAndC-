b223c166354643b7155941e1f9dfe298a6a79f40
徐國堂 <roberthsu2003@gmail.com>
2024-10-23 20:33:25 +0800

    Update main.c

--- 上課內容/lesson2_3/main.c
+++ 上課內容/lesson2_3/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  int top = 2;
+  int top;
+  printf("請輸入top的值:");
+  scanf("%d",&top);
   printf("變數top的值:%d\n", top);
   printf("變數top的記憶體位址:%p\n", &top);
 
-  int bottom = 3;
+  int bottom;
+  printf("請輸入bottom的值:");
+  scanf("%d",&bottom);
   printf("變數bottom的值:%d\n", bottom);
   printf("變數bottom的記憶體位址:%p\n", &bottom);
 
-  int height = 3;
+  int height;
+  printf("請輸入height的值:");
+  scanf("%d",&height);
   printf("變數height的值:%d\n", height);
   printf("變數height的記憶體位址:%p\n", &height);
   return 0;

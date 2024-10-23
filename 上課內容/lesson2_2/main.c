cc4cf99651a6a971336b654194d9d851b91ae555
徐國堂 <roberthsu2003@gmail.com>
2024-10-23 19:52:02 +0800

    Create main.c

---
+++ 上課內容/lesson2_2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int top = 2;
+  int botton = 3;
+  int height = 3;
+  printf("上底:%d\n", top);
+  printf("下底:%d\n",botton);
+  printf("高:%d\n",height);  
+  return 0;
+}

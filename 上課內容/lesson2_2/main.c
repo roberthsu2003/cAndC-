5d99d19e905235b8cfb590312fe64f3b310f4ca5
徐國堂 <roberthsu2003@gmail.com>
2024-10-23 20:04:10 +0800

    Update main.c

--- 上課內容/lesson2_2/main.c
+++ 上課內容/lesson2_2/main.c
@@ -4,8 +4,11 @@ int main(void) {
   int top = 2;
   int botton = 3;
   int height = 3;
+  double area;
   printf("上底:%d\n", top);
-  printf("下底:%d\n",botton);
-  printf("高:%d\n",height);  
+  printf("下底:%d\n", botton);
+  printf("高:%d\n", height);
+  area = (top + botton) * height / 2.0;
+  printf("面積是:%.2lf\n", area);
   return 0;
 }

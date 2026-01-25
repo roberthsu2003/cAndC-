ee95963f98de94014520fc8b561e2b26c35343db
徐國堂 <roberthsu2003@gmail.com>
2026-01-25 11:26:24 +0800

    Create main.cpp

---
+++ 上課內容/lesson1_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// 變數輸出
+
+using namespace std;
+
+int main() { 
+  int score = 95;
+  string name = "張三";
+  double average = 87.5;
+  
+  cout << "學生姓名: " << name << endl;
+  cout << "考試名數: " << score << endl;
+  cout << "平均分數: " << average << endl;
+}

623a880e8e2427109dd967fb0d53a0f0ca6d754d
徐國堂 <roberthsu2003@gmail.com>
2026-01-25 16:11:45 +0800

    Update main.cpp

--- 上課內容/lesson2_3/main.cpp
+++ 上課內容/lesson2_3/main.cpp
@@ -7,19 +7,21 @@ int main() {
   srandom(time(NULL));
   int max = 100;
   int min = 50;
-  int n = 5;
-  int score = 0;
+  int n = 5; //科目數  
+  int m = 10; //學生人數
+  for (int j = 0; j < m; j++) {
+    int score = 0; //學生分數
+    int scores[n];
+    for (int i = 0; i < n; i++) {
+      scores[i] = random() % (max - min + 1) + min;
+    }
 
-  int scores[n];
-  for (int i = 0; i < n; i++) {
-    scores[i] = random() % (max - min + 1) + min;
+    cout << "學生" << j+1 << ":";
+    for (int i = 0; i < n; i++) {
+      score += scores[i];
+      cout << scores[i] << " ";
+    }
+    cout << "總分:" << score;
+    cout << " 平均:" << score / (double)n << endl;
   }
-
-  cout << "學生分數: ";
-  for (int i = 0; i < n; i++) {
-    score += scores[i];
-    cout << scores[i] << " ";
-  }
-  cout << "總分:" << score ;
-  cout << " 平均:" << score / (double)n << endl;
 }

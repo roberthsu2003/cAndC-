ae31b1de24e7039d7d22f52817be42176b7f44f1
徐國堂 <roberthsu2003@gmail.com>
2026-01-25 14:35:15 +0800

    Update main.cpp

--- 上課內容/lesson2_1/main.cpp
+++ 上課內容/lesson2_1/main.cpp
@@ -4,15 +4,21 @@ using namespace std;
 int main() {
   int scores[5];
   cout << "陣列大小是:" << sizeof(scores) << endl;
-  scores[0] = 78;
-  scores[1] = 96;
-  scores[2] = 89;
-  scores[3] = 90;
-  scores[4] = 63;
-
+  cout << "請輸入學生的5個分數(使用空格分隔):";
+  for(int i=0; i<=4; i++){
+    cin >> scores[i];
+  }
+  
+  /*
   cout << scores[0] << endl;
   cout << scores[1] << endl;
   cout << scores[2] << endl;
   cout << scores[3] << endl;
   cout << scores[4] << endl;
+  */
+
+  for(int i=0;i<5; i++){
+    cout << scores[i] << endl;
+  }
+  
 }

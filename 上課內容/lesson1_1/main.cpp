a42dc5d72af61d78cc95b2dea222c0493c5e4787
徐國堂 <roberthsu2003@gmail.com>
2026-01-25 10:56:32 +0800

    Update main.cpp

--- 上課內容/lesson1_1/main.cpp
+++ 上課內容/lesson1_1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() { 
-  std::cout << "Hello World!\n"; 
-  std::cout << "這是C++語言的第一節課\n";
+  std::cout << "Hello World!" << std::endl ; 
+  std::cout << "這是C++語言的第一節課" << std::endl;
   std::cout << "這個專案名稱為:lesson1_1\n";
 }

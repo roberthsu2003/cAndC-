ba5c50df12ecd85eb6893b058da6da2dfca1f192
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 10:09:13 +0800

    Refactor bubble sort implementation with dynamic array size

--- 上課內容/lesson3_1/main.cpp
+++ 上課內容/lesson3_1/main.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main() {
-  int array[4] = {48, 78, 24, 69};
+  int n = 4;
+  int array[] = {48, 78, 24, 69};
   //泡沫排序法
-  for(int i=0; i<4; i++){
+  for(int i=0; i<n-1; i++){
+    for(int j=i+1; j<n; j++){
+      cout << "i=" << i <<",j=" << j << endl;
+    }
+    cout << "==============\n";
+  }
+  for(int i=0; i<n; i++){
     cout << array[i] << endl;
   }
   return 0;

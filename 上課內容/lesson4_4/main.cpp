c33cafb0e758a78262a2854676ba357f245650a5
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 16:23:30 +0800

    Update main.cpp

--- 上課內容/lesson4_4/main.cpp
+++ 上課內容/lesson4_4/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int add(int a, int b){
-  int value = a + b;
+int* add(int a, int b){
+  int* value = new int(0); //動態變數
+  *value = a + b;
   return value;
 }
 
 int main() { 
   int a = 6789;
   int b = 9876;
-  int value = add(a, b);
+  int* value_ptr = add(a, b);
 
-  cout << "a+b=" << value << endl;
+  cout << "a+b=" << *value_ptr << endl;
+  delete value_ptr;
+  return 0;
 }

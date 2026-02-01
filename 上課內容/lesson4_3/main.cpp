d65c5b6c3017cdf368740621a975858c7a20c63b
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 15:10:22 +0800

    Update main.cpp

--- 上課內容/lesson4_3/main.cpp
+++ 上課內容/lesson4_3/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
+//建立指標變數
+//取址運算子 -> &一般變數名稱
+//取值運算子 -> *記憶體位址
+
+int main() {
   int n = 5;
   n = 10;
-  cout << "n=" << n << endl;
-  cout << "n的記憶體位址:" << &n << endl;
+  int *ptr_n = &n;
+
+  *ptr_n = 20; //取值運算子
+  cout << "n變數內的值" << n << endl;
+  cout << "n變數內的值" << *ptr_n << endl;   //取值運算子
+  cout << "n的記憶體位址:" << ptr_n << endl; //指標變數
 }

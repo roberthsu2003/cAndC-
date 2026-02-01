cce101dae6cfba4207c25e3c81ef89724b3c55b0
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 09:50:18 +0800

    Add main.cpp with bubble sort implementation

---
+++ 上課內容/lesson3_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int array[4] = {48, 78, 24, 69};
+  //泡沫排序法
+  for(int i=0; i<4; i++){
+    cout << array[i] << endl;
+  }
+  return 0;
+}

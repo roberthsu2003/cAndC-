832ddf20fc5c419245eccc04de285fa8ea4007df
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 11:29:11 +0800

    Update main.cpp

--- 上課內容/lesson3_3/main.cpp
+++ 上課內容/lesson3_3/main.cpp
@@ -11,15 +11,25 @@ int main() {
   //使用亂數1~99,建立元素值
   int min = 1;
   int max = 99;
-  for(int i=0; i<n; i++){
-    array[i] = random() % (max-min+1) + min;
+  for (int i = 0; i < n; i++) {
+    array[i] = random() % (max - min + 1) + min;
   }
+
+  int max_value = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (max_value < array[i]) {
+      max_value = array[i];
+    }
+  }
+
   cout << "元素數量:" << n << endl;
   for (int i = 0; i < n; i++) {
     cout << array[i] << " ";
   }
   cout << endl;
-  
+
+  cout << "最大值是:" << max_value << endl;
 
   return 0;
 }

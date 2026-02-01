770f0b05d457be3584e940348bd0e9579a027708
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 10:21:12 +0800

    Update main.cpp

--- 上課內容/lesson3_1/main.cpp
+++ 上課內容/lesson3_1/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+  /*
   int n = 4;
   int array[] = {48, 78, 24, 69};
   //泡沫排序法
@@ -14,5 +15,14 @@ int main() {
   for(int i=0; i<n; i++){
     cout << array[i] << endl;
   }
+  */
+  //2數對調
+  int n = 48;
+  int m = 78;
+  int temp;
+  temp = n;
+  n = m;
+  m = temp;
+  cout << "n=" << n << ",m=" << m << endl;
   return 0;
 }

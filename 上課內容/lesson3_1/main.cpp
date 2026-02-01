45c3507f3587279b589d5e7b50ad010ca7f8fdd3
徐國堂 <roberthsu2003@gmail.com>
2026-02-01 10:30:19 +0800

    Remove commented bubble sort and add variable swap

--- 上課內容/lesson3_1/main.cpp
+++ 上課內容/lesson3_1/main.cpp
@@ -2,27 +2,26 @@
 using namespace std;
 
 int main() {
-  /*
+  
   int n = 4;
   int array[] = {48, 78, 24, 69};
+  int temp;
   //泡沫排序法
   for(int i=0; i<n-1; i++){
     for(int j=i+1; j<n; j++){
-      cout << "i=" << i <<",j=" << j << endl;
+      //cout << "i=" << i <<",j=" << j << endl;
+      if(array[i] < array[j]){
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
     }
-    cout << "==============\n";
+    //cout << "==============\n";
   }
   for(int i=0; i<n; i++){
     cout << array[i] << endl;
   }
-  */
-  //2數對調
-  int n = 48;
-  int m = 78;
-  int temp;
-  temp = n;
-  n = m;
-  m = temp;
-  cout << "n=" << n << ",m=" << m << endl;
+  
+  
   return 0;
 }

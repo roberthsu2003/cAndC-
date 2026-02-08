7e171831e7b3a841e9e04842e69c976736262701
徐國堂 <roberthsu2003@gmail.com>
2026-02-08 14:35:24 +0800

    Update main.cpp

--- 上課內容/lesson6_1/main.cpp
+++ 上課內容/lesson6_1/main.cpp
@@ -3,15 +3,38 @@
 
 using namespace std;
 
-void print(const vector<int>& v){
-  for(int elem:v){
+void print(const vector<int> &v) {
+  cout << "元素數量:"<< v.size() << endl;
+  for (int elem : v) {
     cout << elem << " ";
   }
-  cout << endl;
+  
 }
 
-int main() { 
+void make_random(vector<int> &v, int size) {
+  v.clear();
+  int n = random() % size + 1;
+  for (int i = 0; i < n; i++) {
+    v.push_back(random());
+  }
+}
+
+int main() {
+  srandom(123);
   vector<int> list;
   cout << "vector初始化";
   print(list);
+  cout << "=================" << endl;
+  
+  cout << "第1次make_random\n";
+  make_random(list, 20);
+  print(list);
+  cout << endl;
+  cout << "=====================" << endl;
+
+  cout << "第2次make_random\n";
+  make_random(list, 5);
+  print(list);
+  cout << endl;
+  cout << "=====================" << endl;
 }

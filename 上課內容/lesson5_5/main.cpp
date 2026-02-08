0381ae919aaaa0dfcbeed270264f0669d46e05ca
徐國堂 <roberthsu2003@gmail.com>
2026-02-08 11:29:26 +0800

    Create main.cpp

---
+++ 上課內容/lesson5_5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//2數對調
+void swap(int &n, int &m){ //call by address
+  int temp;
+  temp = n;
+  n = m;
+  m = temp;
+}
+
+int main() { 
+  int x = 666;
+  int y = 888;
+  cout << "x=" << x << ",y=" << y << endl;
+  //2數對調
+  swap(x, y);
+  cout << "x=" << x << ",y=" << y << endl;
+}

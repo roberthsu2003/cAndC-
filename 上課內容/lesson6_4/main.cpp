ef89b4d987bca5b97b37f12863189cbd35575d6a
徐國堂 <roberthsu2003@gmail.com>
2026-02-08 16:08:53 +0800

    Add details for Person instance p1

--- 上課內容/lesson6_4/main.cpp
+++ 上課內容/lesson6_4/main.cpp
@@ -18,4 +18,17 @@ int main() {
   cout << "姓名:" << p.name << endl;
   cout << "身高:" << p.height << endl;
   cout << "體重:" << p.weight << endl;
+
+  cout << "====================" << endl;
+  Person p1;
+  p1.name = "jenny";
+  p1.height = 173;
+  p1.weight = 60;
+
+  cout << "p1的實體資料如下:\n";
+  cout << "姓名:" << p1.name << endl;
+  cout << "身高:" << p1.height << endl;
+  cout << "體重:" << p1.weight << endl;
+
+  
 }

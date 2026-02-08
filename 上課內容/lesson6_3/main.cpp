b9f13695d450bee6ab54ef807c4e7cfd1c97b37e
徐國堂 <roberthsu2003@gmail.com>
2026-02-08 15:29:01 +0800

    Update main.cpp

--- 上課內容/lesson6_3/main.cpp
+++ 上課內容/lesson6_3/main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main() { 
+  vector<int> numbers;
   ifstream in;
   in.open("a1.txt");
   if(in.good()){
     cout << "讀檔成功" << endl;;
     int value;
-    in >> value;
-    cout << value << endl;
+    while(in >> value){
+      numbers.push_back(value);
+    }
+    
   }else{
     cout << "讀檔失敗";
   }
+
+  for(int elem:numbers){
+    cout << elem << " ";
+  }
+  cout << endl;
 }

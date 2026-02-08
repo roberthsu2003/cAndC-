30e8f2c3b41474f135a200a84e58c54dd25bbb17
徐國堂 <roberthsu2003@gmail.com>
2026-02-08 16:34:37 +0800

    Update main.cpp

--- 上課內容/lesson6_4/main.cpp
+++ 上課內容/lesson6_4/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class Person{
   public:
-    string name;
+    string name; //欄位,成員
     int height;
     int weight;
 
-    Person(){
+    Person(){ //建構式
       
     }
 
-    Person(string n, int h, int w){
+    Person(string n, int h, int w){ //建構式
       name = n;
       height = h;
       weight = w;
+    }
+
+    double bmi(){
+      return weight / pow((height/100.0), 2);
     }
 };
 
@@ -25,6 +30,7 @@ int main() {
   cout << "姓名:" << p.name << endl;
   cout << "身高:" << p.height << endl;
   cout << "體重:" << p.weight << endl;
+  cout << "BMI:" << p.bmi() << endl;
 
   cout << "====================" << endl;
   Person p1;
@@ -36,6 +42,7 @@ int main() {
   cout << "姓名:" << p1.name << endl;
   cout << "身高:" << p1.height << endl;
   cout << "體重:" << p1.weight << endl;
+  cout << "BMI:" << p1.bmi() << endl;
 
   
 }
